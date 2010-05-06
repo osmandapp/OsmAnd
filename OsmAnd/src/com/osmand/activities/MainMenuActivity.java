927f226c2d9f8b92250b580cbd3587017b6046a7
frolovmo <frolovmo@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2010-05-06 15:45:17 +0000

    change UI menu
    git-svn-id: https://osmand.googlecode.com/svn/trunk@38 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- OsmAnd/src/com/osmand/activities/MainMenuActivity.java
+++ OsmAnd/src/com/osmand/activities/MainMenuActivity.java
@@ -1,5 +1,7 @@
 package com.osmand.activities;
 
+import java.util.concurrent.Callable;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,28 +9,36 @@ import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.osmand.R;
 import com.osmand.ResourceManager;
+import com.osmand.services.LongRunningActionCallback;
+import com.osmand.services.LongRunningActionDispatcher;
 
-public class MainMenuActivity extends Activity {
+public class MainMenuActivity extends Activity implements
+		LongRunningActionCallback {
 
 	private Button showMap;
 	private Button exitButton;
 	private Button settingsButton;
-	
+	private ProgressBar loadProgress;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		requestWindowFeature(Window.FEATURE_NO_TITLE);  
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.menu);
 		
+		
+
 		showMap = (Button) findViewById(R.id.MapButton);
 		showMap.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				final Intent mapIndent = new Intent(MainMenuActivity.this, MapActivity.class);
+				final Intent mapIndent = new Intent(MainMenuActivity.this,
+						MapActivity.class);
 				startActivityForResult(mapIndent, 0);
 
 			}
@@ -37,7 +47,8 @@ public class MainMenuActivity extends Activity {
 		settingsButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				final Intent settings = new Intent(MainMenuActivity.this, SettingsActivity.class);
+				final Intent settings = new Intent(MainMenuActivity.this,
+						SettingsActivity.class);
 				startActivity(settings);
 			}
 		});
@@ -48,12 +59,31 @@ public class MainMenuActivity extends Activity {
 				MainMenuActivity.this.finish();
 			}
 		});
-		
-		ResourceManager.getResourceManager().indexingPoi();
+
+		loadProgress = (ProgressBar) findViewById(R.id.LoadProgressBar);
+		loadProgress.setHorizontalScrollBarEnabled(true);
+
+//		startLongRunningOperation();
+
 	}
-	
+
+	private LongRunningActionDispatcher dispatcher;
+
+	@SuppressWarnings("unchecked")
+	private void startLongRunningOperation() {
+		this.dispatcher = new LongRunningActionDispatcher(this, this);
+		dispatcher.startLongRunningAction(new Callable() {
+			public Void  call() throws Exception {
+				ResourceManager.getResourceManager().indexingPoi();
+				return null;
+			}
+		}, "Dialog Title", "Dialog message");
+	}
+
 	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
+	public void onLongRunningActionFinished(Exception error) {
+		// TODO Auto-generated method stub
+
 	}
+
 }
---
+++ OsmAnd/src/com/osmand/services/LongRunningActionCallback.java
@@ -0,0 +1,5 @@
+package com.osmand.services;
+
+public interface LongRunningActionCallback {
+	   void onLongRunningActionFinished(Exception error);
+}
