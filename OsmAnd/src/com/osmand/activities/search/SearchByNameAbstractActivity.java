761d4358b2bfafea38cf7cd1fb88bb8282c0228e
vics001@gmail.com <vics001@gmail.com@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2010-06-02 19:36:52 +0000

    1. Fix some bugs 2. Implement discussable part (currently make invisible)
    git-svn-id: https://osmand.googlecode.com/svn/trunk@122 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- OsmAnd/src/com/osmand/activities/search/SearchByNameAbstractActivity.java
+++ OsmAnd/src/com/osmand/activities/search/SearchByNameAbstractActivity.java
@@ -93,7 +93,7 @@ public abstract class SearchByNameAbstractActivity<T> extends ListActivity {
 			@Override
 			public void run() {
 				List<T> loadedObjects = getObjects(filter);
-				if(!handlerToLoop.hasMessages(1)){
+				if(handlerToLoop != null && !handlerToLoop.hasMessages(1)){
 					updateUIList(loadedObjects);
 				}
 			}
