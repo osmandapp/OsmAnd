3e536eb61c01f284b0afcfa5a50776cc5aa314d1
Victor Shcherb <victor.shcherb@gmail.com>
2010-06-09 12:27:05 +0000

    fix todos
    git-svn-id: https://osmand.googlecode.com/svn/trunk@148 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- DataExtractionOSM/src/com/osmand/ToDoConstants.java
+++ DataExtractionOSM/src/com/osmand/ToDoConstants.java
@@ -18,13 +18,13 @@ public class ToDoConstants {
 	
 	// PREPARE RELEASE OPTIONS LIST : 
 	// 1. Revise UI (icons/layouts). Support different devices. Add inactive/focus(!) icon versions.
-	// 2. Translation (support russian)
-	// 3. POI Search ( 1) predefined filters, 2) choose subtype's, 3) filter by name, 4) opening hours (filter))
+	// 2. Translation (support russian) +
+	// 3. POI Search ( 1) predefined filters, 2) choose subtype's, 3) filter by name, 4) opening hours (filter)) +
 	// 4. Support old-versionned resources (1) odb indexes, 2) favourites table, 3) atomic settings (?))
-	// 5. Swing refactoring MapPanel (support layers)
-	// 6. Support vector road rendering (new index file)
-	// 7. Suppport navigation for calculated route.
-	// 8. Editing POI on map
+	// 5. Swing refactoring MapPanel (support layers) +
+	// 6. Support vector road rendering (new index file) !
+	// 7. Suppport navigation for calculated route. !
+	// 8. Editing POI on map +
 	
 	
 	
@@ -35,19 +35,16 @@ public class ToDoConstants {
 	
 	 // TODO ANDROID 
 //	  8. Enable change POI directly on map (requires OSM login)
-//   20. Implement save track/route to gpx (?)
-//   26. Show the whole street on map (when it is chosen in search activity). Possibly extend that story to show layer with streets.
 //   30. Performance issue with map drawing : 
-//       introduce one place where refreshMap will be called using postMessage mechanism (delay more than > 50 ? ms).
-//       Introducing cache of file names that are on disk (creating new File() consumes a lot of memory) 	
 //   31. Translation.	
 //   32. Introduce POI predefined filters (car filter(other-fuel, transportation-car_wash, show-car) and others)
 //   33. Build transport locations (investigate)
 //   34. Investigate routing (bicycle, car)
 //   36. Postcode search
 //   37. Get rid of exit button (!). Think about when notification should go & how clear resources if it is necessary
-//   38. Add button in search "navigate to".	
-	
+//   38. Add button in search "navigate to".
+//   20. Implement save track/route to gpx (?)
+//   26. Show the whole street on map (when it is chosen in search activity). Possibly extend that story to show layer with streets.
 	
 	// BUGS Android
 	//  1. Fix bug with navigation layout (less zoom controls) [fixed].
@@ -56,18 +53,15 @@ public class ToDoConstants {
     //  3. Implement clear existing area with tiles (update map) [fixed]
 	//  6. Implement context menu for long press & trackball press [fixed - trackball todo]
 	//  2. Include to amenity index : historic, sport, ....
-	//  5. Implement caching files existing on FS, implement specific method in RM
+	//  5. Improvement : Implement caching files existing on FS, implement specific method in RM
+	//     Introducing cache of file names that are on disk (creating new File() consumes a lot of memory) 	
 
-
-	
 	
 	// TODO swing
 	// 2. Internal (Simplify MapPanel - introduce layers for it)ю
 	// 3. Implement clear progress.
 	// 4. Fix issues with big files (such as netherlands)
 	// 1. Download tiles without using dir tiles
-
-
 
 	
 	
