cdf9215e38b239446659864fdcc6a1b95ab86b43
vics001@gmail.com <vics001@gmail.com@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2010-09-02 20:58:06 +0000

    fix rendering
    git-svn-id: https://osmand.googlecode.com/svn/trunk@486 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- OsmAnd/src/net/osmand/render/OsmandRenderer.java
+++ OsmAnd/src/net/osmand/render/OsmandRenderer.java
@@ -2,12 +2,13 @@ package net.osmand.render;
 
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-
 import net.osmand.LogUtil;
 import net.osmand.osm.MapRenderObject;
 import net.osmand.osm.MapRenderingTypes;
 import net.osmand.osm.MapUtils;
+
+import org.apache.commons.logging.Log;
+
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -54,8 +55,20 @@ public class OsmandRenderer {
 		paintFillWhite.setColor(Color.rgb(241, 238, 232));
 	}
 	
+	public Bitmap getBitmap(){
+		return bmp;
+	}
+	
+	public synchronized void clearBitmap(){
+		if(bmp != null){
+			bmp.recycle();
+		}
+		bmp = null;
+	}
+	
 	public void generateNewBitmap(RectF objectLoc, List<MapRenderObject> objects, int zoom, float rotate) {
 		long now = System.currentTimeMillis();
+		// TODO sort objects first of all 
 		if(bmp != null){
 			bmp.recycle();
 			bmp = null;
@@ -76,47 +89,31 @@ public class OsmandRenderer {
 	}
 
 	
-	protected void draw(MapRenderObject obj, Canvas canvas, double leftTileX, double topTileY, float zoom, float rotate) {
-		float xText = -1;
-		float yText = -1;
-		Path path = null;
-		if (obj.isPoint()) {
-			float x = (float) ((MapUtils.getTileNumberX(zoom, obj.getPointLongitude(0)) - leftTileX) * 256f);
-			float y = (float) ((MapUtils.getTileNumberY(zoom, obj.getPointLatitude(0)) - topTileY) * 256f);
-//			xText = x;
-//			yText = y;
-			canvas.drawCircle(x, y, 6, paintFill);
+	protected void draw(MapRenderObject obj, Canvas canvas, double leftTileX, double topTileY, int zoom, float rotate) {
+		if(obj.isPoint()){
+			drawPoint(obj, canvas, leftTileX, topTileY, zoom, rotate);
+		} else if(obj.isPolyLine() && MapRenderingTypes.isHighway(obj.getType())){
+			drawHighway(obj, canvas, leftTileX, topTileY, zoom, rotate);
 		} else {
-			
 			Paint paint;
+			float xText = 0;
+			float yText = 0;
+			Path path = null;
 			if(obj.isPolygon()){
-				paint = paintFill; 
+				paint = paintFill;
 				// for buildings !
 				if(MapRenderingTypes.isPolygonBuilding(obj.getType())){
 					paint.setColor(Color.rgb(188, 169, 169));
 				} else {
-					paint.setColor(Color.GRAY);
+					paint.setColor(Color.rgb(188, 169, 169));
+//					paint.setColor(Color.GRAY);
 				}
 			} else {
 				paint = paintStroke;
-				if(MapRenderingTypes.isHighway(obj.getType())){
-					int hwType = MapRenderingTypes.getHighwayType(obj.getType());
-					if(hwType == MapRenderingTypes.PL_HW_MOTORWAY || hwType == MapRenderingTypes.PL_HW_TRUNK){
-						paint.setColor(Color.BLUE);
-					} else if(hwType == MapRenderingTypes.PL_HW_PRIMARY){
-						paint.setColor(Color.rgb(235, 152, 154));
-					} else if(hwType == MapRenderingTypes.PL_HW_SECONDARY){
-						paint.setColor(Color.rgb(253, 214, 164));
-					} else {
-						paint.setColor(Color.WHITE);
-					}
-					paint.setStrokeWidth(5);
-					
-				} else {
-					paint.setStrokeWidth(2);
-					paint.setColor(Color.DKGRAY);
-				}
+				paint.setStrokeWidth(2);
+				paint.setColor(Color.DKGRAY);
 			}
+			
 			for (int i = 0; i < obj.getPointsLength(); i++) {
 				float x = (float) ((MapUtils.getTileNumberX(zoom, obj.getPointLongitude(i)) - leftTileX) * 256f);
 				float y = (float) ((MapUtils.getTileNumberY(zoom, obj.getPointLatitude(i)) - topTileY) * 256f);
@@ -129,29 +126,116 @@ public class OsmandRenderer {
 					path.lineTo(x, y);
 				}
 			}
+			
 			if (path != null) {
 				xText /= obj.getPointsLength();
 				yText /= obj.getPointsLength();
 				canvas.drawPath(path, paint);
+				if(obj.getName() != null){
+					canvas.drawText(obj.getName(), xText, yText, paintText);
+				}
 			}
 		}
-		if(obj.getName() != null && xText > 0 && yText > 0){
-			if(obj.isPolyLine()){
-				canvas.drawTextOnPath(obj.getName(), path, 0, 0, paintText);
+		
+	}
+	
+	public void drawPoint(MapRenderObject obj, Canvas canvas, double leftTileX, double topTileY, int zoom, float rotate){
+		if (zoom > 15) {
+//			float x = (float) ((MapUtils.getTileNumberX(zoom, obj.getPointLongitude(0)) - leftTileX) * 256f);
+//			float y = (float) ((MapUtils.getTileNumberY(zoom, obj.getPointLatitude(0)) - topTileY) * 256f);
+//			canvas.drawCircle(x, y, 6, paintFill);
+		}
+
+	}
+	
+	public void drawHighway(MapRenderObject obj, Canvas canvas, double leftTileX, double topTileY, int zoom, float rotate) {
+		if(obj.getPointsLength() == 0){
+			return;
+		}
+		
+		float xText = 0;
+		float yText = 0;
+		
+		Path path = null;
+		float pathRotate = 0;
+		float xLength = 0;
+		float yLength = 0;
+		
+		Paint paint = paintStroke;
+		int hwType = MapRenderingTypes.getHighwayType(obj.getType());
+		if (hwType == MapRenderingTypes.PL_HW_MOTORWAY || hwType == MapRenderingTypes.PL_HW_TRUNK) {
+			paint.setColor(Color.BLUE);
+		} else if (hwType == MapRenderingTypes.PL_HW_PRIMARY) {
+			paint.setColor(Color.rgb(235, 152, 154));
+		} else if (hwType == MapRenderingTypes.PL_HW_SECONDARY) {
+			paint.setColor(Color.rgb(253, 214, 164));
+		} else if (hwType == MapRenderingTypes.PL_HW_SERVICE || hwType == MapRenderingTypes.PL_HW_UNCLASSIFIED
+				|| hwType == MapRenderingTypes.PL_HW_TERTIARY || hwType == MapRenderingTypes.PL_HW_RESIDENTIAL) {
+			paint.setColor(Color.WHITE);
+		} else {
+			// skip for now
+			return;
+		}
+		if (zoom < 16) {
+			paint.setStrokeWidth(6);
+		} else if (zoom == 16) {
+			paint.setStrokeWidth(7);
+		} else if (zoom == 17) {
+			paint.setStrokeWidth(11);
+		} else if (zoom >= 18) {
+			paint.setStrokeWidth(16);
+		}
+
+		float xPrev = 0;
+		float yPrev = 0;
+		int middle = obj.getPointsLength() / 2;
+		for (int i = 0; i < obj.getPointsLength(); i++) {
+			float x = (float) ((MapUtils.getTileNumberX(zoom, obj.getPointLongitude(i)) - leftTileX) * 256f);
+			float y = (float) ((MapUtils.getTileNumberY(zoom, obj.getPointLatitude(i)) - topTileY) * 256f);
+//			xText += x;
+//			yText += y;
+			if (path == null) {
+				path = new Path();
+				path.moveTo(x, y);
 			} else {
-				canvas.drawText(obj.getName(), xText, yText, paintText);
+				if (xPrev > 0) {
+					xLength += x - xPrev; // not abs
+					yLength += y - yPrev; // not abs
+				}
+				if(i == middle){
+					double rot = -Math.atan2(x - xPrev, y - yPrev) * 180 / Math.PI + 90;
+					if (rot < 0) {
+						rot += 360;
+					}
+					if (rot < 270 && rot > 90) {
+						rot += 180;
+					}
+					pathRotate = (float) rot;
+					xText = (x + xPrev) / 2;
+					yText = (y + yPrev) / 2;
+				}
+				if (pathRotate == 0) {
+					
+				}
+				path.lineTo(x, y);
+			}
+			xPrev = x;
+			yPrev = y;
+		}
+		if (path != null) {
+//			xText /= obj.getPointsLength();
+//			yText /= obj.getPointsLength();
+			canvas.drawPath(path, paint);
+			if (obj.getName() != null) {
+				if (paintText.measureText(obj.getName()) < Math.max(Math.abs(xLength), Math.abs(yLength))) {
+					int sv = canvas.save();
+					canvas.rotate(pathRotate, xText, yText);
+					canvas.drawText(obj.getName(), xText, yText, paintText);
+					canvas.restoreToCount(sv);
+				}
 			}
 		}
 	}
 	
-	public Bitmap getBitmap(){
-		return bmp;
-	}
-	
-	public synchronized void clearBitmap(){
-		if(bmp != null){
-			bmp.recycle();
-		}
-		bmp = null;
-	}
+
 }
