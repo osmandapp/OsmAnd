a2ca16fb2a7c10199445a2c3d22f9a291b42cc0d
vics001@gmail.com <vics001@gmail.com@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2010-10-25 15:21:43 +0000

    fix building
    git-svn-id: https://osmand.googlecode.com/svn/trunk@590 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

---
+++ DataExtractionOSM/OsmAndMapCreator.bat
@@ -0,0 +1,4 @@
+REM the JVM. With the below settings the heap size (Available memory for the application)
+REM will range from 64 megabyte up to 512 megabyte.
+
+start javaw.exe -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx512M -cp "./OsmAndMapCreator.jar;./lib/*.jar" net.osmand.swing.OsmExtractionUI
