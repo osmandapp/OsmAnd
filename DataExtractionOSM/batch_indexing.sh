8a4b742bfb4618fa00eb9a5c38882824b00a9a9f
Victor Shcherb <victor.shcherb@gmail.com>
2010-11-06 22:38:00 +0000

    fix issue 151
    git-svn-id: https://osmand.googlecode.com/svn/trunk@640 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- DataExtractionOSM/batch_indexing.sh
+++ DataExtractionOSM/batch_indexing.sh
@@ -4,4 +4,4 @@
 # the JVM. With the below settings the heap size (Available memory for the application)
 # will range from 64 megabyte up to 512 megabyte.
 
-java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx512M -cp "./OsmAndMapCreator.jar;./lib/*.jar" net.osmand.data.index.IndexBatchCreator ./batch.xml
+java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx512M -cp "./OsmAndMapCreator.jar:./lib/*.jar" net.osmand.data.index.IndexBatchCreator ./batch.xml
