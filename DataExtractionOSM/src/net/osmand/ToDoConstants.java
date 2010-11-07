1e8939f9bc901986edf7381eb2f623f62e5c1786
vics001@gmail.com <vics001@gmail.com@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2010-11-07 10:02:33 +0000

    new todos
    git-svn-id: https://osmand.googlecode.com/svn/trunk@649 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- DataExtractionOSM/src/net/osmand/ToDoConstants.java
+++ DataExtractionOSM/src/net/osmand/ToDoConstants.java
@@ -9,21 +9,23 @@ public class ToDoConstants {
 	
 	// TODO max 101
 	// introduce bidforfix on site
-	// !!! Fix progress in DataExtractionOsm
+	
 
-	// 	Outside base 0.4 release
-	//   69. Add phone and site information to POI (enable call to POI and open site)
+	// 	For 0.5 release
+	//  101. 1) Introduce shell script to print information about indexes 2)Introduce activity to show active indexes
+    //       3) Split BinaryMapIndexReader to Adapters
+	//  102. Refactoring rendering schema serializing to xml and prepare script to create fast java builder (support pure xml)
+	//		 Requires 
 	//   86. Allow to add/edit custom tags to POI objects (Issue)
 	//   92. Replace poi index with standard map index and unify POI categories
-	//   94. Revise index to decrease their size (especially address) - replace to float lat/lon and remove for POI 
-	//		 remove en_names from POI (possibly from address)
+	//   69. Add phone and site information to POI (enable call to POI and open site)
 	//   97. For voice navigation consider current speed of vehicle. Especially when speed > 50 pronounce more than 200 m
 	//   98. Implement rendering of different app mode. For Car render streets name with large font.
 	//   96. Introduce settings for MPH, imperial units
+	
 
 	// 	_19. colors for road trunk and motorway
 	// 	_12. Fix : find proper location for streets ! centralize them (when create index)?
-	// 	_28. Fix freeze while map downloading (?)
 	
 	/////////////////////////////  UNKNOWN STATE  ////////////////////
 	// Unscheduled (complex)
@@ -41,7 +43,8 @@ public class ToDoConstants {
 	//   99. Implement better file downloader for big files
 	//  100. Show impoted gpx points (as favorites), sort the by distance
 	//   91. Invent binary format (minimize disk space, maximize speed)
-	
+	//   94. Revise index to decrease their size (especially address) - replace to float lat/lon and remove for POI 
+	//		 remove en_names from POI (possibly from address)	
 	// DONE SWING
 
 	
