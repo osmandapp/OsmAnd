02df6f8895d331306f782c1aa9efa2863a6ba706
vics001@gmail.com <vics001@gmail.com@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2010-11-07 10:02:23 +0000

    fix issue 126
    git-svn-id: https://osmand.googlecode.com/svn/trunk@648 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- OsmAnd/src/net/osmand/activities/MapActivity.java
+++ OsmAnd/src/net/osmand/activities/MapActivity.java
@@ -534,12 +534,12 @@ public class MapActivity extends Activity implements IMapLocationListener, Senso
     	speed *= 3.6;
     	if(speed < 4){
     		return currentZoom;
-    	} else if(speed < 30){
-    		// less than 30 - show 17 
+    	} else if(speed < 33){
+    		// less than 33 - show 17 
     		return 17;
-    	} else if(speed < 50){
+    	} else if(speed < 53){
     		return 16;
-    	} else if(speed < 80){
+    	} else if(speed < 83){
     		return 15;
     	}
     	// more than 80 - show 14 (it is slow)
