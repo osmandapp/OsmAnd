d9b8b042907ca1e72abdfbbdd645bdfc3b37aa89
Victor Shcherb <victor.shcherb@gmail.com>
2010-11-23 21:48:57 +0000

    fix issue 211
    git-svn-id: https://osmand.googlecode.com/svn/trunk@696 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- OsmAnd/src/net/osmand/render/OsmandRenderer.java
+++ OsmAnd/src/net/osmand/render/OsmandRenderer.java
@@ -829,7 +829,7 @@ public class OsmandRenderer {
 					canvas.drawPath(path, paint);
 				}
 			}
-			if (obj.getName() != null) {
+			if (obj.getName() != null && obj.getName().length() > 0) {
 				String name = obj.getName();
 				String ref = null;
 				if(name.charAt(0) == MapRenderingTypes.REF_CHAR){
@@ -845,7 +845,7 @@ public class OsmandRenderer {
 						}
 					}
 				}
-				if(ref != null){
+				if(ref != null && ref.trim().length() > 0){
 					rc.clearText();
 					ref = render.renderObjectText(ref, pair.tag, pair.value, rc, true);
 					TextDrawInfo text = new TextDrawInfo(ref);
@@ -858,7 +858,7 @@ public class OsmandRenderer {
 					
 				}
 				
-				if(name != null && name.length() > 0){
+				if(name != null && name.trim().length() > 0){
 					rc.clearText();
 					name = render.renderObjectText(name, pair.tag, pair.value, rc, false);
 					if (rc.textSize > 0) {
