d24557d6400fe6b43b480fb4e46dfd15f65d8a9e
vics001@gmail.com <vics001@gmail.com@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2010-11-23 21:50:53 +0000

    fix issue 192
    git-svn-id: https://osmand.googlecode.com/svn/trunk@697 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- DataExtractionOSM/src/net/osmand/Version.java
+++ DataExtractionOSM/src/net/osmand/Version.java
@@ -5,7 +5,7 @@ public class Version {
 	public static final String APP_NAME = "OsmAnd"; //$NON-NLS-1$
 	public static final String APP_MAP_CREATOR_NAME = "OsmAndMapCreator"; //$NON-NLS-1$
 	public static final String APP_VERSION = "0.5.1"; //$NON-NLS-1$
-	public static final String APP_DESCRIPTION = "alpha (b4)"; //$NON-NLS-1$
+	public static final String APP_DESCRIPTION = "alpha (b5)"; //$NON-NLS-1$
 	public static final boolean VELCOM_EDITION = false;
 	
 	public static final String APP_NAME_VERSION = APP_NAME + " " + APP_VERSION; //$NON-NLS-1$
