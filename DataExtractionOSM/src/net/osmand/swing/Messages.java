ecf40a9edddf86e59c74d62e6b2c94849b1d1c01
Victor Shcherb <victor.shcherb@gmail.com>
2010-11-28 23:05:30 +0000

    rename messages -> swing_messages
    git-svn-id: https://osmand.googlecode.com/svn/trunk@720 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- DataExtractionOSM/src/net/osmand/swing/Messages.java
+++ DataExtractionOSM/src/net/osmand/swing/Messages.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "net.osmand.swing.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "net.osmand.swing.swing_messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
