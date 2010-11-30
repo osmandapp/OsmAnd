d980d7c5b41f74372938bf6880e1a008da896e65
Victor Shcherb <victor.shcherb@gmail.com>
2010-11-30 22:54:40 +0000

    fix batch indexing
    git-svn-id: https://osmand.googlecode.com/svn/trunk@735 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- DataExtractionOSM/batch_indexing.sh
+++ DataExtractionOSM/batch_indexing.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-
 # This file will start the Osm Extraction UI with custom memory settings for
 # the JVM. With the below settings the heap size (Available memory for the application)
 # will range from 64 megabyte up to 512 megabyte.
 
-java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx512M -cp "./OsmAndMapCreator.jar:./lib/*.jar" net.osmand.data.index.IndexBatchCreator ./batch.xml
+java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx512M -cp "./OsmAndMapCreator.jar:./lib/*.jar" net.osmand.data.index.IndexBatchCreator ./batch.xml 
