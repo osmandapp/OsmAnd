65c0b26076f974f7697cababcfdbe3ef0379cdf2
Victor Shcherb <victor.shcherb@gmail.com>
2010-12-01 23:15:45 +0000

    fix small issues
    git-svn-id: https://osmand.googlecode.com/svn/trunk@745 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- OsmAnd/src/net/osmand/activities/DownloadIndexActivity.java
+++ OsmAnd/src/net/osmand/activities/DownloadIndexActivity.java
@@ -627,21 +627,21 @@ public class DownloadIndexActivity extends ListActivity {
 				boolean f = true;
 				String lowerCase = e.getValue().toLowerCase();
 				if (lowerCase.contains("map")) { //$NON-NLS-1$
-					if (f) {
+					if (!f) {
 						s += ", "; //$NON-NLS-1$
 						f = false;
 					}
 					s += getString(R.string.map_index);
 				}
 				if (lowerCase.contains("transport")) { //$NON-NLS-1$
-					if (f) {
+					if (!f) {
 						s += ", "; //$NON-NLS-1$
 						f = false;
 					}
 					s += getString(R.string.transport);
 				}
 				if (lowerCase.contains("address")) { //$NON-NLS-1$
-					if (f) {
+					if (!f) {
 						s += ", "; //$NON-NLS-1$
 						f = false;
 					}
