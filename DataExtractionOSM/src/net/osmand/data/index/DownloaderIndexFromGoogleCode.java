61c6b086757c0385ada309f72aae73f84ff26ebb
Victor Shcherb <victor.shcherb@gmail.com>
2010-12-06 07:56:57 +0000

    fix compilation error
    git-svn-id: https://osmand.googlecode.com/svn/trunk@765 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- DataExtractionOSM/src/net/osmand/data/index/DownloaderIndexFromGoogleCode.java
+++ DataExtractionOSM/src/net/osmand/data/index/DownloaderIndexFromGoogleCode.java
@@ -35,7 +35,7 @@ public class DownloaderIndexFromGoogleCode {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws URISyntaxException, IOException {
-		Map<String, String> files = DownloaderIndexFromGoogleCode.getIndexFiles(new LinkedHashMap<String, String>(),
+		Map<String, String> files = DownloaderIndexFromGoogleCode.getContent(new LinkedHashMap<String, String>(),
 				BINARY_MAP_VERSION + BINARY_MAP_INDEX_EXT,
 				BINARY_MAP_VERSION + BINARY_MAP_INDEX_EXT_ZIP,
 				VOICE_VERSION + VOICE_INDEX_EXT_ZIP);
