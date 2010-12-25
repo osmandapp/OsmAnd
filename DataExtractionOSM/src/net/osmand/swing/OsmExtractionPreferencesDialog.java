6ae9f3e5460184f02f340670dbaa9339f621d976
vics001 <vics001@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2010-12-25 16:16:18 +0000

    first cut routing
    git-svn-id: https://osmand.googlecode.com/svn/trunk@853 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- DataExtractionOSM/src/net/osmand/swing/OsmExtractionPreferencesDialog.java
+++ DataExtractionOSM/src/net/osmand/swing/OsmExtractionPreferencesDialog.java
@@ -5,8 +5,6 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
