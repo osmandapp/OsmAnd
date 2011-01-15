e94e415f4302a4278aa10d17f9dee1424f081ce9
pavol.zibrita <pavol.zibrita@e29c36b1-1cfa-d876-8d93-3434fc2bb7b8>
2011-01-15 14:55:07 +0000

    Fixed issue 304
    - the extension of images .png, .jpg etc must be determined from the files and not hardcoded to .jpg
    
    git-svn-id: https://osmand.googlecode.com/svn/trunk@891 e29c36b1-1cfa-d876-8d93-3434fc2bb7b8

--- OsmAnd/src/net/osmand/OsmandSettings.java
+++ OsmAnd/src/net/osmand/OsmandSettings.java
@@ -479,7 +479,7 @@ public class OsmandSettings {
 					} catch (IOException e) {
 						Log.d(LogUtil.TAG, "Error reading url " + dir.getName(), e); //$NON-NLS-1$
 					}
-					return new TileSourceManager.TileSourceTemplate(dir.getName(), url);
+					return new TileSourceManager.TileSourceTemplate(dir, dir.getName(), url);
 				}
 			}
 				
