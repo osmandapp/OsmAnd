5fbe56c00fec1d065ef3c89308e34a1c9e4ad51e
Victor Shcherb <victor.shcherb@gmail.com>
2011-01-24 00:32:16 +0100

    Fix issue 331 and issue 306

--- DataExtractionOSM/src/net/osmand/binary/BinaryMapIndexWriter.java
+++ DataExtractionOSM/src/net/osmand/binary/BinaryMapIndexWriter.java
@@ -329,7 +329,7 @@ public class BinaryMapIndexWriter {
 		}
 		
 		int restrictionsSize = 0;
-		if(restrictions.length > 0){
+		if(restrictions != null && restrictions.length > 0){
 			allSize += CodedOutputStream.computeTagSize(OsmandOdb.MapData.RESTRICTIONS_FIELD_NUMBER);
 			for (int i = 0; i < restrictions.length / 8; i++) {
 				long l = Algoritms.parseLongFromBytes(restrictions, i * 8) - stackBaseIds.peek();
@@ -376,7 +376,7 @@ public class BinaryMapIndexWriter {
 			codedOutStream.writeUInt32(OsmandOdb.MapData.STRINGID_FIELD_NUMBER, nameId);
 		} 
 		
-		if(restrictions.length > 0){
+		if(restrictions != null && restrictions.length > 0){
 			codedOutStream.writeTag(OsmandOdb.MapData.RESTRICTIONS_FIELD_NUMBER, WireFormat.FieldType.BYTES.getWireType());
 			codedOutStream.writeRawVarint32(restrictionsSize);
 			for (int i = 0; i < restrictions.length / 8; i++) {
