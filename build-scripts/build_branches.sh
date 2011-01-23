85ea37a887a458ec439454314f8bf8dc68af6f98
Victor <osmand.app@gmail.com>
2011-01-23 12:45:18 +0100

    Add new night build scripts

---
+++ build-scripts/build_branches.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+GIT_DIR="$DIRECTORY"/osmand-git
+GIT_ORIGIN_NAME=origin
+BUILD_DIR="$DIRECTORY"/builds
+VERSION_FILE=./DataExtractionOSM/src/net/osmand/Version.java 
+DATE=$(date +%d-%m-%y)
+
+# clean all files in build directory
+rm -r "$BUILD_DIR"
+mkdir "$BUILD_DIR"
+cd "${GIT_DIR}"
+ 
+git branch -r | while read i 
+do
+  cd "${GIT_DIR}"
+  ch=$(expr index "$i" ">")
+  if [ $ch = 0 ]; then 
+     BRANCH=${i#"$GIT_ORIGIN_NAME/"}
+     echo "Checkouting branch and create build for $BRANCH"
+     ## reset all previous changes in working tree
+     git checkout . 
+     git checkout $BRANCH
+     sed -e "s/\(APP_DESCRIPTION.*=.*\"\).*\(\".*\)/\1$DATE $BRANCH\2/g" $VERSION_FILE >  ${VERSION_FILE}.bak
+     mv ${VERSION_FILE}.bak ${VERSION_FILE}
+
+     ## build map creator
+     cd ./DataExtractionOSM/
+     ant clean compile build
+     mv build.zip "$BUILD_DIR/OsmAndMapCreator-$BRANCH-nb-$DATE.zip"
+
+     ## build osmand app
+     cd ../OsmAnd/
+     cp "$DIRECTORY"/local.properties local.properties
+     rm -r bin
+     mkdir bin
+     if [ ! -d assets ]; then
+       mkdir assets
+     fi
+     ant clean debug
+     mv bin/OsmAnd-debug.apk "$BUILD_DIR/OsmAnd-$BRANCH-nb-$DATE.apk" 
+  fi
+done
---
+++ build-scripts/upload_ftp.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+FTP_SITE=download.osmand.net
+FTP_FOLDER=night-builds
+# FTP_USER in local.properties
+# FTP_PWD= in local.properties
+BUILD_DIR="$DIRECTORY"/builds
+
+. "$DIRECTORY"/local.properties
+# 3. upload to ftp server
+lftp -c open $FTP_USER,$FTP_PWD $FTP_SITE || ls $FTP_FOLDER || mirror -R $FTP_FOLDER $BUILD_DIR
+
+ftp -n -v $FTP_SITE <<SCRIPT 2>&1
+quote USER $FTP_USER
+quote PASS $FTP_PWD
+cd $FTP_FOLDER 
+ls
+quit
+
+
+
