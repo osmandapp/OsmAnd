4daa4045de3d902cf4db8ad100042a29663a62c5
Victor Shcherb <victor.shcherb@gmail.com>
2011-04-24 15:10:42 +0200

    Prepare roadmap for future actions

--- DataExtractionOSM/src/net/osmand/ToDoConstants.java
+++ DataExtractionOSM/src/net/osmand/ToDoConstants.java
@@ -7,47 +7,47 @@ package net.osmand;
  */
 public class ToDoConstants {
 	
-	// TODO max 115
-
-	// ! New UI (+)
-	// !!63. Support simple offline routing(require index file with turn restrictions etc) (Issue)
 	
-	// 	For 0.5 release
-	//  104. Add activity to show current loaded indexes and information about them
+//	    == Osmand application (TODO 122) ==
 
-	//   97. For voice navigation consider current speed of vehicle. Especially when speed > 50 pronounce more than 200 m
-	//  112. Investigate exiting/minimizing app (Issue)
-	//   92. Support poi index with standard map index and unify POI categories (unify done +)
-	//       Done partially (works very slow), 
-	//		 possibly introduce settings to limit usage of functionality or put poi index into binary index
+//		=== Refactoring issues ===
+//		|| 116 || Move all messages.properties related Android application inside strings.xml to have better access with their translation ||
+		
+		
+//		=== Common issues ===
+//      || 121 || Filter network location from GPS location (wait for 10 seconds before start using network location) ||
+//      || 120 || Show icons over poi circle ||
+//      || 119 || Dialog on main screen tips and tricks ||
+//      || 118 || Config switching between maps on different zoom levels <14 (using raster), > 14 vector ||
+//      || 117 || Share location (send/get location from SMS, email) - Issue ||	
+//		|| 112 || Investigate exiting/minimizing app (Issue) ||
+//		|| 97 || For voice navigation consider current speed of vehicle. Especially when speed > 50 pronounce more than 200 m ||
+//		|| 104 || Add activity to show current loaded indexes and information about them ||
+//		|| 86 || Allow to add/edit custom tags to POI objects (Issue) ||
+//		|| 92 || Support poi index with standard map index and unify POI categories (unify done +, works very slow) ||
+//		|| 96 || Introduce settings for MPH, imperial units ||
+//		|| 113 || Calculate speed cameras/bumps on the road (announce about them) ||
+//		|| 110 || Use android voice for pronounce command (could be used in future to pronounce street) (Issue) ||	
+//		|| 111 || Investigate showing street name while driving (Issue) ||	
+//		|| 86 || Allow to add/edit custom tags to POI objects (Issue) ||
+//		|| 96 || Introduce settings for MPH, imperial units (Issue) ||
+
 	
-	// Outside base 0.5 release
-	//  113. Calculate speed cameras/bumps on the road (announce about them)
-	//  110. Use android voice for pronounce command (could be used in future to pronounce street) (Issue)
-	//  111. Investigate showing street name while driving (Issue)
-	//   86. Allow to add/edit custom tags to POI objects (Issue)
-	//   96. Introduce settings for MPH, imperial units (Issue)
-	//  114. Index streets according to city bounds (Issue)
+//		=== Not clear if really needed (Means golden ratio complexity/necessity) ===
+//		|| 66 || Transport routing (show next stop, total distance, show stop get out, voice) ||
+//		|| 85 || Enable on/off screen for bike navigation ||
+//		|| 83 || Add monitoring service to send locations to internet ||
+	
+	
+//		=== Unscheduled (complex) ===
+//		|| 65 || Intermediate points - for better control routing, to avoid traffic jams ...(?) ||
 
 	// 	_19. colors for road trunk and motorway
 	
-	/////////////////////////////  UNKNOWN STATE  ////////////////////
-	// Unscheduled (complex)
-	//   65. Intermediate points - for better control routing, to avoid traffic jams ...(?)
-
-	
-	// Not clear if it is really needed 
-	//   66. Transport routing (show next stop, total distance, show stop get out, voice) (needed ?).
-	//   83. Add monitoring service to send location to internet immediately (?)
-
 	
 	///////////////////////////   DONE //////////////////////////////
 	// DONE ANDROID :
-	//  106. Download region from android device (select minzoom/maxzoom)
-	//  105. Route mode fast/short (Issue)
-	//  109. Update download index activity (introduce select/deselect all, update existing) 
-	//			and make green text for already loaded indexes.
-	//  108. Auto switch at night rendering.
+
 	
 	// DONE SWING
 
