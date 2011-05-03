d5055d479373382de1fde7f77e6234f004d8b666
Victor <osmand.app@gmail.com>
2011-05-04 00:29:02 +0200

    Fix issue 339 partially

--- DataExtractionOSM/src/net/osmand/ToDoConstants.java
+++ DataExtractionOSM/src/net/osmand/ToDoConstants.java
@@ -15,16 +15,15 @@ public class ToDoConstants {
 		
 		
 //		=== Common issues ===
+//	    || 104 || Add activity to show current loaded indexes and information about them (Issue 366) ||
 //      || 123 || Improve gpx file showing (very slow for big files) (Issue 412) ||
 //      || 124 || Animated transition using only raster images (?) - skip animations (!) - don not render vectoring for animations (Issue 238) ||
 //      || 122 || Frozen sqlite db images (bug?). When images are loaded into sqlite the whole map is frozen (issue 413) || 	
 //      || 120 || Show icons over poi circle (issue 414) ||
 //      || 110 || Use android voice for pronounce command (could be used in future to pronounce street) (Issue 70) ||
 //      || 119 || Dialog on main screen tips and tricks (Issue 415) ||
-//		|| 112 || Investigate exiting/minimizing app (Issue 214) ||
 //      || 118 || Config switching between maps on different zoom levels <14 (using raster), > 14 vector (Issue 419) ||
 //		|| 97 || For voice navigation consider current speed of vehicle. Especially when speed > 50 pronounce more than 200 m (Issue 420) ||
-//		|| 104 || Add activity to show current loaded indexes and information about them (Issue 366) ||
 //      || 111 || Investigate showing street name while driving (Issue 286) ||  
 //      || 86 || Allow to add/edit custom tags to POI objects (Issue 44) ||
 //		|| 92 || Support poi index with standard map index and unify POI categories (unify done +, works very slow) (Issue 417) ||
@@ -46,7 +45,8 @@ public class ToDoConstants {
 	
 	///////////////////////////   DONE //////////////////////////////
 	// DONE ANDROID :
-
+//	|| 112 || Investigate exiting/minimizing app (Issue 214) ||
+	
 	// DONE SWING
 
 	
