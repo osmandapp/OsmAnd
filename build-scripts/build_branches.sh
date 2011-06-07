899562c138518bdb3556ec9a3565af4f783d53e2
Pavol Zibrita <zibrita@kopernik.cc.fmph.uniba.sk>
2011-06-07 11:34:17 +0200

    changed year to full one

--- build-scripts/build_branches.sh
+++ build-scripts/build_branches.sh
@@ -6,7 +6,7 @@ GIT_ORIGIN_NAME=origin
 BUILD_DIR="$DIRECTORY"/builds
 LATESTS_DIR="$DIRECTORY"/latests
 VERSION_FILE=./DataExtractionOSM/src/net/osmand/Version.java 
-DATE=$(date +%y-%m-%d)
+DATE=$(date +%Y-%m-%d)
 SHORT_DATE=$(date +%d-%m)
 
 # clean all files in build directory
