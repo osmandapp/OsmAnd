96b4a9fb2b7bc1e2284dd7624151b876132d7e33
Victor Shcherb <victor.shcherb@gmail.com>
2011-06-11 21:42:53 +0200

    Update icons

--- OsmAnd/src/net/osmand/plus/activities/MainMenuActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/MainMenuActivity.java
@@ -163,7 +163,7 @@ public class MainMenuActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.menu);
 		
