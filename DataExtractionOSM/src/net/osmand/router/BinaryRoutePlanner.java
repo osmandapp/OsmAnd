8cb6e2338605604a97ade4aafe3d71e573686d30
arlas <arlas@libero.it>
2011-09-12 17:28:32 +0300

    now deprecated?

--- DataExtractionOSM/src/net/osmand/router/BinaryRoutePlanner.java
+++ DataExtractionOSM/src/net/osmand/router/BinaryRoutePlanner.java
@@ -116,11 +116,11 @@ public class BinaryRoutePlanner {
 			ctx.timeToLoad += (System.nanoTime() - now);
 		}
 	}
-	
+	//now deprecated?
 	// calculate distance from C to AB (distnace doesn't calculate 
 	private static double calculateDistance(int xA, int yA, int xB, int yB, int xC, int yC, double distAB){
 		// Scalar multiplication between (AB', AC)
-		double multiple = (-convert31YToMeters(yB, yA)) * convert31XToMeters(xC,xA) + convert31YToMeters(xB,xA) * convert31YToMeters(yC, yA);
+		double multiple = (-convert31YToMeters(yB, yA)) * convert31XToMeters(xC,xA) + convert31XToMeters(xB,xA) * convert31YToMeters(yC, yA);
 		return multiple / distAB;
 	}
 	
