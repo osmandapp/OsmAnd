eb256ffcd0cfba47aa8f7a6d1db0438af6c6f70d
vshcherb <victor.shcherb@gmail.com>
2011-09-14 01:51:37 +0300

    Fix convert31Y

--- DataExtractionOSM/src/net/osmand/router/BinaryRoutePlanner.java
+++ DataExtractionOSM/src/net/osmand/router/BinaryRoutePlanner.java
@@ -119,7 +119,7 @@ public class BinaryRoutePlanner {
 	// calculate distance from C to AB (distnace doesn't calculate 
     private static double calculateDistance(int xA, int yA, int xB, int yB, int xC, int yC, double distAB){
     	// Scalar multiplication between (AB', AC)
-    	double multiple = (-convert31YToMeters(yB, yA)) * convert31XToMeters(xC,xA) + convert31YToMeters(xB,xA) * convert31YToMeters(yC, yA);
+    	double multiple = (-convert31YToMeters(yB, yA)) * convert31XToMeters(xC,xA) + convert31XToMeters(xB,xA) * convert31YToMeters(yC, yA);
     	return multiple / distAB;
     }
     
