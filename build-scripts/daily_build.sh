470ee5b29ad5be9c5ebccb55ca55b49ae8d005bd
Victor Shcherb <victor.shcherb@gmail.com>
2011-10-11 23:03:56 +0200

    Update night build

--- build-scripts/daily_build.sh
+++ build-scripts/daily_build.sh
@@ -12,7 +12,7 @@ echo > $LOG_FILE
 touch  $CLOG_FILE
 
 cd "${DIRECTORY}"
-git pull --rebase 2>&1 >>$LOG_FILE
+git pull --rebase >>$LOG_FILE 2>&1
 
 # 1. Update git directory
 "${DIRECTORY}/update_git.sh" >>$LOG_FILE 2>&1
@@ -21,9 +21,9 @@ git pull --rebase 2>&1 >>$LOG_FILE
 "${DIRECTORY}/build_branches.sh" >>$LOG_FILE 2>&1
 
 # exit if nothing was changed
- if [ $? = 0 ]; then
-   exit 0
- fi
+# if [ $? = 0 ]; then
+#   exit 0
+# fi
 
 # 3. upload to ftp server 
 #"${DIRECTORY}/upload_ftp.sh" 2>&1 >>$LOG_FILE
