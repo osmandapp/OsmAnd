dba51699ddd3540e8631aec7f14163af5f5b7113
Victor Shcherb <victor.shcherb@gmail.com>
2011-10-11 21:46:41 +0200

    Fix build - update scripts

--- build-scripts/daily_build.sh
+++ build-scripts/daily_build.sh
@@ -19,7 +19,7 @@ git pull --rebase 2>&1 >>$LOG_FILE
 # 2. Go through branches and generates builds
 "${DIRECTORY}/build_branches.sh" >>$LOG_FILE 2>&1
 # exit if nothing was changed
-if [ $? != 0 ]; then
+if [ $? == 0 ]; then
    exit
 fi
 
