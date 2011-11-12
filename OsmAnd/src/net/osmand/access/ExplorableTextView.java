f9f89565c6d2a34fd08d9d02ee9e7e2441e54074
Igor B. Poretsky <poretsky@mlbox.ru>
2011-11-13 00:10:24 +0400

    Implemented accessible exploration with arrow keys for displayed text.
    Used ExplorableTextView for displaying help screens.

---
+++ OsmAnd/src/net/osmand/access/ExplorableTextView.java
@@ -0,0 +1,75 @@
+package net.osmand.access;
+
+import java.lang.Math;
+
+import android.content.Context;
+import android.text.Layout;
+import android.text.method.ArrowKeyMovementMethod;
+import android.text.method.MovementMethod;
+import android.util.AttributeSet;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+// This class is intended to be used in place of the TextView
+// to provide accessible exploration capability by arrow keys.
+//
+public class ExplorableTextView extends TextView {
+
+    private int currentPosition;
+    private int previousPosition;
+    private int selectionStart;
+    private int selectionEnd;
+
+
+    // Conventional constructors.
+
+    public ExplorableTextView(Context context) {
+        super(context);
+    }
+
+    public ExplorableTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExplorableTextView(Context context, AttributeSet attrs, int style) {
+        super(context, attrs, style);
+    }
+
+
+    // Overrided callbacks to provide accessible exploration means.
+
+    public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event) {
+        boolean result = super.dispatchPopulateAccessibilityEvent(event);
+        if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED) {
+            event.setAddedCount(selectionEnd - selectionStart);
+            event.setFromIndex(selectionStart);
+            previousPosition = currentPosition;
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    protected MovementMethod getDefaultMovementMethod() {
+        return ArrowKeyMovementMethod.getInstance();
+    }
+
+    @Override
+    protected void onSelectionChanged(int start, int end) {
+        currentPosition = end;
+        if (currentPosition != previousPosition) {
+            if (Math.abs(currentPosition - previousPosition) > 1) {
+                final Layout layout = getLayout();
+                final int line = layout.getLineForOffset(currentPosition);
+                selectionStart = layout.getLineStart(line);
+                selectionEnd = layout.getLineEnd(line);
+            } else {
+                selectionStart = currentPosition;
+                selectionEnd = currentPosition + 1;
+            }
+            sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
+        }
+    }
+
+}
