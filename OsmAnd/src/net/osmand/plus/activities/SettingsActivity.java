83a429ff14715ba9c1557be2107c5ab7bd92b3d2
Fabien Carrion <fabien.carrion@gmail.com>
2011-11-24 16:35:07 -0600

    Add new activity to display all the POI changes to upload to openstreetmap server

--- OsmAnd/src/net/osmand/plus/activities/SettingsActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/SettingsActivity.java
@@ -66,6 +66,7 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 	
 	private Preference saveCurrentTrack;
 	private Preference testVoiceCommands;
+	private Preference localOpenstreetmapPoints;
 
 	private EditTextPreference applicationDir;
 	private ListPreference tileSourcePreference;
@@ -315,6 +316,8 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 		routeServiceEnabled.setOnPreferenceChangeListener(this);
 		applicationDir = (EditTextPreference) screen.findPreference(OsmandSettings.EXTERNAL_STORAGE_DIR);
 		applicationDir.setOnPreferenceChangeListener(this);
+		localOpenstreetmapPoints = (Preference) screen.findPreference(OsmandSettings.LOCAL_OPENSTREETMAP_POINTS);
+		localOpenstreetmapPoints.setOnPreferenceClickListener(this);
 		
 		
 		
@@ -748,6 +751,9 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 			} else {
 				helper.close();
 			}
+			return true;
+		} else if(preference == localOpenstreetmapPoints){
+			startActivity(new Intent(this, LocalOpenstreetmapActivity.class));
 			return true;
 		}
 		return false;
