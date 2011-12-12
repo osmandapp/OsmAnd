de65c284c4b4b1f5f5630db257e3c80e2b4d4d4a
Stefan Radev <stefan.visions@gmail.com>
2011-12-12 15:11:03 +0100

    new image changes

--- OsmAnd/src/net/osmand/plus/views/FavoritesLayer.java
+++ OsmAnd/src/net/osmand/plus/views/FavoritesLayer.java
@@ -74,7 +74,11 @@ public class FavoritesLayer extends OsmandMapLayer implements ContextMenuLayer.I
 					int x = view.getRotatedMapXForPoint(o.getLatitude(), o.getLongitude());
 					int y = view.getRotatedMapYForPoint(o.getLatitude(), o.getLongitude());
 					canvas.drawBitmap(favoriteIcon, x - favoriteIcon.getWidth() / 2, 
+<<<<<<< HEAD
 							y - favoriteIcon.getHeight() , paint);
+=======
+							y - favoriteIcon.getHeight() / 2, paint);
+>>>>>>> Update favorite icon
 				}
 			}
 		}
