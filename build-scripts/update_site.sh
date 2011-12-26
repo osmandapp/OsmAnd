09a4f3e9ce42cc1b8a17a0337c4fa84311b9fdfc
Victor Shcherb <victor.shcherb@gmail.com>
2011-12-26 18:06:32 +0100

    Update site

--- build-scripts/update_site.sh
+++ build-scripts/update_site.sh
@@ -4,7 +4,10 @@ DIRECTORY=$(cd `dirname $0` && pwd)
 GIT_SITE_DIR=$DIRECTORY/../config/site
 LOCAL_SITE_DIR=/var/www-download
 
-files='*.php resource/* tile_sources.xml favicon.ico'
+files='*.php tile_sources.xml favicon.ico'
 for f in $files ; do
 	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
 done
+
+cp $GIT_SITE_DIR/resource/* $LOCAL_SITE_DIR/resource/* -u;
+
