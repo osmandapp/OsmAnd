1923aeea01c186dc9fa9677922250ed3015cb82f
Victor Shcherb <victor.shcherb@gmail.com>
2011-12-26 17:29:08 +0100

    Update remote resources

--- build-scripts/update_site.sh
+++ build-scripts/update_site.sh
@@ -4,7 +4,7 @@ DIRECTORY=$(cd `dirname $0` && pwd)
 GIT_SITE_DIR=$DIRECTORY/config/site
 LOCAL_SITE_DIR=/var/www-download
 
-files='*.php tile_sources.xml favicon.ico'
+files='*.php resource/* tile_sources.xml favicon.ico'
 for f in $files ; do
 	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
 done
