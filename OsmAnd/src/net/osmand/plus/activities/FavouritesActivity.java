e35ea49070a9172c296a443649eb1cff9fb0ef32
sonora <hm.gglmail@googlemail.com>
2011-12-27 14:46:33 +0100

    fix build for now

--- OsmAnd/src/net/osmand/plus/activities/FavouritesActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/FavouritesActivity.java
@@ -282,7 +282,8 @@ public class FavouritesActivity extends ExpandableListActivity {
 		if (aItem.getItemId() == DELETE_ITEM) {
 			final Resources resources = this.getResources();
 			Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(MessageFormat.format(resources.getString(R.string.favourites_remove_dialog_msg, point.getName())));
+			// builder.setMessage(MessageFormat.format(resources.getString(R.string.favourites_remove_dialog_msg, point.getName())));
+			builder.setMessage(getString(R.string.favourites_remove_dialog_msg, point.getName()));
 			builder.setNegativeButton(R.string.default_buttons_no, null);
 			builder.setPositiveButton(R.string.default_buttons_yes, new DialogInterface.OnClickListener() {
 				@Override
