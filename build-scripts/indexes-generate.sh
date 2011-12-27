5c9bb47d238dcdf8666a53591a22663e54b39702
Victor Shcherb <victor.shcherb@gmail.com>
2011-12-27 01:11:06 +0100

    Update index generate

--- build-scripts/indexes-generate.sh
+++ build-scripts/indexes-generate.sh
@@ -6,5 +6,7 @@ mv ~/indexes/uploaded/*.* backup
 # remove all previous files
 rm -rf ~/indexes
 mkdir ~/indexes
+mkdir ~/indexes/uploaded
+mkdir ~/indexes/osm
 
 java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx720M -cp "DataExtractionOSM/OsmAndMapCreator.jar:DataExtractionOSM/lib/*.jar" net.osmand.data.index.IndexBatchCreator build-scripts/indexes-batch-generate.xml build-scripts/indexes-regions.xml
