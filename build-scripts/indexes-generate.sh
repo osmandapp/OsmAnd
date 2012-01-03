002667293a8892375f326cadf5f5ff31ec927c9b
Victor Shcherb <victor.shcherb@gmail.com>
2012-01-03 23:47:40 +0100

    Change attributes

--- build-scripts/indexes-generate.sh
+++ build-scripts/indexes-generate.sh
@@ -11,4 +11,4 @@ mkdir .work/osm
 if [ -z $INDEXES_FILE ]; then INDEXES_FILE="build-scripts/regions/indexes.xml"; echo "$INDEXES_FILE"; fi
 
 echo 'Running java net.osmand.data.index.IndexBatchCreator with $INDEXES_FILE'
-java -XX:+UseParallelGC -Xmx2048M -Xmn256M -Djava.util.logging.config.file=build-scripts/batch-logging.properties -cp "DataExtractionOSM/OsmAndMapCreator.jar:DataExtractionOSM/lib/*.jar" net.osmand.data.index.IndexBatchCreator build-scripts/indexes-batch-generate.xml "$INDEXES_FILE"
+java -XX:+UseParallelGC -Xmx4096M -Xmn512M -Djava.util.logging.config.file=build-scripts/batch-logging.properties -cp "DataExtractionOSM/OsmAndMapCreator.jar:DataExtractionOSM/lib/*.jar" net.osmand.data.index.IndexBatchCreator build-scripts/indexes-batch-generate.xml "$INDEXES_FILE"
