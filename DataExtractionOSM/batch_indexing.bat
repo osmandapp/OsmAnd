cf6fc437415fa3a2b28943ea6dc77dc788fd658a
Victor Shcherb <victor.shcherb@gmail.com>
2012-01-04 21:05:31 +0100

    Remove deprecated batch indexing API and update new one

--- DataExtractionOSM/batch_indexing.bat
+++
@@ -1,5 +0,0 @@
-REM the JVM. With the below settings the heap size (Available memory for the application)
-REM will range from 64 megabyte up to 512 megabyte.
-
-
-start java.exe -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx720M -cp "./OsmAndMapCreator.jar;./lib/*.jar" net.osmand.data.index.IndexBatchCreator ./batch.xml
--- DataExtractionOSM/batch_indexing.sh
+++
@@ -1,5 +0,0 @@
-# This file will start the Osm Extraction UI with custom memory settings for
-# the JVM. With the below settings the heap size (Available memory for the application)
-# will range from 64 megabyte up to 512 megabyte.
-
-java -Djava.util.logging.config.file=logging.properties -Xms64M -Xmx720M -cp "./OsmAndMapCreator.jar:./lib/*.jar" net.osmand.data.index.IndexBatchCreator ./batch.xml 
