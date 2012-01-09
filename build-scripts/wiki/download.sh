f8e323532018620705f0827420800d8bc123e9b6
Victor Shcherb <victor.shcherb@gmail.com>
2012-01-09 08:57:32 +0100

    Update german wiki

--- build-scripts/wiki/download.sh
+++ build-scripts/wiki/download.sh
@@ -3,9 +3,9 @@ function download {
       echo "Start download $2";
       wget --quiet --output-document="$2"_wiki_1."$1".xml.bz2 http://dumps.wikimedia.org/"$1"wiki/latest/"$1"wiki-latest-pages-articles.xml.bz2
 }
-cd src;
+cd ~/wiki/src;
 download en English;
-download de Deutch;
+download de German;
 download nl Netherlands;
 download fr French;
 download ru Russian;
