d0c7c82a3086c7ec9a69de2e6f4a403392253baf
Victor Shcherb <victor.shcherb@gmail.com>
2012-01-14 00:25:00 +0100

    Add google download statistics

--- build-scripts/update_site.sh
+++ build-scripts/update_site.sh
@@ -9,6 +9,8 @@ for f in $files ; do
 	cp $GIT_SITE_DIR/$f $LOCAL_SITE_DIR/ -u;
 done
 
+mkdir -p $LOCAL_SITE_DIR/resource;
 cp $GIT_SITE_DIR/resource/* $LOCAL_SITE_DIR/resource/ -u;
+mkdir -p $LOCAL_SITE_DIR/GoogleAnalytics;
 cp $GIT_SITE_DIR/GoogleAnalytics/* $LOCAL_SITE_DIR/GoogleAnalytics/ -u;
 
