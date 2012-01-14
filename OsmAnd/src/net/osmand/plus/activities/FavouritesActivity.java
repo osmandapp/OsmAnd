8ce21bd49d4c2c2c189c441185f456cb21a59af2
sonora <hm.gglmail@googlemail.com>
2012-01-14 13:33:12 +0100

    fix build

--- OsmAnd/src/net/osmand/plus/activities/FavouritesActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/FavouritesActivity.java
@@ -203,10 +203,10 @@ public class FavouritesActivity extends ExpandableListActivity {
 //					double d1 = MapUtils.getDistance(mapLocation, object1.getLatitude(), object1.getLongitude());
 //					double d2 = MapUtils.getDistance(mapLocation, object2.getLatitude(), object2.getLongitude());
 //					if(d1 == d2){
-					if(object1.getName == object2.getName){
+					if(object1 == object2){
 						return 0;
 //					} else if(d1 > d2){
-					} else if(object1.getName > object1.getName){
+					} else if(object1 > object2){
 						return 1;
 					}
 					return -1;
