7995b6e6a3de8cb9b4eeef1df0ec1d941da6a2b8
sonora <hm.gglmail@googlemail.com>
2012-01-28 19:18:48 +0100

    display switching-to-cloudmade notification in sync with other routing error messages only

--- OsmAnd/src/net/osmand/plus/routing/RoutingHelper.java
+++ OsmAnd/src/net/osmand/plus/routing/RoutingHelper.java
@@ -462,7 +462,10 @@ public class RoutingHelper {
 		if (serviceToUse == RouteService.OSMAND && !settings.USE_OSMAND_ROUTING_SERVICE_ALWAYS.get()) {
 			double distance = MapUtils.getDistance(end, start.getLatitude(), start.getLongitude());
 			if (distance > DISTANCE_TO_USE_OSMAND_ROUTER) {
-				showMessage(context.getString(R.string.osmand_routing_experimental), Toast.LENGTH_LONG);
+				// display notification in sync with other routing error messages below
+				if (System.currentTimeMillis() - lastTimeEvaluatedRoute > evalWaitInterval) {
+					showMessage(context.getString(R.string.osmand_routing_experimental), Toast.LENGTH_LONG);
+				}
 				serviceToUse = RouteService.CLOUDMADE;
 			}
 		}
