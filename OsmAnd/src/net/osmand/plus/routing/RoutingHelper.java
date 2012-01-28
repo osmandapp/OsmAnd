0db09af5fb9017a9774d52ce1573f2a07dd5de50
sonora <hm.gglmail@googlemail.com>
2012-01-28 10:07:37 +0100

    increase time error message cloudmade-fallback is displayed

--- OsmAnd/src/net/osmand/plus/routing/RoutingHelper.java
+++ OsmAnd/src/net/osmand/plus/routing/RoutingHelper.java
@@ -462,7 +462,7 @@ public class RoutingHelper {
 		if (serviceToUse == RouteService.OSMAND && !settings.USE_OSMAND_ROUTING_SERVICE_ALWAYS.get()) {
 			double distance = MapUtils.getDistance(end, start.getLatitude(), start.getLongitude());
 			if (distance > DISTANCE_TO_USE_OSMAND_ROUTER) {
-				showMessage(context.getString(R.string.osmand_routing_experimental));
+				showMessage(context.getString(R.string.osmand_routing_experimental), Toast.LENGTH_LONG);
 				serviceToUse = RouteService.CLOUDMADE;
 			}
 		}
