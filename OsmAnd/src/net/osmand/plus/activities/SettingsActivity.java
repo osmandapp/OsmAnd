c01ee885d0c29d2feb9e3fb9f96f5d7c1cfe5a1d
sonora <hm.gglmail@googlemail.com>
2012-02-09 22:16:06 +0100

    include nl locale

--- OsmAnd/src/net/osmand/plus/activities/SettingsActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/SettingsActivity.java
@@ -229,7 +229,7 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 		registerListPreference(osmandSettings.METRIC_SYSTEM, screen, entries, MetricsConstants.values());
 		
 		//getResources().getAssets().getLocales();
-		entrieValues = new String[] { "", "en", "cs", "de", "es", "jp", "fr", "hu", "it", "pl", "pt", "ru", "sk", "vi" };
+		entrieValues = new String[] { "", "en", "cs", "de", "es", "fr", "hu", "it", "jp", "nl", "pl", "pt", "ru", "sk", "vi" };
 		entries = new String[entrieValues.length];
 		entries[0] = getString(R.string.system_locale);
 		for (int i = 1; i < entries.length; i++) {
