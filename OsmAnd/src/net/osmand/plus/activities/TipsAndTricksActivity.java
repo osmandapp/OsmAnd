c8d8202ceef0f207c58bd1fc0dfdff82965a3883
Victor Shcherb <victor.shcherb@gmail.com>
2012-02-15 00:37:11 +0100

    Prepare to 0.7.1

--- OsmAnd/src/net/osmand/plus/activities/TipsAndTricksActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/TipsAndTricksActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 
 public class TipsAndTricksActivity {
 	private int[][] tipNamesAndDescriptions = new int[][] {
-			{R.string.tip_recent_changes,R.string.tip_recent_changes_0_7_0_t},
+			{R.string.tip_recent_changes,R.string.tip_recent_changes_0_7_1_t},
 			{R.string.tip_rotation_switching,R.string.tip_rotation_switching_t},
 			{R.string.tip_update_index,R.string.tip_update_index_t},
 			{R.string.tip_day_night_mode,R.string.tip_day_night_mode_t},
