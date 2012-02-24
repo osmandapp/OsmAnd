e2e978783d6f5bc506fa23fecf0be745298e2927
sonora <hm.gglmail@googlemail.com>
2012-02-24 01:31:40 +0100

    add comment to finish later

--- OsmAnd/src/net/osmand/plus/activities/SettingsActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/SettingsActivity.java
@@ -487,14 +487,15 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 		routerServicePreference.setSummary(getString(R.string.router_service_descr) + "  [" + osmandSettings.ROUTER_SERVICE.get() + "]");
 
 /*
+		// TODO: Add Profile name to screen title (only, not to Preference title) of Profile specific settings
 		PreferenceScreen screen = getPreferenceScreen();
-		if (screen.getTitle().startsWith(getString(R.string.rendering_settings))) {
+		if (screen.getKey().toString().equals("map_settings")) {
 			 screen.setTitle(getString(R.string.rendering_settings) + "  [" + ApplicationMode.toHumanString(osmandSettings.APPLICATION_MODE.get(), this) + "]");
-		} else if (screen.getTitle().startsWith(getString(R.string.appearance_settings))) {
+		} else if (screen.getKey().toString().equals("appearance_settings")) {
 			 screen.setTitle(getString(R.string.appearance_settings) + "  [" + ApplicationMode.toHumanString(osmandSettings.APPLICATION_MODE.get(), this) + "]");
-		} else if (screen.getTitle().startsWith(getString(R.string.monitor_preferences))) {
+		} else if (screen.getKey().toString().equals("monitor_settings")) {
 			 screen.setTitle(getString(R.string.monitor_preferences) + "  [" + ApplicationMode.toHumanString(osmandSettings.APPLICATION_MODE.get(), this) + "]");
-		} else if (screen.getTitle().startsWith(getString(R.string.routing_settings))) {
+		} else if (screen.getKey().toString().equals("routing_settings")) {
 			 screen.setTitle(getString(R.string.routing_settings) + "  [" + ApplicationMode.toHumanString(osmandSettings.APPLICATION_MODE.get(), this) + "]");
 		}
 */
