e9b3c8c309e579e14d3cbd3fa03e4991d64bc41a
sonora <hm.gglmail@googlemail.com>
2012-02-26 09:36:54 +0100

    fix bug caused by rcent change from Australia to Oceania

--- OsmAnd/src/net/osmand/plus/activities/DownloadIndexActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/DownloadIndexActivity.java
@@ -814,7 +814,7 @@ public class DownloadIndexActivity extends OsmandExpandableListActivity {
 			} else if(lc.contains("_asia_")) {
 				nameId = R.string.index_name_asia;
 				order = 50;
-			} else if(lc.contains("_oceania_")) {
+			} else if(lc.contains("_oceania_") || lc.contains("_australia") ) {
 				nameId = R.string.index_name_oceania;
 				order = 70;
 			} else if(lc.contains("_wiki_")) {
