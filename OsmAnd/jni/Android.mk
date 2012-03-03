232da9c70b4416b23260559f6dabf7bac86acfeb
vshcherb <victor.shcherb@gmail.com>
2012-03-03 18:10:03 +0100

    Trigger build (nothing changed)

--- OsmAnd/jni/Android.mk
+++ OsmAnd/jni/Android.mk
@@ -3,6 +3,7 @@ OSMAND_MAKEFILES := $(all-subdir-makefiles)
 # By default, include makefiles only once
 include $(OSMAND_MAKEFILES)
 
+
 # If we may support NEON, include them once more
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 
