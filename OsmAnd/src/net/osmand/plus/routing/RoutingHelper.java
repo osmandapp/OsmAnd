5638b379bec35c1eebe227774ea1d147bbefb245
sonora <hm.gglmail@googlemail.com>
2012-03-16 01:16:30 +0100

    missing )

--- OsmAnd/src/net/osmand/plus/routing/RoutingHelper.java
+++ OsmAnd/src/net/osmand/plus/routing/RoutingHelper.java
@@ -306,7 +306,7 @@ public class RoutingHelper {
 		lastFixedLocation = currentLocation;
 
 		// 8. Strange Direction? Then re-calculate route. (Added new, may possibly even replace triggers 4, 5a, 5b ?)
-		if(suppressTurnPrompt && (currentLocation.distanceTo(routeNodes.get(currentRoute) > 100) ){
+		if(suppressTurnPrompt && (currentLocation.distanceTo(routeNodes.get(currentRoute)) > 100) ){
 			calculateRoute = true;
 		}
 
