e56d47d204c46b18761c27d7b63f4edb3049057f
Pavol Zibrita <pavol.zibrita@gmail.com>
2012-04-13 03:28:58 +0200

    Fixed IndexOutOfBound exception, possible when navigating.

--- OsmAnd/src/net/osmand/plus/routing/RoutingHelper.java
+++ OsmAnd/src/net/osmand/plus/routing/RoutingHelper.java
@@ -308,7 +308,7 @@ public class RoutingHelper {
 		lastFixedLocation = currentLocation;
 
 		// 8. Strange Direction? Then re-calculate route. (Added new, may possibly even replace triggers 4, 5a, 5b ?)
-		if(suppressTurnPrompt && (currentLocation.distanceTo(routeNodes.get(currentRoute)) > POSITION_TOLERANCE) ){
+		if(suppressTurnPrompt && (routeNodes.isEmpty() || (currentLocation.distanceTo(routeNodes.get(currentRoute)) > POSITION_TOLERANCE))){
 			calculateRoute = true;
 		}
 
