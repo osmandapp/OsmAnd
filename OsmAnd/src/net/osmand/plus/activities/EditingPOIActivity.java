ab0bb97ec77529265ca4a70156e8ff26b6b6bba6
Pavol Zibrita <pavol.zibrita@gmail.com>
2012-04-13 10:00:36 +0200

    Fixed issue 875

--- OsmAnd/src/net/osmand/plus/activities/EditingPOIActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/EditingPOIActivity.java
@@ -138,7 +138,7 @@ public class EditingPOIActivity implements DialogProvider {
 					public void run() {
 						AccessibleToast.makeText(ctx, ctx.getResources().getString(R.string.poi_remove_success), Toast.LENGTH_LONG).show();
 						if(ctx.getMapView() != null){
-							ctx.getMapView().refreshMap();
+							ctx.getMapView().refreshMap(true);
 						}						
 					}
 				});
@@ -283,7 +283,7 @@ public class EditingPOIActivity implements DialogProvider {
 						AccessibleToast.makeText(ctx, MessageFormat.format(ctx.getResources().getString(R.string.poi_action_succeded_template), msg),
 								Toast.LENGTH_LONG).show();
 						if (ctx.getMapView() != null) {
-							ctx.getMapView().refreshMap();
+							ctx.getMapView().refreshMap(true);
 						}
 						ctx.dismissDialog(dialogID);
 					}
