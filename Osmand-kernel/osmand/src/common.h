fb62e433fb6f3d278650d19e38c43586f5b09bfd
Victor Shcherb <victor.shcherb@gmail.com>
2012-05-01 09:50:36 +0200

    Fix build

--- Osmand-kernel/osmand/src/common.h
+++ Osmand-kernel/osmand/src/common.h
@@ -8,27 +8,7 @@
 #include <hash_map>
 #include <hash_set>
 
-#ifdef _MSC_VER
-typedef __int8  int8;
-typedef __int16 int16;
-typedef __int32 int32;
-typedef __int64 int64;
 
-typedef unsigned __int8  uint8;
-typedef unsigned __int16 uint16;
-typedef unsigned __int32 uint32;
-typedef unsigned __int64 uint64;
-#else
-typedef int8_t  int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-
-typedef uint8_t  uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-#endif
 
 #ifdef HAVING_HASH
 
@@ -59,6 +39,29 @@ namespace __gnu_cxx {
       { return __x; }
     };
 }
+
+#ifdef _MSC_VER
+typedef __int8  int8;
+typedef __int16 int16;
+typedef __int32 int32;
+typedef __int64 int64;
+
+typedef unsigned __int8  uint8;
+typedef unsigned __int16 uint16;
+typedef unsigned __int32 uint32;
+typedef unsigned __int64 uint64;
+#else
+typedef int8_t  int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+#endif
+
 
 #else
 
