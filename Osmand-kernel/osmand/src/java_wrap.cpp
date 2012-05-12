7e0e71a424cc8508fbab1a076cdd97ae8acf8d78
Jindřich Makovička <makovick@gmail.com>
2012-05-12 18:13:48 +0200

    fix warning

--- Osmand-kernel/osmand/src/java_wrap.cpp
+++ Osmand-kernel/osmand/src/java_wrap.cpp
@@ -18,7 +18,7 @@ JavaVM* globalJVM = NULL;
 void loadJniRenderingContext(JNIEnv* env);
 void loadJniRenderingRules(JNIEnv* env);
 
-static int simplePngSize = 93;
+static const int simplePngSize = 93;
 static void* simplePng = new uint8[simplePngSize]{
 		0x89 ,0x50 ,0x4E ,0x47 ,0x0D ,0x0A ,0x1A ,0x0A ,
 		0x00 ,0x00 ,0x00 ,0x0D ,0x49 ,0x48 ,0x44 ,0x52 ,
