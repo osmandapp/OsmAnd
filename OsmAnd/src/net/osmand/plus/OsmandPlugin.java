7e17b38bd14b9e7aeec3c13829a920b7d233c0f1
sonora <hm.gglmail@googlemail.com>
2012-06-04 23:15:35 +0200

    sort Plugin Manager by average significance

--- OsmAnd/src/net/osmand/plus/OsmandPlugin.java
+++ OsmAnd/src/net/osmand/plus/OsmandPlugin.java
@@ -50,11 +50,11 @@ public abstract class OsmandPlugin {
 		OsmandSettings settings = app.getSettings();
 		OsmandRasterMapsPlugin rasterMapsPlugin = new OsmandRasterMapsPlugin(app);
 		installedPlugins.add(rasterMapsPlugin);
-		installedPlugins.add(new OsmandMonitoringPlugin(app));
-		installedPlugins.add(new OsmEditingPlugin(app));
-		installedPlugins.add(new OsmandBackgroundServicePlugin(app));
-		installedPlugins.add(new AccessibilityPlugin(app));
 		installedPlugins.add(new OsmandExtraSettings(app));
+		installedPlugins.add(new OsmandMonitoringPlugin(app));
+		installedPlugins.add(new OsmandBackgroundServicePlugin(app));
+		installedPlugins.add(new OsmEditingPlugin(app));
+		installedPlugins.add(new AccessibilityPlugin(app));
 		installedPlugins.add(new OsmandDevelopmentPlugin(app));
 		
 		Set<String> enabledPlugins = settings.getEnabledPlugins();
