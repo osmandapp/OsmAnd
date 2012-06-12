b2ae6b0f0e89b62fc735ea59f30c258cd66a5b06
Victor Shcherb <victor.shcherb@gmail.com>
2012-06-13 00:57:54 +0200

    Fix NPE

--- OsmAnd/src/net/osmand/plus/routing/RouteProvider.java
+++ OsmAnd/src/net/osmand/plus/routing/RouteProvider.java
@@ -248,7 +248,7 @@ public class RouteProvider {
 			}
 		}
 		private void introduceFirstPoint(Location start) {
-			if (locations.get(0).distanceTo(start) > 200) {
+			if (!locations.isEmpty() && locations.get(0).distanceTo(start) > 200) {
 				// add start point
 				locations.add(0, start);
 				if (directions != null) {
