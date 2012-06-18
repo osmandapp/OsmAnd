cbd8ea9aeb0c2c6ed3c026c44a632f97ef98a1de
Alexey Pelykh <alexey.pelykh@gmail.com>
2012-06-18 22:39:31 +0300

    Modified zlib script

--- Osmand-kernel/zlib/configure.sh
+++ Osmand-kernel/zlib/configure.sh
@@ -1,3 +1,5 @@
+rm -rf ./zlib_library
 wget -qO - http://zlib.net/zlib-1.2.7.tar.gz | tar xzvf -
-mv -f zlib-1.2.7/* zlib_library/
-rm -r zlib-1.2.7
+mkdir -p ./zlib_library
+mv -f ./zlib-1.2.7/* ./zlib_library
+rm -r ./zlib-1.2.7
