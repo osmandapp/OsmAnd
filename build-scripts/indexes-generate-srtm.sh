112ceb2a0858b2b9340a654b6731dc2685322226
Victor Shcherb <victor.shcherb@gmail.com>
2012-06-23 21:49:27 +0200

    Add srtm proccess

--- build-scripts/indexes-generate-srtm.sh
+++ build-scripts/indexes-generate-srtm.sh
@@ -2,7 +2,7 @@ cd ~/srtm-proccess/
 rm -rf .work
 mkdir .work
 mkdir .work/osm
-if [ -z $INDEXES_FILE ]; then INDEXES_FILE="build-scripts/regions/indexes.xml"; echo "$INDEXES_FILE"; fi
+if [ -z $INDEXES_FILE ]; then INDEXES_FILE="build-scripts/regions/srtm_indexes.xml"; echo "$INDEXES_FILE"; fi
 
 echo 'Running java net.osmand.data.index.IndexBatchCreator with $INDEXES_FILE'
 java -XX:+UseParallelGC -Xmx4096M -Xmn512M -Djava.util.logging.config.file=build-scripts/batch-logging.properties -cp "DataExtractionOSM/OsmAndMapCreator.jar:DataExtractionOSM/lib/*.jar" net.osmand.data.index.IndexBatchCreator build-scripts/srtm-batch-generate.xml "$INDEXES_FILE"
