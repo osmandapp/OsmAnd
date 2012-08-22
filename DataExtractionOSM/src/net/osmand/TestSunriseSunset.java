1cd15df2013bf31ec63c9e882e8ad83d8a8b5ecf
Victor Shcherb <victor.shcherb@gmail.com>
2012-08-22 09:38:02 +0200

    Add tests

--- DataExtractionOSM/src/net/osmand/TestSunriseSunset.java
+++ DataExtractionOSM/src/net/osmand/TestSunriseSunset.java
@@ -1,29 +1,35 @@
 package net.osmand;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 public class TestSunriseSunset {
 	
+	public static final String MARCH = "05.03.2012";
 	public static final String AUGUST = "05.08.2012";
 	public static final String OCTOBER = "05.10.2012";
 	public static final String DECEMBER = "05.12.2012";
 	public static final String FEBRUARY = "05.02.2012";
 	public static final String MAY = "05.05.2012";
-	public static final String[] DATES = new String[] {AUGUST, OCTOBER, DECEMBER, FEBRUARY, MAY};
+	public static final String[] DATES = new String[] {MARCH, AUGUST, OCTOBER, DECEMBER, FEBRUARY, MAY};
 	
-	public static SunriseSunset printSunriseSunset(String date, float lat, float lon, TimeZone tz) throws ParseException {
+	public static void printSunriseSunset(String date, float lat, float lon, TimeZone tz) throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-		sdf.setTimeZone(tz);
 		SimpleDateFormat prt = new SimpleDateFormat("dd.MM.yyyy HH:MM");
-		prt	.setTimeZone(tz);
-		SunriseSunset ss = new SunriseSunset(lat, lon, sdf.parse(date), tz);
-		System.out.println("Sunrise : " + prt.format(ss.getSunrise()));
-		System.out.println("Sunset  : " + prt.format(ss.getSunset()));
-		return ss;
+		prt.setTimeZone(TimeZone.getTimeZone("GMT"));
+		Date time = sdf.parse(date);
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(time);
+		SunriseSunset ss = new SunriseSunset(lat, lon, time, TimeZone.getTimeZone("GMT"));
+//		Location location = new Location(lat, lon);
+//		SunriseSunsetCalculator calc = new SunriseSunsetCalculator(location, TimeZone.getTimeZone("GMT"));
+		System.out.println("Sunrise : " + prt.format(ss.getSunrise()) + " GMT");
+//		System.out.println("Sunrise : " + prt.format(calc.getOfficialSunriseCalendarForDate(calendar).getTime()));
+//		System.out.println("Sunset  : " + prt.format( calc.getOfficialSunsetCalendarForDate(calendar).getTime()));
+		System.out.println("Sunset  : " + prt.format(ss.getSunset()) + " GMT");
 	}
 	
 	public static void testPCT() throws ParseException{
@@ -38,16 +44,12 @@ public class TestSunriseSunset {
 		for (String d : DATES) {
 			printSunriseSunset(d, 52.88f, 4.86f, TimeZone.getTimeZone("CET"));
 		}
-		System.out.println("Amsterdam 0");
-		for (String d : DATES) {
-			printSunriseSunset(d, 0, 4.86f, TimeZone.getTimeZone("UTC"));
-		}
 	}
 	
 	public static void testEDT() throws ParseException{
 		System.out.println("New york");
 		for (String d : DATES) {
-			printSunriseSunset(d, 40.88f, -73.86f, TimeZone.getTimeZone("EDT"));
+			printSunriseSunset(d, 40.88f, -73.86f, TimeZone.getTimeZone("America/New_York"));
 		}
 	}
 	
