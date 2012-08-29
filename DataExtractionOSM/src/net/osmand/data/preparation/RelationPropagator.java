f928ad4eb2ae75149f523ecd560343a6cfc266f1
prqek <prqek@centrum.cz>
2012-08-29 22:00:03 +0200

    Relation tags propageted recursivesly.

--- DataExtractionOSM/src/net/osmand/data/preparation/RelationPropagator.java
+++ DataExtractionOSM/src/net/osmand/data/preparation/RelationPropagator.java
@@ -17,54 +17,60 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 
-public class RelationPropagator{
+public class RelationPropagator {
 
- private static final Log log = LogFactory.getLog(RelationPropagator.class);
- private final MapRenderingTypes renderingTypes;
+	private static final Log log = LogFactory.getLog(RelationPropagator.class);
+	private final MapRenderingTypes renderingTypes;
 
- public RelationPropagator(MapRenderingTypes renderingTypes) {
-   this.renderingTypes = renderingTypes;
- }
+	public RelationPropagator(MapRenderingTypes renderingTypes) {
+		this.renderingTypes = renderingTypes;
+	}	
 
- public void iterateRelation(Relation r, OsmDbAccessorContext ctx) throws SQLException {
- 
-   ctx.loadEntityRelation(r);
+	public void iterateRelation(Relation r, OsmDbAccessorContext ctx) throws SQLException {
+		iterateRelation(r,ctx,null);
+	}	
 
-   List<Tag> tagsToPropagate = new ArrayList<Tag>();
+	public void iterateRelation(Relation r, OsmDbAccessorContext ctx, Collection<String> tagKeySet) throws SQLException {
 
-    
-   Collection<String> tagKeySet = r.getTagKeySet();
-   for (String tag : tagKeySet) {
-     String val = r.getTag(tag);
-     boolean exists = renderingTypes.ruleExists(tag,val);
-     if(exists)tagsToPropagate.add(new Tag(tag,val));
-   }
+		ctx.loadEntityRelation(r);
 
-   Collection<Entity> members = r.getMembers(null);
-   for(Iterator iter = members.iterator(); iter.hasNext();){
-    Entity e = (Entity)iter.next();
-    if(e instanceof Way){
-     ctx.loadEntityWay((Way)e);
-     Iterator it = tagsToPropagate.iterator();
-     while(it.hasNext()){
-      Tag t = (Tag)it.next();
-      log.info("adding tag " + t.name + ":" + t.value + " to " + e);
-      e.putTag(t.name,t.value);
-     }
-     if(e.isDirty())ctx.saveEntityWay((Way)e);
-    }
+		List<Tag> tagsToPropagate = new ArrayList<Tag>();
 
-   }
+		if(tagKeySet == null)tagKeySet = r.getTagKeySet();
 
- }
+		for (String tag : tagKeySet) {
+			String val = r.getTag(tag);
+			boolean exists = renderingTypes.ruleExists(tag,val);
+			if(exists)tagsToPropagate.add(new Tag(tag,val));
+		}
 
- private class Tag{
-  public String name;
-  public String value;
-  public Tag(String name, String value){
-   this.name = name;
-   this.value = value;
-  }
- }
+		Collection<Entity> members = r.getMembers(null);
+		for(Iterator iter = members.iterator(); iter.hasNext();) {
+			Entity e = (Entity)iter.next();
+			if(e instanceof Way) {
+				ctx.loadEntityWay((Way)e);
+				Iterator it = tagsToPropagate.iterator();
+				while(it.hasNext()) {
+					Tag t = (Tag)it.next();
+					log.info("adding tag " + t.name + ":" + t.value + " to " + e);
+					e.putTag(t.name,t.value);
+				}
+				if(e.isDirty())ctx.saveEntityWay((Way)e);
+			}
+			if(e instanceof Relation)iterateRelation((Relation)e,ctx,tagKeySet);
+
+		}			
+
+	}
+
+	private class Tag {
+		public String name;
+		public String value;
+		
+		public Tag(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
+	}
 
 }
