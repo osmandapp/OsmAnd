6c91cce268755ba8369282846b085488c58fde4a
Victor Shcherb <victor.shcherb@gmail.com>
2012-09-05 22:17:26 +0200

    Fix zoom

--- OsmAnd/src/net/osmand/plus/views/MapControlsLayer.java
+++ OsmAnd/src/net/osmand/plus/views/MapControlsLayer.java
@@ -35,8 +35,8 @@ public class MapControlsLayer extends OsmandMapLayer {
 
 	private static final int SHOW_ZOOM_LEVEL_MSG_ID = 3;
 	private static final int SHOW_ZOOM_LEVEL_DELAY = 2000;
-	//private static final float ZOOM_DELTA = 1;
-	private static final float ZOOM_DELTA = OsmandMapTileView.ZOOM_DELTA_1; 
+	private static final float ZOOM_DELTA = 1;
+//	private static final float ZOOM_DELTA = OsmandMapTileView.ZOOM_DELTA_1; 
 	
 
 	private OsmandMapTileView view;
@@ -312,9 +312,9 @@ public class MapControlsLayer extends OsmandMapLayer {
 			@Override
 			public void onClick(View v) {
 				if (view.isZooming()) {
-					activity.changeZoom(view.getZoom() + 2 * ZOOM_DELTA);
+					activity.changeZoom(view.getFloatZoom() + 2 * ZOOM_DELTA);
 				} else {
-					activity.changeZoom(view.getZoom() + 1 * ZOOM_DELTA);
+					activity.changeZoom(view.getFloatZoom() + 1 * ZOOM_DELTA);
 				}
 
 			}
