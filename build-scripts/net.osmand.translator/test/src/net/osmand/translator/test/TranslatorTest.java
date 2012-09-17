32a997148e4884c9ee276d6a54bb85a95e36dfd7
Victor Shcherb <victor.shcherb@gmail.com>
2012-09-18 01:28:08 +0200

    Let test run

--- build-scripts/net.osmand.translator/test/src/net/osmand/translator/test/TranslatorTest.java
+++ build-scripts/net.osmand.translator/test/src/net/osmand/translator/test/TranslatorTest.java
@@ -1,13 +1,17 @@
 package net.osmand.translator.test;
 
+import java.io.File;
+import java.io.IOException;
+
+import net.osmand.translator.handlers.TranslationHandler;
+
 import org.junit.Test;
 
 public class TranslatorTest {
 	
-	
 	@Test
-	public void simpleTest(){
-		
+	public void simpleTest() throws IOException{
+		TranslationHandler.execute(new File("test/resources/MapUtils.java").getAbsolutePath(), System.out);
 		System.out.println("SUCCESS !!!!! ");
 	}
 
