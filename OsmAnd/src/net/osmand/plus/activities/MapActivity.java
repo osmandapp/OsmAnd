8c25fa101215c355b1e4c1e75632f33dfcdacf5b
sonora <hm.gglmail@googlemail.com>
2012-10-14 12:43:04 +0200

    fix build

--- OsmAnd/src/net/osmand/plus/activities/MapActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/MapActivity.java
@@ -131,7 +131,7 @@ public class MapActivity extends AccessibleActivity implements IMapLocationListe
 	private RouteAnimation routeAnimation = new RouteAnimation();
 
 	// this remembers the isMapLinkedToLocation state after map loses focus
-	private boolean linkMapToLocation = false;
+	private static boolean linkMapToLocation = false;
 
 	private boolean isMapLinkedToLocation = false;
 	private ProgressDialog startProgressDialog;
@@ -1384,6 +1384,7 @@ public class MapActivity extends AccessibleActivity implements IMapLocationListe
 	}
 	
 	public static void launchMapActivityMoveToTop(Context activity){
+		// linkMapToLocation memory needs to be reset if coming back from search or similar action
 		linkMapToLocation = false;
 		Intent newIntent = new Intent(activity, OsmandIntents.getMapActivity());
 		newIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
