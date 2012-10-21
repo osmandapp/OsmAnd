282f8a784d73432d78cbabbfb77fa9df2e42fb3b
Victor Shcherb <victor.shcherb@gmail.com>
2012-10-21 22:57:17 +0200

    Fix wrong UTurn indication (according to left/right side)

--- DataExtractionOSM/src/net/osmand/router/RouteResultPreparation.java
+++ DataExtractionOSM/src/net/osmand/router/RouteResultPreparation.java
@@ -314,7 +314,7 @@ public class RouteResultPreparation {
 					t = TurnType.valueOf(TurnType.TSLL, leftSide);
 				} else if (mpi < 120) {
 					t = TurnType.valueOf(TurnType.TL, leftSide);
-				} else if (mpi < 135) {
+				} else if (mpi < 135 || leftSide) {
 					t = TurnType.valueOf(TurnType.TSHL, leftSide);
 				} else {
 					t = TurnType.valueOf(TurnType.TU, leftSide);
@@ -324,7 +324,7 @@ public class RouteResultPreparation {
 					t = TurnType.valueOf(TurnType.TSLR, leftSide);
 				} else if (mpi > -120) {
 					t = TurnType.valueOf(TurnType.TR, leftSide);
-				} else if (mpi > -135) {
+				} else if (mpi > -135 || !leftSide) {
 					t = TurnType.valueOf(TurnType.TSHR, leftSide);
 				} else {
 					t = TurnType.valueOf(TurnType.TU, leftSide);
