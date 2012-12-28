9454de091dc7dced3ef2085905804db9a585d7fd
Victor Shcherb <osmand.app@gmail.com>
2012-12-28 16:30:52 +0300

    Fix issue with sqlite 17th zoom

--- OsmAnd/src/net/osmand/plus/SQLiteTileSource.java
+++ OsmAnd/src/net/osmand/plus/SQLiteTileSource.java
@@ -152,9 +152,9 @@ public class SQLiteTileSource implements ITileSource {
 			try {
 				long z;
 				z = db.compileStatement("SELECT minzoom FROM info").simpleQueryForLong(); //$NON-NLS-1$
-				if (z < 17 && z >= 0)
+				if (z < 17 )
 					baseZoom = 17 - (int)z; // sqlite base zoom, =11 for SRTM hillshade
-				maxZoom = 17; // Cheat to have tiles request even if zoom level not in sqlite
+				maxZoom = 24; // Cheat to have tiles request even if zoom level not in sqlite
 				// decrease maxZoom if too much scaling would be required
 				while ((tileSize >> (maxZoom - baseZoom)) < minScaledSize)
 					maxZoom--;
