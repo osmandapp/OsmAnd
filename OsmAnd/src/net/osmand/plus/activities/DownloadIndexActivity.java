227c9ff45eb0f72af7d41898250d55afb1a0fbfa
Victor Shcherb <victor.shcherb@gmail.com>
2013-02-11 00:52:11 +0100

    Fix missing contour lines

--- OsmAnd/src/net/osmand/plus/activities/DownloadIndexActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/DownloadIndexActivity.java
@@ -396,7 +396,7 @@ public class DownloadIndexActivity extends OsmandExpandableListActivity {
 				cachedSRTMFiles = new ArrayList<SrtmIndexItem>();
 				List<RegionCountry> countries = RegionRegistry.getRegionRegistry().getCountries();
 				for (RegionCountry rc : countries) {
-					if (rc.tiles.size() > 35) {
+					if (rc.tiles.size() > 35 && rc.getSubRegions().size() > 0) {
 						for (RegionCountry ch : rc.getSubRegions()) {
 							cachedSRTMFiles.add(new SrtmIndexItem(ch, indexFileNames));
 						}
