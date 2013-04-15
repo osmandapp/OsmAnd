8e0b520dca357e42182ce0ffcafea62bedf801ef
Victor Shcherb <victor.shcherb@gmail.com>
2013-04-16 00:28:19 +0200

    Update build

--- OsmAnd/jni/Android.mk
+++ OsmAnd/jni/Android.mk
@@ -1,9 +1,19 @@
+ifdef BUILD_ONLY_OLD_LIB
+# TODO
+	OSMAND_MAKEFILES := \
+	$(all-subdir-makefiles) \
+	$(call my-dir)/../../../jni/Android.mk \
+	$(call my-dir)/../../../core/Android.mk \
+	$(call all-makefiles-under,$(call my-dir)/../../../core/externals) \
+	$(call my-dir)/../../../core/utils/Android.mk
+else 
 OSMAND_MAKEFILES := \
 	$(all-subdir-makefiles) \
 	$(call my-dir)/../../../jni/Android.mk \
 	$(call my-dir)/../../../core/Android.mk \
 	$(call all-makefiles-under,$(call my-dir)/../../../core/externals) \
 	$(call my-dir)/../../../core/utils/Android.mk
+endif
 
 # Protect from previous builds
 ifneq ($(TARGET_ARCH_ABI),armeabi-v7a)
