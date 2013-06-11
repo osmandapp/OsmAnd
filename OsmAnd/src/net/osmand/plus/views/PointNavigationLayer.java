eb14744b83e1f86645b4f1d53903bf7f8ed12138
sonora <hm.gglmail@gmail.com>
2013-06-11 21:17:38 +0200

    add name display for waypoint markers

--- OsmAnd/src/net/osmand/plus/views/PointNavigationLayer.java
+++ OsmAnd/src/net/osmand/plus/views/PointNavigationLayer.java
@@ -158,6 +158,7 @@ public class PointNavigationLayer extends OsmandMapLayer implements IContextMenu
 	public void collectObjectsFromPoint(PointF point, List<Object> o) {
 		TargetPointsHelper tg = map.getMyApplication().getTargetPointsHelper();
 		List<LatLon> intermediatePoints = tg.getIntermediatePointsWithTarget();
+		List<String> names = tg.getIntermediatePointNamesWithTarget();
 		int r = getRadiusPoi(view.getZoom());
 		for (int i = 0; i < intermediatePoints.size(); i++) {
 			LatLon latLon = intermediatePoints.get(i);
@@ -172,9 +173,9 @@ public class PointNavigationLayer extends OsmandMapLayer implements IContextMenu
 					tp.location = latLon;
 					tp.intermediate = !target;
 					if (target) {
-						tp.name = view.getContext().getString(R.string.destination_point, "");
+						tp.name = view.getContext().getString(R.string.destination_point, "")  + " : " + names.get(i);
 					} else {
-						tp.name = (i + 1) + ". " + view.getContext().getString(R.string.intermediate_point, "");
+						tp.name = (i + 1) + ". " + view.getContext().getString(R.string.intermediate_point, "")  + " : " + names.get(i);
 					}
 					tp.index = i;
 					o.add(tp);
