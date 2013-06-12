c664cc0eadd87318cc7b262d1df80743f5960a0c
sonora <hm.gglmail@gmail.com>
2013-06-12 14:30:09 +0200

    fix typo

--- OsmAnd/src/net/osmand/plus/activities/MapActivityActions.java
+++ OsmAnd/src/net/osmand/plus/activities/MapActivityActions.java
@@ -804,14 +804,14 @@ public class MapActivityActions implements DialogProvider {
 					// Issue 1929 TODO: insert navigatePointDialog, but without Directions:
 					navigatePointDialogAndLaunchMap(mapActivity, latitude, longitude, null);
 					//targets.navigateToPoint(new LatLon(latitude, longitude), true, targets.getIntermediatePoints().size());
-					MapActivity.launchMapActivityMoveToTop(act);
+					MapActivity.launchMapActivityMoveToTop(mapActivity);
 					IntermediatePointsDialog.openIntermediatePointsDialog(mapActivity);
 				// For button-less search UI
 				} else if (standardId == R.string.context_menu_item_destination_point) {
 					// Issue 1929 TODO: insert navigatePointDialog, but without Directions:
 					navigatePointDialogAndLaunchMap(mapActivity, latitude, longitude, null);
 					//targets.navigateToPoint(new LatLon(latitude, longitude), true, -1);
-					MapActivity.launchMapActivityMoveToTop(act);
+					MapActivity.launchMapActivityMoveToTop(mapActivity);
 					IntermediatePointsDialog.openIntermediatePointsDialog(mapActivity);
 				} else if (standardId == R.string.context_menu_item_share_location) {
 					shareLocation(latitude, longitude, mapActivity.getMapView().getZoom());
