cf9fccba9a0802d776b69b6fa40480632059584a
sonora <hm.gglmail@gmail.com>
2013-06-13 23:16:54 +0200

    do not delete waypoints when chosing directions

--- OsmAnd/src/net/osmand/plus/activities/MapActivityActions.java
+++ OsmAnd/src/net/osmand/plus/activities/MapActivityActions.java
@@ -1256,7 +1256,12 @@ public class MapActivityActions implements DialogProvider {
 				if(onShow != null) {
 					onShow.onClick(v);
 				}
-				targetPointsHelper.setSingleDestination(location.getLatitude(), location.getLongitude(), name);
+				//targetPointsHelper.setSingleDestination(location.getLatitude(), location.getLongitude(), name);
+				getMyApplication().getSettings().setPointToNavigate(latitude, longitude, true, name);
+				targetPointsHelper.updatePointsFromSettings();
+				// always enable and follow and let calculate it (GPS is not accessible in garage)
+				if(!routingHelper.isRouteBeingCalculated() && !routingHelper.isRouteCalculated() ) {
+					getDirections(null, new LatLon(latitude, longitude), true);
 				MapActivity.launchMapActivityMoveToTop(activity);
 				qa.dismiss();
 			}
