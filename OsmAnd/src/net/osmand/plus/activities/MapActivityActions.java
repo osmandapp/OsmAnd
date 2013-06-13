75c51a8d48513ab478d6ca189a71af1eaff0d139
sonora <hm.gglmail@gmail.com>
2013-06-13 15:21:19 +0200

    enhance map waypoint selection by at least coordinate display

--- OsmAnd/src/net/osmand/plus/activities/MapActivityActions.java
+++ OsmAnd/src/net/osmand/plus/activities/MapActivityActions.java
@@ -804,14 +804,16 @@ public class MapActivityActions implements DialogProvider {
 					// Issue 1929: Consistently show IntermediatePointDialog, without subsequent Directions screen
 					//targets.navigateToPoint(new LatLon(latitude, longitude), true, targets.getIntermediatePoints().size());
 					int sz = targets.getIntermediatePoints().size();
-					settings.insertIntermediatePoint(latitude, longitude, "x", sz, false); //$NON-NLS-1$
+					// TODO: enhance next statement to transmit getSelectedObjectName for clicked objects like favorites instead of just coordinates
+					settings.insertIntermediatePoint(latitude, longitude, getString(R.string.point_on_map, latitude, longitude), sz, false);
 					targets.updatePointsFromSettings();
 					IntermediatePointsDialog.openIntermediatePointsDialog(mapActivity);
 				// For button-less search UI
 				} else if (standardId == R.string.context_menu_item_destination_point) {
 					// Issue 1929: Consistently show IntermediatePointDialog, without subsequent Directions screen
 					//targets.navigateToPoint(new LatLon(latitude, longitude), true, -1);
-					settings.setPointToNavigate(latitude, longitude, false, "z"); //$NON-NLS-1$
+					// TODO: enhance next statement to transmit getSelectedObjectName for clicked objects like favorites instead of just coordinates
+					settings.setPointToNavigate(latitude, longitude, false, getString(R.string.point_on_map, latitude, longitude));
 					targets.updatePointsFromSettings();
 					IntermediatePointsDialog.openIntermediatePointsDialog(mapActivity);
 				} else if (standardId == R.string.context_menu_item_share_location) {
