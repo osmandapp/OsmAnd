8520323018777ed967e15ca1c764bad295ea0db3
Victor Shcherb <victor.shcherb@gmail.com>
2013-06-29 16:55:46 +0200

    Try to fix synchronized map bug

--- OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
+++ OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
@@ -12,6 +12,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -52,7 +53,7 @@ public class DownloadIndexesThread {
 	private DownloadIndexActivity uiActivity = null;
 	private IndexFileList indexFiles = null;
 	private List<SrtmIndexItem> cachedSRTMFiles;
-	private Map<IndexItem, List<DownloadEntry>> entriesToDownload = Collections.synchronizedMap(new TreeMap<IndexItem, List<DownloadEntry>>());
+	private Map<IndexItem, List<DownloadEntry>> entriesToDownload = new ConcurrentHashMap<IndexItem, List<DownloadEntry>>();
 	private Set<DownloadEntry> currentDownloads = new HashSet<DownloadEntry>();
 	private final Context ctx;
 	private OsmandApplication app;
