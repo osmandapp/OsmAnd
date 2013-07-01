90c8a4832dcad2818a9a39887100b0130479be8b
sonora <hm.gglmail@gmail.com>
2013-07-01 10:01:05 +0200

    use optimzed log scale for search radius

--- OsmAnd/src/net/osmand/plus/PoiFilter.java
+++ OsmAnd/src/net/osmand/plus/PoiFilter.java
@@ -34,7 +34,7 @@ public class PoiFilter {
 	
 	protected int distanceInd = 1;
 	// in kilometers
-	protected double[] distanceToSearchValues = new double[] {1, 2, 3, 5, 10, 30, 100, 250 };
+	protected double[] distanceToSearchValues = new double[] {1, 2, 5, 10, 20, 50, 100, 200, 500 };
 	
 	
 	// constructor for standard filters
@@ -87,7 +87,7 @@ public class PoiFilter {
 		for(AmenityType t : AmenityType.values()){
 			acceptedTypes.put(t, null);
 		}
-		distanceToSearchValues = new double[] {0.5, 1, 2, 3, 5, 10, 15, 30, 100};
+		distanceToSearchValues = new double[] {0.5, 1, 2, 5, 10, 20, 50, 100};
 	}
 	
 	
