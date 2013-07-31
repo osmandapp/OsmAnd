a0de8b31b30e7cf063107ee92db66230049fb13b
Nelson A. de Oliveira <naoliv@debian.org>
2013-07-31 15:08:29 -0300

    Fix string concatenation in StringBuffer.append()
    Such calls may profitably be turned into chained append calls on the
    existing StringBuffer/Builder/Appendable, saving the cost of an extra
    StringBuffer/Builder allocation.

--- OsmAnd-java/src/net/osmand/binary/BinaryMapIndexReader.java
+++ OsmAnd-java/src/net/osmand/binary/BinaryMapIndexReader.java
@@ -2013,7 +2013,7 @@ public class BinaryMapIndexReader {
 					if(pair == null) {
 						throw new NullPointerException("Type " + types[j] + "was not found");
 					}
-					b.append(pair.toSimpleString()+"("+types[j]+")");
+					b.append(pair.toSimpleString()).append("(").append(types[j]).append(")");
 				}
 				b.append("]");
 				if(obj.getAdditionalTypes() != null && obj.getAdditionalTypes().length > 0){
@@ -2026,7 +2026,7 @@ public class BinaryMapIndexReader {
 						if(pair == null) {
 							throw new NullPointerException("Type " + obj.getAdditionalTypes()[j] + "was not found");
 						}
-						b.append(pair.toSimpleString()+"("+obj.getAdditionalTypes()[j]+")");
+						b.append(pair.toSimpleString()).append("(").append(obj.getAdditionalTypes()[j]).append(")");
 						
 					}
 					b.append("]");
@@ -2043,7 +2043,7 @@ public class BinaryMapIndexReader {
 						if(pair == null) {
 							throw new NullPointerException("Type " + keys[j] + "was not found");
 						}
-						b.append(pair.toSimpleString()+"("+keys[j]+")");
+						b.append(pair.toSimpleString()).append("(").append(keys[j]).append(")");
 						b.append(" - ").append(names.get(keys[j]));
 					}
 					b.append("]");
