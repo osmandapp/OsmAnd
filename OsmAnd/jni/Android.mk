c69d37c7f0b37c2e557801a4f5ee62486c15efb6
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-08-04 09:42:34 +0300

    Modify android makefiles

--- OsmAnd/jni/Android.mk
+++ OsmAnd/jni/Android.mk
@@ -2,23 +2,23 @@ ifdef BUILD_ONLY_OLD_LIB
 OSMAND_MAKEFILES := $(all-subdir-makefiles) $(call all-makefiles-under,$(call my-dir)/../../../core/targets/android)
 else 
 OSMAND_MAKEFILES := \
-	$(all-subdir-makefiles) \
-	$(call my-dir)/../../../jni/Android.mk \
-	$(call my-dir)/../../../core/Android.mk \
-	$(call all-makefiles-under,$(call my-dir)/../../../core/externals) \
-	$(call my-dir)/../../../core/utils/Android.mk
+    $(all-subdir-makefiles) \
+    $(call my-dir)/../../../jni/Android.mk \
+    $(call my-dir)/../../../core/Android.mk \
+    $(call all-makefiles-under,$(call my-dir)/../../../core/externals) \
+    $(call my-dir)/../../../core/utils/Android.mk
 endif
 
 # Protect from previous builds
 ifneq ($(TARGET_ARCH_ABI),armeabi-v7a)
-	OSMAND_BUILDING_NEON_LIBRARY := false
+    OSMAND_BUILDING_NEON_LIBRARY := false
 endif
 
 # OSMAND_FORCE_NEON_SUPPORT is used to force only NEON support on ARMv7a
 ifdef OSMAND_FORCE_NEON_SUPPORT
-	ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-		OSMAND_BUILDING_NEON_LIBRARY := true
-	endif
+    ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+        OSMAND_BUILDING_NEON_LIBRARY := true
+    endif
 endif
 
 # By default, include makefiles only once
@@ -27,12 +27,12 @@ include $(OSMAND_MAKEFILES)
 # If we're not asked not to support NEON and not asked to support only NEON ARMv7a, then
 # make additional build
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-	ifndef OSMAND_SKIP_NEON_SUPPORT
-		ifndef OSMAND_FORCE_NEON_SUPPORT
+    ifndef OSMAND_SKIP_NEON_SUPPORT
+        ifndef OSMAND_FORCE_NEON_SUPPORT
 
-			OSMAND_BUILDING_NEON_LIBRARY := true
-			include $(OSMAND_MAKEFILES)
+            OSMAND_BUILDING_NEON_LIBRARY := true
+            include $(OSMAND_MAKEFILES)
 
-		endif
-	endif
+        endif
+    endif
 endif
