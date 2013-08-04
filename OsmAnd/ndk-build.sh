09b5c39946ca34f765b0df9f628a98d1d64d7274
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-08-04 11:07:52 +0300

    Control number of CPUs used for building

--- OsmAnd/ndk-build.sh
+++ OsmAnd/ndk-build.sh
@@ -16,8 +16,19 @@ if [ -z "$OSMAND_ARCHITECTURES_SET" ]; then
 	export OSMAND_ARCHITECTURES_SET
 fi
 
+if [[ "$(uname -a)" =~ Linux ]]; then
+	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
+		OSMAND_BUILD_CPU_CORES_NUM=`nproc`
+	fi
+fi
+if [[ "$(uname -a)" =~ Darwin ]]; then
+	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
+		OSMAND_BUILD_CPU_CORES_NUM=`sysctl hw.ncpu | awk '{print $2}'`
+	fi
+fi
+
 OSMAND_ANDROID_EXTERNAL_DEPENDENCIES=(expat freetype gdal giflib glm glsl-optimizer harfbuzz jpeg libpng protobuf qtbase-android skia)
 "$SCRIPT_LOC/../../core/externals/configure.sh" ${OSMAND_ANDROID_EXTERNAL_DEPENDENCIES[*]}
 "$SCRIPT_LOC/../../core/externals/build.sh" ${OSMAND_ANDROID_EXTERNAL_DEPENDENCIES[*]}
 "$SCRIPT_LOC/../../jni/generate.sh"
-(cd "$SCRIPT_LOC" && "$ANDROID_NDK/ndk-build" -j`nproc`)
+(cd "$SCRIPT_LOC" && "$ANDROID_NDK/ndk-build" -j$OSMAND_BUILD_CPU_CORES_NUM)
