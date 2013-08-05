4447b06c034ac8dbb59f10f4fced43d014db8398
Victor Shcherb <victor.shcherb@gmail.com>
2013-08-06 00:26:33 +0200

    Add osrm provider

--- OsmAnd/src/net/osmand/plus/OsmandSettings.java
+++ OsmAnd/src/net/osmand/plus/OsmandSettings.java
@@ -662,7 +662,7 @@ public class OsmandSettings {
 	// this value string is synchronized with settings_pref.xml preference name
 	public final OsmandPreference<RouteService> ROUTER_SERVICE = 
 		new EnumIntPreference<RouteService>("router_service", RouteService.OSMAND, 
-				new RouteService[] {RouteService.OSMAND, RouteService.YOURS, RouteService.ORS}).makeProfile();
+				RouteService.values()).makeProfile();
 	
 	public final CommonPreference<Boolean> SNAP_TO_ROAD = new BooleanPreference("snap_to_road", false).makeProfile().cache();
 	{
