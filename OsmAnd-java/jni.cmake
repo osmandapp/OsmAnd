c90f560a7bc13667300ad8bc5a47432c35ebcaf1
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-08-08 11:57:47 +0300

    fix

--- OsmAnd-java/jni.cmake
+++ OsmAnd-java/jni.cmake
@@ -1,3 +1,3 @@
 # OsmAnd JNI
-add_subdirectory("${OSMAND_ROOT}/android/OsmAnd-java" "jni")
+add_subdirectory("${OSMAND_ROOT}/android/OsmAnd-java" "android/OsmAnd-java")
 add_dependencies(OsmAndJNI OsmAndCore)
