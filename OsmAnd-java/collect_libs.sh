acba5fd4e79fc0e5b8162d3083e8703d1491a97f
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-08-08 07:20:57 +0300

    Update collect_libs.sh

--- OsmAnd-java/collect_libs.sh
+++ OsmAnd-java/collect_libs.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 SCRIPT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_LOC="$SCRIPT_LOC/../.."
+
+if [[ "$(uname -a)" =~ Linux ]]; then
+	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
+		OSMAND_BUILD_CPU_CORES_NUM=`nproc`
+	fi
+fi
+if [[ "$(uname -a)" =~ Darwin ]]; then
+	if [[ -z "$OSMAND_BUILD_CPU_CORES_NUM" ]]; then
+		OSMAND_BUILD_CPU_CORES_NUM=`sysctl hw.ncpu | awk '{print $2}'`
+	fi
+fi
+
 function copyLibs {
 	if [ -d "$ROOT_LOC/binaries/$1/$2" ]; then 
 		echo "Copy binaries $1 $2";
 		cp "$ROOT_LOC"/binaries/$1/$2/libOsmAndJNI.$4 bin/OsmAndJNI-$1-$3.lib
 		cp "$ROOT_LOC"/binaries/$1/$2/libOsmAndCoreUtils.$4 bin/OsmAndCoreUtils-$1-$3.lib
 		cp "$ROOT_LOC"/binaries/$1/$2/libOsmAndCore.$4 bin/OsmAndCore-$1-$3.lib
-		cp "$ROOT_LOC"/core/externals/qtbase-desktop/upstream.patched.$1.$2/lib/libQt5Core.$4.5.0.2 bin/Qt5Core-$1-$3.lib
 	fi
 }
 
 function compile {
-	"$ROOT_LOC/core/externals/configure.sh"
-	"$ROOT_LOC/core/externals/qtbase-desktop/build.sh"
-	"$ROOT_LOC/tools/map-viewer/externals/freeglut/configure.sh"
+	OSMAND_ANDROID_EXTERNAL_DEPENDENCIES=(expat freetype gdal giflib glm glsl-optimizer harfbuzz jpeg libpng protobuf qtbase-desktop skia)
+	"$ROOT_LOC/core/externals/configure.sh" ${OSMAND_ANDROID_EXTERNAL_DEPENDENCIES[*]}
+	"$ROOT_LOC/core/externals/build.sh" ${OSMAND_ANDROID_EXTERNAL_DEPENDENCIES[*]}
 	if [ ! -d "$ROOT_LOC/amd64-linux-gcc-release.makefile" ]; then 
 		"$ROOT_LOC/build/amd64-linux-gcc.sh" release
 	fi
-	(cd "$ROOT_LOC/baked/amd64-linux-gcc-release.makefile" && make -j`nproc` OsmAndJNI)
+	(cd "$ROOT_LOC/baked/amd64-linux-gcc-release.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndJNI)
 	if [ ! -d "$ROOT_LOC/baked/i686-linux-gcc-release.makefile" ]; then 
 		"$ROOT_LOC/build/i686-linux-gcc.sh" release
 	fi
-	(cd "$ROOT_LOC/baked/i686-linux-gcc-release.makefile" && make -j`nproc` OsmAndJNI)
+	(cd "$ROOT_LOC/baked/i686-linux-gcc-release.makefile" && make -j$OSMAND_BUILD_CPU_CORES_NUM OsmAndJNI)
 }
 
 compile
