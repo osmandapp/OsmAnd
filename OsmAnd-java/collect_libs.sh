7f69ea1c003767bce874a2d65aebdb05f3a1efb3
Alexey Pelykh <alexey.pelykh@gmail.com>
2013-08-08 07:22:52 +0300

    Update collect_libs.sh

--- OsmAnd-java/collect_libs.sh
+++ OsmAnd-java/collect_libs.sh
@@ -17,8 +17,6 @@ function copyLibs {
 	if [ -d "$ROOT_LOC/binaries/$1/$2" ]; then 
 		echo "Copy binaries $1 $2";
 		cp "$ROOT_LOC"/binaries/$1/$2/libOsmAndJNI.$4 bin/OsmAndJNI-$1-$3.lib
-		cp "$ROOT_LOC"/binaries/$1/$2/libOsmAndCoreUtils.$4 bin/OsmAndCoreUtils-$1-$3.lib
-		cp "$ROOT_LOC"/binaries/$1/$2/libOsmAndCore.$4 bin/OsmAndCore-$1-$3.lib
 	fi
 }
 
