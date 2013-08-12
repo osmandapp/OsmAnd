90e16706695208d46cc0a9607205896828de63da
sonora <hm.gglmail@gmail.com>
2013-08-12 09:19:14 +0200

    some tips streamlining, intial commit

--- OsmAnd/src/net/osmand/plus/activities/TipsAndTricksActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/TipsAndTricksActivity.java
@@ -17,21 +17,19 @@ import android.widget.TextView;
 public class TipsAndTricksActivity {
 	private int[][] tipNamesAndDescriptions = new int[][] {
 			{R.string.tip_recent_changes,R.string.tip_recent_changes_1_5_t},
-			{R.string.tip_rotation_switching,R.string.tip_rotation_switching_t_v2},
 			{R.string.tip_update_index,R.string.tip_update_index_t},
-			{R.string.tip_day_night_mode,R.string.tip_day_night_mode_t_v2},
-			{R.string.tip_location_sharing,R.string.tip_location_sharing_t},
-			{R.string.tip_favorites,R.string.tip_favorites_t},
-			{R.string.tip_osm_improve,R.string.tip_osm_improve_t},
-			{R.string.tip_map_context_menu,R.string.tip_map_context_menu_t},
-			{R.string.tip_search,R.string.tip_search_t},
-			{R.string.tip_select_destination_point,R.string.tip_select_destination_point_t},
 			{R.string.tip_navigation,R.string.tip_navigation_t},
 			{R.string.tip_app_mode,R.string.tip_app_mode_t_v2},
+			{R.string.tip_search,R.string.tip_search_t},
+			{R.string.tip_favorites,R.string.tip_favorites_t},
+			{R.string.tip_map_context_menu,R.string.tip_map_context_menu_t},
+			{R.string.tip_rotation_switching,R.string.tip_rotation_switching_t_v2},
+			{R.string.tip_day_night_mode,R.string.tip_day_night_mode_t_v2},
 			{R.string.tip_altitude_offset,R.string.tip_altitude_offset_t},
-			{R.string.tip_recent_changes,R.string.tip_recent_changes_1_1_0_t},
-			
+			{R.string.tip_location_sharing,R.string.tip_location_sharing_t},
+			{R.string.tip_osm_improve,R.string.tip_osm_improve_t},
 			{R.string.tip_map_switch,R.string.tip_map_switch_t_v2},
+			{R.string.tip_recent_changes,R.string.tip_recent_changes_1_1_0_t},
 			};
 
 	private final Context ctx;
