2272251e483086d6f3e37efa62e37904e27e1a47
sonora <hm.gglmail@gmail.com>
2013-08-28 11:13:22 +0200

    correct syntax

--- OsmAnd/src/net/osmand/plus/routing/VoiceRouter.java
+++ OsmAnd/src/net/osmand/plus/routing/VoiceRouter.java
@@ -438,7 +438,7 @@ public class VoiceRouter {
 		if(pn != null) {
 			pn = pn.replace('-', ' ');
 			pn = pn.replace(':', ' ');
-			if (AbstractPrologCommandPlayer.getLanguage != "de"){
+			if (!AbstractPrologCommandPlayer.getLanguage().equals("de")){
 				pn = pn.replace("\u00df", "ss"); // helps non-German tts voices to pronounce German Strasse (=street)
 			}
 		}
