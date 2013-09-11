c8db27b24897f258611ddf98a167ca9e687a450b
vshcherb <victor.shcherb@gmail.com>
2013-09-11 16:14:42 +0200

    Update Application.mk
    Test compilation

--- OsmAnd/jni/Application.mk
+++ OsmAnd/jni/Application.mk
@@ -1,11 +1,12 @@
 APP_STL := gnustl_shared
 APP_CPPFLAGS := -std=c++11 -fexceptions -frtti
 
-ifeq ($(wildcard $(ANDROID_NDK)/toolchains/*-4.7),)
-    NDK_TOOLCHAIN_VERSION := 4.8
-else
-    NDK_TOOLCHAIN_VERSION := 4.7
-endif
+# ifeq ($(wildcard $(ANDROID_NDK)/toolchains/*-4.7),)
+    # NDK_TOOLCHAIN_VERSION := 4.8
+# else
+    # NDK_TOOLCHAIN_VERSION := 4.7
+# endif
+NDK_TOOLCHAIN_VERSION := clang
 
 APP_ABI :=
 ifneq ($(filter x86,$(OSMAND_ARCHITECTURES_SET)),)
