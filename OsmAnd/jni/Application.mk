16180bb2068e0c1215c962e9d997f35e43796339
vshcherb <victor.shcherb@gmail.com>
2013-09-11 17:03:57 +0200

    Update Application.mk

--- OsmAnd/jni/Application.mk
+++ OsmAnd/jni/Application.mk
@@ -1,12 +1,12 @@
 APP_STL := gnustl_shared
 APP_CPPFLAGS := -std=c++11 -fexceptions -frtti
 
-# ifeq ($(wildcard $(ANDROID_NDK)/toolchains/*-4.7),)
-    # NDK_TOOLCHAIN_VERSION := 4.8
-# else
-    # NDK_TOOLCHAIN_VERSION := 4.7
-# endif
-NDK_TOOLCHAIN_VERSION := clang3.3
+ifeq ($(wildcard $(ANDROID_NDK)/toolchains/*-4.7),)
+	NDK_TOOLCHAIN_VERSION := 4.8
+else
+	NDK_TOOLCHAIN_VERSION := 4.7
+endif
+
 
 
 APP_ABI :=
