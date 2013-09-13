b9d8edef8fcf6755e73b2a5ee6c3a60ce40f2787
sonora <hm.gglmail@gmail.com>
2013-09-14 00:37:19 +0200

    fix renderingRuleProperty

--- OsmAnd-java/src/net/osmand/render/RenderingRuleProperty.java
+++ OsmAnd-java/src/net/osmand/render/RenderingRuleProperty.java
@@ -137,7 +137,7 @@ public class RenderingRuleProperty {
 				int colon = value.indexOf(':');
 				if(colon != -1) {
 					int c  = 0;
-					if(c > 0) {
+					if(colon > 0) {
 						c += Integer.parseInt(value.substring(0, colon));
 					}
 					c += Integer.parseInt(value.substring(colon + 1));
@@ -171,7 +171,7 @@ public class RenderingRuleProperty {
 				int colon = value.indexOf(':');
 				if(colon != -1) {
 					float c = 0;
-					if(c > 0) {
+					if(colon > 0) {
 						c += Float.parseFloat(value.substring(0, colon));
 					}
 					c += Float.parseFloat(value.substring(colon + 1));
