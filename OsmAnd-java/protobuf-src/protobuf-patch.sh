77b8c8cb6629c5295f81b2dfb79ce4633d523de2
vshcherb <victor.shcherb@gmail.com>
2013-11-11 01:36:53 +0100

    Update protobuf

--- OsmAnd-java/protobuf-src/protobuf-patch.sh
+++ OsmAnd-java/protobuf-src/protobuf-patch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p ../src/com
-cp com/google/protobuf/CodedInputStream.java com/google/protobuf/CodedInputStreamRAF.java
 cp -Rf com/* ../src/com/
 (cd ../ && git apply -v protobuf-src/protobuf-2.3.patch)
