4c8b83f139b8fcdfdcaea6562a80bf828507410b
vshcherb <victor.shcherb@gmail.com>
2014-02-28 11:37:22 +0200

    Fix fast route mode (latest bug)

--- OsmAnd/src/net/osmand/plus/routing/RouteProvider.java
+++ OsmAnd/src/net/osmand/plus/routing/RouteProvider.java
@@ -405,7 +405,7 @@ public class RouteProvider {
 			RoutingParameter pr = e.getValue();
 			String vl;
 			if(key.equals(GeneralRouter.USE_SHORTEST_WAY)) {
-				Boolean bool = settings.FAST_ROUTE_MODE.getModeValue(params.mode);
+				Boolean bool = !settings.FAST_ROUTE_MODE.getModeValue(params.mode);
 				vl = bool ? "true" : null;
 			} else if(pr.getType() == RoutingParameterType.BOOLEAN) {
 				CommonPreference<Boolean> pref = settings.getCustomRoutingBooleanProperty(key);
