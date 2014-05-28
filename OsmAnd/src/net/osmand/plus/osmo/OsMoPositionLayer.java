620b7ad92d9e99665b3547dd89c1ba2d6f37118b
vshcherb <victor.shcherb@gmail.com>
2014-05-28 02:11:47 +0200

    Fix npe

--- OsmAnd/src/net/osmand/plus/osmo/OsMoPositionLayer.java
+++ OsmAnd/src/net/osmand/plus/osmo/OsMoPositionLayer.java
@@ -86,7 +86,7 @@ public class OsMoPositionLayer extends OsmandMapLayer implements ContextMenuLaye
 		final int r = getRadiusPoi(tb);
 		for (OsMoDevice t : getTrackingDevices()) {
 			Location l = t.getLastLocation();
-			if (t != null) {
+			if (l != null) {
 				int x = (int) tb.getPixXFromLatLon(l.getLatitude(), l.getLongitude());
 				int y = (int) tb.getPixYFromLatLon(l.getLatitude(), l.getLongitude());
 				pointAltUI.setColor(t.getColor());
