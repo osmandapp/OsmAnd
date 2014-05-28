d7187b8c4798e487debb0a768a6f0d03de7bb624
vshcherb <victor.shcherb@gmail.com>
2014-05-29 00:56:50 +0200

    Fix compilation issue

--- OsmAnd/src/net/osmand/plus/osmo/OsMoPlugin.java
+++ OsmAnd/src/net/osmand/plus/osmo/OsMoPlugin.java
@@ -49,7 +49,7 @@ public class OsMoPlugin extends OsmandPlugin implements MonitoringInfoControlSer
 		service = new OsMoService(app);
 		tracker = new OsMoTracker(service, app.getSettings().OSMO_SAVE_TRACK_INTERVAL,
 				app.getSettings().OSMO_AUTO_SEND_LOCATIONS);
-		new OsMoControlDevice(app, service);
+		new OsMoControlDevice(app, service, tracker);
 		groups = new OsMoGroups(service, tracker, app.getSettings());
 		this.app = app;
 		ApplicationMode.regWidget("osmo_control", (ApplicationMode[])null);
