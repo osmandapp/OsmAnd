94c927c26fac477a01a219a5a973356156638ef6
Victor Shcherb <victor.shcherb@gmail.com>
2014-06-27 02:41:50 +0200

    Fix issue

--- OsmAnd/src/net/osmand/plus/activities/AvailableGPXFragment.java
+++ OsmAnd/src/net/osmand/plus/activities/AvailableGPXFragment.java
@@ -396,7 +396,8 @@ public class AvailableGPXFragment extends OsmandExpandableListFragment {
 			public void onContextMenuClick(int resId, int pos, boolean isChecked, DialogInterface dialog) {
 				if (resId == R.string.local_index_mi_rename) {
 					renameFile(info);
-				} else if (resId == R.string.local_index_unselect_gpx_file) {
+				} else if (resId == R.string.local_index_unselect_gpx_file ||
+						resId == R.string.local_index_select_gpx_file) {
 					getMyApplication().getSelectedGpxHelper().selectGpxFile(info.gpx, resId == R.string.local_index_select_gpx_file, true);
 					listAdapter.notifyDataSetChanged();
 					Runnable r = selectedGpxHelper.getUiListener();
