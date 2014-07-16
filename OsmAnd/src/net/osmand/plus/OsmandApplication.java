fc33a71cabb7f51c1118263e83573711f2946d02
Victor Shcherb <victor.shcherb@gmail.com>
2014-07-16 23:04:28 +0200

    Fix BB release

--- OsmAnd/src/net/osmand/plus/OsmandApplication.java
+++ OsmAnd/src/net/osmand/plus/OsmandApplication.java
@@ -511,7 +511,8 @@ public class OsmandApplication extends Application {
 				LOG.error("I/O exception", e);
 				warnings.add("Error while reading the special phrases. Restart OsmAnd if possible");
 			}
-			if (!Version.isBlackberry(this)) {
+			
+			if (!Version.isBlackberry(this) || !"qnx".equals(System.getProperty("os.name"))) {
 				if (osmandSettings.NATIVE_RENDERING_FAILED.get()) {
 					osmandSettings.SAFE_MODE.set(true);
 					osmandSettings.NATIVE_RENDERING_FAILED.set(false);
