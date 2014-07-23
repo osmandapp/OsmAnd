696b833bc765cb7c0034a01c8cd0ce6cbc7f49b3
Denis <barsik107@gmail.com>
2014-07-23 18:38:32 +0400

    Fixed bug of usage String.isEmpty method which is prohibited for old android devices

--- OsmAnd/src/net/osmand/plus/rastermaps/OsmandRasterMapsPlugin.java
+++ OsmAnd/src/net/osmand/plus/rastermaps/OsmandRasterMapsPlugin.java
@@ -390,7 +390,7 @@ public class OsmandRasterMapsPlugin extends OsmandPlugin {
 					r.setMinZoom(Integer.parseInt(minZoom.getText().toString()));
 					r.setMaxZoom(Integer.parseInt(maxZoom.getText().toString()));
 					r.setEllipticYTile(elliptic.isChecked());
-					r.setUrlToLoad(urlToLoad.getText().toString().isEmpty() ? null : urlToLoad.getText().toString().replace("{$x}", "{1}")
+					r.setUrlToLoad(urlToLoad.getText().toString().equals("") ? null : urlToLoad.getText().toString().replace("{$x}", "{1}")
 							.replace("{$y}", "{2}").replace("{$z}", "{0}"));
 					if (r != null && r.getName().length() > 0) {
 						if (settings.installTileSource(r)) {
