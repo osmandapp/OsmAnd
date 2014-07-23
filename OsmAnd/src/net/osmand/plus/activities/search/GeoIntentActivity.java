043de3091784fa9cdb73e9dcb90c20f3baef7c10
Zahnstocher <openstreetmap.org@nurfuerspam.de>
2014-07-23 09:49:52 +0200

    format

--- OsmAnd/src/net/osmand/plus/activities/search/GeoIntentActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/search/GeoIntentActivity.java
@@ -336,13 +336,13 @@ public class GeoIntentActivity extends OsmandListActivity {
                     {
                         return new GeoPointSearch(lat, lon, Integer.valueOf(matcher.group(4)));
                     }
-				} else if (matcher2.matches()) {
-					final double lat = Double.valueOf(matcher2.group(1));
-					final double lon = Double.valueOf(matcher2.group(2));
-					return new GeoPointSearch(lat, lon);
-				} else {
-					return null;
-				}
+                } else if (matcher2.matches()) {
+                    final double lat = Double.valueOf(matcher2.group(1));
+                    final double lon = Double.valueOf(matcher2.group(2));
+                    return new GeoPointSearch(lat, lon);
+                } else {
+                    return null;
+                }
             }
         }
         return null;
