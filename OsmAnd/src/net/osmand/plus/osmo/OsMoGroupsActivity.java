a70ea6c83cbd214cc4f9e50d6cb7bcf9e3b9aec7
Victor Shcherb <victor.shcherb@gmail.com>
2014-07-31 00:17:38 +0200

    Fix OsMo

--- OsmAnd/src/net/osmand/plus/osmo/OsMoGroupsActivity.java
+++ OsmAnd/src/net/osmand/plus/osmo/OsMoGroupsActivity.java
@@ -431,7 +431,9 @@ public class OsMoGroupsActivity extends OsmandExpandableListActivity implements 
 					Builder bld = new AlertDialog.Builder(OsMoGroupsActivity.this);
 					String name = (selectedObject instanceof OsMoDevice)? ((OsMoDevice) selectedObject).getVisibleName() :
 						((OsMoGroup) selectedObject).getVisibleName(OsMoGroupsActivity.this);
-					bld.setTitle(getString(R.string.delete_confirmation_msg, name));
+					bld.setTitle(getString(
+							selectedObject instanceof OsMoDevice? R.string.delete_confirmation_msg :
+								R.string.osmo_leave_confirmation_msg, name));
 					bld.setPositiveButton(R.string .default_buttons_yes, new DialogInterface.OnClickListener() {
 						
 						@Override
@@ -814,11 +816,15 @@ public class OsMoGroupsActivity extends OsmandExpandableListActivity implements 
 				if(isChecked) {
 					labelTracker.setText(R.string.osmo_connect_to_device_tracker_id);
 					labelName.setText(R.string.osmo_connect_to_device_name);
+					name.setVisibility(View.VISIBLE);
+					labelName.setVisibility(View.VISIBLE);
 					mgv.setVisibility(View.GONE);
 				} else {
 					labelTracker.setText(R.string.osmo_connect_to_group_id);
 					labelName.setText(R.string.osmo_group_name);
 					name.setHint(R.string.osmo_use_server_name);
+					name.setVisibility(View.GONE);
+					labelName.setVisibility(View.GONE);
 					mgv.setVisibility(View.VISIBLE);
 				}
 			}
@@ -833,6 +839,11 @@ public class OsMoGroupsActivity extends OsmandExpandableListActivity implements 
 				final String nameUser = name.getText().toString();
 				final String id = tracker.getText().toString();
 				final String nick = nickname.getText().toString();
+				if(id.length() == 0) {
+					app.showToastMessage(R.string.osmo_specify_tracker_id);
+					connectToDevice();
+					return;
+				}
 				if(device.isChecked()) {
 					OsMoDevice dev = osMoPlugin.getGroups().addConnectedDevice(id, nameUser, 
 							ColorDialogs.getRandomColor());
