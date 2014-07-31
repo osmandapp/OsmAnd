aebd125b9a200fb96f98bbe29328ae24b1ea160a
Denis <barsik107@gmail.com>
2014-07-31 17:55:23 +0300

    Fixed some expetions for android 2.3.3

--- OsmAnd/src/net/osmand/plus/monitoring/SettingsMonitoringActivity.java
+++ OsmAnd/src/net/osmand/plus/monitoring/SettingsMonitoringActivity.java
@@ -39,6 +39,7 @@ public class SettingsMonitoringActivity extends SettingsBaseActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		requestWindowFeature(Window.FEATURE_PROGRESS);
 		super.onCreate(savedInstanceState);
 		setSupportProgressBarIndeterminateVisibility(false);
 		getSupportActionBar().setTitle(R.string.monitoring_settings);
