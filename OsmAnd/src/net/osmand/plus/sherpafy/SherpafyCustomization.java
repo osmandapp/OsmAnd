a7cb1feeb3a8cb3e2e13edb8f21b288fa8d7b2df
Denis <barsik107@gmail.com>
2014-08-15 17:00:06 +0300

    Stage completed dialog

--- OsmAnd/src/net/osmand/plus/sherpafy/SherpafyCustomization.java
+++ OsmAnd/src/net/osmand/plus/sherpafy/SherpafyCustomization.java
@@ -25,6 +25,7 @@ import net.osmand.plus.Version;
 import net.osmand.plus.activities.DownloadIndexActivity;
 import net.osmand.plus.activities.MapActivity;
 import net.osmand.plus.activities.MapActivityLayers;
+import net.osmand.plus.activities.SelectedGPXFragment;
 import net.osmand.plus.api.FileSettingsAPIImpl;
 import net.osmand.plus.api.SettingsAPI;
 import net.osmand.plus.download.DownloadActivityType;
@@ -47,7 +48,7 @@ import android.widget.TextView;
 import com.actionbarsherlock.app.ActionBar;
 
 public class SherpafyCustomization extends OsmAndAppCustomization {
-	
+
 	private static final String SELECTED_TOUR = "selected_tour";
 	private static final String ACCESS_CODE = "access_code";
 	private static final String SELECTED_STAGE = "selected_stage_int";
@@ -65,7 +66,7 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 	private SettingsAPI originalApi;
 	private CommonPreference<String> saveGPXFolder;
 	public static final String TOUR_SERVER = "download.osmand.net";
-	private static final String SAVE_GPX_FOLDER = "save_gpx_folder";	
+	private static final String SAVE_GPX_FOLDER = "save_gpx_folder";
 
 	@Override
 	public void setup(OsmandApplication app) {
@@ -73,7 +74,7 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 		originalApi = osmandSettings.getSettingsAPI();
 		selectedTourPref = osmandSettings.registerStringPreference(SELECTED_TOUR, null).makeGlobal();
 		saveGPXFolder = osmandSettings.registerStringPreference(SAVE_GPX_FOLDER, null).makeGlobal();
-		if(osmandSettings.OSMAND_THEME.get() != OsmandSettings.OSMAND_LIGHT_THEME) {
+		if (osmandSettings.OSMAND_THEME.get() != OsmandSettings.OSMAND_LIGHT_THEME) {
 			osmandSettings.OSMAND_THEME.set(OsmandSettings.OSMAND_LIGHT_THEME);
 		}
 		accessCodePref = osmandSettings.registerStringPreference(ACCESS_CODE, "").makeGlobal();
@@ -82,10 +83,10 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 
 	public boolean setAccessCode(String acCode) {
 		acCode = acCode.toUpperCase();
-		if(DownloadIndexActivity.downloadListIndexThread != null) {
+		if (DownloadIndexActivity.downloadListIndexThread != null) {
 			DownloadIndexActivity.downloadListIndexThread.clear();
 		}
-		if(validate(acCode) || Algorithms.isEmpty(acCode)) {
+		if (validate(acCode) || Algorithms.isEmpty(acCode)) {
 			accessCodePref.set(acCode);
 			return true;
 		}
@@ -103,10 +104,10 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 		return (k % 10) == (acCode.charAt(acCode.length() - 1) - '0');
 	}
 
-	public boolean isSettingsAvailable(){
+	public boolean isSettingsAvailable() {
 		return accessCodePref.get().startsWith("BAB2");
 	}
-	
+
 	public String getAccessCode() {
 		return accessCodePref.get();
 	}
@@ -114,7 +115,7 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 	public boolean isTourSelected() {
 		return selectedTourPref.get() != null;
 	}
-	
+
 	public boolean checkExceptionsOnStart() {
 		return false;
 	}
@@ -126,13 +127,13 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 	public boolean checkBasemapDownloadedOnStart() {
 		return false;
 	}
-	
+
 	@Override
 	public void customizeMainMenu(Window window, final Activity activity) {
 		// Update app name
-		TextView v =  (TextView) window.findViewById(R.id.AppName);
+		TextView v = (TextView) window.findViewById(R.id.AppName);
 		v.setText("Sherpafy");
-		
+
 //		TextView toursButtonText = (TextView) window.findViewById(R.id.SettingsButtonText);
 //		toursButtonText.setText(R.string.tour);
 //		View toursButton = window.findViewById(R.id.SettingsButton);
@@ -148,29 +149,29 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 
 		// the image could be also updated
 	}
-	
-	
+
+
 	@Override
 	public void getDownloadTypes(List<DownloadActivityType> items) {
 		super.getDownloadTypes(items);
 		items.add(0, TourDownloadType.TOUR);
 	}
-	
-	public void updatedLoadedFiles(java.util.Map<String,String> indexFileNames, java.util.Map<String,String> indexActivatedFileNames) {
+
+	public void updatedLoadedFiles(java.util.Map<String, String> indexFileNames, java.util.Map<String, String> indexActivatedFileNames) {
 		DownloadIndexActivity.listWithAlternatives(app.getResourceManager().getDateFormat(),
 				toursFolder, "", indexFileNames);
 	}
-	
+
 	public List<String> onIndexingFiles(IProgress progress, Map<String, String> indexFileNames) {
 		ArrayList<TourInformation> tourPresent = new ArrayList<TourInformation>();
 		List<String> warns = new ArrayList<String>();
 		selectedTour = null;
 		final HashSet<String> suggestToDownloadMap = new HashSet<String>();
-		if(toursFolder.exists()) {
+		if (toursFolder.exists()) {
 			File[] availableTours = toursFolder.listFiles();
-			if(availableTours != null) {
+			if (availableTours != null) {
 				String selectedName = selectedTourPref.get();
-				for(File tr : availableTours) {
+				for (File tr : availableTours) {
 					if (tr.isDirectory()) {
 						String date = app.getResourceManager().getDateFormat()
 								.format(new Date(DownloadIndexActivity.findFileInDir(tr).lastModified()));
@@ -183,7 +184,7 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 							e.printStackTrace();
 						}
 						// check that tour was downloaded
-						if(toursIndexed) {
+						if (toursIndexed) {
 							for (String map : tourInformation.getMaps()) {
 								if (!new File(toursFolder.getParentFile(), map + ".obf").exists()) {
 									suggestToDownloadMap.add(map);
@@ -203,7 +204,7 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 			toursIndexed = true;
 		}
 		this.tourPresent = tourPresent;
-		if(!suggestToDownloadMap.isEmpty()) {
+		if (!suggestToDownloadMap.isEmpty()) {
 			final DownloadIndexActivity da = app.getDownloadActivity();
 			if (da != null) {
 				app.runInUIThread(new Runnable() {
@@ -222,13 +223,13 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 	public List<TourInformation> getTourInformations() {
 		return tourPresent;
 	}
-	
+
 	public TourInformation getSelectedTour() {
 		return selectedTour;
 	}
 
 	private void reloadSelectedTour(IProgress progress, final TourInformation tourInformation) {
-		if(progress != null) {
+		if (progress != null) {
 			progress.startTask(app.getString(R.string.indexing_tour, tourInformation.getName()), -1);
 		}
 		File settingsFile = new File(tourInformation.getFolder(), "settings.props");
@@ -251,19 +252,19 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 
 	private void selectNextAvailableStage(final TourInformation tourInformation) {
 		Integer it = selectedStagePref.get();
-		while(it >= 0 && isStageVisited(it) ){
+		while (it >= 0 && isStageVisited(it)) {
 			it++;
 		}
-		if(it >= 0 && it < tourInformation.getStageInformation().size()) {
+		if (it >= 0 && it < tourInformation.getStageInformation().size()) {
 			selectedStage = tourInformation.getStageInformation().get(it);
 		}
 	}
-	
+
 	public boolean isStageVisited(int stageOrder) {
 		Integer gi = visitedStagesPref.get();
 		return (gi & (1 << stageOrder)) > 0;
 	}
-	
+
 	public void markStageAsCompleted(StageInformation si) {
 		Integer gi = visitedStagesPref.get();
 		gi |= (1 << si.getOrder());
@@ -273,52 +274,67 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 	}
 
 	protected void saveCurrentGPXTrack() {
-		if(!Algorithms.isEmpty(saveGPXFolder.get())) {
+		if (!Algorithms.isEmpty(saveGPXFolder.get())) {
 			app.getSavingTrackHelper().saveDataToGpx(new File(saveGPXFolder.get()));
 		}
 	}
-	
+
 	public File getTracksDir() {
-		if(!Algorithms.isEmpty(saveGPXFolder.get())) {
+		if (!Algorithms.isEmpty(saveGPXFolder.get())) {
 			return new File(saveGPXFolder.get());
 		}
 		return app.getAppPath(IndexConstants.GPX_RECORDED_INDEX_DIR);
 	}
-	
+
+	@Override
+	public boolean onDestinationReached(MapActivity mapActivity) {
+		showCompleteStageFragment(mapActivity, selectedStage);
+		selectNextAvailableStage(selectedTour);
+		return false;
+	}
+
 	public static class CompleteStageFragment extends DialogFragment {
 
 		@Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-//			Bundle args = getArguments();
-//			SherpafyFavoriteFragment ssf = new SherpafyFavoriteFragment();
-//			ssf.setArguments(args);
-//			ssf.onAttach(getActivity());
-            AlertDialog dlg = new AlertDialog.Builder(getActivity())
-//            		.setView(ssf.onCreateView(getActivity().getLayoutInflater(), null, savedInstanceState))
-            		.setMessage("Stage is completed TODO")
-                    .setPositiveButton(R.string.default_buttons_ok,
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                            }
-                        }
-                    )
-                    .create();
-            return dlg;
-        }
-    }
-	
-	protected void showCompleteStageFragment(FragmentActivity activity, StageInformation stage) {
-		// TODO Auto-generated method stub
-		
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			Bundle args = getArguments();
+			SelectedGPXFragment sgf = new SelectedGPXFragment();
+			sgf.setArguments(args);
+			sgf.onAttach(getActivity());
+			AlertDialog dlg = new AlertDialog.Builder(getActivity())
+            		.setView(sgf.onCreateView(getActivity().getLayoutInflater(), null, savedInstanceState))
+					.setMessage("Stage is completed TODO")
+					.setPositiveButton(R.string.default_buttons_ok,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int whichButton) {
+								}
+							}
+					)
+					.create();
+			return dlg;
+		}
 	}
-	
+
+	protected void showCompleteStageFragment(MapActivity mapActivity, StageInformation stage) {
+		File file = getStageGpxRec(stage);
+		GPXUtilities.GPXFile gpx = GPXUtilities.loadGPXFile(app, file);
+		if (gpx == null) {
+			return;
+		}
+		CompleteStageFragment csf = new CompleteStageFragment();
+		app.getSelectedGpxHelper().clearAllGpxFileToShow();
+		app.getSelectedGpxHelper().selectGpxFile(gpx, true, false);
+		csf.show(mapActivity.getSupportFragmentManager().beginTransaction(), "DialogFragment");
+
+	}
+
 	public StageInformation getSelectedStage() {
 		return selectedStage;
 	}
 
 	public void selectStage(StageInformation stage, IProgress progress) {
 		saveCurrentGPXTrack();
-		if(stage == null) {
+		if (stage == null) {
 			selectedStagePref.set(-1);
 			saveGPXFolder.set(null);
 			selectedStage = null;
@@ -339,14 +355,14 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 	private void loadSelectedStage() {
 		final StageInformation st = selectedStage;
 		cachedFavorites = new ArrayList<StageFavorite>();
-		for(Object o : st.favorites ) {
-			if(o instanceof StageFavorite) {
+		for (Object o : st.favorites) {
+			if (o instanceof StageFavorite) {
 				StageFavorite sf = (StageFavorite) o;
 				cachedFavorites.add(sf);
 			}
 		}
-		if(st != null && st.gpxFile != null) {
-			if(st.gpx == null) {
+		if (st != null && st.gpxFile != null) {
+			if (st.gpx == null) {
 				st.gpx = GPXUtilities.loadGPXFile(app, st.gpxFile);
 			}
 		}
@@ -367,29 +383,29 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 	public void prepareLayerContextMenu(MapActivity activity, ContextMenuAdapter adapter) {
 		filter(adapter, R.string.layer_poi, R.string.layer_amenity_label/*, R.string.layer_favorites*/);
 	}
-	
+
 	@Override
 	public void prepareLocationMenu(final MapActivity mapActivity, ContextMenuAdapter adapter) {
 		filter(adapter, R.string.context_menu_item_directions_to,
 				R.string.context_menu_item_destination_point, R.string.context_menu_item_search,
 				R.string.context_menu_item_share_location/*, R.string.context_menu_item_add_favorite*/);
 		MapActivityLayers layers = mapActivity.getMapLayers();
-		if(layers.getContextMenuLayer().getFirstSelectedObject() instanceof StageFavorite) {
-			final StageFavorite sf = ((StageFavorite)layers.getContextMenuLayer().getFirstSelectedObject());
-			if(selectedStage != null) {
-				adapter.item(R.string.show_waypoint_information).icons(R.drawable.ic_action_info_dark, R.drawable.ic_action_info_light ).position(0)
-				.listen(new OnContextMenuClick() {
-					@Override
-					public void onContextMenuClick(int itemId, int pos, boolean isChecked, DialogInterface dialog) {
-						showFavoriteDialog(mapActivity, selectedStage, sf);
-					}
-				}).reg();
-				
-				
+		if (layers.getContextMenuLayer().getFirstSelectedObject() instanceof StageFavorite) {
+			final StageFavorite sf = ((StageFavorite) layers.getContextMenuLayer().getFirstSelectedObject());
+			if (selectedStage != null) {
+				adapter.item(R.string.show_waypoint_information).icons(R.drawable.ic_action_info_dark, R.drawable.ic_action_info_light).position(0)
+						.listen(new OnContextMenuClick() {
+							@Override
+							public void onContextMenuClick(int itemId, int pos, boolean isChecked, DialogInterface dialog) {
+								showFavoriteDialog(mapActivity, selectedStage, sf);
+							}
+						}).reg();
+
+
 			}
 		}
 	}
-	
+
 	public void showFavoriteDialog(MapActivity mapActivity, StageInformation stage, StageFavorite sf) {
 		Bundle bl = new Bundle();
 		bl.putInt(SherpafyFavoriteFragment.STAGE_PARAM, stage.getOrder());
@@ -400,35 +416,35 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 		ffd.setArguments(bl);
 		ffd.show(fragmentManager.beginTransaction(), "DialogFragment");
 	}
-	
+
 	public static class FavoriteDialogFragment extends DialogFragment {
-		
+
 		@Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
 			Bundle args = getArguments();
 			SherpafyFavoriteFragment ssf = new SherpafyFavoriteFragment();
 			ssf.setArguments(args);
 			ssf.onAttach(getActivity());
-            AlertDialog dlg = new AlertDialog.Builder(getActivity())
-            		.setView(ssf.onCreateView(getActivity().getLayoutInflater(), null, savedInstanceState))
-                    .setPositiveButton(R.string.default_buttons_ok,
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                            }
-                        }
-                    )
-                    .create();
-            return dlg;
-        }
-    }
+			AlertDialog dlg = new AlertDialog.Builder(getActivity())
+					.setView(ssf.onCreateView(getActivity().getLayoutInflater(), null, savedInstanceState))
+					.setPositiveButton(R.string.default_buttons_ok,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int whichButton) {
+								}
+							}
+					)
+					.create();
+			return dlg;
+		}
+	}
 
 	@Override
 	public void prepareOptionsMenu(final MapActivity mapActivity, ContextMenuAdapter adapter) {
-		
+
 		filter(adapter, R.string.exit_Button, R.string.menu_layers,
-				R.string.pause_navigation, R.string.continue_navigation,  
+				R.string.pause_navigation, R.string.continue_navigation,
 				R.string.cancel_navigation, R.string.cancel_route, R.string.clear_destination,
-				R.string.get_directions, 
+				R.string.get_directions,
 				R.string.menu_mute_on, R.string.menu_mute_off,
 				R.string.where_am_i);
 		final StageInformation stage = getSelectedStage();
@@ -436,14 +452,14 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 			adapter.item(R.string.complete_stage)
 					.icons(R.drawable.ic_action_flage_dark, R.drawable.ic_action_flage_light)
 					.position(adapter.length() - 1).listen(new OnContextMenuClick() {
-						@Override
-						public void onContextMenuClick(int itemId, int pos, boolean isChecked, DialogInterface dialog) {
-							markStageAsCompleted(stage);
-							showCompleteStageFragment(mapActivity, stage);
-						}
-					}).reg();
+				@Override
+				public void onContextMenuClick(int itemId, int pos, boolean isChecked, DialogInterface dialog) {
+					markStageAsCompleted(stage);
+					showCompleteStageFragment(mapActivity, stage);
+				}
+			}).reg();
 		}
-		adapter.item(R.string.sherpafy_tour_info_txt).icons(R.drawable.ic_action_info_dark, R.drawable.ic_action_info_light ).position(adapter.length() - 1)
+		adapter.item(R.string.sherpafy_tour_info_txt).icons(R.drawable.ic_action_info_dark, R.drawable.ic_action_info_light).position(adapter.length() - 1)
 				.listen(new OnContextMenuClick() {
 					@Override
 					public void onContextMenuClick(int itemId, int pos, boolean isChecked, DialogInterface dialog) {
@@ -451,53 +467,52 @@ public class SherpafyCustomization extends OsmAndAppCustomization {
 						// newIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 						mapActivity.startActivity(newIntent);
 					}
-			}).reg();
+				}).reg();
 	}
-	
-	
+
 
 	public void filter(ContextMenuAdapter a, Integer... ids) {
-		if(isSettingsAvailable()) {
+		if (isSettingsAvailable()) {
 			return;
 		}
 		TreeSet<Integer> set = new TreeSet<Integer>(Arrays.asList(ids));
-		for(int i =0; i < a.length();) {
+		for (int i = 0; i < a.length(); ) {
 			int itemId = a.getItemId(i);
-			if(set.contains(itemId)) {
+			if (set.contains(itemId)) {
 				i++;
 			} else {
 				a.removeItem(i);
 			}
 		}
 	}
-	
+
 	@Override
 	public List<StageFavorite> getWaypoints() {
 		return cachedFavorites;
 	}
-	
+
 	@Override
 	public String getIndexesUrl() {
-		String s = "http://"+TOUR_SERVER+"/tours.php?gzip&" + Version.getVersionAsURLParam(app);
-		if(!Algorithms.isEmpty(accessCodePref.get())) {
-			s += "&code="+accessCodePref.get();
+		String s = "http://" + TOUR_SERVER + "/tours.php?gzip&" + Version.getVersionAsURLParam(app);
+		if (!Algorithms.isEmpty(accessCodePref.get())) {
+			s += "&code=" + accessCodePref.get();
 		}
 		return s;
 	}
-	
+
 	public void preDownloadActivity(final DownloadIndexActivity da, final List<DownloadActivityType> downloadTypes, ActionBar actionBar) {
 		actionBar.setTitle(TourDownloadType.TOUR.getString(da));
 	}
-	
+
 	@Override
 	public boolean showDownloadExtraActions() {
 		return false;
 	}
-	
+
 	public boolean saveGPXPoint(Location location) {
 		return app.getRoutingHelper().isFollowingMode() && !Algorithms.isEmpty(saveGPXFolder.get());
 	}
-	
+
 	@Override
 	public void createLayers(OsmandMapTileView mapView, MapActivity activity) {
 		mapView.addLayer(new StageFavoritesLayer(this), 4.1f);
