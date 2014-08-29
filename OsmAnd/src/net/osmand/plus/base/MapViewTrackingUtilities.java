800361ab3a34e3b84e0b7d96d915425d8331e933
sonora <hm.gglmail@gmail.com>
2014-08-29 17:41:06 +0200

    better status display for map orientation

--- OsmAnd/src/net/osmand/plus/base/MapViewTrackingUtilities.java
+++ OsmAnd/src/net/osmand/plus/base/MapViewTrackingUtilities.java
@@ -265,7 +265,7 @@ public class MapViewTrackingUtilities implements OsmAndLocationListener, IMapLoc
 	}
 	
 	public void switchRotateMapMode(){
-		int resId = R.string.rotate_map_none_opt;
+		int resId = R.string.rotate_map_to_bearing + ":\n" + R.string.rotate_map_none_opt;
 		if (settings.ROTATE_MAP.get() == OsmandSettings.ROTATE_MAP_NONE && mapView.getRotate() != 0) {
 			// reset manual rotation
 		} else {
@@ -273,9 +273,9 @@ public class MapViewTrackingUtilities implements OsmAndLocationListener, IMapLoc
 			settings.ROTATE_MAP.set(vl);
 
 			if (settings.ROTATE_MAP.get() == OsmandSettings.ROTATE_MAP_COMPASS) {
-				resId = R.string.rotate_map_compass_opt;
+				resId = R.string.rotate_map_to_bearing + ":\n" + R.string.rotate_map_compass_opt;
 			} else if (settings.ROTATE_MAP.get() == OsmandSettings.ROTATE_MAP_BEARING) {
-				resId = R.string.rotate_map_bearing_opt;
+				resId = R.string.rotate_map_to_bearing + ":\n" + R.string.rotate_map_bearing_opt;
 			}
 		}
 		app.showShortToastMessage(resId);
