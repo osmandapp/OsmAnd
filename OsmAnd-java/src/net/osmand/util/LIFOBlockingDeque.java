7bbfcc0b7e1a5bcdba11f3c18d3b6e164e029d1e
jeepingben <ben_deering@jeepingben.net>
2014-09-05 08:34:34 -0400

    Create LIFOBlockingDeque.java
    Add support for LIFO blocking queue (deque actually)

---
+++ OsmAnd-java/src/net/osmand/util/LIFOBlockingDeque.java
@@ -0,0 +1,18 @@
+package net.osmand.util;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class LIFOBlockingDeque<T> extends LinkedBlockingDeque<T> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	 public boolean offer(T t) {
+	  return super.offerFirst(t);
+	 }
+
+	 @Override
+	 public T remove() {
+	  return super.removeFirst();
+	 }
+}
