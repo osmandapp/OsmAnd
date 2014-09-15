20c940a0c874126fd8ee851e1745f1636129ac06
Denis <barsik107@gmail.com>
2014-09-15 15:50:12 +0300

    Updated onChildClick functionality for listview

--- OsmAnd/src/net/osmand/plus/download/LocalIndexesFragment.java
+++ OsmAnd/src/net/osmand/plus/download/LocalIndexesFragment.java
@@ -868,13 +868,6 @@ public class LocalIndexesFragment extends OsmandExpandableListFragment {
 				LayoutInflater inflater = (LayoutInflater) getDownloadActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 				v = inflater.inflate(net.osmand.plus.R.layout.local_index_list_item, parent, false);
 			}
-			final View row = v;
-			row.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View view) {
-					onChildClick(getExpandableListView(), row, groupPosition, childPosition, getChildId(groupPosition, childPosition));
-				}
-			});
 			TextView viewName = ((TextView) v.findViewById(R.id.local_index_name));
 			String mapDescr = getMapDescription(child.getFileName());
 			String mapName = FileNameTranslationHelper.getFileName(ctx, ((OsmandApplication) getDownloadActivity().getApplication()).getResourceManager().getOsmandRegions(), child.getFileName());
