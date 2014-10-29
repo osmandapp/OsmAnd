d810f1ffe60e49ea74f16633b1042b18cb537fbc
sonora <hm.gglmail@gmail.com>
2014-10-29 14:35:27 +0100

    fix order of menu items as we had it

--- OsmAnd/src/net/osmand/plus/rastermaps/OsmandRasterMapsPlugin.java
+++ OsmAnd/src/net/osmand/plus/rastermaps/OsmandRasterMapsPlugin.java
@@ -212,9 +212,9 @@ public class OsmandRasterMapsPlugin extends OsmandPlugin {
 		adapter.item(R.string.layer_map).icons(R.drawable.ic_action_globus_dark, R.drawable.ic_action_globus_light)
 				.listen(listener).position(0).reg();
 		adapter.item(R.string.layer_overlay).selected(overlayLayer.getMap() != null ? 1 : 0).
-				icons(R.drawable.ic_action_up_dark, R.drawable.ic_action_up_light).listen(listener).position(10).reg();
+				icons(R.drawable.ic_action_up_dark, R.drawable.ic_action_up_light).listen(listener).position(14).reg();
 		adapter.item(R.string.layer_underlay).selected(underlayLayer.getMap() != null ? 1 : 0) 
-				.icons(R.drawable.ic_action_down_dark, R.drawable.ic_action_down_light).listen(listener).position(11).reg();
+				.icons(R.drawable.ic_action_down_dark, R.drawable.ic_action_down_light).listen(listener).position(15).reg();
 	}
 	
 	
