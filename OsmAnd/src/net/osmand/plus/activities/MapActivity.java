7368948cf6a959dcc867260928a61bb6f199cf81
Zahnstocher <openstreetmap.org@nurfuerspam.de>
2014-11-05 01:33:50 +0100

    WindowManager

--- OsmAnd/src/net/osmand/plus/activities/MapActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/MapActivity.java
@@ -76,6 +76,7 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.ViewStub;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
