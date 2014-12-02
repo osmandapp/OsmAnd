667af697678e70844d80555944e7cc3b5478c334
Zahnstocher <openstreetmap.org@nurfuerspam.de>
2014-12-02 21:48:41 +0100

    Fix null pointer

--- OsmAnd/src/net/osmand/plus/routing/VoiceRouter.java
+++ OsmAnd/src/net/osmand/plus/routing/VoiceRouter.java
@@ -77,7 +77,7 @@ public class VoiceRouter {
     public interface VoiceMessageListener {
     	void onVoiceMessage();
     }
-    private ConcurrentHashMap<VoiceMessageListener, Void> voiceMessageListeners;
+    private ConcurrentHashMap<VoiceMessageListener, Integer> voiceMessageListeners;
     
 	public VoiceRouter(RoutingHelper router, final OsmandSettings settings, CommandPlayer player) {
 		this.router = router;
@@ -85,7 +85,7 @@ public class VoiceRouter {
         this.settings = settings;
 
 		empty = new Struct("");
-		voiceMessageListeners = new ConcurrentHashMap<VoiceRouter.VoiceMessageListener, Void>();
+		voiceMessageListeners = new ConcurrentHashMap<VoiceRouter.VoiceMessageListener, Integer>();
 	}
 	
 	public void setPlayer(CommandPlayer player) {
@@ -842,7 +842,7 @@ public class VoiceRouter {
 	}
 
 	public void addVoiceMessageListener(VoiceMessageListener voiceMessageListener) {
-		voiceMessageListeners.put(voiceMessageListener, null);
+		voiceMessageListeners.put(voiceMessageListener, 0);
 	}
 	
 	public void removeVoiceMessageListener(VoiceMessageListener voiceMessageListener) {
