df46b01dcdf32c9514f66357202cbb3ba751f15e
sonora <hm.gglmail@gmail.com>
2014-12-06 11:04:03 +0100

    comment out code related to creating copyright notes directly on dashboard

--- OsmAnd/src/net/osmand/plus/activities/DashboardActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/DashboardActivity.java
@@ -113,32 +113,28 @@ public class DashboardActivity extends BaseDownloadActivity {
 	
 	protected void setupContributionVersion() {
 		findViewById(R.id.credentials).setVisibility(View.VISIBLE);
-		final TextView textVersionView = (TextView) findViewById(R.id.Copyright);
-		final Calendar inst = Calendar.getInstance();
-		inst.setTime(new Date());
-		final String textVersion = "\u00A9 OsmAnd " + inst.get(Calendar.YEAR);
-		textVersionView.setText(textVersion);
+//Copyright notes and links have been put on the 'About' screen
+//		final TextView textVersionView = (TextView) findViewById(R.id.Copyright);
+//		final Calendar inst = Calendar.getInstance();
+//		inst.setTime(new Date());
+//		final String textVersion = "\u00A9 OsmAnd " + inst.get(Calendar.YEAR);
+//		textVersionView.setText(textVersion);
 		final SharedPreferences prefs = getApplicationContext().getSharedPreferences("net.osmand.settings", MODE_WORLD_READABLE);
-
-	//TODO: All Copyright notes and links should be removed here, have been put on the 'About' screen
-		textVersionView.setVisibility(View.GONE);
-
-		textVersionView.setOnClickListener(new OnClickListener(){
-
-			int i = 0;
-			@Override
-			public void onClick(View v) {
-				if(i++ > 8 && Version.isDeveloperVersion(getMyApplication())) {
-					prefs.edit().putBoolean(CONTRIBUTION_VERSION_FLAG, true).commit();
-					enableLink(DashboardActivity.this, textVersion, textVersionView);
-				}
-			}
-		});
+//		textVersionView.setOnClickListener(new OnClickListener(){
+//			int i = 0;
+//			@Override
+//			public void onClick(View v) {
+//				if(i++ > 8 && Version.isDeveloperVersion(getMyApplication())) {
+//					prefs.edit().putBoolean(CONTRIBUTION_VERSION_FLAG, true).commit();
+//					enableLink(DashboardActivity.this, textVersion, textVersionView);
+//				}
+//			}
+//		});
 		// only one commit should be with contribution version flag
 //		 prefs.edit().putBoolean(CONTRIBUTION_VERSION_FLAG, true).commit();
-		if (prefs.contains(CONTRIBUTION_VERSION_FLAG) && Version.isDeveloperVersion(getMyApplication())) {
-			enableLink(this, textVersion, textVersionView);
-		}
+//		if (prefs.contains(CONTRIBUTION_VERSION_FLAG) && Version.isDeveloperVersion(getMyApplication())) {
+//			enableLink(this, textVersion, textVersionView);
+//		}
 		final TextView about = (TextView) findViewById(R.id.About);
 		final String aboutString = getString(R.string.about_settings);
 		SpannableString ss = new SpannableString(aboutString);
@@ -405,19 +401,19 @@ public class DashboardActivity extends BaseDownloadActivity {
 		
 	}
 	
-	private static void enableLink(final Activity activity, String textVersion, TextView textVersionView) {
-		SpannableString content = new SpannableString(textVersion);
-		content.setSpan(new ClickableSpan() {
-
-			@Override
-			public void onClick(View widget) {
-				final Intent mapIntent = new Intent(activity, ContributionVersionActivity.class);
-				activity.startActivityForResult(mapIntent, 0);
-			}
-		}, 0, content.length(), 0);
-		textVersionView.setText(content);
-		textVersionView.setMovementMethod(LinkMovementMethod.getInstance());
-	}
+//	private static void enableLink(final Activity activity, String textVersion, TextView textVersionView) {
+//		SpannableString content = new SpannableString(textVersion);
+//		content.setSpan(new ClickableSpan() {
+//
+//			@Override
+//			public void onClick(View widget) {
+//				final Intent mapIntent = new Intent(activity, ContributionVersionActivity.class);
+//				activity.startActivityForResult(mapIntent, 0);
+//			}
+//		}, 0, content.length(), 0);
+//		textVersionView.setText(content);
+//		textVersionView.setMovementMethod(LinkMovementMethod.getInstance());
+//	}
 
 	@Override
 	public void updateProgress(boolean updateOnlyProgress) {
