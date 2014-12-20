65520e882a16cd16b6115a7c742d7e1a43db64e2
sonora <hm.gglmail@gmail.com>
2014-12-20 22:14:22 +0100

    support setting for announcement of pedestrian crosswalks

--- OsmAnd/src/net/osmand/plus/activities/SettingsNavigationActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/SettingsNavigationActivity.java
@@ -333,12 +333,14 @@ public class SettingsNavigationActivity extends SettingsBaseActivity {
 					settings.SHOW_PEDESTRIAN, settings.SHOW_CAMERAS, settings.SHOW_LANES }, preference.getTitle());
 			return true;
 		} else if (preference == speakAlarms) {
-			showBooleanSettings(new String[] { getString(R.string.speak_street_names),  getString(R.string.speak_traffic_warnings), 
+			showBooleanSettings(new String[] { getString(R.string.speak_street_names), getString(R.string.speak_traffic_warnings),
+					getString(R.string.speak_pedestrian),
 					getString(R.string.speak_speed_limit), getString(R.string.speak_cameras),
 					getString(R.string.announce_gpx_waypoints),
 					getString(R.string.speak_favorites),
 					getString(R.string.speak_poi)},
-					new OsmandPreference[] { settings.SPEAK_STREET_NAMES, settings.SPEAK_TRAFFIC_WARNINGS, 
+					new OsmandPreference[] { settings.SPEAK_STREET_NAMES, settings.SPEAK_TRAFFIC_WARNINGS,
+					settings.SPEAK_PEDESTRIAN,
 					settings.SPEAK_SPEED_LIMIT, settings.SPEAK_SPEED_CAMERA, 
 					settings.ANNOUNCE_WPT, settings.ANNOUNCE_NEARBY_FAVORITES, settings.ANNOUNCE_NEARBY_POI}, preference.getTitle());
 			return true;
