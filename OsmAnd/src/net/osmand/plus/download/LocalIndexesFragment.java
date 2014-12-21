b91a5ce217fd2dcb1e1a6ca9392f6f6d94e76b04
sonora <hm.gglmail@gmail.com>
2014-12-21 11:31:42 +0100

    comment temporary fix for NPE in LocalIndexesFragment

--- OsmAnd/src/net/osmand/plus/download/LocalIndexesFragment.java
+++ OsmAnd/src/net/osmand/plus/download/LocalIndexesFragment.java
@@ -479,6 +479,7 @@ public class LocalIndexesFragment extends OsmandExpandableListFragment {
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		//fixes issue when local files not shown after switching tabs
+		//TODO: But this throws NPE in some circumstances and needs fixing, so commented out for now
 		//if (listAdapter.getGroupCount() == 0 && getDownloadActivity().getLocalIndexInfos().size() > 0){
 		//	for(LocalIndexInfo info : getDownloadActivity().getLocalIndexInfos()){
 		//		listAdapter.addLocalIndexInfo(info);
