78a84760c40984f97b93d8fd68fa773cf3f030f1
sonora <hm.gglmail@gmail.com>
2014-12-21 11:14:44 +0100

    temporarily dactivate code to test NPE

--- OsmAnd/src/net/osmand/plus/download/LocalIndexesFragment.java
+++ OsmAnd/src/net/osmand/plus/download/LocalIndexesFragment.java
@@ -479,13 +479,13 @@ public class LocalIndexesFragment extends OsmandExpandableListFragment {
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		//fixes issue when local files not shown after switching tabs
-		if (listAdapter.getGroupCount() == 0 && getDownloadActivity().getLocalIndexInfos().size() > 0){
-			for(LocalIndexInfo info : getDownloadActivity().getLocalIndexInfos()){
-				listAdapter.addLocalIndexInfo(info);
-			}
-			listAdapter.sortData();
-			getExpandableListView().setAdapter(listAdapter);
-		}
+		//if (listAdapter.getGroupCount() == 0 && getDownloadActivity().getLocalIndexInfos().size() > 0){
+		//	for(LocalIndexInfo info : getDownloadActivity().getLocalIndexInfos()){
+		//		listAdapter.addLocalIndexInfo(info);
+		//	}
+		//	listAdapter.sortData();
+		//	getExpandableListView().setAdapter(listAdapter);
+		//}
 		ActionBar actionBar = getDownloadActivity().getSupportActionBar();
 		//hide action bar from downloadindexfragment
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
