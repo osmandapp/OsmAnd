83af9ac05205319561875327cf28cc0b1c002f27
Denis <barsik107@gmail.com>
2014-12-25 15:37:51 +0200

    Refactored dashaudiovideonotesactivity. Added delete recording function

--- OsmAnd/src/net/osmand/plus/dashboard/DashAudioVideoNotesActivity.java
+++ OsmAnd/src/net/osmand/plus/dashboard/DashAudioVideoNotesActivity.java
@@ -1,22 +1,45 @@
 package net.osmand.plus.dashboard;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.internal.widget.IcsAdapterView;
 import com.actionbarsherlock.view.MenuItem;
+import net.osmand.plus.ContextMenuAdapter;
+import net.osmand.plus.OsmandPlugin;
 import net.osmand.plus.R;
 import net.osmand.plus.activities.MainMenuActivity;
+import net.osmand.plus.audionotes.AudioVideoNotesPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Denis on 23.12.2014.
  */
-public class DashAudioVideoNotesActivity extends SherlockFragmentActivity {
+public class DashAudioVideoNotesActivity extends SherlockListActivity {
+	AudioVideoNotesPlugin plugin;
+	List<AudioVideoNotesPlugin.Recording> items;
+	NotesAdapter listAdapter;
 
 	@Override
-	protected void onResume() {
-		super.onResume();
-		setContentView(R.layout.audio_video_notes_all);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.editing_poi_filter);
+
+		plugin = OsmandPlugin.getEnabledPlugin(AudioVideoNotesPlugin.class);
 
 		ColorDrawable color = new ColorDrawable(getResources().getColor(R.color.actionbar_color));
 		ActionBar actionBar = getSupportActionBar();
@@ -25,6 +48,52 @@ public class DashAudioVideoNotesActivity extends SherlockFragmentActivity {
 		actionBar.setIcon(android.R.color.transparent);
 		actionBar.setHomeButtonEnabled(true);
 		actionBar.setDisplayHomeAsUpEnabled(true);
+		getListView().setBackgroundColor(getResources().getColor(R.color.dashboard_background));
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		items = new ArrayList<AudioVideoNotesPlugin.Recording>(plugin.getAllRecordings());
+		listAdapter = new NotesAdapter(items);
+		setListAdapter(listAdapter);
+	}
+
+	private void showContextMenu(final AudioVideoNotesPlugin.Recording recording){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		final ContextMenuAdapter adapter = new ContextMenuAdapter(this);
+		ContextMenuAdapter.OnContextMenuClick listener = new ContextMenuAdapter.OnContextMenuClick() {
+			@Override
+			public boolean onContextMenuClick(final ArrayAdapter<?> adapter, int resId, int pos, boolean isChecked) {
+				if (resId == R.string.local_index_mi_delete) {
+					AlertDialog.Builder confirm = new AlertDialog.Builder(DashAudioVideoNotesActivity.this);
+					confirm.setPositiveButton(R.string.default_buttons_yes, new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							plugin.deleteRecording(recording);
+							items.remove(recording);
+							listAdapter.notifyDataSetChanged();
+						}
+					});
+					confirm.setNegativeButton(R.string.default_buttons_no, null);
+					confirm.setMessage(getString(R.string.delete_confirmation_msg, recording.file.getName()));
+					confirm.show();
+				}
+				return true;
+			}
+		};
+		adapter.item(R.string.local_index_mi_delete).listen(listener).position(0).reg();
+
+		builder.setItems(adapter.getItemNames(), new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				ContextMenuAdapter.OnContextMenuClick clk = adapter.getClickAdapter(which);
+				if (clk != null){
+					clk.onContextMenuClick(null, adapter.getElementId(which), which, false);
+				}
+			}
+		});
+		builder.show();
 	}
 
 	@Override
@@ -34,6 +103,34 @@ public class DashAudioVideoNotesActivity extends SherlockFragmentActivity {
 				this.finish();
 			default:
 				return super.onOptionsItemSelected(item);
+		}
+	}
+
+
+
+	class NotesAdapter extends ArrayAdapter<AudioVideoNotesPlugin.Recording> {
+		NotesAdapter(List<AudioVideoNotesPlugin.Recording> recordingList) {
+			super(DashAudioVideoNotesActivity.this, R.layout.dash_note_item, recordingList);
+		}
+
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = getLayoutInflater();
+			View row = convertView;
+			if (row == null){
+				row = inflater.inflate(R.layout.dash_note_item, parent, false);
+				row.findViewById(R.id.divider).setVisibility(View.GONE);
+			}
+			final AudioVideoNotesPlugin.Recording recording = getItem(position);
+			DashAudioVideoNotesFragment.getNoteView(recording, row, DashAudioVideoNotesActivity.this, plugin);
+			row.setOnLongClickListener(new View.OnLongClickListener() {
+				@Override
+				public boolean onLongClick(View v) {
+					showContextMenu(recording);
+					return true;
+				}
+			});
+			return row;
 		}
 	}
 }
