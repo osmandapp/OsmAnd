81767bcfaa618583e01d59608f92365e689ba12b
sonora <hm.gglmail@gmail.com>
2015-01-17 09:23:09 +0100

    import IProgress

--- OsmAnd/src/net/osmand/plus/activities/search/SearchCityByNameActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/search/SearchCityByNameActivity.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 
+import net.osmand.IProgress;
 import net.osmand.CollatorStringMatcher;
 import net.osmand.CollatorStringMatcher.StringMatcherMode;
 import net.osmand.OsmAndCollator;
