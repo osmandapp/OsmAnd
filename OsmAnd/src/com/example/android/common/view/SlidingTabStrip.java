9ba508d9e18e49c10a536ce75495e3effa786487
vshcherb <victor.shcherb@gmail.com>
2015-01-20 16:02:25 +0100

    Update SlidingTabStrip.java

--- OsmAnd/src/com/example/android/common/view/SlidingTabStrip.java
+++ OsmAnd/src/com/example/android/common/view/SlidingTabStrip.java
@@ -28,12 +28,12 @@ import android.widget.LinearLayout;
  
 class SlidingTabStrip extends LinearLayout {
  
-    private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 2;
+    private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 0;
     private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x26;
-    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 8;
-    private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFF33B5E5;
+    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 2;
+    private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFFFF8F00;
  
-    private static final int DEFAULT_DIVIDER_THICKNESS_DIPS = 1;
+    private static final int DEFAULT_DIVIDER_THICKNESS_DIPS = 0;
     private static final byte DEFAULT_DIVIDER_COLOR_ALPHA = 0x20;
     private static final float DEFAULT_DIVIDER_HEIGHT = 0.5f;
  
