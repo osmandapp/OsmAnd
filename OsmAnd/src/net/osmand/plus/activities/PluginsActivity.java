a25e3ca8b3513378810b0ad10b5b7a1efe6c44ee
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-01-30 14:58:42 +0200

    Plugins screen redesign

--- OsmAnd/src/net/osmand/plus/activities/PluginsActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/PluginsActivity.java
@@ -1,126 +1,151 @@
 package net.osmand.plus.activities;
 
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.OsmandPlugin;
 import net.osmand.plus.R;
+import net.osmand.plus.osmo.OsMoPlugin;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PluginsActivity extends OsmandListActivity {
-
 	public static final int ACTIVE_PLUGINS_LIST_MODIFIED = 1;
 
-	private List<OsmandPlugin> availablePlugins;
-	private Set<String> clickedPlugins = new LinkedHashSet<String>();
-	private Set<String> restartPlugins = new LinkedHashSet<String>();
-	
+	private boolean activePluginsListModified = false;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		((OsmandApplication) getApplication()).applyTheme(this);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.plugins);
 		getSupportActionBar().setTitle(R.string.plugins_screen);
-		
-		availablePlugins = OsmandPlugin.getAvailablePlugins();
-		List<OsmandPlugin> enabledPlugins = OsmandPlugin.getEnabledPlugins();
-		for(OsmandPlugin p : enabledPlugins) {
-			restartPlugins.add(p.getId());
-		}
-		setListAdapter(new OsmandPluginsAdapter(availablePlugins));
+
+		setListAdapter(new PluginsListAdapter());
 	}
-	
-	
-	private void click(int position) {
-		OsmandPlugin item = getListAdapter().getItem(position);
-		boolean enable = !restartPlugins.contains(item.getId());
-		boolean ok = OsmandPlugin.enablePlugin(((OsmandApplication) getApplication()), item, enable);
-		if (ok) {
-			if (!enable) {
-				restartPlugins.remove(item.getId());
-			} else {
-				restartPlugins.add(item.getId());
-			}
-			setResult(ACTIVE_PLUGINS_LIST_MODIFIED);
-		}
-		clickedPlugins.add(item.getId());
-		getListAdapter().notifyDataSetChanged();
-	}
-	
+
 	@Override
-	public OsmandPluginsAdapter getListAdapter() {
-		return (OsmandPluginsAdapter) super.getListAdapter();
+	public PluginsListAdapter getListAdapter() {
+		return (PluginsListAdapter) super.getListAdapter();
 	}
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		click(position);
+		OsmandPlugin plugin = view.getTag() instanceof OsmandPlugin
+				? (OsmandPlugin)view.getTag()
+				: null;
+		if (plugin == null) {
+			return;
+		}
+
+		//TODO: open details
+		togglePluginEnabled(plugin);
 	}
 
-	protected class OsmandPluginsAdapter extends ArrayAdapter<OsmandPlugin> {
-		
-		public OsmandPluginsAdapter(List<OsmandPlugin> plugins) {
-			super(PluginsActivity.this, R.layout.plugins_list_item, plugins);
+	private void togglePluginEnabled(OsmandPlugin plugin) {
+		boolean isEnabled = OsmandPlugin.getEnabledPlugins().contains(plugin);
+		enableDisablePlugin(plugin, !isEnabled);
+	}
+
+	private void enableDisablePlugin(OsmandPlugin plugin, boolean enable) {
+		boolean ok = OsmandPlugin.enablePlugin(((OsmandApplication) getApplication()), plugin,
+				enable);
+		if (!ok) {
+			return;
+		}
+
+		if (!activePluginsListModified) {
+			setResult(ACTIVE_PLUGINS_LIST_MODIFIED);
+			activePluginsListModified = true;
+		}
+		getListAdapter().notifyDataSetChanged();
+	}
+
+	protected class PluginsListAdapter extends ArrayAdapter<OsmandPlugin> {
+		public PluginsListAdapter() {
+			super(PluginsActivity.this, R.layout.plugins_list_item,
+					OsmandPlugin.getAvailablePlugins());
 		}
 
 		@Override
-		public View getView(final int position, View convertView, ViewGroup parent) {
-			View v = convertView;
-			if (v == null) {
-				LayoutInflater inflater = getLayoutInflater();
-				v = inflater.inflate(net.osmand.plus.R.layout.plugins_list_item, parent, false);
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			if (view == null) {
+				view = getLayoutInflater().inflate(R.layout.plugins_list_item, parent, false);
 			}
-			OsmandPlugin plugin = getItem(position);
-			boolean toBeEnabled = restartPlugins.contains(plugin.getId());
-			
-			final View row = v;
-			CheckBox ch = (CheckBox) row.findViewById(R.id.check_item);
-			ch.setOnClickListener(null);
-			ch.setChecked(toBeEnabled);
-			ch.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					click(position);
-				}
-			});
-			TextView nameView = (TextView) row.findViewById(R.id.plugin_name);
-			nameView.setText(plugin.getName());
-			nameView.setContentDescription(plugin.getName() + " " + getString(toBeEnabled ? R.string.item_checked : R.string.item_unchecked));
-			
-			View rw = row.findViewById(R.id.settings);
-			final Class<? extends Activity> st = plugin.getSettingsActivity();
-			rw.setVisibility(toBeEnabled && st != null ? View.VISIBLE : View.GONE);
-			rw.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					if (st != null) {
-						startActivity(new Intent(PluginsActivity.this, st));
-					}
-				}
-			});
-			
-			
-			TextView description = (TextView) row.findViewById(R.id.plugin_descr);
-			description.setText(plugin.getDescription());
-			description.setVisibility(clickedPlugins.contains(plugin.getId()) ||
-					!restartPlugins.contains(plugin.getId()) ? View.VISIBLE : View.GONE);
 
-			return row;
+			final OsmandPlugin plugin = getItem(position);
+			final boolean isEnabled = OsmandPlugin.getEnabledPlugins().contains(plugin);
+
+			view.setTag(plugin);
+
+			ImageView pluginLogo = (ImageView)view.findViewById(R.id.plugin_logo);
+			if (plugin.getId().equals(OsMoPlugin.ID)) {
+				pluginLogo.setImageResource(R.drawable.ic_osmo_dark);
+			}
+
+			TextView pluginName = (TextView)view.findViewById(R.id.plugin_name);
+			pluginName.setText(plugin.getName());
+			pluginName.setContentDescription(plugin.getName() + " " + getString(isEnabled
+					? R.string.item_checked
+					: R.string.item_unchecked));
+
+			TextView pluginDescription = (TextView)view.findViewById(R.id.plugin_description);
+			pluginDescription.setText(plugin.getDescription());
+
+			View pluginIsEnabled = view.findViewById(R.id.plugin_is_enabled);
+			pluginIsEnabled.setVisibility(isEnabled ? View.VISIBLE : View.INVISIBLE);
+
+			View pluginOptions = view.findViewById(R.id.plugin_options);
+			pluginOptions.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					showOptionsMenu(v, plugin);
+				}
+			});
+
+			return view;
 		}
-		
 	}
-	
 
+	private void showOptionsMenu(View v, final OsmandPlugin plugin) {
+		final boolean isEnabled = OsmandPlugin.getEnabledPlugins().contains(plugin);
+		final Class<? extends Activity> settingsActivity = plugin.getSettingsActivity();
+
+		final PopupMenu optionsMenu = new PopupMenu(this, v);
+
+		MenuItem enableDisableItem = optionsMenu.getMenu().add(isEnabled
+				? R.string.disable_plugin
+				: R.string.enable_plugin);
+		enableDisableItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				enableDisablePlugin(plugin, !isEnabled);
+				optionsMenu.dismiss();
+				return true;
+			}
+		});
+
+		if (settingsActivity != null) {
+			MenuItem settingsItem = optionsMenu.getMenu().add(R.string.settings);
+			settingsItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+				@Override
+				public boolean onMenuItemClick(MenuItem item) {
+					startActivity(new Intent(PluginsActivity.this, settingsActivity));
+					optionsMenu.dismiss();
+					return true;
+				}
+			});
+			settingsItem.setEnabled(isEnabled);
+		}
+
+		optionsMenu.show();
+	}
 }
