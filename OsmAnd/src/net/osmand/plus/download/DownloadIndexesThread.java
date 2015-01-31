e39d26f8d9c0edcd3158c285a69df2a6ba139fbb
sonora <hm.gglmail@gmail.com>
2015-01-31 18:56:28 +0100

    fix display of updatable indexes

--- OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
+++ OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
@@ -557,7 +557,8 @@ public class DownloadIndexesThread {
 			itemsToUpdate.clear();
 			for (IndexItem item : filtered) {
 				boolean outdated = checkIfItemOutdated(item);
-				if(outdated) {
+				//include only activated files here
+				if(outdated && indexActivatedFileNames.containsKey(item.getTargetFileName())) {
 					itemsToUpdate.add(item);
 				}
 			}
@@ -576,9 +577,9 @@ public class DownloadIndexesThread {
 		String indexfilesdate = indexFileNames.get(sfName);
 		if (date != null &&
 				!date.equals(indexactivateddate) &&
-				!date.equals(indexfilesdate) &&
-				//Discrimintaion if item is deactivated should actually be made elsewhere in the code (DonwloadIndexAdapter, UpdateIndexFragment, etc.)
-				indexActivatedFileNames.containsKey(sfName)) {
+				!date.equals(indexfilesdate)) {
+				//Discrimination if item is deactivated should actually be made elsewhere in the code (DownloadIndexAdapter, UpdateIndexFragment, etc.)
+				//indexActivatedFileNames.containsKey(sfName)) {
 			if ((item.getType() == DownloadActivityType.NORMAL_FILE && !item.extra) ||
 					item.getType() == DownloadActivityType.ROADS_FILE ||
 					item.getType() == DownloadActivityType.SRTM_COUNTRY_FILE){
