2bc4443e4adf27674832cce536a9da72654568f9
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-02-10 15:54:29 +0200

    Victor request

--- OsmAnd/src/net/osmand/plus/rastermaps/OsmandRasterMapsPlugin.java
+++ OsmAnd/src/net/osmand/plus/rastermaps/OsmandRasterMapsPlugin.java
@@ -116,7 +116,7 @@ public class OsmandRasterMapsPlugin extends OsmandPlugin {
 		if(!Algorithms.objectEquals(overlay, layer.getMap())){
 			if(overlay == null){
 				mapView.removeLayer(layer);
-			} else {
+			} else if (mapView.getMapRenderer() == null) {
 				mapView.addLayer(layer, layerOrder);
 			}
 			layer.setMap(overlay);
