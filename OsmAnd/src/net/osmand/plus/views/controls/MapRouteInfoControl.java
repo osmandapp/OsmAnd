62735ccff6e40f3df49257b21f09800c8329ac66
Victor Shcherb <victor.shcherb@gmail.com>
2015-03-08 14:40:52 +0100

    Fix compilation

--- OsmAnd/src/net/osmand/plus/views/controls/MapRouteInfoControl.java
+++ OsmAnd/src/net/osmand/plus/views/controls/MapRouteInfoControl.java
@@ -424,7 +424,7 @@ public class MapRouteInfoControl extends MapControls implements IRouteInformatio
 			toActions.add(mapActivity.getString(R.string.route_descr_select_destination));			
 		}
 		toActions.add(mapActivity.getString(R.string.shared_string_favorite) + mapActivity.getString(R.string.shared_string_ellipsis));
-		toActions.add(mapActivity.getString(R.string.route_descr_select_on_map));
+		toActions.add(mapActivity.getString(R.string.shared_string_select_on_map));
 		
 		ArrayAdapter<String> toAdapter = new ArrayAdapter<String>(view.getContext(), 
 				android.R.layout.simple_spinner_item, 
