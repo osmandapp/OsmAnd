df8f0a27334ddd4a890083720bd4ebc8ccdfc1a0
sonora <hm.gglmail@gmail.com>
2015-04-14 22:12:08 +0200

    fix size

--- OsmAnd/src/net/osmand/plus/activities/MapActivityLayers.java
+++ OsmAnd/src/net/osmand/plus/activities/MapActivityLayers.java
@@ -269,7 +269,7 @@ public class MapActivityLayers {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				if (which == 0) {
-					which = list.size() + 1;
+					which = list.size() + 2;
 				}
 				PoiLegacyFilter pf = list.get(which - 1);
 				String filterId = pf.getFilterId();
