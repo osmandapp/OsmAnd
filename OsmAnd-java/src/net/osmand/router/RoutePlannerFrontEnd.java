c107cc7644b0c71affbb2c5732689f3a22fe18b1
Victor Shcherb <victor.shcherb@gmail.com>
2015-05-06 23:21:28 +0200

    Fix shorten route calculation

--- OsmAnd-java/src/net/osmand/router/RoutePlannerFrontEnd.java
+++ OsmAnd-java/src/net/osmand/router/RoutePlannerFrontEnd.java
@@ -334,7 +334,7 @@ public class RoutePlannerFrontEnd {
 			runRecalculation = rlist.size() > 0;
 			if (rlist.size() > 0) {
 				RouteSegment previous = null;
-				for (int i = 0; i < rlist.size() - 1; i++) {
+				for (int i = 0; i <= rlist.size() - 1; i++) {
 					RouteSegmentResult rr = rlist.get(i);
 					RouteSegment segment = new RouteSegment(rr.getObject(), rr.getEndPointIndex());
 					if (previous != null) {
