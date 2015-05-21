9752222adef14a85babb304921c8bf4d98f7e55f
sonora <hm.gglmail@gmail.com>
2015-05-22 01:31:39 +0200

    move less used map layer item below more frequently used items

--- OsmAnd/src/net/osmand/plus/dialogs/ConfigureMapMenu.java
+++ OsmAnd/src/net/osmand/plus/dialogs/ConfigureMapMenu.java
@@ -114,14 +114,6 @@ public class ConfigureMapMenu {
 				if (isChecked) {
 					selectPOILayer(settings);
 				}
-			} else if (itemId == R.string.layer_map) {
-				if(OsmandPlugin.getEnabledPlugin(OsmandRasterMapsPlugin.class) == null) {
-					Intent intent = new Intent(ma, PluginActivity.class);
-					intent.putExtra(PluginActivity.EXTRA_PLUGIN_ID, OsmandRasterMapsPlugin.ID);
-					ma.startActivity(intent);
-				} else {
-					ma.getMapLayers().selectMapLayer(ma.getMapView());
-				}
 			} else if (itemId == R.string.layer_amenity_label) {
 				settings.SHOW_POI_LABEL.set(isChecked);
 			} else if (itemId == R.string.shared_string_favorites) {
@@ -138,6 +130,14 @@ public class ConfigureMapMenu {
 							adapter.notifyDataSetChanged();
 						}
 					});
+				}
+			} else if (itemId == R.string.layer_map) {
+				if(OsmandPlugin.getEnabledPlugin(OsmandRasterMapsPlugin.class) == null) {
+					Intent intent = new Intent(ma, PluginActivity.class);
+					intent.putExtra(PluginActivity.EXTRA_PLUGIN_ID, OsmandRasterMapsPlugin.ID);
+					ma.startActivity(intent);
+				} else {
+					ma.getMapLayers().selectMapLayer(ma.getMapView());
 				}
 			} else if (itemId == R.string.layer_transport_route) {
 				ma.getMapLayers().getTransportInfoLayer().setVisible(isChecked);
@@ -168,8 +168,6 @@ public class ConfigureMapMenu {
 		// String appMode = " [" + settings.getApplicationMode().toHumanString(view.getApplication()) +"] ";
 		adapter.item(R.string.layer_poi).selected(settings.SELECTED_POI_FILTER_FOR_MAP.get() != null ? 1 : 0)
 				.iconColor(R.drawable.ic_action_info_dark).listen(l).reg();
-		adapter.item(R.string.layer_map).iconColor(R.drawable.ic_world_globe_dark)
-				.listen(l).reg();
 		adapter.item(R.string.layer_amenity_label).selected(settings.SHOW_POI_LABEL.get() ? 1 : 0)
 				.iconColor(R.drawable.ic_action_text_dark).listen(l).reg();
 		adapter.item(R.string.shared_string_favorites).selected(settings.SHOW_FAVORITES.get() ? 1 : 0)
@@ -177,6 +175,8 @@ public class ConfigureMapMenu {
 		adapter.item(R.string.layer_gpx_layer).selected(
 				app.getSelectedGpxHelper().isShowingAnyGpxFiles() ? 1 : 0)
 				.iconColor(R.drawable.ic_action_polygom_dark)
+				.listen(l).reg();
+		adapter.item(R.string.layer_map).iconColor(R.drawable.ic_world_globe_dark)
 				.listen(l).reg();
 		if(TransportRouteHelper.getInstance().routeIsCalculated()){
 			adapter.item(R.string.layer_transport_route).selected(1)
