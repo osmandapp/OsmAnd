2d449ae64f921b5def78b0a1d5dc28cf658e8705
Victor Shcherb <victor.shcherb@gmail.com>
2015-05-26 01:01:39 +0200

    Simplify osmo menu

--- OsmAnd/src/net/osmand/plus/osmo/OsMoGroupsActivity.java
+++ OsmAnd/src/net/osmand/plus/osmo/OsMoGroupsActivity.java
@@ -1083,11 +1083,12 @@ public class OsMoGroupsActivity extends OsmandExpandableListActivity implements 
 		createMenuItem(menu, CONNECT_TO, R.string.osmo_connect, 
 				0, 0,/*R.drawable.ic_action_marker_light,*/
 				MenuItemCompat.SHOW_AS_ACTION_ALWAYS);
-		createMenuItem(menu, SHARE_SESSION, R.string.osmo_share_session, 
-				R.drawable.ic_action_gshare_dark,
-				MenuItemCompat.SHOW_AS_ACTION_ALWAYS);
-		createMenuItem(menu, CREATE_GROUP, R.string.osmo_create_group, 
-				R.drawable.ic_group_add,
+//		createMenuItem(menu, SHARE_SESSION, R.string.osmo_share_session, 
+//				R.drawable.ic_action_gshare_dark,
+//				MenuItemCompat.SHOW_AS_ACTION_ALWAYS);
+		createMenuItem(menu, CREATE_GROUP, R.string.osmo_create_group,
+				0, 0,
+//				R.drawable.ic_group_add,
 				MenuItemCompat.SHOW_AS_ACTION_ALWAYS);
 		
 		return super.onCreateOptionsMenu(menu);
