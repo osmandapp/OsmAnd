95f8644db62e05544a54f40a93d8491a09de6331
Victor Shcherb <victor.shcherb@gmail.com>
2015-06-01 01:15:50 +0200

    Fix

--- OsmAnd/src/net/osmand/plus/audionotes/AudioVideoNotesPlugin.java
+++ OsmAnd/src/net/osmand/plus/audionotes/AudioVideoNotesPlugin.java
@@ -201,7 +201,7 @@ public class AudioVideoNotesPlugin extends OsmandPlugin {
 			String fileName = file.getName();
 
 			int hashInd = fileName.lastIndexOf('_');
-			if (hashInd != -1) {
+			if (hashInd != -1 && hashInd < 8) {
 				return fileName.substring(0, hashInd);
 			} else if (this.isAudio()) {
 				return ctx.getResources().getString(R.string.shared_string_audio);
@@ -992,7 +992,7 @@ public class AudioVideoNotesPlugin extends OsmandPlugin {
 		Recording r = new Recording(f);
 		String encodeName = f.getName();
 		int i = encodeName.lastIndexOf('_');
-		if (i > 0) {
+		if (i > 0 && i < 8) {
 			encodeName = encodeName.substring(i + 1);
 		}
 		i = encodeName.indexOf('.');
