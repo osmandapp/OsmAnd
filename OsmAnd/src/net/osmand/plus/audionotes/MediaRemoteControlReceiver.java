0ae6c02f6583a6fb0a86bff7d2b96c1442b0ef3e
Victor Shcherb <victor.shcherb@gmail.com>
2015-06-09 01:53:34 +0200

    Fix https://github.com/osmandapp/Osmand/issues/1262

--- OsmAnd/src/net/osmand/plus/audionotes/MediaRemoteControlReceiver.java
+++ OsmAnd/src/net/osmand/plus/audionotes/MediaRemoteControlReceiver.java
@@ -1,6 +1,5 @@
 package net.osmand.plus.audionotes;
 
-import net.osmand.plus.OsmandPlugin;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,14 +8,16 @@ public class MediaRemoteControlReceiver extends BroadcastReceiver {
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
-			AudioVideoNotesPlugin plugin = OsmandPlugin.getEnabledPlugin(AudioVideoNotesPlugin.class);
-			if(plugin != null && intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT) != null && 
-					plugin.getActivity() != null) {
+		// DO NOTHING 	https://github.com/osmandapp/Osmand/issues/1262
+//		if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
+//			AudioVideoNotesPlugin plugin = OsmandPlugin.getEnabledPlugin(AudioVideoNotesPlugin.class);
+//			if(plugin != null && intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT) != null && 
+//					plugin.getActivity() != null) {
+//
 //				System.out.println("OsmAnd AV Button pressed " + intent.getIntExtra(Intent.EXTRA_KEY_EVENT, 0));
 				// Toast.makeText(context, "Button pressed " + intent.getIntExtra(Intent.EXTRA_KEY_EVENT, 0), Toast.LENGTH_LONG).show();
-				plugin.defaultAction(plugin.getActivity());
-			}
-		}
+//				plugin.defaultAction(plugin.getActivity());
+//			}
+//		}
 	}
 }
