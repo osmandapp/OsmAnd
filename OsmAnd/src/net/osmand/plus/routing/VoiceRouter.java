4562fadf5ff0e3bc91ed86b3124ebd2163c15e67
sonora <hm.gglmail@gmail.com>
2015-08-02 09:43:37 +0200

    fix build

--- OsmAnd/src/net/osmand/plus/routing/VoiceRouter.java
+++ OsmAnd/src/net/osmand/plus/routing/VoiceRouter.java
@@ -584,7 +584,7 @@ public class VoiceRouter {
 		if(pn != null) {
 			pn = pn.replace('-', ' ');
 			pn = pn.replace(':', ' ');
-			pn = pn.replace(';', "; "); // Trailing blank prevents punctuation beng prnoinced
+			pn = pn.replace(";", "; "); // Trailing blank prevents punctuation being pronounced
 			if ((player != null) && (!"de".equals(player.getLanguage()))) {
 				pn = pn.replace("\u00df", "ss"); // Helps non-German tts voices to pronounce German Strasse (=street)
 			}
