653995556f575635f59b931be9a6e46af3952ee8
Victor Shcherb <victor.shcherb@gmail.com>
2015-09-27 22:32:39 +0200

    Update notification

--- OsmAnd/src/net/osmand/plus/OsmandApplication.java
+++ OsmAnd/src/net/osmand/plus/OsmandApplication.java
@@ -176,6 +176,7 @@ public class OsmandApplication extends Application {
         if(DashRateUsFragment.shouldShow(osmandSettings)) {
             osmandSettings.RATE_US_STATE.set(DashRateUsFragment.RateUsState.IGNORED);
         }
+        getNotificationHelper().removeServiceNotification();
 	}
 
 	public RendererRegistry getRendererRegistry() {
