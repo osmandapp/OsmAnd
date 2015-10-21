e880d2a1a23a3931304c2b6467a9501f83e6d360
Victor Shcherb <victor.shcherb@gmail.com>
2015-10-21 11:54:59 +0200

    Small refactoring

--- OsmAnd/src/net/osmand/plus/dashboard/tools/NumberPickerDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/dashboard/tools/NumberPickerDialogFragment.java
@@ -22,6 +22,7 @@ public class NumberPickerDialogFragment extends DialogFragment {
 	private static final String HEADER_TEXT = "header_text";
 	private static final String SUBHEADER_TEXT = "subheader_text";
 	private static final String NUMBER_OF_ITEMS = "number_of_items";
+	private static final String CURRENT_NUMBER = "current_number";
 
 	@NonNull
 	@Override
@@ -34,13 +35,14 @@ public class NumberPickerDialogFragment extends DialogFragment {
 		String headerText = args.getString(HEADER_TEXT);
 		String subHeaderText = args.getString(SUBHEADER_TEXT);
 		int numberOfItems = args.getInt(NUMBER_OF_ITEMS);
+		int currentNumber = args.getInt(CURRENT_NUMBER);
 
 		String[] items = new String[numberOfItems];
 		for (int i = 0; i < numberOfItems; i++) {
 			items[i] = String.valueOf(i + 1);
 		}
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		builder.setSingleChoiceItems(items, 0, null)
+		builder.setSingleChoiceItems(items, Math.max(0, currentNumber - 1), null)
 				.setPositiveButton(R.string.shared_string_ok, new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
@@ -65,12 +67,13 @@ public class NumberPickerDialogFragment extends DialogFragment {
 	}
 
 	public static NumberPickerDialogFragment createInstance(String header, String subheader,
-															String tag, int number) {
+															String tag, int currentRow, int maxNumber) {
 		Bundle args = new Bundle();
 		args.putString(HEADER_TEXT, header);
 		args.putString(SUBHEADER_TEXT, subheader);
 		args.putString(NUMBER_TAG, tag);
-		args.putInt(NUMBER_OF_ITEMS, number);
+		args.putInt(CURRENT_NUMBER, currentRow);
+		args.putInt(NUMBER_OF_ITEMS, maxNumber);
 		NumberPickerDialogFragment fragment = new NumberPickerDialogFragment();
 		fragment.setArguments(args);
 		return fragment;
