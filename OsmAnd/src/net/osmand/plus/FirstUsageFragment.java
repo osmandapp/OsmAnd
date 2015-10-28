c8dbf813e9b929cae1fb06d6c92e9620d4e38331
GaidamakUA <jmazurevich@gmail.com>
2015-10-28 11:47:26 +0200

    First fragment fix, and multiline strings are now allowed in Poi Search

--- OsmAnd/src/net/osmand/plus/FirstUsageFragment.java
+++ OsmAnd/src/net/osmand/plus/FirstUsageFragment.java
@@ -26,7 +26,7 @@ public class FirstUsageFragment extends Fragment {
 		View view = inflater.inflate(R.layout.first_usage_fragment, container, false);
 		final EditText editText = (EditText) view.findViewById(R.id.searchEditText);
 		Drawable searchIcon = ((MapActivity) getActivity()).getMyApplication().getIconsCache()
-				.getContentIcon(R.drawable.ic_action_search_dark);
+				.getIcon(R.drawable.ic_action_search_dark, true);
 		editText.setCompoundDrawablesWithIntrinsicBounds(searchIcon, null, null, null);
 		Button skipButton = (Button) view.findViewById(R.id.skip_button);
 		skipButton.setOnClickListener(new View.OnClickListener() {
