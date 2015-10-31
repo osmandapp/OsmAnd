0f0dbb2180028cd403523369119e63a8c620ae39
sonora <hm.gglmail@gmail.com>
2015-10-31 13:23:47 +0100

    add file indication to error message to facilitate debugging

--- OsmAnd-java/src/net/osmand/binary/BinaryMapIndexReader.java
+++ OsmAnd-java/src/net/osmand/binary/BinaryMapIndexReader.java
@@ -1,4 +1,4 @@
-package net.osmand.binary;
+﻿package net.osmand.binary;
 
 
 import gnu.trove.list.array.TIntArrayList;
@@ -162,7 +162,8 @@ public class BinaryMapIndexReader {
 			switch (tag) {
 			case 0:
 				if(!initCorrectly){
-					throw new IOException("Corrupted file. It should be ended as it starts with version"); //$NON-NLS-1$
+					//throw new IOException("Corrupted file. It should be ended as it starts with version"); //$NON-NLS-1$
+					throw new IOException("Corrupt file, it should have ended as it starts with version: " + file.getName()); //$NON-NLS-1$
 				}
 				return;
 			case OsmandOdb.OsmAndStructure.VERSION_FIELD_NUMBER :
