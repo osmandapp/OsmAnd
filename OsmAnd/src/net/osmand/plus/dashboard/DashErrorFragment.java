1c9b7a71c73975851b6a651b3b0258bfd17f556c
GaidamakUA <jmazurevich@gmail.com>
2015-11-16 17:50:50 +0200

    Created card to choose between dashboard and drawer

--- OsmAnd/src/net/osmand/plus/dashboard/DashErrorFragment.java
+++ OsmAnd/src/net/osmand/plus/dashboard/DashErrorFragment.java
@@ -30,10 +30,6 @@ import net.osmand.plus.helpers.FontCache;
 import java.io.File;
 import java.text.MessageFormat;
 
-/**
- * Created by Denis
- * on 02.12.14.
- */
 public class DashErrorFragment extends DashBaseFragment {
 
 	public static final String TAG = "DASH_ERROR_FRAGMENT";
