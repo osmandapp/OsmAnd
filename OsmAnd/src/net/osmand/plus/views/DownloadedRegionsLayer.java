1197a14e55615872350e6dd35b3fe25b66c401d6
Alexey Kulish <alexey.kulish@osmand.net>
2015-11-19 17:43:53 +0300

    Map multi menu fixes

--- OsmAnd/src/net/osmand/plus/views/DownloadedRegionsLayer.java
+++ OsmAnd/src/net/osmand/plus/views/DownloadedRegionsLayer.java
@@ -301,7 +301,11 @@ public class DownloadedRegionsLayer extends OsmandMapLayer implements IContextMe
 			if (region != null && region.getRegionDownloadName() != null) {
 				List<IndexItem> indexItems = app.getDownloadThread().getIndexes().getIndexItems(region);
 				for (IndexItem item : indexItems) {
-					if (item.getType() == DownloadActivityType.NORMAL_FILE) {
+					if (item.getType() == DownloadActivityType.NORMAL_FILE
+							|| item.getType() == DownloadActivityType.ROADS_FILE
+							|| item.getType() == DownloadActivityType.SRTM_COUNTRY_FILE
+							|| item.getType() == DownloadActivityType.HILLSHADE_FILE
+							|| item.getType() == DownloadActivityType.WIKIPEDIA_FILE) {
 						if (app.getDownloadThread().isDownloading(item)) {
 							downloadingObjects.add(o);
 						} else if (item.isOutdated()) {
@@ -505,7 +509,7 @@ public class DownloadedRegionsLayer extends OsmandMapLayer implements IContextMe
 					List<IndexItem> dataItems = new LinkedList<>();
 					IndexItem regularMapItem = null;
 					for (IndexItem item : indexItems) {
-						if (item.isDownloaded()) {
+						if (item.isDownloaded() || app.getDownloadThread().isDownloading(item)) {
 							dataItems.add(item);
 							if (item.getType() == DownloadActivityType.NORMAL_FILE) {
 								regularMapItem = item;
