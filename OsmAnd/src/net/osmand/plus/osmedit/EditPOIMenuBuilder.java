9e8e3159664d55b8ef48b0157dd8acf925f3a599
Alexey Kulish <alexey.kulish@osmand.net>
2015-11-20 16:53:05 +0300

    Added poi/category type icons to amenity and edit poi context menues

--- OsmAnd/src/net/osmand/plus/osmedit/EditPOIMenuBuilder.java
+++ OsmAnd/src/net/osmand/plus/osmedit/EditPOIMenuBuilder.java
@@ -2,9 +2,12 @@ package net.osmand.plus.osmedit;
 
 import android.view.View;
 
+import net.osmand.osm.MapPoiTypes;
+import net.osmand.osm.PoiType;
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.R;
 import net.osmand.plus.mapcontextmenu.MenuBuilder;
+import net.osmand.plus.render.RenderingIcons;
 
 import java.util.Map;
 
@@ -29,12 +32,35 @@ public class EditPOIMenuBuilder extends MenuBuilder {
 			OpenstreetmapPoint point = (OpenstreetmapPoint) osmPoint;
 
 			for (Map.Entry<String, String> e : point.getEntity().getTags().entrySet()) {
-				String text;
 				if (EditPoiData.POI_TYPE_TAG.equals(e.getKey())) {
-					text = e.getValue();
-				} else {
-					text = e.getKey() + "=" + e.getValue();
+					String poiTranslation = e.getValue();
+					Map<String, PoiType> poiTypeMap = MapPoiTypes.getDefault().getAllTranslatedNames(false);
+					PoiType poiType = poiTypeMap.get(poiTranslation.toLowerCase());
+					int resId = 0;
+					if (poiType != null) {
+						String id = null;
+						if (RenderingIcons.containsBigIcon(poiType.getIconKeyName())) {
+							id = poiType.getIconKeyName();
+						} else if (RenderingIcons.containsBigIcon(poiType.getOsmTag() + "_" + poiType.getOsmValue())) {
+							id = poiType.getOsmTag() + "_" + poiType.getOsmValue();
+						}
+						if (id != null) {
+							resId = RenderingIcons.getBigIconResourceId(id);
+						}
+					}
+					if (resId == 0) {
+						resId = R.drawable.ic_action_folder_stroke;
+					}
+					buildRow(view, resId, poiTranslation, 0, false, 0);
+					break;
 				}
+			}
+
+			for (Map.Entry<String, String> e : point.getEntity().getTags().entrySet()) {
+				if (EditPoiData.POI_TYPE_TAG.equals(e.getKey())) {
+					continue;
+				}
+				String text = e.getKey() + "=" + e.getValue();
 				buildRow(view, R.drawable.ic_action_info_dark, text, 0, false, 0);
 			}
 		}
