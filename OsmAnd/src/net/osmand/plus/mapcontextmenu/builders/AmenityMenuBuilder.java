a9558bcec376c659c1f8eaf65a8120081f48006a
Alexey Kulish <alexey.kulish@osmand.net>
2015-12-04 11:32:04 +0300

    Fix poi type icon color. Fix tr translation

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/AmenityMenuBuilder.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/AmenityMenuBuilder.java
@@ -4,7 +4,6 @@ import android.graphics.drawable.Drawable;
 import android.text.Html;
 import android.text.TextUtils;
 import android.text.util.Linkify;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
