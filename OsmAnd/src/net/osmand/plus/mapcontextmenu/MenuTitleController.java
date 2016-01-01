646ada2a28d691fc5ce9d53369065eec757e6b99
sonora <hm.gglmail@gmail.com>
2016-01-01 22:13:33 +0100

    next test

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/MenuTitleController.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/MenuTitleController.java
@@ -69,7 +69,8 @@ public abstract class MenuTitleController {
 		MenuController menuController = getMenuController();
 		if (menuController != null && menuController.needStreetName()) {
 			// Special case for "My Position" context: Display "Looking up address..." status
-			if (typeStr.equals(getMapActivity().getString(R.string.shared_string_my_location)) && searchingAddress) {
+			//if (typeStr.equals(getMapActivity().getString(R.string.shared_string_my_location)) && searchingAddress) {
+			if (typeStr.equals(getMapActivity().getString(R.string.shared_string_my_location))) {
 				return addressNotKnownStr;
 			} else {
 				return streetStr;
