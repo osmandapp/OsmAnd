60830a9977fe9da1a59ab60e80202a8a49e9af4e
sonora <hm.gglmail@gmail.com>
2016-01-01 19:50:33 +0100

    typos

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/MenuTitleController.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/MenuTitleController.java
@@ -69,7 +69,7 @@ public abstract class MenuTitleController {
 		MenuController menuController = getMenuController();
 		if (menuController != null && menuController.needStreetName()) {
 			// Special case for "My Position" context: Display "Looking up address..." status
-			if (typeStr.equals(getMapActivity().getString(R.string.shared_strng_my_location)) && (nameStr == addressNotKnownStr)) {
+			if (typeStr.equals(getMapActivity().getString(R.string.shared_string_my_location)) && (nameStr.equals(addressNotKnownStr))) {
 				return addressNotKnownStr;
 			} else {
 				return streetStr;
