e32e373c1c90a2e72971086e07bcae0c6e6af937
sonora <hm.gglmail@gmail.com>
2016-02-13 00:26:14 +0100

    Try fix Issue 2213

--- OsmAnd/src/net/osmand/plus/routing/AlarmInfo.java
+++ OsmAnd/src/net/osmand/plus/routing/AlarmInfo.java
@@ -119,7 +119,7 @@ public class AlarmInfo implements LocationPoint {
 	
 	public int updateDistanceAndGetPriority(float time, float distance) {
 		if (distance > 1500) {
-			return 0;
+			return Integer.MAX_VALUE;
 		}
 		// 1 level of priorities
 		if (time < 8 || distance < 100 || type == AlarmInfoType.SPEED_LIMIT) {
@@ -129,10 +129,10 @@ public class AlarmInfo implements LocationPoint {
 			return type.getPriority();
 		}
 		// 2nd level
-		if (time < 10 || distance < 150) {
+		if (time < 7 || distance < 100) {
 			return type.getPriority() + AlarmInfoType.MAXIMUM.getPriority();
 		}
-		return 0;
+		return Integer.MAX_VALUE;
 	}
 	
 	@Override
