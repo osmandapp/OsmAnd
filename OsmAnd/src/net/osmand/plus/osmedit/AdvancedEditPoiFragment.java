8f7fe2be86a24fbc1d9c90383426119854853a8a
GaidamakUA <jmazurevich@gmail.com>
2016-03-12 12:24:23 +0200

    Removed workaround, as bug related to it was fixed.

--- OsmAnd/src/net/osmand/plus/osmedit/AdvancedEditPoiFragment.java
+++ OsmAnd/src/net/osmand/plus/osmedit/AdvancedEditPoiFragment.java
@@ -1,12 +1,10 @@
 package net.osmand.plus.osmedit;
 
-import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -57,12 +55,6 @@ public class AdvancedEditPoiFragment extends BaseOsmAndFragment
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View view = inflater.inflate(R.layout.fragment_edit_poi_advanced, container, false);
-
-		Display display = getActivity().getWindowManager().getDefaultDisplay();
-		Point size = new Point();
-		display.getSize(size);
-		int height = size.y;
-		view.findViewById(R.id.screenFiller).setMinimumHeight(height);
 
 		deleteDrawable = getPaintedContentIcon(R.drawable.ic_action_remove_dark,
 				getActivity().getResources().getColor(R.color.dash_search_icon_dark));
