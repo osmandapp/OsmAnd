ace24289752c7d11991606971da0b5ffb825bcc3
Alexey Kulish <alexey.kulish@osmand.net>
2016-04-07 12:38:46 +0300

    Fix opening hours parser test

--- OsmAnd-java/src/net/osmand/util/OpeningHoursParser.java
+++ OsmAnd-java/src/net/osmand/util/OpeningHoursParser.java
@@ -1008,7 +1008,7 @@ public class OpeningHoursParser {
 		String assembledString = hours.toStringNoMonths();
 		boolean isCorrect = assembledString.equalsIgnoreCase(timeString);
 		System.out.printf("  %sok: Expected: \"%s\" got: \"%s\"\n",
-				(isCorrect ? "NOT " : ""), timeString, assembledString);
+				(!isCorrect ? "NOT " : ""), timeString, assembledString);
 		if (!isCorrect) {
 			throw new IllegalArgumentException("BUG!!!");
 		}
