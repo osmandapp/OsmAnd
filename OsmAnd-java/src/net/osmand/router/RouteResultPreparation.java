d31b6288f07a7c641282a4ffae9ad79207ec8107
Victor Shcherb <victor.shcherb@gmail.com>
2016-04-12 22:20:01 +0200

    Fix #2452

--- OsmAnd-java/src/net/osmand/router/RouteResultPreparation.java
+++ OsmAnd-java/src/net/osmand/router/RouteResultPreparation.java
@@ -440,11 +440,11 @@ public class RouteResultPreparation {
 				}
 				if (ut) {
 					tnext.setSkipToSpeak(true);
-					if (tl && TurnType.isLeftTurnNoUTurn(t.getValue())) {
+					if (tl && TurnType.isLeftTurnNoUTurn(tnext.getValue())) {
 						TurnType tt = TurnType.valueOf(TurnType.TU, false);
 						tt.setLanes(t.getLanes());
 						return tt;
-					} else if (tr && TurnType.isRightTurnNoUTurn(t.getValue())) {
+					} else if (tr && TurnType.isRightTurnNoUTurn(tnext.getValue())) {
 						TurnType tt = TurnType.valueOf(TurnType.TU, true);
 						tt.setLanes(t.getLanes());
 						return tt;
