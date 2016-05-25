cde96e5848ff5adcb82c9061a4b1a27d63eda054
Roman Inflianskas <infroma@gmail.com>
2016-05-25 11:22:40 +0300

    BinaryInspector: fix section order

--- OsmAnd-java/src/net/osmand/binary/BinaryInspector.java
+++ OsmAnd-java/src/net/osmand/binary/BinaryInspector.java
@@ -631,7 +631,7 @@ public class BinaryInspector {
 			for (City c : cities) {
 				int size = index.preloadStreets(c, null);
 				List<Street> streets = new ArrayList<Street>(c.getStreets());
-				String cityDescription = (j == BinaryMapAddressReaderAdapter.POSTCODES_TYPE ?
+				String cityDescription = (type == BinaryMapAddressReaderAdapter.POSTCODES_TYPE ?
 						MessageFormat.format("\t\t''{0}'' {1,number,#} street(s) size {2,number,#} bytes", c.getName(verbose.lang), streets.size(), size) :
 						MessageFormat.format("\t\t''{0}'' [{1,number,#}], {2,number,#} street(s) size {3,number,#} bytes", c.getName(verbose.lang), c.getId(), streets.size(), size));
 				print(cityDescription);
