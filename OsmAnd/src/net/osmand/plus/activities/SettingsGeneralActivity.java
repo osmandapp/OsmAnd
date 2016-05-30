e8f31d75f84ae137130fd010caaf5dd5f411cede
sonora <hm.gglmail@gmail.com>
2016-05-30 10:49:06 +0200

    fix order

--- OsmAnd/src/net/osmand/plus/activities/SettingsGeneralActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/SettingsGeneralActivity.java
@@ -264,7 +264,7 @@ public class SettingsGeneralActivity extends SettingsBaseActivity implements OnR
 				getString(R.string.lang_cy) + incompleteSuffix,};
 		String[] valuesPl = ConfigureMapMenu.getSortedMapNamesIds(this, entrieValues, entrieValues);
 		String[] idsPl = ConfigureMapMenu.getSortedMapNamesIds(this, entries, valuesPl);
-		registerListPreference(settings.PREFERRED_LOCALE, screen, idsPl, valuesPl);
+		registerListPreference(settings.PREFERRED_LOCALE, screen, valuesPl, idsPl);
 
 		// Display "Device language" in Latin for all non-en languages
 		if (!getResources().getString(R.string.preferred_locale).equals(getResources().getString(R.string.preferred_locale_no_translate))) {
--- OsmAnd/src/net/osmand/plus/dialogs/ConfigureMapMenu.java
+++ OsmAnd/src/net/osmand/plus/dialogs/ConfigureMapMenu.java
@@ -534,7 +534,6 @@ public class ConfigureMapMenu {
 	public static String[] mapNamesIds = new String[]{"", "en", "als", "af", "ar", "az", "be", "bg", "bn", "bpy", "br", "bs", "ca", "ceb", "cs", "cy", "da", "de", "el", "eo", "et", "es", "eu", "fa", "fi", "fr", "fy", "ga", "gl", "he", "hi", "hr", "ht", "hu", "hy", "id", "is", "it", "ja", "ka", "ko", "ku", "la", "lb", "lt", "lv", "mk", "ml", "mr", "ms", "nds", "new", "nl", "nn", "no", "nv", "os", "pl", "pms", "pt", "ro", "ru", "sh", "sc", "sk", "sl", "sq", "sr", "sv", "sw", "ta", "te", "th", "tl", "tr", "uk", "vi", "vo", "zh"};
 
 	public static String[] getSortedMapNamesIds(Context ctx, String[] ids, String[] values) {
-		//String[] vls = getMapNamesValues(ctx, mapNamesIds);
 		final Map<String, String> mp = new HashMap<>();
 		for (int i = 0; i < ids.length; i++) {
 			mp.put(ids[i], values[i]);
