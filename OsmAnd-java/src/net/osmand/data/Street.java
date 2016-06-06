2ae4229ed9aa4e4d1470ee1347f5d3fce890cd0c
Roman Inflianskas <infroma@gmail.com>
2016-06-06 13:16:23 +0300

    Formatting

--- OsmAnd-java/src/net/osmand/data/Street.java
+++ OsmAnd-java/src/net/osmand/data/Street.java
@@ -9,61 +9,59 @@ import net.osmand.util.Algorithms;
 
 
 public class Street extends MapObject {
-	
-	protected List<Building> buildings = new ArrayList<Building>(); 
+
+	protected List<Building> buildings = new ArrayList<Building>();
 	protected List<Street> intersectedStreets = null;
 	protected final City city;
 
 	public Street(City city) {
 		this.city = city;
 	}
-	
-	public void addBuilding(Building building){
+
+	public void addBuilding(Building building) {
 		buildings.add(building);
 	}
-	
+
 	public List<Street> getIntersectedStreets() {
-		if(intersectedStreets == null){
+		if (intersectedStreets == null) {
 			return Collections.emptyList();
 		}
 		return intersectedStreets;
 	}
-	
-	public void addIntersectedStreet(Street s){
-		if(intersectedStreets == null) {
+
+	public void addIntersectedStreet(Street s) {
+		if (intersectedStreets == null) {
 			intersectedStreets = new ArrayList<Street>();
 		}
 		intersectedStreets.add(s);
 	}
-	
-	public void addBuildingCheckById(Building building){
-		for(Building b : buildings) {
-			if(b.getId().longValue() == building.getId().longValue()){
+
+	public void addBuildingCheckById(Building building) {
+		for (Building b : buildings) {
+			if (b.getId().longValue() == building.getId().longValue()) {
 				return;
 			}
 		}
 		buildings.add(building);
 	}
-	
+
 	public List<Building> getBuildings() {
 		return buildings;
 	}
-	
-	
-	
+
 	public City getCity() {
 		return city;
 	}
-	
-	public void sortBuildings(){
-		Collections.sort(buildings, new Comparator<Building>(){
+
+	public void sortBuildings() {
+		Collections.sort(buildings, new Comparator<Building>() {
 			@Override
 			public int compare(Building o1, Building o2) {
 				String s1 = o1.getName();
 				String s2 = o2.getName();
 				int i1 = Algorithms.extractFirstIntegerNumber(s1);
 				int i2 = Algorithms.extractFirstIntegerNumber(s2);
-				if(i1 == i2) {
+				if (i1 == i2) {
 					String t1 = Algorithms.extractIntegerSuffix(s1);
 					String t2 = Algorithms.extractIntegerSuffix(s2);
 					return t1.compareTo(t2);
@@ -72,10 +70,9 @@ public class Street extends MapObject {
 			}
 		});
 	}
-	
+
 	/// GENERATION
-	
-	
+
 	public void mergeWith(Street street) {
 		buildings.addAll(street.getBuildings());
 		copyNames(street);
@@ -89,6 +86,5 @@ public class Street extends MapObject {
 		}
 		return nm;
 	}
-
 
 }
