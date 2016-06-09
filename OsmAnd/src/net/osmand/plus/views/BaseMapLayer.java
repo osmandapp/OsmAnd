d1816150d1551971f259ddcb47972e2f982a7471
GaidamakUA <jmazurevich@gmail.com>
2016-06-09 17:05:05 +0300

    Hardcoding maximum zoom for gestures to 21. fixes #2648

--- OsmAnd/src/net/osmand/plus/views/BaseMapLayer.java
+++ OsmAnd/src/net/osmand/plus/views/BaseMapLayer.java
@@ -2,11 +2,12 @@ package net.osmand.plus.views;
 
 public abstract class BaseMapLayer extends OsmandMapLayer {
 
+	public static final int DEFAULT_MAX_ZOOM = 21;
 	private int alpha = 255;
 	protected int warningToSwitchMapShown = 0;
 
 	public int getMaximumShownMapZoom(){
-		return 21;
+		return DEFAULT_MAX_ZOOM;
 	}
 	
 	public int getMinimumShownMapZoom(){
