25c8979ba8541117bbdaa76ea7097f539536fa4a
Victor Shcherb <victor.shcherb@gmail.com>
2016-06-13 00:36:47 +0200

    Fix reverse geocoding for pedestrian streets #2655 (make custom profile geocoding)

--- OsmAnd/src/net/osmand/plus/CurrentPositionHelper.java
+++ OsmAnd/src/net/osmand/plus/CurrentPositionHelper.java
@@ -38,15 +38,15 @@ public class CurrentPositionHelper {
 
 	private void initCtx(OsmandApplication app) {
 		am = app.getSettings().getApplicationMode();
-		GeneralRouterProfile p ;
+		String p ;
 		if (am.isDerivedRoutingFrom(ApplicationMode.BICYCLE)) {
-			p = GeneralRouterProfile.BICYCLE;
+			p = GeneralRouterProfile.BICYCLE.name().toLowerCase();
 		} else if (am.isDerivedRoutingFrom(ApplicationMode.PEDESTRIAN)) {
-			p = GeneralRouterProfile.PEDESTRIAN;
+			p = GeneralRouterProfile.PEDESTRIAN.name().toLowerCase();
 		} else if (am.isDerivedRoutingFrom(ApplicationMode.CAR)) {
-			p = GeneralRouterProfile.CAR;
+			p = GeneralRouterProfile.CAR.name().toLowerCase();
 		} else {
-			p = GeneralRouterProfile.CAR;
+			p = "geocoding";
 		}
 		BinaryMapIndexReader[] rs = new BinaryMapIndexReader[app.getResourceManager().getAddressRepositories().size()];
 		if (rs.length > 0) {
@@ -54,10 +54,10 @@ public class CurrentPositionHelper {
 			for (RegionAddressRepository rep : app.getResourceManager().getAddressRepositories()) {
 				rs[i++] = rep.getFile();
 			}
-			RoutingConfiguration cfg = app.getDefaultRoutingConfig().build(p.name().toLowerCase(), 10,
+			RoutingConfiguration cfg = app.getDefaultRoutingConfig().build(p, 10,
 					new HashMap<String, String>());
 			ctx = new RoutePlannerFrontEnd(false).buildRoutingContext(cfg, null, rs);
-			RoutingConfiguration defCfg = app.getDefaultRoutingConfig().build(GeneralRouterProfile.CAR.name().toLowerCase(), 10,
+			RoutingConfiguration defCfg = app.getDefaultRoutingConfig().build("geocoding", 10,
 					new HashMap<String, String>());
 			defCtx = new RoutePlannerFrontEnd(false).buildRoutingContext(defCfg, null, rs);
 		} else {
