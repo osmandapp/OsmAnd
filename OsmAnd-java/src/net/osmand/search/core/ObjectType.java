61e2d908ec76dc930569bc05115b0cc919aab35b
Victor Shcherb <victor.shcherb@gmail.com>
2016-07-15 09:49:11 +0200

    Update search location

--- OsmAnd-java/src/net/osmand/search/core/ObjectType.java
+++ OsmAnd-java/src/net/osmand/search/core/ObjectType.java
@@ -1,10 +1,16 @@
 package net.osmand.search.core;
 
 public enum ObjectType {
-	CITY(true), VILLAGE(true), POSTCODE(true), STREET(true), HOUSE(true),
-	STREET_INTERSECTION(true), POI_TYPE(false), POI(true), LOCATION(true), PARTIAL_LOCATION(false), FAVORITE(true),
-	SEARCH_API_FINISHED(false),
-	REGION(true), RECENT_OBJ(true), WPT(true), UNKNOWN_NAME_FILTER(false);
+	// ADDRESS
+	CITY(true), VILLAGE(true), POSTCODE(true), STREET(true), HOUSE(true),STREET_INTERSECTION(true),
+	// POI
+	POI_TYPE(false), POI(true),
+	// LOCATION
+	LOCATION(true), PARTIAL_LOCATION(false),
+	// UI OBJECTS
+	FAVORITE(true), WPT(true), RECENT_OBJ(true), 
+	REGION(true), 
+	SEARCH_API_FINISHED(false), UNKNOWN_NAME_FILTER(false);
 	private boolean hasLocation;
 	private ObjectType(boolean location) {
 		this.hasLocation = location;
