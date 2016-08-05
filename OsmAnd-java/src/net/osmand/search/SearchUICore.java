1106994c014f2c9f9268ab1c20d490e211b092cc
Victor Shcherb <victor.shcherb@gmail.com>
2016-08-05 14:33:29 +0300

    Update stable search restuls

--- OsmAnd-java/src/net/osmand/search/SearchUICore.java
+++ OsmAnd-java/src/net/osmand/search/SearchUICore.java
@@ -1,4 +1,14 @@
 package net.osmand.search;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import net.osmand.Collator;
 import net.osmand.OsmAndCollator;
@@ -25,23 +35,12 @@ import net.osmand.util.MapUtils;
 
 import org.apache.commons.logging.Log;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class SearchUICore {
 
 	private static final int TIMEOUT_BETWEEN_CHARS = 200;
 	private static final Log LOG = PlatformUtil.getLog(SearchUICore.class); 
 	private SearchPhrase phrase;
-	private SearchResultCollection currentSearchResult = new SearchResultCollection(); 
+	private SearchResultCollection  currentSearchResult;
 	
 	private ThreadPoolExecutor singleThreadedExecutor;
 	private LinkedBlockingQueue<Runnable> taskQueue;
@@ -52,7 +51,6 @@ public class SearchUICore {
 	List<SearchCoreAPI> apis = new ArrayList<>();
 	private SearchSettings searchSettings;
 	private MapPoiTypes poiTypes;
-	private Collator collator;
 	
 	
 	public SearchUICore(MapPoiTypes poiTypes, String locale) {
@@ -60,30 +58,145 @@ public class SearchUICore {
 		taskQueue = new LinkedBlockingQueue<Runnable>();
 		searchSettings = new SearchSettings(new ArrayList<BinaryMapIndexReader>());
 		searchSettings = searchSettings.setLang(locale);
-		phrase = new SearchPhrase(searchSettings);
+		phrase = new SearchPhrase(searchSettings, OsmAndCollator.primaryCollator());
+		currentSearchResult = new SearchResultCollection(phrase);
 		singleThreadedExecutor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, taskQueue);
-		collator = OsmAndCollator.primaryCollator();
 	}
 	
 	public static class SearchResultCollection {
 		private List<SearchResult> searchResults;
 		private SearchPhrase phrase;
 		
-		public SearchResultCollection(List<SearchResult> requestResults, SearchPhrase phrase) {
-			searchResults = requestResults;
+		public SearchResultCollection(SearchPhrase phrase) {
+			searchResults = new ArrayList<>();
 			this.phrase = phrase;
 		}
 		
-		public SearchResultCollection() {
-			searchResults = new ArrayList<>();
+		public SearchResultCollection combineWithCollection(SearchResultCollection collection, boolean resort, boolean removeDuplicates) {
+			SearchResultCollection src = new SearchResultCollection(phrase);
+			src.addSearchResults(searchResults, false, false);
+			src.addSearchResults(collection.searchResults, resort, removeDuplicates);
+			return src;
 		}
-
+		 
+		
+		
+		public void addSearchResults(List<SearchResult> sr, boolean resortAll, boolean removeDuplicates) {
+			if(resortAll) {
+				this.searchResults.addAll(sr);
+				sortSearchResults();
+				if(removeDuplicates) {
+					filterSearchDuplicateResults();
+				}
+			} else {
+				if(removeDuplicates) {
+					ArrayList<SearchResult> addedResults = new ArrayList<>(sr);
+					SearchResultComparator cmp = new SearchResultComparator(phrase);
+					Collections.sort(addedResults, cmp);
+					filterSearchDuplicateResults(addedResults);
+					int i = 0; 
+					int j = 0;
+					while(j < addedResults.size()) {
+						SearchResult addedResult = addedResults.get(j);
+						if(i >= searchResults.size()) {
+							if(searchResults.size() == 0 || 
+									!sameSearchResult(addedResult, searchResults.get(searchResults.size() - 1))) {
+								searchResults.add(addedResult);
+							}
+							j++;
+							continue;
+						}
+						SearchResult existingResult = searchResults.get(i);
+						if(sameSearchResult(addedResult, existingResult)) {
+							j++;
+							continue;
+						}
+						int compare = cmp.compare(existingResult, addedResult);
+						if(compare == 0) {
+							// existingResult == addedResult
+							j++;
+						} else if(compare > 0) {
+							// existingResult > addedResult
+							this.searchResults.add(addedResults.get(j));
+							j++;
+						} else {
+							// existingResult < addedResult
+							i++;
+						}
+					}
+					
+				}
+			}
+			
+		}
+		
 		public List<SearchResult> getCurrentSearchResults() {
-			return searchResults;
+			return Collections.unmodifiableList(searchResults);
 		}
 		
 		public SearchPhrase getPhrase() {
 			return phrase;
+		}
+
+		public void sortSearchResults() {
+			Collections.sort(searchResults, new SearchResultComparator(phrase));			
+		}
+
+		public void filterSearchDuplicateResults() {
+			filterSearchDuplicateResults(searchResults);
+		}
+		
+		private void filterSearchDuplicateResults(List<SearchResult> lst) {
+			Iterator<SearchResult> it = lst.iterator();
+			SearchResult found = null;
+			while(it.hasNext()) {
+				SearchResult r = it.next();
+				if(found != null && sameSearchResult(found, r)) {
+					it.remove();
+				} else {
+					found = r;
+				}
+			}			
+		}
+		
+		
+		
+		public boolean sameSearchResult(SearchResult r1, SearchResult r2) {
+			if(r1.location != null && r2.location != null) {
+				Amenity a1 = null;
+				if(r1.object instanceof Amenity) {
+					a1 = (Amenity) r1.object;
+				}
+				Amenity a2 = null;
+				if(r2.object instanceof Amenity) {
+					a2 = (Amenity) r2.object;
+				}
+				if (r1.localeName.equals(r2.localeName)) {
+					double similarityRadius = 30;
+					if (a1 != null && a2 != null) {
+						// here 2 points are amenity
+						String type1 = a1.getType().getKeyName();
+						String type2 = a2.getType().getKeyName();
+						String subType1 = a1.getSubType();
+						String subType2 = a2.getSubType();
+						if (!type1.equals(type2)) {
+							return false;
+						}
+						if (type1.equals("natural")) {
+							similarityRadius = 10000;
+						} else if (subType1.equals(subType2)) {
+							if (subType1.contains("cn_ref") || subType1.contains("wn_ref")
+									|| (subType1.startsWith("route_hiking_") && subType1.endsWith("n_poi"))) {
+								similarityRadius = 10000;
+							}
+						}
+					} else if(ObjectType.isAddress(r1.objectType) && ObjectType.isAddress(r2.objectType)) {
+						similarityRadius = 100;
+					}
+					return MapUtils.getDistance(r1.location, r2.location) < similarityRadius;
+				}
+			}
+			return false;
 		}
 	}
 	
@@ -111,7 +224,6 @@ public class SearchUICore {
 	
 	public <T extends SearchCoreAPI> SearchResultCollection shallowSearch(Class<T> cl,
 			String text, final ResultMatcher<SearchResult> matcher) throws IOException {
-		SearchResultCollection quickRes = new SearchResultCollection();
 		T api = getApiByClass(cl);
 		if(api != null) {
 			SearchPhrase sphrase = this.phrase.generateNewPhrase(text, searchSettings);
@@ -120,14 +232,13 @@ public class SearchUICore {
 			SearchResultMatcher rm = new SearchResultMatcher(matcher, ai.get(), ai, totalLimit);
 			api.search(sphrase, rm);
 			
-			sortSearchResults(sphrase, rm.getRequestResults());
-			filterSearchDuplicateResults(sphrase, rm.getRequestResults());
-			
-			LOG.info(">> Shallow Search phrase " + phrase + " " + rm.getRequestResults().size());
-			return new SearchResultCollection(rm.getRequestResults(),
+			SearchResultCollection collection = new SearchResultCollection(
 					sphrase);
+			collection.addSearchResults(rm.getRequestResults(), true, true);
+			LOG.info(">> Shallow Search phrase " + phrase + " " + rm.getRequestResults().size());
+			return collection;
 		}
-		return quickRes;
+		return null;
 	}
 	
 	public void init() {
@@ -201,11 +312,10 @@ public class SearchUICore {
 	}
 
 	public SearchResultCollection search(final String text, final ResultMatcher<SearchResult> matcher) {
-		SearchResultCollection quickRes = new SearchResultCollection();
 		final int request = requestNumber.incrementAndGet();
 		final SearchPhrase phrase = this.phrase.generateNewPhrase(text, searchSettings);
 		this.phrase = phrase;
-		quickRes.phrase = phrase;
+		SearchResultCollection quickRes = new SearchResultCollection(phrase);
 		filterCurrentResults(quickRes.searchResults, phrase);
 		LOG.info("> Search phrase " + phrase + " " + quickRes.searchResults.size());
 		singleThreadedExecutor.submit(new Runnable() {
@@ -222,12 +332,11 @@ public class SearchUICore {
 					}
 					searchInBackground(phrase, rm);
 					if (!rm.isCancelled()) {
-						sortSearchResults(phrase, rm.getRequestResults());
-						filterSearchDuplicateResults(phrase, rm.getRequestResults());
 						
-						LOG.info(">> Search phrase " + phrase + " " + rm.getRequestResults().size());
-						SearchResultCollection collection = new SearchResultCollection(rm.getRequestResults(),
+						SearchResultCollection collection = new SearchResultCollection(
 								phrase);
+						collection.addSearchResults(rm.getRequestResults(), true, true);
+						LOG.info(">> Search phrase " + phrase + " " + rm.getRequestResults().size());
 						currentSearchResult = collection;
 						if (onResultsComplete != null) {
 							onResultsComplete.run();
@@ -267,6 +376,7 @@ public class SearchUICore {
 			}
 			try {
 				api.search(phrase, matcher);
+				
 				matcher.apiSearchFinished(api, phrase);
 			} catch (Throwable e) {
 				e.printStackTrace();
@@ -284,92 +394,10 @@ public class SearchUICore {
 		phrase.sortFiles();
 	}
 	
-	public boolean sameSearchResult(SearchResult r1, SearchResult r2) {
-		if(r1.location != null && r2.location != null) {
-			Amenity a1 = null;
-			if(r1.object instanceof Amenity) {
-				a1 = (Amenity) r1.object;
-			}
-			Amenity a2 = null;
-			if(r2.object instanceof Amenity) {
-				a2 = (Amenity) r2.object;
-			}
-			if (r1.localeName.equals(r2.localeName)) {
-				double similarityRadius = 30;
-				if (a1 != null && a2 != null) {
-					// here 2 points are amenity
-					String type1 = a1.getType().getKeyName();
-					String type2 = a2.getType().getKeyName();
-					String subType1 = a1.getSubType();
-					String subType2 = a2.getSubType();
-					if (!type1.equals(type2)) {
-						return false;
-					}
-					if (type1.equals("natural")) {
-						similarityRadius = 10000;
-					} else if (subType1.equals(subType2)) {
-						if (subType1.contains("cn_ref") || subType1.contains("wn_ref")
-								|| (subType1.startsWith("route_hiking_") && subType1.endsWith("n_poi"))) {
-							similarityRadius = 10000;
-						}
-					}
-				} else if(ObjectType.isAddress(r1.objectType) && ObjectType.isAddress(r2.objectType)) {
-					similarityRadius = 100;
-				}
-				return MapUtils.getDistance(r1.location, r2.location) < similarityRadius;
-			}
-		}
-		return false;
-	}
-	
-	public void filterSearchDuplicateResults(SearchPhrase sp, List<SearchResult> searchResults) {
-		Iterator<SearchResult> it = searchResults.iterator();
-		SearchResult found = null;
-		while(it.hasNext()) {
-			SearchResult r = it.next();
-			if(found != null && sameSearchResult(found, r)) {
-				it.remove();
-			} else {
-				found = r;
-			}
-		}
-	}
 	
 	
-	public void sortSearchResults(SearchPhrase sp, List<SearchResult> searchResults) {
-		// sort SearchResult by 1. searchDistance 2. Name
-		final LatLon loc = sp.getLastTokenLocation();
-		Collections.sort(searchResults, new Comparator<SearchResult>() {
-
-			@Override
-			public int compare(SearchResult o1, SearchResult o2) {
-				if(o1.getFoundWordCount() != o2.getFoundWordCount()) {
-					return -Algorithms.compare(o1.getFoundWordCount(), o2.getFoundWordCount());
-				}
-				double s1 = o1.getSearchDistance(loc);
-				double s2 = o2.getSearchDistance(loc);
-				int cmp = Double.compare(s1, s2);
-				if(cmp != 0) {
-					return cmp;
-				}
-				int st1 = Algorithms.extractFirstIntegerNumber(o1.localeName);
-				int st2 = Algorithms.extractFirstIntegerNumber(o2.localeName);
-				if(st1 != st2) {
-					return Algorithms.compare(st1, st2);
-				}
-				cmp = collator.compare(o1.localeName, o2.localeName);
-				if(cmp != 0) {
-					return cmp;
-				}
-				s1 = o1.getSearchDistance(loc, 1);
-				s2 = o2.getSearchDistance(loc, 1);
-				return Double.compare(s1, s2);
-			}
-		});
-	}
 	
-	public static class SearchResultMatcher
-	implements  ResultMatcher<SearchResult>{
+	public static class SearchResultMatcher implements  ResultMatcher<SearchResult>{
 		private final List<SearchResult> requestResults = new ArrayList<>();
 		private final ResultMatcher<SearchResult> matcher;
 		private final int request;
@@ -439,5 +467,43 @@ public class SearchUICore {
 			boolean cancelled = request != requestNumber.get();
 			return cancelled || (matcher != null && matcher.isCancelled());
 		}		
-	}	
+	}
+	
+	public static class SearchResultComparator implements Comparator<SearchResult> {
+		private SearchPhrase sp;
+		private Collator collator;
+		private LatLon loc;
+		
+		public SearchResultComparator(SearchPhrase sp) {
+			this.sp = sp;
+			this.collator = sp.getCollator();
+			loc = sp.getLastTokenLocation();
+		}
+
+		@Override
+		public int compare(SearchResult o1, SearchResult o2) {
+			if(o1.getFoundWordCount() != o2.getFoundWordCount()) {
+				return -Algorithms.compare(o1.getFoundWordCount(), o2.getFoundWordCount());
+			}
+			double s1 = o1.getSearchDistance(loc);
+			double s2 = o2.getSearchDistance(loc);
+			int cmp = Double.compare(s1, s2);
+			if(cmp != 0) {
+				return cmp;
+			}
+			int st1 = Algorithms.extractFirstIntegerNumber(o1.localeName);
+			int st2 = Algorithms.extractFirstIntegerNumber(o2.localeName);
+			if(st1 != st2) {
+				return Algorithms.compare(st1, st2);
+			}
+			cmp = collator.compare(o1.localeName, o2.localeName);
+			if(cmp != 0) {
+				return cmp;
+			}
+			s1 = o1.getSearchDistance(loc, 1);
+			s2 = o2.getSearchDistance(loc, 1);
+			return Double.compare(s1, s2);
+		}
+		
+	}
 }
