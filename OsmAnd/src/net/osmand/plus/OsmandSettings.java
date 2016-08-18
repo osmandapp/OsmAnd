cb5aa0dee3dbd39b7e68e932705cf78331a63451
sonora <hm.gglmail@gmail.com>
2016-08-18 20:37:13 +0200

    Let's not mix unit systems In the US, "miles and meters" is a no-go, almost like a "vegetarian steakhouse" ... :) I am surprised we have requests from the UK to mix 2 different measurement systems, and I have left this as default for the UK, although I am not sure this really makes sense other than documenting how 2 different systems got mixed up in our everyday lives :)

--- OsmAnd/src/net/osmand/plus/OsmandSettings.java
+++ OsmAnd/src/net/osmand/plus/OsmandSettings.java
@@ -2882,8 +2882,8 @@ public class OsmandSettings {
 
 	public enum MetricsConstants {
 		KILOMETERS_AND_METERS(R.string.si_km_m, "km-m"),
-		MILES_AND_METERS(R.string.si_mi_meters, "mi-m"),
 		MILES_AND_FOOTS(R.string.si_mi_foots, "mi-f"),
+		MILES_AND_METERS(R.string.si_mi_meters, "mi-m"),
 		NAUTICAL_MILES(R.string.si_nm, "nm"),
 		MILES_AND_YARDS(R.string.si_mi_yard, "mi-y");
 
@@ -2929,7 +2929,7 @@ public class OsmandSettings {
 	public enum DrivingRegion {
 
 		EUROPE_ASIA(R.string.driving_region_europe_asia, MetricsConstants.KILOMETERS_AND_METERS, false, false),
-		US(R.string.driving_region_us, MetricsConstants.MILES_AND_METERS, false, true),
+		US(R.string.driving_region_us, MetricsConstants.MILES_AND_FOOTS, false, true),
 		CANADA(R.string.driving_region_canada, MetricsConstants.KILOMETERS_AND_METERS, false, true),
 		UK_AND_OTHERS(R.string.driving_region_uk, MetricsConstants.MILES_AND_METERS, true, false),
 		JAPAN(R.string.driving_region_japan, MetricsConstants.KILOMETERS_AND_METERS, true, false);
