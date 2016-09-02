3d13b0ba9d8a51c53cc9ed65f61292900e606564
sonora <hm.gglmail@gmail.com>
2016-09-02 19:18:48 +0200

    Fix upload dialog text

--- OsmAnd/src/net/osmand/plus/osmedit/DashOsmEditsFragment.java
+++ OsmAnd/src/net/osmand/plus/osmedit/DashOsmEditsFragment.java
@@ -141,7 +141,7 @@ public class DashOsmEditsFragment extends DashBaseFragment
 	// TODO: 9/7/15 Redesign osm notes.
 	private void uploadItem(final OsmPoint point) {
 		AlertDialog.Builder b = new AlertDialog.Builder(getActivity());
-		b.setMessage(getString(R.string.local_osm_changes_upload_all_confirm));
+		b.setMessage(getString(R.string.local_osm_changes_upload_all_confirm), 1);
 		b.setPositiveButton(R.string.shared_string_yes, new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
