5e94c5ddd0d812b5573a390f0d9fd43325e3a62c
Alexey Kulish <alexey.kulish@osmand.net>
2016-09-15 16:54:56 +0300

    Revert SQLiteAPIImpl changes

--- OsmAnd/src/net/osmand/plus/api/SQLiteAPIImpl.java
+++ OsmAnd/src/net/osmand/plus/api/SQLiteAPIImpl.java
@@ -14,9 +14,8 @@ public class SQLiteAPIImpl implements SQLiteAPI {
 
 	@Override
 	public SQLiteConnection getOrCreateDatabase(String name, boolean readOnly) {
-		android.database.sqlite.SQLiteDatabase db = SQLiteDatabase.openDatabase(app.getDatabasePath(name).getAbsolutePath(), null,
-				(readOnly? SQLiteDatabase.OPEN_READONLY : (SQLiteDatabase.OPEN_READWRITE | SQLiteDatabase.ENABLE_WRITE_AHEAD_LOGGING))
-				| SQLiteDatabase.CREATE_IF_NECESSARY);
+		android.database.sqlite.SQLiteDatabase db = app.openOrCreateDatabase(name,
+				readOnly ? SQLiteDatabase.OPEN_READONLY : (SQLiteDatabase.OPEN_READWRITE | SQLiteDatabase.ENABLE_WRITE_AHEAD_LOGGING), null);
 		if(db == null) {
 			return null;
 		}
