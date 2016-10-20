25e1ff5b400b4238b0410564583ea29c817e2a5f
Alexey Kulish <alexey.kulish@osmand.net>
2016-10-20 15:34:39 +0300

    Fix crash while selecting poi on map

--- OsmAnd/src/net/osmand/plus/views/ContextMenuLayer.java
+++ OsmAnd/src/net/osmand/plus/views/ContextMenuLayer.java
@@ -390,7 +390,7 @@ public class ContextMenuLayer extends OsmandMapLayer {
 			MapRenderRepositories maps = activity.getMyApplication().getResourceManager().getRenderer();
 			RenderingContext rc = maps.getVisibleRenderingContext();
 			RenderedObject[] renderedObjects = null;
-			if (rc.zoom == tileBox.getZoom()) {
+			if (rc != null && rc.zoom == tileBox.getZoom()) {
 				double lat = MapUtils.get31LatitudeY((int) (rc.topY * rc.tileDivisor));
 				double lon = MapUtils.get31LongitudeX((int) (rc.leftX * rc.tileDivisor));
 				float x = tileBox.getPixXFromLatLon(lat, lon);
