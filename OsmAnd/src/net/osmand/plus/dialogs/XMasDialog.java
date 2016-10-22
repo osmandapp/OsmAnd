af3b7421e111450b6a99325b1f552fee99892051
Victor Shcherb <victor.shcherb@gmail.com>
2016-10-22 13:25:34 +0200

    Change to kmh

--- OsmAnd/src/net/osmand/plus/dialogs/XMasDialog.java
+++ OsmAnd/src/net/osmand/plus/dialogs/XMasDialog.java
@@ -16,7 +16,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class OtherDialogs {
+public class XMasDialog {
 
 	private static boolean XmasDialogWasProcessed = false;
 
