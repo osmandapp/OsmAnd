421cac7b8c70ae74b68a910d13312cec4ad8348b
Alexey Kulish <alexey.kulish@osmand.net>
2016-10-25 22:46:36 +0300

    OsMo notifiation done

--- OsmAnd/src/net/osmand/plus/notifications/OsMoNotification.java
+++ OsmAnd/src/net/osmand/plus/notifications/OsMoNotification.java
@@ -1,7 +1,10 @@
 package net.osmand.plus.notifications;
 
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.BigTextStyle;
 import android.support.v4.app.NotificationCompat.Builder;
@@ -10,18 +13,82 @@ import net.osmand.plus.NavigationService;
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.OsmandPlugin;
 import net.osmand.plus.R;
-import net.osmand.plus.Version;
-import net.osmand.plus.activities.MapActivity;
 import net.osmand.plus.osmo.OsMoPlugin;
 
-import static android.support.v7.app.NotificationCompat.*;
-import static net.osmand.plus.NavigationService.USED_BY_GPX;
 import static net.osmand.plus.NavigationService.USED_BY_LIVE;
 
 public class OsMoNotification extends OsmandNotification {
 
+	public final static String OSMAND_START_OSMO_SERVICE_ACTION = "OSMAND_START_OSMO_SERVICE_ACTION";
+	public final static String OSMAND_STOP_OSMO_SERVICE_ACTION = "OSMAND_STOP_OSMO_SERVICE_ACTION";
+	public final static String OSMAND_START_SHARE_LOCATION_ACTION = "OSMAND_START_SHARE_LOCATION_ACTION";
+	public final static String OSMAND_STOP_SHARE_LOCATION_ACTION = "OSMAND_STOP_SHARE_LOCATION_ACTION";
+
 	public OsMoNotification(OsmandApplication app) {
 		super(app);
+	}
+
+	@Override
+	public void init() {
+		app.registerReceiver(new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				OsMoPlugin osMoPlugin = OsmandPlugin.getEnabledPlugin(OsMoPlugin.class);
+				if (osMoPlugin != null) {
+					osMoPlugin.getService().connect(true);
+					osMoPlugin.refreshMap();
+				}
+			}
+		}, new IntentFilter(OSMAND_START_OSMO_SERVICE_ACTION));
+
+		app.registerReceiver(new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				OsMoPlugin osMoPlugin = OsmandPlugin.getEnabledPlugin(OsMoPlugin.class);
+				if (osMoPlugin != null) {
+					osMoPlugin.getTracker().disableTracker();
+					osMoPlugin.getService().disconnect();
+					if (app.getNavigationService() != null) {
+						app.getNavigationService().stopIfNeeded(app, NavigationService.USED_BY_LIVE);
+					}
+					osMoPlugin.refreshMap();
+				}
+			}
+		}, new IntentFilter(OSMAND_STOP_OSMO_SERVICE_ACTION));
+
+		app.registerReceiver(new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				OsMoPlugin osMoPlugin = OsmandPlugin.getEnabledPlugin(OsMoPlugin.class);
+				if (osMoPlugin != null) {
+					osMoPlugin.getService().connect(true);
+					if (osMoPlugin.getTracker() != null) {
+						osMoPlugin.getTracker().enableTracker();
+					}
+					app.startNavigationService(NavigationService.USED_BY_LIVE, 0);
+				}
+			}
+		}, new IntentFilter(OSMAND_START_SHARE_LOCATION_ACTION));
+
+		app.registerReceiver(new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				OsMoPlugin osMoPlugin = OsmandPlugin.getEnabledPlugin(OsMoPlugin.class);
+				if (osMoPlugin != null) {
+					if (osMoPlugin.getTracker() != null) {
+						osMoPlugin.getTracker().disableTracker();
+					}
+					if (app.getNavigationService() != null) {
+						app.getNavigationService()
+								.stopIfNeeded(app, NavigationService.USED_BY_LIVE);
+					}
+				}
+			}
+		}, new IntentFilter(OSMAND_STOP_SHARE_LOCATION_ACTION));
 	}
 
 	@Override
@@ -44,26 +111,61 @@ public class OsMoNotification extends OsmandNotification {
 
 	@Override
 	public boolean isEnabled() {
-		return OsmandPlugin.getEnabledPlugin(OsMoPlugin.class) != null;
+		OsMoPlugin osMoPlugin = OsmandPlugin.getEnabledPlugin(OsMoPlugin.class);
+		return osMoPlugin != null && osMoPlugin.getService().isEnabled();
 	}
 
 	@Override
 	public Builder buildNotification() {
+		OsMoPlugin osMoPlugin = OsmandPlugin.getEnabledPlugin(OsMoPlugin.class);
+		if (osMoPlugin == null) {
+			return null;
+		}
+
 		String notificationTitle;
 		String notificationText;
 		color = 0;
 		icon = R.drawable.ic_osmo_dark;
 		color = app.getResources().getColor(R.color.osmand_orange);
-		notificationTitle = Version.getAppName(app);
-		notificationText = app.getString(R.string.osmo);
+		notificationTitle = app.getString(R.string.osmo_plugin_name);
+		notificationText = app.getString(R.string.osmo_service_running);
 
 		final Builder notificationBuilder = createBuilder()
 				.setContentTitle(notificationTitle)
 				.setStyle(new BigTextStyle().bigText(notificationText));
 
+		if (osMoPlugin.getService().isEnabled()) {
+			Intent stopServiceIntent = new Intent(OSMAND_STOP_OSMO_SERVICE_ACTION);
+			PendingIntent stopServicePendingIntent = PendingIntent.getBroadcast(app, 0, stopServiceIntent,
+					PendingIntent.FLAG_UPDATE_CURRENT);
+			notificationBuilder.addAction(R.drawable.ic_action_rec_stop,
+					app.getString(R.string.shared_string_control_stop), stopServicePendingIntent);
+
+			if (osMoPlugin.getTracker() != null) {
+				if (osMoPlugin.getTracker().isEnabledTracker()) {
+					Intent stopShareLocatiponIntent = new Intent(OSMAND_STOP_SHARE_LOCATION_ACTION);
+					PendingIntent stopShareLocatiponIntentPendingIntent = PendingIntent.getBroadcast(app, 0, stopShareLocatiponIntent,
+							PendingIntent.FLAG_UPDATE_CURRENT);
+					notificationBuilder.addAction(R.drawable.ic_action_remove_dark,
+							app.getString(R.string.osmo_pause_location), stopShareLocatiponIntentPendingIntent);
+				} else {
+					Intent startShareLocationIntent = new Intent(OSMAND_START_SHARE_LOCATION_ACTION);
+					PendingIntent startShareLocationPendingIntent = PendingIntent.getBroadcast(app, 0, startShareLocationIntent,
+							PendingIntent.FLAG_UPDATE_CURRENT);
+					notificationBuilder.addAction(R.drawable.ic_action_gshare_dark,
+							app.getString(R.string.osmo_share_location), startShareLocationPendingIntent);
+				}
+			}
+		} else {
+			Intent startServiceIntent = new Intent(OSMAND_START_OSMO_SERVICE_ACTION);
+			PendingIntent startServicePendingIntent = PendingIntent.getBroadcast(app, 0, startServiceIntent,
+					PendingIntent.FLAG_UPDATE_CURRENT);
+			notificationBuilder.addAction(R.drawable.ic_play_dark,
+					app.getString(R.string.shared_string_control_start), startServicePendingIntent);
+		}
+
 		return notificationBuilder;
 	}
-
 
 
 	@Override
