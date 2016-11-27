ba9564d53e369fd1c7c238d76d417c53451781b1
sonora <hm.gglmail@gmail.com>
2016-11-27 20:56:53 +0100

    cosmetics

--- OsmAnd/src/net/osmand/plus/myplaces/ElevationView.java
+++ OsmAnd/src/net/osmand/plus/myplaces/ElevationView.java
@@ -43,8 +43,9 @@ public class ElevationView extends ImageView {
 		final int stepBase = useFeet  ? 200 : 100;
 		final float convEle = useFeet  ? 3.28084f : 1.0f;
 
-		final int maxBase = ((int)(maxElevation * convEle / stepBase) + 1) * stepBase, minBase = (int)(minElevation * convEle / stepBase) * stepBase;
-		final float yDistance = (maxBase - minBase);
+		final int maxBase = ((int)(maxElevation * convEle / stepBase) + 1) * stepBase;
+		final int minBase = (int)(minElevation * convEle / stepBase) * stepBase;
+		final float yDistance = maxBase - minBase;
 		final float xPer = (float)canvas.getWidth() / xDistance;
 		final float yPer = (float)canvas.getHeight() / yDistance;
 		final float canvasRight = (float)canvas.getWidth() - 1f;
