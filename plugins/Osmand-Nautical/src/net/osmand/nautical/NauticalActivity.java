10591f7a2016724989d68775ca7ba74a5edabfc1
Alexey Kulish <alexey.kulish@osmand.net>
2016-12-09 19:05:17 +0300

    Improved nautical plugin

--- plugins/Osmand-Nautical/src/net/osmand/nautical/NauticalActivity.java
+++ plugins/Osmand-Nautical/src/net/osmand/nautical/NauticalActivity.java
@@ -1,16 +1,21 @@
 package net.osmand.nautical;
 
-import net.osmand.nauticalPlugin.R;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
+
+import net.osmand.nauticalPlugin.R;
+
+import java.lang.reflect.Method;
 
 public class NauticalActivity extends Activity {
 	private static final String OSMAND_COMPONENT = "net.osmand"; //$NON-NLS-1$
@@ -23,6 +28,9 @@ public class NauticalActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		TextView descriptionTextView = (TextView) findViewById(R.id.descriptionTextView);
+		descriptionTextView.setText(Html.fromHtml(getString(R.string.plugin_description)));
+
 		Intent intentPlus = new Intent();
 		intentPlus.setComponent(new ComponentName(OSMAND_COMPONENT_PLUS, OSMAND_ACTIVITY));
 		intentPlus.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
@@ -30,6 +38,7 @@ public class NauticalActivity extends Activity {
 		if(resolved != null) {
 			stopService(intentPlus);
 			startActivity(intentPlus);
+			finish();
 		} else {
 			Intent intentNormal = new Intent();
 			intentNormal.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
@@ -38,24 +47,37 @@ public class NauticalActivity extends Activity {
 			if (resolved != null) {
 				stopService(intentNormal);
 				startActivity(intentNormal);
+				finish();
 			} else {
-				AlertDialog.Builder builder = new AlertDialog.Builder(this);
-				builder.setMessage(getString(R.string.osmand_app_not_found));
-				builder.setPositiveButton(getString(R.string.shared_string_yes), new DialogInterface.OnClickListener() {
+				logEvent(this, "open_dialog");
+				findViewById(R.id.buyButton).setOnClickListener(new View.OnClickListener() {
 					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pname:" + OSMAND_COMPONENT_PLUS));
+					public void onClick(View v) {
+						String appName = OSMAND_COMPONENT;
+						logEvent(NauticalActivity.this, "open_play_store_" + appName);
+						Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pname:" + appName));
 						try {
-							stopService(intent);
+							//stopService(intent);
 							startActivity(intent);
+							finish();
 						} catch (ActivityNotFoundException e) {
+							// ignore
 						}
 					}
 				});
-				builder.setNegativeButton(getString(R.string.shared_string_no), null);
-				builder.show();
 			}
 		}
 	}
-    
+
+	public void logEvent(Activity ctx, String event) {
+		try {
+			Class<?> cl = Class.forName("com.google.firebase.analytics.FirebaseAnalytics");
+			Method mm = cl.getMethod("getInstance", Context.class);
+			Object inst = mm.invoke(null, ctx == null ? this : ctx);
+			Method log = cl.getMethod("logEvent", String.class, Bundle.class);
+			log.invoke(inst, event, new Bundle());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
