5f70f06a980dcef225e955cdfad8ebb4fa949d72
sonora <hm.gglmail@gmail.com>
2016-12-29 15:09:17 +0100

    also mask onScroll to react on ACTION_MOVE only

--- OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
+++ OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
@@ -1108,7 +1108,9 @@ public class OsmandMapTileView implements IMapDownloaderCallback {
 
 		@Override
 		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-			dragToAnimate(e2.getX() + distanceX, e2.getY() + distanceY, e2.getX(), e2.getY(), true);
+			if ((e2.getAction() & 255) == MotionEvent.ACTION_MOVE) {
+				dragToAnimate(e2.getX() + distanceX, e2.getY() + distanceY, e2.getX(), e2.getY(), true);
+			}
 			return true;
 		}
 
