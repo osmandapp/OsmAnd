ff94e08b6ced894716c11f80b478979b48da5188
sonora <hm.gglmail@gmail.com>
2017-01-05 02:49:53 +0100

    fix build

--- OsmAnd/src/net/osmand/plus/views/POIMapLayer.java
+++ OsmAnd/src/net/osmand/plus/views/POIMapLayer.java
@@ -27,6 +27,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
+import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.LinearLayout;
