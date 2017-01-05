ab29b380a0ecea0e547b1cd98f845ec13dbc2b0f
sonora <hm.gglmail@gmail.com>
2017-01-05 02:32:45 +0100

    make Wiki webview single column

--- OsmAndCore-sample/src/net/osmand/core/samples/android/sample1/mapcontextmenu/ContextMenuHelper.java
+++ OsmAndCore-sample/src/net/osmand/core/samples/android/sample1/mapcontextmenu/ContextMenuHelper.java
@@ -105,6 +105,7 @@ public class ContextMenuHelper {
 		settings.setBuiltInZoomControls(true);
 		settings.setDisplayZoomControls(false);
 		settings.setSupportZoom(true);
+		settings.setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
 
 		wv.loadDataWithBaseURL(null, content, "text/html", "UTF-8", null);
 //		wv.loadUrl(OsMoService.SIGN_IN_URL + app.getSettings().OSMO_DEVICE_KEY.get());
