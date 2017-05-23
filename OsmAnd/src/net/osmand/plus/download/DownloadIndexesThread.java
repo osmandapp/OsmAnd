718fb65041bea094c12948946b0b71e837ce454c
PavelRatushny <pavelratushniy@gmail.com>
2017-05-23 14:53:46 +0300

    Fix #3750

--- OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
+++ OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
@@ -437,6 +437,12 @@ public class DownloadIndexesThread {
 					} else {
 						dbHelper.update(entry, DatabaseHelper.DOWNLOAD_ENTRY);
 					}
+				} else if (o instanceof String) {
+					String message = (String) o;
+					// ctx.getString(R.string.shared_string_io_error) +": Interrupted";
+					if (!message.toLowerCase().contains("interrupted") && !message.equals(app.getString(R.string.shared_string_download_successful))) {
+						app.showToastMessage(message);
+					}
 				}
 			}
 			downloadInProgress();
