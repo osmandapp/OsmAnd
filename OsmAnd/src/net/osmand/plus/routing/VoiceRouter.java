c216ae5fa167827f119248cc3ccbe227b97695dc
Victor Shcherb <victor.shcherb@gmail.com>
2017-05-29 21:46:10 +0200

    Fix frequent NPE

--- OsmAnd/src/net/osmand/plus/routing/VoiceRouter.java
+++ OsmAnd/src/net/osmand/plus/routing/VoiceRouter.java
@@ -420,7 +420,7 @@ public class VoiceRouter {
 
 		NextDirectionInfo nextInfo = router.getNextRouteDirectionInfo(new NextDirectionInfo(), true);
 		RouteSegmentResult currentSegment = router.getCurrentSegmentResult();
-		if (nextInfo.directionInfo == null) {
+		if (nextInfo == null || nextInfo.directionInfo == null) {
 			return;
 		}
 		int dist = nextInfo.distanceTo;
