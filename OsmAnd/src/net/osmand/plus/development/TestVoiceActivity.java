592ea3dd4d6bb78b3ae4f59a302b420f0296d348
sonora <hm.gglmail@gmail.com>
2017-06-15 21:37:14 +0200

    fix padding

--- OsmAnd/src/net/osmand/plus/development/TestVoiceActivity.java
+++ OsmAnd/src/net/osmand/plus/development/TestVoiceActivity.java
@@ -295,9 +295,9 @@ public class TestVoiceActivity extends OsmandActionBarActivity {
 		button.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
 		if (!description.startsWith("\u25BA (")) {
 			// Section headline buttons
-			button.setPadding(80, 20, 80, 5);
+			button.setPadding(10, 20, 10, 5);
 		} else {
-			button.setPadding(10, 5, 10, 5);
+			button.setPadding(50, 5, 10, 5);
 		}
 		if (description.startsWith("\u25BA (11.1)")) {
 			infoButton = button;
