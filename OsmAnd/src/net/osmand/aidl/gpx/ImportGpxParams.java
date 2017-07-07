8a671f8572ba53f8632d22a20bc2ff18e3d25e06
Alexey Kulish <alexey.kulish@osmand.net>
2017-07-07 19:31:25 +0300

    Improved aidl import gpx

--- OsmAnd/src/net/osmand/aidl/gpx/ImportGpxParams.java
+++ OsmAnd/src/net/osmand/aidl/gpx/ImportGpxParams.java
@@ -12,20 +12,28 @@ public class ImportGpxParams implements Parcelable {
 	private Uri gpxUri;
 	private String sourceRawData;
 	private String destinationPath;
+	private String color;
+	private boolean show;
 
-	public ImportGpxParams(File gpxFile, String destinationPath) {
+	public ImportGpxParams(File gpxFile, String destinationPath, String color, boolean show) {
 		this.gpxFile = gpxFile;
 		this.destinationPath = destinationPath;
+		this.color = color;
+		this.show = show;
 	}
 
-	public ImportGpxParams(Uri gpxUri, String destinationPath) {
+	public ImportGpxParams(Uri gpxUri, String destinationPath, String color, boolean show) {
 		this.gpxUri = gpxUri;
 		this.destinationPath = destinationPath;
+		this.color = color;
+		this.show = show;
 	}
 
-	public ImportGpxParams(String sourceRawData, String destinationPath) {
+	public ImportGpxParams(String sourceRawData, String destinationPath, String color, boolean show) {
 		this.sourceRawData = sourceRawData;
 		this.destinationPath = destinationPath;
+		this.color = color;
+		this.show = show;
 	}
 
 	public ImportGpxParams(Parcel in) {
@@ -59,6 +67,14 @@ public class ImportGpxParams implements Parcelable {
 		return destinationPath;
 	}
 
+	public String getColor() {
+		return color;
+	}
+
+	public boolean isShow() {
+		return show;
+	}
+
 	public void writeToParcel(Parcel out, int flags) {
 		if (gpxFile != null) {
 			out.writeString(gpxFile.getAbsolutePath());
@@ -68,6 +84,8 @@ public class ImportGpxParams implements Parcelable {
 		out.writeParcelable(gpxUri, flags);
 		out.writeString(sourceRawData);
 		out.writeString(destinationPath);
+		out.writeString(color);
+		out.writeByte((byte) (show ? 1 : 0));
 	}
 
 	private void readFromParcel(Parcel in) {
@@ -78,6 +96,8 @@ public class ImportGpxParams implements Parcelable {
 		gpxUri = in.readParcelable(Uri.class.getClassLoader());
 		sourceRawData = in.readString();
 		destinationPath = in.readString();
+		color = in.readString();
+		show = in.readByte() == 1;
 	}
 
 	public int describeContents() {
