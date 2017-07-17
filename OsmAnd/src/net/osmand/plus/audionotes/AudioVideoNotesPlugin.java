23e7e53ad224f8d0d90847f5be483d88fe47d9c8
Alexander Sytnyk <asytnyk0@gmail.com>
2017-07-17 16:49:19 +0300

    Fix #3879

--- OsmAnd/src/net/osmand/plus/audionotes/AudioVideoNotesPlugin.java
+++ OsmAnd/src/net/osmand/plus/audionotes/AudioVideoNotesPlugin.java
@@ -1658,7 +1658,7 @@ public class AudioVideoNotesPlugin extends OsmandPlugin {
 		if (isRecording()) {
 			AVActionType type = currentRecording.type;
 			finishRecording();
-			if (!AV_RECORDER_SPLIT.get() || type != AVActionType.REC_VIDEO) {
+			if (type != AVActionType.REC_AUDIO && (!AV_RECORDER_SPLIT.get() || type != AVActionType.REC_VIDEO)) {
 				final Recording recordingForMenu = r;
 				app.runInUIThread(new Runnable() {
 					@Override
