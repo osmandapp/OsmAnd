253519e861a640d7b41739c0117ec5425cc5127b
Alexey Kulish <alexey.kulish@osmand.net>
2017-07-26 14:46:36 +0300

    Fix mapillary layer activity after rotation

--- OsmAnd/src/net/osmand/plus/dashboard/DashboardOnMap.java
+++ OsmAnd/src/net/osmand/plus/dashboard/DashboardOnMap.java
@@ -793,6 +793,7 @@ public class DashboardOnMap implements ObservableScrollViewCallbacks, DynamicLis
 		if (swipeDismissListener != null) {
 			swipeDismissListener.discardUndo();
 		}
+		removeMapillaryFiltersFragment();
 
 		if (visible) {
 			mapActivity.getContextMenu().hideMenues();
@@ -818,7 +819,6 @@ public class DashboardOnMap implements ObservableScrollViewCallbacks, DynamicLis
 			View listViewLayout = dashboardView.findViewById(R.id.dash_list_view_layout);
 			ScrollView scrollView = (ScrollView) dashboardView.findViewById(R.id.main_scroll);
 			if (visibleType == DashboardType.DASHBOARD || visibleType == DashboardType.MAPILLARY) {
-				removeMapillaryFiltersFragment();
 				if (visibleType == DashboardType.DASHBOARD) {
 					addOrUpdateDashboardFragments();
 				} else {
