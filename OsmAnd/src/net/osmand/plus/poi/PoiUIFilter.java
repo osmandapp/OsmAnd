490a2fb74ea6e65b1c3d227f0987cbb1fa360187
Alexander Sytnyk <asytnyk0@gmail.com>
2017-07-27 13:32:36 +0300

    Fix #4208

--- OsmAnd/src/net/osmand/plus/poi/PoiUIFilter.java
+++ OsmAnd/src/net/osmand/plus/poi/PoiUIFilter.java
@@ -585,7 +585,7 @@ public class PoiUIFilter implements SearchPoiTypeFilter, Comparable<PoiUIFilter>
 					acceptedTypes.put(category, null);
 				}
 			} else {
-				acceptedTypes.put(category, types.get(category));
+				acceptedTypes.put(category, (LinkedHashSet<String>) types.get(category).clone());
 			}
 		}
 	}
