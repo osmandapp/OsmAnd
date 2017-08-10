ee09074abf8b5df60d7c6b4a8f1fa2406b37d2de
Alexander Sytnyk <asytnyk0@gmail.com>
2017-08-10 17:48:36 +0300

    Show application modes in bottom sheet

--- OsmAnd/src/net/osmand/plus/measurementtool/SnapToRoadBottomSheetDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/measurementtool/SnapToRoadBottomSheetDialogFragment.java
@@ -3,16 +3,21 @@ package net.osmand.plus.measurementtool;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import net.osmand.plus.ApplicationMode;
+import net.osmand.plus.OsmandSettings;
 import net.osmand.plus.R;
 import net.osmand.plus.base.BottomSheetDialogFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SnapToRoadBottomSheetDialogFragment extends BottomSheetDialogFragment {
 
@@ -21,31 +26,36 @@ public class SnapToRoadBottomSheetDialogFragment extends BottomSheetDialogFragme
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		final OsmandSettings settings = getMyApplication().getSettings();
 		final boolean nightMode = getMyApplication().getDaynightHelper().isNightModeForMapControls();
+		final int themeRes = nightMode ? R.style.OsmandDarkTheme : R.style.OsmandLightTheme;
 		final int backgroundColor = ContextCompat.getColor(getActivity(),
 				nightMode ? R.color.ctx_menu_info_view_bg_dark : R.color.ctx_menu_info_view_bg_light);
 
-		View view = inflater.inflate(R.layout.fragment_snap_to_road_bottom_sheet_dialog, container, false);
+		View view = View.inflate(new ContextThemeWrapper(getContext(), themeRes), R.layout.fragment_snap_to_road_bottom_sheet_dialog, container);
 		view.setBackgroundColor(backgroundColor);
-
-		FrameLayout navigationTypesContainer = (FrameLayout) view.findViewById(R.id.navigation_types_container);
-
-		View carNavigation = inflater.inflate(R.layout.list_item_icon_and_title, navigationTypesContainer, true);
-		((ImageView) carNavigation.findViewById(R.id.icon)).setImageDrawable(getContentIcon(R.drawable.ic_action_car_dark));
-		((TextView) carNavigation.findViewById(R.id.title)).setText(getString(R.string.rendering_value_car_name));
-		carNavigation.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Toast.makeText(getActivity(), "Car", Toast.LENGTH_SHORT).show();
-			}
-		});
-
-		view.findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+		if (nightMode) {
+			((TextView) view.findViewById(R.id.cancel_row_text))
+					.setTextColor(ContextCompat.getColor(getActivity(), R.color.dashboard_general_button_text_dark));
+		} else {
+			view.findViewById(R.id.divider).setBackgroundResource(R.drawable.divider);
+		}
+		view.findViewById(R.id.cancel_row).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				dismiss();
 			}
 		});
+
+		LinearLayout navContainer = (LinearLayout) view.findViewById(R.id.navigation_types_container);
+		final List<ApplicationMode> modes = new ArrayList<>(ApplicationMode.values(settings));
+		modes.remove(ApplicationMode.DEFAULT);
+		for (ApplicationMode mode : modes) {
+			View row = View.inflate(new ContextThemeWrapper(getContext(), themeRes), R.layout.list_item_icon_and_title, null);
+			((ImageView) row.findViewById(R.id.icon)).setImageDrawable(getContentIcon(mode.getSmallIconDark()));
+			((TextView) row.findViewById(R.id.title)).setText(mode.toHumanString(getContext()));
+			navContainer.addView(row);
+		}
 
 		return view;
 	}
