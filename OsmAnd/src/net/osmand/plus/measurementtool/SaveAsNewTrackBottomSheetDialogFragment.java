a8cb9d4743714fea2f9d6c66a0a78abad475d877
PavelRatushny <pavelratushniy@gmail.com>
2017-08-17 14:18:25 +0300

    Add images to save as menu

--- OsmAnd/src/net/osmand/plus/measurementtool/SaveAsNewTrackBottomSheetDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/measurementtool/SaveAsNewTrackBottomSheetDialogFragment.java
@@ -1,10 +1,13 @@
 package net.osmand.plus.measurementtool;
 
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -14,7 +17,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.osmand.AndroidUtils;
-import net.osmand.plus.OsmandSettings;
 import net.osmand.plus.R;
 import net.osmand.plus.base.BottomSheetDialogFragment;
 import net.osmand.plus.helpers.AndroidUiHelper;
@@ -36,7 +38,6 @@ public class SaveAsNewTrackBottomSheetDialogFragment extends BottomSheetDialogFr
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		nightMode = getMyApplication().getDaynightHelper().isNightModeForMapControls();
 		portrait = AndroidUiHelper.isOrientationPortrait(getActivity());
-		final OsmandSettings settings = getMyApplication().getSettings();
 		final int themeRes = nightMode ? R.style.OsmandDarkTheme : R.style.OsmandLightTheme;
 
 		final View mainView = View.inflate(new ContextThemeWrapper(getContext(), themeRes), R.layout.fragment_save_as_new_track_bottom_sheet_dialog, container);
@@ -44,32 +45,38 @@ public class SaveAsNewTrackBottomSheetDialogFragment extends BottomSheetDialogFr
 			AndroidUtils.setBackground(getActivity(), mainView, nightMode, R.drawable.bg_bottom_menu_light, R.drawable.bg_bottom_menu_dark);
 		}
 
+		final ImageView routePointImage = (ImageView) mainView.findViewById(R.id.route_point_image);
+		ImageView lineImage = (ImageView) mainView.findViewById(R.id.line_image);
 		if (nightMode) {
 			((TextView) mainView.findViewById(R.id.save_as_new_track_title)).setTextColor(getResources().getColor(R.color.ctx_menu_info_text_dark));
+
+			routePointImage.setImageResource(R.drawable.img_help_trip_route_points_night);
+			lineImage.setImageResource(R.drawable.img_help_trip_track_night);
+		} else {
+			routePointImage.setImageResource(R.drawable.img_help_trip_route_points_day);
+			lineImage.setImageResource(R.drawable.img_help_trip_track_day);
 		}
 
 		((ImageView) mainView.findViewById(R.id.route_point_icon)).setImageDrawable(getContentIcon(R.drawable.ic_action_route_points));
 		((ImageView) mainView.findViewById(R.id.line_icon)).setImageDrawable(getContentIcon(R.drawable.ic_action_split_interval));
 
-		mainView.findViewById(R.id.save_as_line_row).setOnClickListener(new View.OnClickListener() {
+		mainView.findViewById(R.id.line_text).setOnTouchListener(new View.OnTouchListener() {
 			@Override
-			public void onClick(View view) {
-				if (listener != null) {
-					listener.saveAsLineOnClick();
-				}
-				dismiss();
+			public boolean onTouch(View view, MotionEvent motionEvent) {
+				return false;
 			}
 		});
+		lineImage.setOnClickListener(saveAsLineOnClickListener);
+		mainView.findViewById(R.id.save_as_line_row).setOnClickListener(saveAsLineOnClickListener);
 
-		mainView.findViewById(R.id.save_as_route_point_row).setOnClickListener(new View.OnClickListener() {
+		mainView.findViewById(R.id.route_point_text).setOnTouchListener(new View.OnTouchListener() {
 			@Override
-			public void onClick(View view) {
-				if (listener != null) {
-					listener.saveAsRoutePointOnClick();
-				}
-				dismiss();
+			public boolean onTouch(View view, MotionEvent motionEvent) {
+				return false;
 			}
 		});
+		routePointImage.setOnClickListener(saveAsLineOnClickListener);
+		mainView.findViewById(R.id.save_as_route_point_row).setOnClickListener(saveAsRoutePointOnClickListener);
 
 		mainView.findViewById(R.id.cancel_row).setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -128,6 +135,31 @@ public class SaveAsNewTrackBottomSheetDialogFragment extends BottomSheetDialogFr
 			window.setAttributes(params);
 		}
 	}
+
+	@Override
+	protected Drawable getContentIcon(@DrawableRes int id) {
+		return getIcon(id, nightMode ? R.color.ctx_menu_info_text_dark : R.color.on_map_icon_color);
+	}
+
+	private View.OnClickListener saveAsLineOnClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			if (listener != null) {
+				listener.saveAsLineOnClick();
+			}
+			dismiss();
+		}
+	};
+
+	private View.OnClickListener saveAsRoutePointOnClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			if (listener != null) {
+				listener.saveAsRoutePointOnClick();
+			}
+			dismiss();
+		}
+	};
 
 	interface SaveAsNewTrackFragmentListener {
 
