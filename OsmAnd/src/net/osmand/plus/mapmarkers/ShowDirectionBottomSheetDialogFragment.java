6d57ac5a8c442d889fd6c09db5fec2004b8cf984
PavelRatushny <pavelratushniy@gmail.com>
2017-09-13 12:49:16 +0300

    Highlight selected item in show direction menu

--- OsmAnd/src/net/osmand/plus/mapmarkers/ShowDirectionBottomSheetDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/mapmarkers/ShowDirectionBottomSheetDialogFragment.java
@@ -3,6 +3,7 @@ package net.osmand.plus.mapmarkers;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -12,6 +13,7 @@ import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import net.osmand.AndroidUtils;
@@ -26,6 +28,8 @@ public class ShowDirectionBottomSheetDialogFragment extends BottomSheetDialogFra
 
 	private ShowDirectionFragmentListener listener;
 	private boolean portrait;
+	private View mainView;
+	private boolean night;
 
 	public void setListener(ShowDirectionFragmentListener listener) {
 		this.listener = listener;
@@ -35,20 +39,23 @@ public class ShowDirectionBottomSheetDialogFragment extends BottomSheetDialogFra
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		portrait = AndroidUiHelper.isOrientationPortrait(getActivity());
-		boolean nightMode = getMyApplication().getDaynightHelper().isNightModeForMapControls();
-		final int themeRes = nightMode ? R.style.OsmandDarkTheme : R.style.OsmandLightTheme;
+		night = !getMyApplication().getSettings().isLightContent();
+		final int themeRes = night ? R.style.OsmandDarkTheme : R.style.OsmandLightTheme;
 
-		final View mainView = View.inflate(new ContextThemeWrapper(getContext(), themeRes), R.layout.fragment_marker_show_direction_bottom_sheet_dialog, container);
+		mainView = View.inflate(new ContextThemeWrapper(getContext(), themeRes), R.layout.fragment_marker_show_direction_bottom_sheet_dialog, container);
 		if (portrait) {
-			AndroidUtils.setBackground(getActivity(), mainView, nightMode, R.drawable.bg_bottom_menu_light, R.drawable.bg_bottom_menu_dark);
+			AndroidUtils.setBackground(getActivity(), mainView, night, R.drawable.bg_bottom_menu_light, R.drawable.bg_bottom_menu_dark);
 		}
+
+		OsmandSettings.MapMarkersMode mode = getMyApplication().getSettings().MAP_MARKERS_MODE.get();
+		highlightSelectedItem(mode, true);
 
 		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
 			mainView.findViewById(R.id.images_row).setVisibility(View.GONE);
 		} else {
 			ImageView topBarImage = (ImageView) mainView.findViewById(R.id.top_bar_image);
 			ImageView widgetImage = (ImageView) mainView.findViewById(R.id.widget_image);
-			if (nightMode) {
+			if (night) {
 				topBarImage.setImageResource(R.drawable.img_help_markers_topbar_night);
 				widgetImage.setImageResource(R.drawable.img_help_markers_widgets_night);
 			} else {
@@ -56,7 +63,7 @@ public class ShowDirectionBottomSheetDialogFragment extends BottomSheetDialogFra
 				widgetImage.setImageResource(R.drawable.img_help_markers_widgets_day);
 			}
 
-			mainView.findViewById(R.id.top_bar_text).setOnTouchListener(new View.OnTouchListener() {
+			mainView.findViewById(R.id.top_bar_image_text).setOnTouchListener(new View.OnTouchListener() {
 				@Override
 				public boolean onTouch(View view, MotionEvent motionEvent) {
 					return false;
@@ -64,7 +71,7 @@ public class ShowDirectionBottomSheetDialogFragment extends BottomSheetDialogFra
 			});
 			topBarImage.setOnClickListener(showDirectionOnClickListener);
 
-			mainView.findViewById(R.id.widget_text).setOnTouchListener(new View.OnTouchListener() {
+			mainView.findViewById(R.id.widget_image_text).setOnTouchListener(new View.OnTouchListener() {
 				@Override
 				public boolean onTouch(View view, MotionEvent motionEvent) {
 					return false;
@@ -73,7 +80,7 @@ public class ShowDirectionBottomSheetDialogFragment extends BottomSheetDialogFra
 			widgetImage.setOnClickListener(showDirectionOnClickListener);
 		}
 
-		if (nightMode) {
+		if (night) {
 			((TextView) mainView.findViewById(R.id.show_direction_title)).setTextColor(getResources().getColor(R.color.ctx_menu_info_text_dark));
 		}
 
@@ -150,21 +157,69 @@ public class ShowDirectionBottomSheetDialogFragment extends BottomSheetDialogFra
 		}
 	}
 
+	private void highlightSelectedItem(OsmandSettings.MapMarkersMode mode, boolean check) {
+		int iconBgColor = check ? R.color.dashboard_blue : R.color.on_map_icon_color;
+		int iconColor = check ? R.color.color_dialog_buttons_dark : R.color.dashboard_blue;
+		int textColor = ContextCompat.getColor(getContext(), check ? R.color.dashboard_blue : night ? R.color.color_white : R.color.color_black);
+		switch (mode) {
+			case TOOLBAR:
+				((RadioButton) mainView.findViewById(R.id.top_bar_radio_button)).setChecked(check);
+				ImageView topBarIcon = (ImageView) mainView.findViewById(R.id.top_bar_icon);
+				if (check) {
+					mainView.findViewById(R.id.top_bar_row).setBackgroundColor(ContextCompat.getColor(getContext(), R.color.show_direction_menu_selected_item_bg));
+				} else {
+					mainView.findViewById(R.id.top_bar_row).setBackgroundResource(0);
+				}
+				((TextView) mainView.findViewById(R.id.top_bar_text)).setTextColor(textColor);
+				topBarIcon.setBackgroundDrawable(getIcon(R.drawable.ic_action_device_top, iconBgColor));
+				topBarIcon.setImageDrawable(getIcon(R.drawable.ic_action_device_topbar, iconColor));
+				break;
+			case WIDGETS:
+				((RadioButton) mainView.findViewById(R.id.widget_radio_button)).setChecked(check);
+				ImageView widgetIcon = (ImageView) mainView.findViewById(R.id.widget_icon);
+				if (check) {
+					mainView.findViewById(R.id.widget_row).setBackgroundColor(ContextCompat.getColor(getContext(), R.color.show_direction_menu_selected_item_bg));
+				} else {
+					mainView.findViewById(R.id.widget_row).setBackgroundResource(0);
+				}
+				((TextView) mainView.findViewById(R.id.widget_text)).setTextColor(textColor);
+				widgetIcon.setBackgroundDrawable(getIcon(R.drawable.ic_action_device_top, iconBgColor));
+				widgetIcon.setImageDrawable(getIcon(R.drawable.ic_action_device_widget, iconColor));
+				break;
+			case NONE:
+				((RadioButton) mainView.findViewById(R.id.none_radio_button)).setChecked(check);
+				ImageView noneIcon = (ImageView) mainView.findViewById(R.id.none_icon);
+				if (check) {
+					mainView.findViewById(R.id.none_row).setBackgroundColor(ContextCompat.getColor(getContext(), R.color.show_direction_menu_selected_item_bg));
+				} else {
+					mainView.findViewById(R.id.none_row).setBackgroundResource(0);
+				}
+				((TextView) mainView.findViewById(R.id.none_text)).setTextColor(textColor);
+				noneIcon.setBackgroundDrawable(getIcon(R.drawable.ic_action_device_top, iconBgColor));
+				break;
+		}
+	}
+
 	private View.OnClickListener showDirectionOnClickListener = new View.OnClickListener() {
 
 		@Override
 		public void onClick(View view) {
+			OsmandSettings.MapMarkersMode previousMode = getMyApplication().getSettings().MAP_MARKERS_MODE.get();
+			highlightSelectedItem(previousMode, false);
 			switch (view.getId()) {
 				case R.id.top_bar_image:
 				case R.id.top_bar_row:
 					getMyApplication().getSettings().MAP_MARKERS_MODE.set(OsmandSettings.MapMarkersMode.TOOLBAR);
+					highlightSelectedItem(OsmandSettings.MapMarkersMode.TOOLBAR, true);
 					break;
 				case R.id.widget_image:
 				case R.id.widget_row:
 					getMyApplication().getSettings().MAP_MARKERS_MODE.set(OsmandSettings.MapMarkersMode.WIDGETS);
+					highlightSelectedItem(OsmandSettings.MapMarkersMode.WIDGETS, true);
 					break;
 				case R.id.none_row:
 					getMyApplication().getSettings().MAP_MARKERS_MODE.set(OsmandSettings.MapMarkersMode.NONE);
+					highlightSelectedItem(OsmandSettings.MapMarkersMode.NONE, true);
 					break;
 			}
 			if (listener != null) {
