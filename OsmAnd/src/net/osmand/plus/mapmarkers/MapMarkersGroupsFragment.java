b7ee3507910abc77a19614d4a16ef089d111d8ae
PavelRatushnyi <pavelratushniy@gmail.com>
2017-09-24 10:22:59 +0300

    Fix history icons

--- OsmAnd/src/net/osmand/plus/mapmarkers/MapMarkersGroupsFragment.java
+++ OsmAnd/src/net/osmand/plus/mapmarkers/MapMarkersGroupsFragment.java
@@ -81,7 +81,7 @@ public class MapMarkersGroupsFragment extends Fragment implements OsmAndCompassL
 		ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
 			private float marginSides = getResources().getDimension(R.dimen.list_content_padding);
 			private Bitmap deleteBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_delete_dark);
-			private Bitmap historyBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_marker_passed);
+			private Bitmap historyBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_history2);
 			private boolean iconHidden;
 
 			@Override
