b4bedcd4488d87788c5cc5f1e04304200a86ec42
PavelRatushny <pavelratushniy@gmail.com>
2017-10-19 11:41:55 +0300

    Fix tablet UI
    (cherry picked from commit 6a6a648)

--- OsmAnd/src/net/osmand/plus/mapmarkers/OptionsBottomSheetDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/mapmarkers/OptionsBottomSheetDialogFragment.java
@@ -179,7 +179,7 @@ public class OptionsBottomSheetDialogFragment extends BottomSheetDialogFragment 
 
 				if (!portrait) {
 					if (screenHeight - statusBarHeight - mainView.getHeight()
-							>= AndroidUtils.dpToPx(getActivity(), 8)) {
+							>= getResources().getDimension(R.dimen.bottom_sheet_content_padding_small)) {
 						AndroidUtils.setBackground(getActivity(), mainView, nightMode,
 								R.drawable.bg_bottom_sheet_topsides_landscape_light, R.drawable.bg_bottom_sheet_topsides_landscape_dark);
 					} else {
