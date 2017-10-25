f18fc16c3a4a15c8b8ec8d11c05aa6410d3e605c
Alexander Sytnyk <asytnyk0@gmail.com>
2017-10-25 17:52:58 +0300

    Disable online search without online maps plugin enabled

--- OsmAnd/src/net/osmand/plus/search/QuickSearchListAdapter.java
+++ OsmAnd/src/net/osmand/plus/search/QuickSearchListAdapter.java
@@ -20,8 +20,10 @@ import net.osmand.access.AccessibilityAssistant;
 import net.osmand.data.Amenity;
 import net.osmand.data.LatLon;
 import net.osmand.plus.OsmandApplication;
+import net.osmand.plus.OsmandPlugin;
 import net.osmand.plus.R;
 import net.osmand.plus.dashboard.DashLocationFragment;
+import net.osmand.plus.rastermaps.OsmandRasterMapsPlugin;
 import net.osmand.plus.search.listitems.QuickSearchHeaderListItem;
 import net.osmand.plus.search.listitems.QuickSearchListItem;
 import net.osmand.plus.search.listitems.QuickSearchListItemType;
@@ -233,18 +235,21 @@ public class QuickSearchListAdapter extends ArrayAdapter<QuickSearchListItem> {
 				view.findViewById(R.id.empty_search).setVisibility(View.GONE);
 				view.findViewById(R.id.more_divider).setVisibility(View.GONE);
 			}
-			view.findViewById(R.id.increase_radius_row).setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View view) {
-					((QuickSearchMoreListItem) listItem).increaseRadiusOnClick();
-				}
-			});
-			view.findViewById(R.id.online_search_row).setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View view) {
-					((QuickSearchMoreListItem) listItem).onlineSearchOnClick();
-				}
-			});
+			if (OsmandPlugin.getEnabledPlugin(OsmandRasterMapsPlugin.class) != null) {
+				view.findViewById(R.id.online_search_row).setVisibility(View.VISIBLE);
+				view.findViewById(R.id.increase_radius_row).setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						((QuickSearchMoreListItem) listItem).increaseRadiusOnClick();
+					}
+				});
+				view.findViewById(R.id.online_search_row).setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						((QuickSearchMoreListItem) listItem).onlineSearchOnClick();
+					}
+				});
+			}
 		} else if (type == QuickSearchListItemType.BUTTON) {
 			if (convertView == null) {
 				LayoutInflater inflater = (LayoutInflater) app
