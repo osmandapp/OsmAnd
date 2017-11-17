7e1de273f5deab626a92ec1ee47f143a6d0a05db
Alexander Sytnyk <asytnyk0@gmail.com>
2017-11-17 11:50:48 +0200

    Change layout for note list item

--- OsmAnd/src/net/osmand/plus/audionotes/adapters/NotesAdapter.java
+++ OsmAnd/src/net/osmand/plus/audionotes/adapters/NotesAdapter.java
@@ -67,7 +67,7 @@ public class NotesAdapter extends ArrayAdapter<Object> {
 				HeaderViewHolder hHolder = new HeaderViewHolder(row);
 				row.setTag(hHolder);
 			} else {
-				row = inflater.inflate(R.layout.note, parent, false);
+				row = inflater.inflate(R.layout.note_list_item, parent, false);
 				ItemViewHolder iHolder = new ItemViewHolder(row);
 				row.setTag(iHolder);
 			}
@@ -121,10 +121,10 @@ public class NotesAdapter extends ArrayAdapter<Object> {
 					holder.description.setText(recording.getNewSmallDescription(app));
 					int iconRes = recording.isAudio() ? R.drawable.ic_type_audio
 							: (recording.isVideo() ? R.drawable.ic_type_video : R.drawable.ic_type_img);
-					holder.icon.setImageDrawable(app.getIconsCache().getIcon(iconRes, R.color.color_distance));
+					int colorRes = app.getSettings().isLightContent() ? R.color.icon_color : R.color.ctx_menu_info_text_dark;
+					holder.icon.setImageDrawable(app.getIconsCache().getIcon(iconRes, colorRes));
 				}
 
-				holder.play.setVisibility(View.GONE);
 				holder.bottomDivider.setVisibility(View.VISIBLE);
 				holder.icon.setVisibility(selectionMode ? View.GONE : View.VISIBLE);
 				holder.checkBox.setVisibility(selectionMode ? View.VISIBLE : View.GONE);
@@ -218,17 +218,15 @@ public class NotesAdapter extends ArrayAdapter<Object> {
 		final TextView title;
 		final TextView description;
 		final ImageButton options;
-		final View play;
 		final View bottomDivider;
 
 		ItemViewHolder(View view) {
 			this.view = view;
 			icon = (ImageView) view.findViewById(R.id.icon);
-			checkBox = (CheckBox) view.findViewById(R.id.check_local_index);
-			title = (TextView) view.findViewById(R.id.name);
+			checkBox = (CheckBox) view.findViewById(R.id.check_box);
+			title = (TextView) view.findViewById(R.id.title);
 			description = (TextView) view.findViewById(R.id.description);
 			options = (ImageButton) view.findViewById(R.id.options);
-			play = view.findViewById(R.id.play);
 			bottomDivider = view.findViewById(R.id.bottom_divider);
 		}
 	}
