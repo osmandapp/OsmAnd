0bac55aab6cbb004778ef15464b85fe0aa81a772
PavelRatushny <pavelratushniy@gmail.com>
2017-11-20 17:32:10 +0200

    Revert context menu

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/FavouritePointMenuBuilder.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/FavouritePointMenuBuilder.java
@@ -2,16 +2,47 @@ package net.osmand.plus.mapcontextmenu.builders;
 
 import android.view.View;
 
+import net.osmand.ResultMatcher;
+import net.osmand.binary.BinaryMapIndexReader;
 import net.osmand.data.Amenity;
 import net.osmand.data.FavouritePoint;
+import net.osmand.data.QuadRect;
+import net.osmand.data.TransportStop;
+import net.osmand.osm.PoiCategory;
+import net.osmand.plus.R;
 import net.osmand.plus.activities.MapActivity;
+import net.osmand.plus.mapcontextmenu.MenuBuilder;
+import net.osmand.plus.mapillary.MapillaryPlugin;
+import net.osmand.util.MapUtils;
 
-public class FavouritePointMenuBuilder extends SyncedItemMenuBuilder {
+import java.util.List;
+
+public class FavouritePointMenuBuilder extends MenuBuilder {
+
+	private final FavouritePoint fav;
+	private Object originObject;
 
 	public FavouritePointMenuBuilder(MapActivity mapActivity, final FavouritePoint fav) {
 		super(mapActivity);
-		this.favouritePoint = fav;
+		this.fav = fav;
+		setShowNearestWiki(true);
 		acquireOriginObject();
+	}
+
+	public void acquireOriginObject()
+	{
+		String originObjectName = fav.getOriginObjectName();
+		if (originObjectName.length() > 0) {
+			if (originObjectName.startsWith(Amenity.class.getSimpleName())) {
+				originObject = findAmenity(originObjectName, fav.getLatitude(), fav.getLongitude());
+			} else if (originObjectName.startsWith(TransportStop.class.getSimpleName())) {
+				originObject = findTransportStop(originObjectName, fav.getLatitude(), fav.getLongitude());
+			}
+		}
+	}
+
+	public Object getOriginObject() {
+		return originObject;
 	}
 
 	@Override
@@ -23,6 +54,61 @@ public class FavouritePointMenuBuilder extends SyncedItemMenuBuilder {
 
 	@Override
 	public void buildInternal(View view) {
-		buildFavouriteInternal(view);
+		if (originObject != null && originObject instanceof Amenity) {
+			AmenityMenuBuilder builder = new AmenityMenuBuilder(mapActivity, (Amenity) originObject);
+			builder.setLatLon(getLatLon());
+			builder.setLight(light);
+			builder.buildInternal(view);
+		}
+	}
+
+	private Amenity findAmenity(String nameStringEn, double lat, double lon) {
+		QuadRect rect = MapUtils.calculateLatLonBbox(lat, lon, 15);
+		List<Amenity> amenities = app.getResourceManager().searchAmenities(
+				new BinaryMapIndexReader.SearchPoiTypeFilter() {
+					@Override
+					public boolean accept(PoiCategory type, String subcategory) {
+						return true;
+					}
+
+					@Override
+					public boolean isEmpty() {
+						return false;
+					}
+				}, rect.top, rect.left, rect.bottom, rect.right, -1, null);
+
+		for (Amenity amenity : amenities) {
+			String stringEn = amenity.toStringEn();
+			if (stringEn.equals(nameStringEn)) {
+				return amenity;
+			}
+		}
+		return null;
+	}
+
+	private TransportStop findTransportStop(String nameStringEn, double lat, double lon) {
+
+		QuadRect rect = MapUtils.calculateLatLonBbox(lat, lon, 15);
+		List<TransportStop> res = app.getResourceManager().searchTransportSync(rect.top, rect.left,
+				rect.bottom, rect.right, new ResultMatcher<TransportStop>() {
+
+					@Override
+					public boolean publish(TransportStop object) {
+						return true;
+					}
+
+					@Override
+					public boolean isCancelled() {
+						return false;
+					}
+				});
+
+		for (TransportStop stop : res) {
+			String stringEn = stop.toStringEn();
+			if (stringEn.equals(nameStringEn)) {
+				return stop;
+			}
+		}
+		return null;
 	}
 }
--- OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/WptPtMenuBuilder.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/WptPtMenuBuilder.java
@@ -6,17 +6,22 @@ import net.osmand.plus.GPXUtilities.WptPt;
 import net.osmand.plus.OsmAndFormatter;
 import net.osmand.plus.R;
 import net.osmand.plus.activities.MapActivity;
+import net.osmand.plus.mapcontextmenu.MenuBuilder;
+import net.osmand.plus.mapillary.MapillaryPlugin;
 import net.osmand.plus.views.POIMapLayer;
 import net.osmand.util.Algorithms;
 
 import java.text.DateFormat;
 import java.util.Date;
 
-public class WptPtMenuBuilder extends SyncedItemMenuBuilder {
+public class WptPtMenuBuilder extends MenuBuilder {
+
+	private final WptPt wpt;
 
 	public WptPtMenuBuilder(MapActivity mapActivity, final WptPt wpt) {
 		super(mapActivity);
-		this.wptPt = wpt;
+		this.wpt = wpt;
+		setShowNearestWiki(true);
 	}
 
 	@Override
@@ -26,9 +31,48 @@ public class WptPtMenuBuilder extends SyncedItemMenuBuilder {
 
 	@Override
 	public void buildInternal(View view) {
-		buildWptPtInternal(view);
+		if (wpt.time > 0) {
+			DateFormat dateFormat = android.text.format.DateFormat.getMediumDateFormat(view.getContext());
+			DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(view.getContext());
+			Date date = new Date(wpt.time);
+			buildRow(view, R.drawable.ic_action_data,
+					dateFormat.format(date) + " — " + timeFormat.format(date), 0, false, null, false, 0, false, null);
+		}
+		if (wpt.speed > 0) {
+			buildRow(view, R.drawable.ic_action_speed,
+					OsmAndFormatter.getFormattedSpeed((float)wpt.speed, app), 0, false, null, false, 0, false, null);
+		}
+		if (!Double.isNaN(wpt.ele)) {
+			buildRow(view, R.drawable.ic_action_altitude,
+					OsmAndFormatter.getFormattedDistance((float) wpt.ele, app), 0, false, null, false, 0, false, null);
+		}
+		if (!Double.isNaN(wpt.hdop)) {
+			buildRow(view, R.drawable.ic_action_gps_info,
+					Algorithms.capitalizeFirstLetterAndLowercase(app.getString(R.string.plugin_distance_point_hdop)) + ": " + (int)wpt.hdop, 0,
+					false, null, false, 0, false, null);
+		}
+		if (!Algorithms.isEmpty(wpt.desc)) {
+			final View row = buildRow(view, R.drawable.ic_action_note_dark, wpt.desc, 0, false, null, true, 10, false, null);
+			row.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					POIMapLayer.showDescriptionDialog(row.getContext(), app, wpt.desc,
+							row.getResources().getString(R.string.description));
+				}
+			});
+		}
+		if (!Algorithms.isEmpty(wpt.comment)) {
+			final View rowc = buildRow(view, R.drawable.ic_action_note_dark, wpt.comment, 0,
+					false, null, true, 10, false, null);
+			rowc.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					POIMapLayer.showDescriptionDialog(rowc.getContext(), app, wpt.comment,
+							rowc.getResources().getString(R.string.poi_dialog_comment));
+				}
+			});
+		}
 
 		buildPlainMenuItems(view);
 	}
 }
-
