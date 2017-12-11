6e8ca20e3481f431f99df82f5e94df529b80266b
Alexander Sytnyk <asytnyk0@gmail.com>
2017-12-11 17:15:53 +0200

    Fix #3597

--- OsmAnd/src/net/osmand/plus/TargetPointsHelper.java
+++ OsmAnd/src/net/osmand/plus/TargetPointsHelper.java
@@ -474,7 +474,7 @@ public class TargetPointsHelper {
 
 
 	public boolean hasTooLongDistanceToNavigate() {
-		if(settings.ROUTER_SERVICE.get() != RouteService.OSMAND) {
+		if(settings.ROUTER_SERVICE.getModeValue(routingHelper.getAppMode()) != RouteService.OSMAND) {
 			return false;
 		}
 		Location current = routingHelper.getLastProjection();
