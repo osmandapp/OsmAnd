c29186a6a025700c55d3448ef56e4cffdc687179
Alexander Sytnyk <asytnyk0@gmail.com>
2018-01-23 17:52:26 +0200

    Fix #4918

--- OsmAnd/src/net/osmand/plus/dashboard/DashboardOnMap.java
+++ OsmAnd/src/net/osmand/plus/dashboard/DashboardOnMap.java
@@ -1238,7 +1238,7 @@ public class DashboardOnMap implements ObservableScrollViewCallbacks, DynamicLis
 			animators.add(ObjectAnimator.ofFloat(content, View.TRANSLATION_X, initialValueX, finalValueX));
 			animators.add(ObjectAnimator.ofFloat(content, View.TRANSLATION_Y, initialValueY, finalValueY));
 		}
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && ViewCompat.isAttachedToWindow(content)) {
 			int centerX = content.getMeasuredWidth() / 2;
 			int centerY = content.getMeasuredHeight() / 2;
 			float initialRadius = show ? 0 : (float) Math.sqrt(Math.pow(content.getWidth() / 2, 2) + Math.pow(content.getHeight() / 2, 2));
