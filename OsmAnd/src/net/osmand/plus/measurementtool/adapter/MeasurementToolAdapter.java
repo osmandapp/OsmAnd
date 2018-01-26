939ba0558aeddaebf6a9f555824fa4319bb59c0e
PaulStets <stetsenko.pavlo@gmail.com>
2018-01-26 16:51:56 +0200

    Removed unused import

--- OsmAnd/src/net/osmand/plus/measurementtool/adapter/MeasurementToolAdapter.java
+++ OsmAnd/src/net/osmand/plus/measurementtool/adapter/MeasurementToolAdapter.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import net.osmand.plus.GPXUtilities.WptPt;
