95d1c7370e637af53b7798e2704b46dd56ed585f
Alexander Sytnyk <asytnyk0@gmail.com>
2018-02-02 12:06:37 +0200

    Fix NPE that occurs when swapping start and finish points

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/other/MapRouteInfoMenu.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/other/MapRouteInfoMenu.java
@@ -306,7 +306,7 @@ public class MapRouteInfoMenu implements IRouteInformationListener {
 					}
 				}
 
-				if (startPoint != null) {
+				if (startPoint != null && endPoint != null) {
 					targetPointsHelper.navigateToPoint(startPoint.point, false, -1, startPoint.getPointDescription(mapActivity));
 					targetPointsHelper.setStartPoint(endPoint.point, false, endPoint.getPointDescription(mapActivity));
 					targetPointsHelper.updateRouteAndRefresh(true);
