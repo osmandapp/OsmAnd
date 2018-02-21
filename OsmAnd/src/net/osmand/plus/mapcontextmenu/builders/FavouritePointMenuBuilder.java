5dc839c9c0f66bcc4b07b0815835eb58187a60b4
Chumva <chumva.forever@gmail.com>
2018-02-21 12:02:08 +0200

    added free space between methods

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/FavouritePointMenuBuilder.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/FavouritePointMenuBuilder.java
@@ -67,6 +67,7 @@ public class FavouritePointMenuBuilder extends MenuBuilder {
 		super.buildTopInternal(view);
 		buildGroupFavouritesView(view);
 	}
+
 	@Override
 	public void buildInternal(View view) {
 		if (originObject != null && originObject instanceof Amenity) {
