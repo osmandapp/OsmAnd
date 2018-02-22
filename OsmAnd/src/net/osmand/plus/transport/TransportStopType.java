cd74a268aa6223e0ce2d19965d5463bbb349c8dc
Chumva <chumva.forever@gmail.com>
2018-02-22 17:53:40 +0200

    deleted unnecessary stuff in TransportStopType

--- OsmAnd/src/net/osmand/plus/transport/TransportStopType.java
+++ OsmAnd/src/net/osmand/plus/transport/TransportStopType.java
@@ -3,29 +3,27 @@ package net.osmand.plus.transport;
 import net.osmand.plus.R;
 
 public enum TransportStopType {
-	BUS(R.drawable.mx_route_bus_ref, R.drawable.mx_route_bus_ref, R.drawable.mm_route_bus_ref, "routeBusColor","route_bus_ref"),
-	FERRY(R.drawable.mx_route_ferry_ref, R.drawable.mx_route_ferry_ref, R.drawable.mm_route_ferry_ref, "routeFerryColor","route_ferry_ref"),
-	FUNICULAR(R.drawable.mx_route_funicular_ref, R.drawable.mx_route_funicular_ref, R.drawable.mm_route_funicular_ref, "routeFunicularColor","route_funicular_ref"),
-	LIGHT_RAIL(R.drawable.mx_route_light_rail_ref, R.drawable.mx_route_light_rail_ref, R.drawable.mm_route_light_rail_ref, "routeLightrailColor","route_light_rail_ref"),
-	MONORAIL(R.drawable.mx_route_monorail_ref, R.drawable.mx_route_monorail_ref, R.drawable.mm_route_monorail_ref, "routeLightrailColor","route_monorail_ref"),
-	RAILWAY(R.drawable.mx_route_railway_ref, R.drawable.mx_route_railway_ref, R.drawable.mm_route_railway_ref, "routeTrainColor","route_railway_ref"),
-	SHARE_TAXI(R.drawable.mx_route_share_taxi_ref, R.drawable.mx_route_share_taxi_ref, R.drawable.mm_route_share_taxi_ref, "routeShareTaxiColor","route_share_taxi_ref"),
-	TRAIN(R.drawable.mx_route_train_ref, R.drawable.mx_route_train_ref, R.drawable.mm_route_train_ref, "routeTrainColor","route_train_ref"),
-	TRAM(R.drawable.mx_route_tram_ref, R.drawable.mx_railway_tram_stop, R.drawable.mx_route_tram_ref, "routeTramColor","route_tram_ref"),
-	TROLLEYBUS(R.drawable.mx_route_trolleybus_ref, R.drawable.mx_route_trolleybus_ref, R.drawable.mm_route_trolleybus_ref, "routeTrolleybusColor","route_trolleybus_ref"),
-	SUBWAY(R.drawable.mx_subway_station, R.drawable.mx_subway_station, R.drawable.mm_subway_station, "routeTrainColor","subway_station");
+	BUS(R.drawable.mx_route_bus_ref, R.drawable.mx_route_bus_ref, "routeBusColor", "route_bus_ref"),
+	FERRY(R.drawable.mx_route_ferry_ref, R.drawable.mx_route_ferry_ref, "routeFerryColor", "route_ferry_ref"),
+	FUNICULAR(R.drawable.mx_route_funicular_ref, R.drawable.mx_route_funicular_ref, "routeFunicularColor", "route_funicular_ref"),
+	LIGHT_RAIL(R.drawable.mx_route_light_rail_ref, R.drawable.mx_route_light_rail_ref, "routeLightrailColor", "route_light_rail_ref"),
+	MONORAIL(R.drawable.mx_route_monorail_ref, R.drawable.mx_route_monorail_ref, "routeLightrailColor", "route_monorail_ref"),
+	RAILWAY(R.drawable.mx_route_railway_ref, R.drawable.mx_route_railway_ref, "routeTrainColor", "route_railway_ref"),
+	SHARE_TAXI(R.drawable.mx_route_share_taxi_ref, R.drawable.mx_route_share_taxi_ref, "routeShareTaxiColor", "route_share_taxi_ref"),
+	TRAIN(R.drawable.mx_route_train_ref, R.drawable.mx_route_tram_ref, "routeTrainColor", "route_train_ref"),
+	TRAM(R.drawable.mx_route_tram_ref, R.drawable.mx_railway_tram_stop, "routeTramColor", "route_tram_ref"),
+	TROLLEYBUS(R.drawable.mx_route_trolleybus_ref, R.drawable.mx_route_trolleybus_ref, "routeTrolleybusColor", "route_trolleybus_ref"),
+	SUBWAY(R.drawable.mx_subway_station, R.drawable.mx_subway_station, "routeTrainColor", "subway_station");
 
 	final int resId;
 	final int topResId;
-	final int smallResId;
 	final String renderAttr;
 	final String nameDrawable;
 
-	TransportStopType(int resId, int topResId, int smallResId, String renderAttr, String nameDrawable) {
+	TransportStopType(int resId, int topResId, String renderAttr, String nameDrawable) {
 		this.resId = resId;
 		this.topResId = topResId;
 		this.renderAttr = renderAttr;
-		this.smallResId = smallResId;
 		this.nameDrawable = nameDrawable;
 	}
 
@@ -35,10 +33,6 @@ public enum TransportStopType {
 
 	public int getTopResourceId() {
 		return topResId;
-	}
-
-	public int getSmallResId() {
-		return smallResId;
 	}
 
 	public String getNameDrawable() {
