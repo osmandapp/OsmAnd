da60b8ef7d0b4ea20a7afe0adc6c98aa12dd2e5d
Chumva <chumva.forever@gmail.com>
2018-03-05 12:08:49 +0200

    now we check link for wikipedia

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/AmenityMenuBuilder.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/AmenityMenuBuilder.java
@@ -290,7 +290,7 @@ public class AmenityMenuBuilder extends MenuBuilder {
 			ll.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
-					if (textPrefix.equals("Wikipedia")) {
+					if (text.contains("wikipedia.org")) {
 						WikipediaDialogFragment.showFullArticle(v.getContext(), Uri.parse(text), !light);
 					} else {
 						Intent intent = new Intent(Intent.ACTION_VIEW);
