ce25dfe0ae7448e0b9e8f7944240c441a4630016
Chumva <chumva.forever@gmail.com>
2018-03-21 14:56:50 +0200

    added locRes

--- OsmAnd/src/net/osmand/plus/AppInitializer.java
+++ OsmAnd/src/net/osmand/plus/AppInitializer.java
@@ -331,6 +331,7 @@ public class AppInitializer implements IProgress {
 		}
 
 		final Resources en = getLocalizedResources("en");
+		final Resources locRes = getLocalizedResources(app.getLanguage());
 
 		app.poiTypes.setPoiTranslator(new MapPoiTypes.PoiTranslator() {
 
@@ -365,13 +366,14 @@ public class AppInitializer implements IProgress {
 				return getSynonyms(type.getIconKeyName());
 			}
 
+
 			@Override
 			public String getSynonyms(String keyName) {
 				try {
 					Field f = R.string.class.getField("synonyms_poi_" + keyName);
 					if (f != null) {
 						Integer in = (Integer) f.get(null);
-						return app.getString(in);
+						return locRes.getString(in);
 					}
 				} catch (Exception e) {
 				}
