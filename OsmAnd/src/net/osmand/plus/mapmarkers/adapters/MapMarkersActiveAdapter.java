e63524f1e77098349ebc7ea89c4cf8f44cb939c0
Chumva <chumva.forever@gmail.com>
2018-03-28 13:48:46 +0300

    Change check for empty category

--- OsmAnd/src/net/osmand/plus/mapmarkers/adapters/MapMarkersActiveAdapter.java
+++ OsmAnd/src/net/osmand/plus/mapmarkers/adapters/MapMarkersActiveAdapter.java
@@ -18,6 +18,7 @@ import net.osmand.plus.MapMarkersHelper.MapMarker;
 import net.osmand.plus.R;
 import net.osmand.plus.activities.MapActivity;
 import net.osmand.plus.dashboard.DashLocationFragment;
+import net.osmand.util.Algorithms;
 
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -160,7 +161,7 @@ public class MapMarkersActiveAdapter extends RecyclerView.Adapter<MapMarkerItemV
 			String day = new SimpleDateFormat("d", Locale.getDefault()).format(date);
 			descr = month + " " + day;
 		}
-		if (marker.wptPt != null && marker.wptPt.category != null) {
+		if (marker.wptPt != null && !Algorithms.isEmpty(marker.wptPt.category)) {
 			descr = marker.wptPt.category + ", " + descr;
 		}
 		holder.description.setText(descr);
