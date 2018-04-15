fa65d7337b81e2bde258397ab3bb2d1053227d2e
PaulStets <stetsenko.pavlo@gmail.com>
2018-04-15 11:22:40 +0300

    Fixed contents

--- OsmAnd/src/net/osmand/plus/wikivoyage/article/WikivoyageArticleDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/wikivoyage/article/WikivoyageArticleDialogFragment.java
@@ -48,8 +48,7 @@ public class WikivoyageArticleDialogFragment extends WikivoyageBaseDialogFragmen
 			"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n" +
 			"<meta http-equiv=\"cleartype\" content=\"on\" />\n" +
 			"<link href=\"article_style.css\" type=\"text/css\" rel=\"stylesheet\"/>\n" +
-			"</head><body>\n" + "<script>" + "function scrollAnchor(id) {" +
-			"window.location.hash = id;}</script>";
+			"</head><body>\n";
 	private static final String FOOTER_INNER = "<script>var coll = document.getElementsByTagName(\"H2\");" +
 			"var i;" +
 			"for (i = 0; i < coll.length; i++) {" +
@@ -62,8 +61,16 @@ public class WikivoyageArticleDialogFragment extends WikivoyageBaseDialogFragmen
 			"      content.style.display = \"block\";" +
 			"    }" +
 			"  });" +
+			"}" + "function scrollAnchor(id, title) {" +
+			"openContent(title);" +
+			"window.location.hash = id;}\n" +
+			"function openContent(id) {\n" +
+			"var doc = document.getElementById(id).parentElement;\n" +
+			"    doc.classList.toggle(\"active\");\n" +
+			"    var content = doc.nextElementSibling;\n" +
+			"content.style.display = \"block\";\n" +
 			"}</script>"
-			+ "</div></body></html>";
+			+ "</body></html>";
 
 	private long cityId = NO_VALUE;
 	private ArrayList<String> langs;
@@ -157,7 +164,8 @@ public class WikivoyageArticleDialogFragment extends WikivoyageBaseDialogFragmen
 		super.onActivityResult(requestCode, resultCode, data);
 		if (requestCode == WikivoyageArticleContentsFragment.REQUEST_LINK_CODE) {
 			String link = data.getStringExtra(WikivoyageArticleContentsFragment.CONTENTS_LINK_KEY);
-			moveToAnchor(link);
+			String title = data.getStringExtra(WikivoyageArticleContentsFragment.CONTENTS_TITLE_KEY);
+			moveToAnchor(link, title);
 		}
 	}
 
@@ -240,8 +248,8 @@ public class WikivoyageArticleDialogFragment extends WikivoyageBaseDialogFragmen
 		contentWebView.loadDataWithBaseURL(getBaseUrl(), createHtmlContent(article), "text/html", "UTF-8", null);
 	}
 
-	private void moveToAnchor(String id) {
-		contentWebView.loadUrl("javascript:scrollAnchor(\"" + id + "\")");
+	private void moveToAnchor(String id, String title) {
+		contentWebView.loadUrl("javascript:scrollAnchor(\"" + id + "\", \"" + title.trim() + "\")");
 	}
 
 	@NonNull
