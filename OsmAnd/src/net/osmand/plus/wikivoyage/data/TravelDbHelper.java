7ce8b69be15e69b5d92947a11b6f6ad7c1be270f
Chumva <chumva.forever@gmail.com>
2018-04-26 19:04:10 +0300

    change sqlQuery for random articles

--- OsmAnd/src/net/osmand/plus/wikivoyage/data/TravelDbHelper.java
+++ OsmAnd/src/net/osmand/plus/wikivoyage/data/TravelDbHelper.java
@@ -208,11 +208,10 @@ public class TravelDbHelper {
 		SQLiteConnection conn = openConnection();
 		if (conn != null) {
 			TravelArticle travelArticle;
-			SQLiteCursor cursor = conn.rawQuery("SELECT * FROM "
-					+ ARTICLES_TABLE_NAME
-					+ " WHERE article_id IN (SELECT article_id FROM "
-					+ ARTICLES_TABLE_NAME
-					+ " ORDER BY RANDOM() LIMIT 100) LIMIT 100", null);
+			String lang = application.getLanguage();
+			SQLiteCursor cursor = conn.rawQuery("SELECT * FROM wikivoyage_articles"
+					+ " WHERE title IN (SELECT title FROM popular_articles WHERE lang = ?"
+					+ " ORDER BY RANDOM()) ORDER BY RANDOM() LIMIT 100", new String[]{lang});
 			if (cursor.moveToFirst()) {
 				do {
 					travelArticle = readArticle(cursor);
