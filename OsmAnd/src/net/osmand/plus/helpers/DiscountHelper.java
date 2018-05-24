d62f5a53dfccd9ff5b6a23c7e128305efe33c597
Chumva <chumva.forever@gmail.com>
2018-05-24 18:44:10 +0300

    add intent and intent-filter for sharing wikivoyage links

--- OsmAnd/src/net/osmand/plus/helpers/DiscountHelper.java
+++ OsmAnd/src/net/osmand/plus/helpers/DiscountHelper.java
@@ -18,6 +18,7 @@ import net.osmand.plus.chooseplan.ChoosePlanDialogFragment;
 import net.osmand.plus.inapp.InAppPurchaseHelper;
 import net.osmand.plus.views.mapwidgets.MapInfoWidgetsFactory.TopToolbarController;
 import net.osmand.plus.views.mapwidgets.MapInfoWidgetsFactory.TopToolbarControllerType;
+import net.osmand.plus.wikipedia.WikiArticleHelper;
 import net.osmand.util.Algorithms;
 
 import org.json.JSONObject;
@@ -37,9 +38,12 @@ public class DiscountHelper {
 	private static String mDescription;
 	private static String mIcon;
 	private static String mUrl;
+	private static String mTravelUrl;
+	private static String mLang;
 	private static boolean mBannerVisible;
 	private static final String URL = "https://osmand.net/api/motd";
 	private static final String INAPP_PREFIX = "osmand-in-app:";
+	private static final String TRAVEL_PREFIX = "travel:";
 
 
 	public static void checkAndDisplay(final MapActivity mapActivity) {
@@ -49,7 +53,7 @@ public class DiscountHelper {
 			return;
 		}
 		if (mBannerVisible) {
-			showDiscountBanner(mapActivity, mTitle, mDescription, mIcon, mUrl);
+			showDiscountBanner(mapActivity, mTitle, mDescription, mIcon, mUrl, mTravelUrl, mLang);
 		}
 		if (System.currentTimeMillis() - mLastCheckTime < 1000 * 60 * 60 * 24
 				|| !settings.isInternetConnectionAvailable()
@@ -100,6 +104,8 @@ public class DiscountHelper {
 			String description = obj.getString("description");
 			String icon = obj.getString("icon");
 			String url = parseUrl(app, obj.getString("url"));
+			String travelUrl = parseUrl(app, obj.getString("travel"));
+			String lang = parseUrl(app, obj.getString("lang"));
 			SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm");
 			Date start = df.parse(obj.getString("start"));
 			Date end = df.parse(obj.getString("end"));
@@ -136,7 +142,7 @@ public class DiscountHelper {
 						settings.DISCOUNT_TOTAL_SHOW.set(settings.DISCOUNT_TOTAL_SHOW.get() + 1);
 						settings.DISCOUNT_SHOW_NUMBER_OF_STARTS.set(app.getAppInitializer().getNumberOfStarts());
 						settings.DISCOUNT_SHOW_DATETIME_MS.set(System.currentTimeMillis());
-						showDiscountBanner(mapActivity, message, description, icon, url);	
+						showDiscountBanner(mapActivity, message, description, icon, url, travelUrl, lang);
 					}
 				}
 			}
@@ -166,8 +172,8 @@ public class DiscountHelper {
 		return result;
 	}
 
-	private static void showDiscountBanner(final MapActivity mapActivity, final String title,
-										   final String description, final String icon, final String url) {
+	private static void showDiscountBanner(final MapActivity mapActivity, final String title, final String description,
+	                                       final String icon, final String url, final String travelUrl, final String lang) {
 		final DiscountBarController toolbarController = new DiscountBarController();
 		toolbarController.setTitle(title);
 		toolbarController.setDescription(description);
@@ -180,7 +186,7 @@ public class DiscountHelper {
 					mapActivity.getMyApplication().logEvent(mapActivity, "motd_click");
 					mBannerVisible = false;
 					mapActivity.hideTopToolbar(toolbarController);
-					openUrl(mapActivity, url);
+					openUrl(mapActivity, url, lang);
 				}
 			});
 			toolbarController.setOnTitleClickListener(new View.OnClickListener() {
@@ -189,7 +195,7 @@ public class DiscountHelper {
 					mapActivity.getMyApplication().logEvent(mapActivity, "motd_click");
 					mBannerVisible = false;
 					mapActivity.hideTopToolbar(toolbarController);
-					openUrl(mapActivity, url);
+					openUrl(mapActivity, url, lang);
 				}
 			});
 		}
@@ -206,12 +212,14 @@ public class DiscountHelper {
 		mDescription = description;
 		mIcon = icon;
 		mUrl = url;
+		mTravelUrl = travelUrl;
+		mLang = lang;
 		mBannerVisible = true;
 
 		mapActivity.showTopToolbar(toolbarController);
 	}
 
-	private static void openUrl(final MapActivity mapActivity, String url) {
+	private static void openUrl(final MapActivity mapActivity, String url, String lang) {
 		if (url.startsWith(INAPP_PREFIX)) {
 			if (url.contains(InAppPurchaseHelper.SKU_FULL_VERSION_PRICE)) {
 				OsmandApplication app = mapActivity.getMyApplication();
@@ -223,6 +231,11 @@ public class DiscountHelper {
 			} else if (url.contains(InAppPurchaseHelper.SKU_LIVE_UPDATES)){
 				ChoosePlanDialogFragment.showOsmLiveInstance(mapActivity.getSupportFragmentManager());
 			}
+		} else if (url.startsWith(TRAVEL_PREFIX)) {
+			Intent intent = new Intent(Intent.ACTION_SEND);
+			intent.putExtra(Intent.EXTRA_TEXT, WikiArticleHelper.buildTravelUrl(url, lang));
+			intent.setType("text/plain");
+			mapActivity.startActivity(Intent.createChooser(intent, mapActivity.getString(R.string.share_article)));
 		} else {
 			Intent intent = new Intent(Intent.ACTION_VIEW);
 			intent.setData(Uri.parse(url));
