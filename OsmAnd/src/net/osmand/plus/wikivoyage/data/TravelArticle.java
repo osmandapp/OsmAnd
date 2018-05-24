24c6c96b7caeb9d841934276fb6a7da999362571
PaulStets <stetsenko.pavlo@gmail.com>
2018-05-24 15:32:10 +0300

    Removed unused imports

--- OsmAnd/src/net/osmand/plus/wikivoyage/data/TravelArticle.java
+++ OsmAnd/src/net/osmand/plus/wikivoyage/data/TravelArticle.java
@@ -8,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.Size;
 import android.text.TextUtils;
-
 import net.osmand.plus.GPXUtilities.GPXFile;
 
 import org.apache.commons.codec.binary.Hex;
