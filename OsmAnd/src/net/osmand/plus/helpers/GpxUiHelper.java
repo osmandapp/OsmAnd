0cd0fb59d35286ac3b40523e7cd7e13ebed1f602
Victor Shcherb <victor.shcherb@gmail.com>
2018-05-25 10:07:22 +0200

    Fix crash

--- OsmAnd/src/net/osmand/plus/helpers/GpxUiHelper.java
+++ OsmAnd/src/net/osmand/plus/helpers/GpxUiHelper.java
@@ -381,7 +381,7 @@ public class GpxUiHelper {
 					public void onClick(DialogInterface dialog, int which) {
 
 						int position = selectedPosition[0];
-						if (position != -1) {
+						if (position != -1 && position < list.size()) {
 							if (showCurrentGpx && position == 0) {
 								callbackWithObject.processResult(null);
 								app.getSettings().LAST_SELECTED_GPX_TRACK_FOR_NEW_POINT.set(null);
