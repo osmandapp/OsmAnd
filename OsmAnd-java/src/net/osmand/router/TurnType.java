402d4f4d60434e339f10f76184577873bfd9d6f6
Victor Shcherb <victor.shcherb@gmail.com>
2018-05-26 00:02:57 +0200

    Fix turn lanes bug (a test fix)

--- OsmAnd-java/src/net/osmand/router/TurnType.java
+++ OsmAnd-java/src/net/osmand/router/TurnType.java
@@ -416,11 +416,13 @@ public class TurnType {
 	public static int convertType(String lane) {
 		int turn;
 		// merge should be recognized as continue route (but it could displayed differently)
-		if (lane.equals("none") || lane.equals("through") 
-				|| lane.equals("merge_to_left") 
-				|| lane.equals("merge_to_right")) {
+		if(lane.equals("merge_to_left")) {
 			turn = TurnType.C;
-		} else if (lane.equals("slight_right") ) {
+		} else if(lane.equals("merge_to_right")) {
+			turn = TurnType.C;
+		} else if (lane.equals("none") || lane.equals("through")) {
+			turn = TurnType.C;
+		} else if (lane.equals("slight_right")) {
 			turn = TurnType.TSLR;
 		} else if (lane.equals("slight_left") ) {
 			turn = TurnType.TSLL;
