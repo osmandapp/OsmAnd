2223bae32df338f9fec0c8007de6644354086734
Victor Shcherb <victor.shcherb@gmail.com>
2018-05-29 23:39:12 +0200

    Fix double progress

--- OsmAnd-java/src/net/osmand/router/RoutePlannerFrontEnd.java
+++ OsmAnd-java/src/net/osmand/router/RoutePlannerFrontEnd.java
@@ -159,7 +159,6 @@ public class RoutePlannerFrontEnd {
 		if (ctx.calculationMode == RouteCalculationMode.COMPLEX && routeDirection == null
 				&& maxDistance > ctx.config.DEVIATION_RADIUS * 6) {
 			ctx.calculationProgress.totalIterations++;
-			ctx.calculationProgress.iteration++;
 			RoutingContext nctx = buildRoutingContext(ctx.config, ctx.nativeLib, ctx.getMaps(), RouteCalculationMode.BASE);
 			nctx.calculationProgress = ctx.calculationProgress;
 			List<RouteSegmentResult> ls = searchRoute(nctx, start, end, intermediates);
