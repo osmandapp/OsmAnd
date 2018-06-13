0df007057c2668eed030205c0b6b11cabf2d7963
Chumva <chumva.forever@gmail.com>
2018-06-13 14:43:29 +0300

    fix direction arrow with location from map in history/addresses

--- OsmAnd/src/net/osmand/plus/search/QuickSearchListAdapter.java
+++ OsmAnd/src/net/osmand/plus/search/QuickSearchListAdapter.java
@@ -486,7 +486,7 @@ public class QuickSearchListAdapter extends ArrayAdapter<QuickSearchListItem> {
 		ImageView direction = (ImageView) view.findViewById(R.id.direction);
 		SearchPhrase phrase = listItem.getSearchResult().requiredSearchPhrase;
 		updateLocationViewCache.specialFrom =  null;
-		if(phrase != null && useMapCenter) {
+		if (phrase != null && !Algorithms.isEmpty(phrase.getUnknownSearchPhrase()) && useMapCenter) {
 			updateLocationViewCache.specialFrom = phrase.getSettings().getOriginalLocation();
 		}
 		LatLon toloc = listItem.getSearchResult().location;
