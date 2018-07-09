2aa0060febecc3f0812b2ceac86b61d44f323408
Alex Sytnyk <asytnyk0@gmail.com>
2018-07-09 13:44:53 +0300

    Add the api method for opening OsmAnd at given location or AMapPoint

--- OsmAnd/src/net/osmand/plus/views/AidlMapLayer.java
+++ OsmAnd/src/net/osmand/plus/views/AidlMapLayer.java
@@ -8,6 +8,8 @@ import android.graphics.Paint;
 import android.graphics.PointF;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import net.osmand.AndroidUtils;
@@ -206,6 +208,11 @@ public class AidlMapLayer extends OsmandMapLayer implements IContextMenuProvider
 		} else {
 			return null;
 		}
+	}
+
+	@Nullable
+	public AMapPoint getPoint(@NonNull String id) {
+		return aidlLayer.getPoint(id);
 	}
 
 	private void getFromPoint(RotatedTileBox tb, PointF point, List<? super AMapPoint> points) {
