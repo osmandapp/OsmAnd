c1ef6650a984200397abe81603b84983d751d7a0
Victor Shcherb <victor.shcherb@gmail.com>
2018-07-25 23:59:17 +0200

    Clean toolchain

--- OsmAnd/jni/Application.mk
+++ OsmAnd/jni/Application.mk
@@ -5,15 +5,15 @@ APP_SHORT_COMMANDS := true
 # Specify least supported Android platform version
 APP_PLATFORM := android-14
 
-ifeq ($(wildcard $(ANDROID_NDK)/toolchains/*-4.7),)
-	ifeq ($(wildcard $(ANDROID_NDK)/toolchains/*-4.8),)
-		NDK_TOOLCHAIN_VERSION := 4.9
-	else
-		NDK_TOOLCHAIN_VERSION := 4.8
-	endif
-else
-	NDK_TOOLCHAIN_VERSION := 4.7
-endif
+# ifeq ($(wildcard $(ANDROID_NDK)/toolchains/*-4.7),)
+# 	ifeq ($(wildcard $(ANDROID_NDK)/toolchains/*-4.8),)
+# 		NDK_TOOLCHAIN_VERSION := 4.9
+# 	else
+# 		NDK_TOOLCHAIN_VERSION := 4.8
+# 	endif
+# else
+# 	NDK_TOOLCHAIN_VERSION := 4.7
+# endif
 
 APP_ABI := x86 armeabi-v7a arm64-v8a
 ifndef OSMAND_DEBUG_NATIVE
