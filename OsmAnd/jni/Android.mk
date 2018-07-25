fececbcf021141190a946c7359db1cfefbca1a1c
Victor Shcherb <victor.shcherb@gmail.com>
2018-07-26 01:01:45 +0200

    cleanup properties

--- OsmAnd/jni/Android.mk
+++ OsmAnd/jni/Android.mk
@@ -2,19 +2,16 @@ LOCAL_PATH := $(call my-dir)
 ROOT_PATH := $(LOCAL_PATH)/../../..
 $(info OsmAnd root: $(ROOT_PATH))
 
-# LEGACY {
 ifdef BUILD_ONLY_OLD_LIB
 OSMAND_MAKEFILES := \
     $(all-subdir-makefiles) \
     $(call all-makefiles-under,$(ROOT_PATH)/core-legacy/targets/android)
 else 
-# } LEGACY
 OSMAND_MAKEFILES := \
     $(all-subdir-makefiles) \
 	$(call all-makefiles-under,$(ROOT_PATH)/core/wrappers/android)
-# LEGACY {
 endif
-# } LEGACY
+
 $(info OsmAnd makefiles: $(OSMAND_MAKEFILES))
 
 # By default, include makefiles only once
