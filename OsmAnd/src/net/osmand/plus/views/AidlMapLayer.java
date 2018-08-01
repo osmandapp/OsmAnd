e654d0224180942b6745c9cd3363e507831e014b
Alex Sytnyk <asytnyk0@gmail.com>
2018-08-01 16:39:45 +0300

    Fix vertical offset
    (cherry picked from commit cd688b4)

--- OsmAnd/src/net/osmand/plus/views/AidlMapLayer.java
+++ OsmAnd/src/net/osmand/plus/views/AidlMapLayer.java
@@ -173,8 +173,7 @@ public class AidlMapLayer extends OsmandMapLayer implements IContextMenuProvider
 			canvas.drawBitmap(image, null, getDstRect(x, y, smallIconSize / 2), bitmapPaint);
 		} else if (pointsType == PointsType.BIG_ICON) {
 			bitmapPaint.setColorFilter(null);
-			// FIXME: vertical offset
-			float vOffset = bigIconBg.getHeight() * 0.9f;
+			float vOffset = bigIconBg.getHeight() * 0.91f;
 			int imageCenterY = (int) (y - vOffset + bigIconBg.getHeight() / 2);
 			canvas.drawBitmap(bigIconBg, x - bigIconBg.getWidth() / 2, y - vOffset, bitmapPaint);
 			canvas.drawBitmap(image, null, getDstRect(x, imageCenterY, bigIconSize / 2), bitmapPaint);
