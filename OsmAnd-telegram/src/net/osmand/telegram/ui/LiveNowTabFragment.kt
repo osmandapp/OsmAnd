f047a655c8a0e8ac2d16b36fa6bed3ccc43af313
Chumva <chumva.forever@gmail.com>
2018-08-10 12:25:36 +0300

    clean imports

--- OsmAnd-telegram/src/net/osmand/telegram/ui/LiveNowTabFragment.kt
+++ OsmAnd-telegram/src/net/osmand/telegram/ui/LiveNowTabFragment.kt
@@ -1,7 +1,6 @@
 package net.osmand.telegram.ui
 
 import android.os.Bundle
-import android.support.design.widget.AppBarLayout
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.ListPopupWindow
