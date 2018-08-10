0fbe99feeab03e8bce1ca2fca57d903326793fcf
PaulStets <stetsenko.pavlo@gmail.com>
2018-08-10 10:23:14 +0300

    Removed unused imports

--- OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
+++ OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
@@ -43,10 +43,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
