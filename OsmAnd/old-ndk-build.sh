27557b042c05f087033e7b6a1b63b698620f01ea
vshcherb <victor.shcherb@gmail.com>
2018-08-24 18:36:06 +0200

    Update old-ndk-build.sh

--- OsmAnd/old-ndk-build.sh
+++ OsmAnd/old-ndk-build.sh
@@ -2,8 +2,7 @@
 
 SCRIPT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME=$(basename $(dirname "${BASH_SOURCE[0]}") )
-echo "Test param $1"
-
+env 
 if [ -d "$ANDROID_HOME" ]; then
     # for backwards compatbility
     export ANDROID_SDK=$ANDROID_HOME
