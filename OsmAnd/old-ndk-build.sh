abd4e45d8fb77bdd8a1c6170fec4520f01ea7c6d
Victor Shcherb <victor.shcherb@gmail.com>
2018-09-30 20:50:14 +0200

    Clean ndk build

--- OsmAnd/old-ndk-build.sh
+++ OsmAnd/old-ndk-build.sh
@@ -4,23 +4,19 @@ SCRIPT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME=$(basename $(dirname "${BASH_SOURCE[0]}") )
 if [ -d "$ANDROID_HOME" ]; then
     # for backwards compatbility
-    export ANDROID_SDK=$ANDROID_HOME
-else
-	echo "ANDROID_HOME is not set, trying ANDROID_SDK:"
-    if [ -d "$ANDROID_SDK" ]; then
-        export ANDROID_HOME=$ANDROID_SDK
-    else
-	    echo "ANDROID_SDK is also not set"
-	    exit
-    fi
+    export ANDROID_SDK_ROOT=$ANDROID_HOME
 fi
-if [ ! -d "$ANDROID_NDK" ]; then
+if [ -d "$ANDROID_NDK" ]; then
+    export ANDROID_NDK_ROOT=$ANDROID_NDK
+fi
+if [ ! -d "$ANDROID_SDK_ROOT" ]; then
+    echo "ANDROID_SDK is not set"
+    exit
+fi
+if [ ! -d "$ANDROID_NDK_ROOT" ]; then
 	echo "ANDROID_NDK is not set"
 	exit
 fi
-export ANDROID_SDK_ROOT=$ANDROID_HOME
-export ANDROID_NDK_ROOT=$ANDROID_NDK
-#export ANDROID_NDK_TOOLCHAIN_VERSION=4.7
 export BUILD_ONLY_OLD_LIB=1
 "$SCRIPT_LOC/../../core-legacy/externals/configure.sh"
-(cd "$SCRIPT_LOC" && "$ANDROID_NDK/ndk-build" -j2)
+(cd "$SCRIPT_LOC" && "$ANDROID_NDK_ROOT/ndk-build" -j2)
