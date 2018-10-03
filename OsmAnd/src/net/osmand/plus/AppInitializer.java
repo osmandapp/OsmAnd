a8e7fa0f7712fc4c4499b1285753ccac0acf4a1f
crimean <alexey.kulish@osmand.net>
2018-10-03 15:25:22 +0300

    Fix #5845

--- OsmAnd/src/net/osmand/plus/AppInitializer.java
+++ OsmAnd/src/net/osmand/plus/AppInitializer.java
@@ -425,7 +425,7 @@ public class AppInitializer implements IProgress {
 					Field f = R.string.class.getField("poi_" + keyName);
 					if (f != null) {
 						Integer in = (Integer) f.get(null);
-						String val = app.getString(in);
+						String val = en.getString(in);
 						if(val != null) {
 							int ind = val.indexOf(';');
 							if(ind > 0) {
