f6ff93bd5bfaa8623b0e95a3cf1d90f2971a5a1d
crimean <alexey.kulish@osmand.net>
2018-11-28 18:16:46 +0300

    Fix #6137

--- OsmAnd-java/src/main/java/net/osmand/search/core/SearchCoreFactory.java
+++ OsmAnd-java/src/main/java/net/osmand/search/core/SearchCoreFactory.java
@@ -666,12 +666,15 @@ public class SearchCoreFactory {
 			List<AbstractPoiType> results = new ArrayList<AbstractPoiType>();
 			NameStringMatcher nm =
 					new NameStringMatcher(phrase.getUnknownSearchPhrase(), StringMatcherMode.CHECK_ONLY_STARTS_WITH_TRIM);
+
+			Set<String> filters = new HashSet<>();
 			for (AbstractPoiType pf : topVisibleFilters) {
 				if (!phrase.isUnknownSearchWordPresent()
 						|| nm.matches(pf.getTranslation())
 						|| nm.matches(pf.getEnTranslation())
 						|| nm.matches(pf.getSynonyms())) {
 					results.add(pf);
+					filters.add(pf.getTranslation());
 				}
 			}
 			if (phrase.isUnknownSearchWordPresent()) {
@@ -681,6 +684,7 @@ public class SearchCoreFactory {
 							|| nm.matches(c.getEnTranslation())
 							|| nm.matches(c.getSynonyms()))) {
 						results.add(c);
+						filters.add(c.getTranslation());
 					}
 				}
 				Iterator<Entry<String, PoiType>> it = translatedNames.entrySet().iterator();
@@ -688,7 +692,7 @@ public class SearchCoreFactory {
 					Entry<String, PoiType> e = it.next();
 					PoiType pt = e.getValue();
 					if (pt.getCategory() != types.getOtherMapCategory()) {
-						if (!results.contains(pt)
+						if (!results.contains(pt) && !filters.contains(pt.getTranslation())
 								&& (nm.matches(pt.getEnTranslation())
 								|| nm.matches(pt.getTranslation())
 								|| nm.matches(pt.getSynonyms()))) {
