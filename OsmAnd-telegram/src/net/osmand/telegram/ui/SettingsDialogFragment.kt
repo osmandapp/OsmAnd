fe6390075a90d29fbe433808f391f3523507fcb5
Chumva <chumva.forever@gmail.com>
2019-04-25 00:45:21 +0300

    Fix minor ui bugs

--- OsmAnd-telegram/src/net/osmand/telegram/ui/SettingsDialogFragment.kt
+++ OsmAnd-telegram/src/net/osmand/telegram/ui/SettingsDialogFragment.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.FragmentManager
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.ListPopupWindow
 import android.support.v7.widget.Toolbar
 import android.text.SpannableStringBuilder
@@ -39,14 +40,14 @@ class SettingsDialogFragment : BaseDialogFragment() {
 	): View {
 		val mainView = inflater.inflate(R.layout.fragement_settings_dialog, parent)
 
-		val appBarLayout = mainView.findViewById<View>(R.id.app_bar_layout)
-		AndroidUtils.addStatusBarPadding19v(context!!, appBarLayout)
-
 		mainView.findViewById<Toolbar>(R.id.toolbar).apply {
 			navigationIcon = uiUtils.getThemedIcon(R.drawable.ic_arrow_back)
 			setNavigationOnClickListener { dismiss() }
 		}
-
+		val window = dialog.window
+		if (window != null && Build.VERSION.SDK_INT >= 21) {
+			window.statusBarColor = ContextCompat.getColor(app, R.color.card_bg_light)
+		}
 		var container = mainView.findViewById<ViewGroup>(R.id.gps_and_loc_container)
 		for (pref in settings.gpsAndLocPrefs) {
 			inflater.inflate(R.layout.item_with_desc_and_right_value, container, false).apply {
