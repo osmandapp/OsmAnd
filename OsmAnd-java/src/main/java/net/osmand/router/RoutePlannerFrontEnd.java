7d58cf110db2fb319c9d9eb5d11f4389ecccc8cf
Victor Shcherb <victor.shcherb@gmail.com>
2019-06-08 12:40:19 +0300

    Fix tests

--- OsmAnd-java/src/main/java/net/osmand/router/RoutePlannerFrontEnd.java
+++ OsmAnd-java/src/main/java/net/osmand/router/RoutePlannerFrontEnd.java
@@ -26,7 +26,7 @@ import org.apache.commons.logging.Log;
 public class RoutePlannerFrontEnd {
 
 	protected static final Log log = PlatformUtil.getLog(RoutePlannerFrontEnd.class);
-	protected static double GPS_POSSIBLE_ERROR = 10;
+	protected static final double GPS_POSSIBLE_ERROR = 10;
 	public boolean useSmartRouteRecalculation = true;
 
 	
--- OsmAnd-java/src/test/java/net/osmand/router/RouteResultPreparationTest.java
+++ OsmAnd-java/src/test/java/net/osmand/router/RouteResultPreparationTest.java
@@ -69,7 +69,6 @@ public class RouteResultPreparationTest {
                 RoutePlannerFrontEnd.RouteCalculationMode.NORMAL);
         ctx.leftSideNavigation = false;
         RouteResultPreparation.PRINT_TO_CONSOLE_ROUTE_INFORMATION_TO_TEST = true;
-        RoutePlannerFrontEnd.GPS_POSSIBLE_ERROR = 0;
 
     }
 
