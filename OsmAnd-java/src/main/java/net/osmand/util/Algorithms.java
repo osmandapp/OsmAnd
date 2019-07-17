459236c3a109a65522b51ede7a97b0cdb6642212
crimean <alexey.kulish@osmand.net>
2019-07-17 11:11:11 +0300

    Fix travel UI

--- OsmAnd-java/src/main/java/net/osmand/util/Algorithms.java
+++ OsmAnd-java/src/main/java/net/osmand/util/Algorithms.java
@@ -461,14 +461,7 @@ public class Algorithms {
 
 	public static String gzipToString(byte[] gzip) throws IOException {
 		GZIPInputStream gzipIs = new GZIPInputStream(new ByteArrayInputStream(gzip));
-		BufferedReader br = new BufferedReader(new InputStreamReader(gzipIs, "UTF-8"));
-		StringBuilder sb = new StringBuilder();
-		String s;
-		while ((s = br.readLine()) != null) {
-			sb.append(s);
-		}
-		br.close();
-		return sb.toString();
+		return readFromInputStream(gzipIs).toString();
 	}
 
 	public static boolean removeAllFiles(File f) {
