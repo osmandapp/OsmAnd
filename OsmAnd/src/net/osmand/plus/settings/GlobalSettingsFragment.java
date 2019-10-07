b9e5648c5ee593ea08d747178f5cc4cb4410503a
Nazar <vitaminkuna@gmail.com>
2019-10-07 15:30:26 +0300

    DataStorage initial commit

--- OsmAnd/src/net/osmand/plus/settings/GlobalSettingsFragment.java
+++ OsmAnd/src/net/osmand/plus/settings/GlobalSettingsFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.SwitchPreferenceCompat;
 import android.util.Pair;
 
+import net.osmand.AndroidUtils;
 import net.osmand.plus.ApplicationMode;
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.OsmandSettings;
@@ -15,6 +16,9 @@ import net.osmand.plus.activities.SettingsGeneralActivity;
 import net.osmand.plus.dialogs.SendAnalyticsBottomSheetDialogFragment;
 import net.osmand.plus.settings.preferences.ListPreferenceEx;
 import net.osmand.plus.settings.preferences.SwitchPreferenceEx;
+
+import java.io.File;
+import java.text.DecimalFormat;
 
 
 public class GlobalSettingsFragment extends BaseSettingsFragment implements SendAnalyticsBottomSheetDialogFragment.OnSendAnalyticsPrefsUpdate, OnPreferenceChanged {
@@ -137,6 +141,14 @@ public class GlobalSettingsFragment extends BaseSettingsFragment implements Send
 		Preference externalStorageDir = (Preference) findPreference(OsmandSettings.EXTERNAL_STORAGE_DIR);
 		externalStorageDir.setIcon(getContentIcon(R.drawable.ic_action_folder));
 
+		DataStorageItemsHolder holder = DataStorageItemsHolder.refreshInfo(app);
+		DataStorageMenuItem currentStorage = holder.getCurrentStorage();
+		File dir = new File(currentStorage.getDirectory());
+		DecimalFormat formatter = new DecimalFormat("#.##");
+		String summary = String.format(getString(R.string.data_storage_preference_summary),
+						currentStorage.getTitle(),
+						formatter.format(AndroidUtils.getUsedSpaceGb(dir)));
+		externalStorageDir.setSummary(summary);
 	}
 
 	private void setupSendAnonymousDataPref() {
