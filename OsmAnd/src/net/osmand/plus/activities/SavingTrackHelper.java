46210a4b07850dcfadec1879dc15c95f32d0df5c
jensMF <jens.freudenthal@zoho.com>
2019-10-10 01:39:49 +0200

    implemented own function to determine target directory

--- OsmAnd/src/net/osmand/plus/activities/SavingTrackHelper.java
+++ OsmAnd/src/net/osmand/plus/activities/SavingTrackHelper.java
@@ -188,6 +188,24 @@ public class SavingTrackHelper extends SQLiteOpenHelper {
 
 		return false;
 	}
+
+	private File createTargetDir(File dir, WptPt pt) {
+		File targetDir = dir;
+		Integer track_storage_directory = ctx.getSettings().TRACK_STORAGE_DIRECTORY.get();
+		if (track_storage_directory != OsmandSettings.REC_DIRECTORY) {
+			SimpleDateFormat dateDirFormat = new SimpleDateFormat("yyyy-MM");
+			if (track_storage_directory == OsmandSettings.DAILY_DIRECTORY) {
+				dateDirFormat = new SimpleDateFormat("yyyy-MM-dd");
+			}
+			String dateDirName = dateDirFormat.format(new Date(pt.time));
+			File dateDir = new File(dir, dateDirName);
+			dateDir.mkdirs();
+			if (dateDir.exists()) {
+				targetDir = dateDir;
+			}
+		}
+		return  targetDir;
+	}
 	
 	/**
 	 * @return warnings, filenames
@@ -206,21 +224,7 @@ public class SavingTrackHelper extends SQLiteOpenHelper {
 					File fout = new File(dir, f + ".gpx"); //$NON-NLS-1$
 					if (!data.get(f).isEmpty()) {
 						WptPt pt = data.get(f).findPointToShow();
-						File targetDir = dir;
-						Integer track_storage_directory = ctx.getSettings().TRACK_STORAGE_DIRECTORY.get();
-						if (track_storage_directory != OsmandSettings.REC_DIRECTORY) {
-							SimpleDateFormat dateDirFormat = new SimpleDateFormat("yyyy-MM");
-							if (track_storage_directory == OsmandSettings.DAILY_DIRECTORY) {
-								dateDirFormat = new SimpleDateFormat("yyyy-MM-dd");
-							}
-							String dateDirName = dateDirFormat.format(new Date(pt.time));
-							File dateDir = new File(dir, dateDirName);
-							dateDir.mkdirs();
-							if (dateDir.exists()) {
-								targetDir = dateDir;
-							}
-						}
-
+						File targetDir = createTargetDir(dir, pt);
 						String fileName = f + "_" + new SimpleDateFormat("HH-mm_EEE", Locale.US).format(new Date(pt.time)); //$NON-NLS-1$
 						filenames.add(fileName);
 						fout = new File(targetDir, fileName + ".gpx"); //$NON-NLS-1$
