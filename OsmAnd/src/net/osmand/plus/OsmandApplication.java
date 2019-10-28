dcb0446d046b09f8d3204991dc7b01006e076a74
Chumva <faiver.unknown@gmail.com>
2019-10-28 18:42:55 +0200

    Try to fix voice provider initialization for app mode

--- OsmAnd/src/net/osmand/plus/OsmandApplication.java
+++ OsmAnd/src/net/osmand/plus/OsmandApplication.java
@@ -451,7 +451,7 @@ public class OsmandApplication extends MultiDexApplication {
 
 	public void initVoiceCommandPlayer(final Activity uiContext, ApplicationMode applicationMode,
 									   boolean warningNoneProvider, Runnable run, boolean showDialog, boolean force) {
-		String voiceProvider = osmandSettings.VOICE_PROVIDER.get();
+		String voiceProvider = osmandSettings.VOICE_PROVIDER.getModeValue(applicationMode);
 		if (voiceProvider == null || OsmandSettings.VOICE_PROVIDER_NOT_USE.equals(voiceProvider)) {
 			if (warningNoneProvider && voiceProvider == null) {
 				final AlertDialog.Builder builder = new AlertDialog.Builder(uiContext);
