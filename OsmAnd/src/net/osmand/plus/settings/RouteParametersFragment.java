a23fc026c2d60899f0bf83ffec331e8af8c46b01
Chumva <chumva.forever@gmail.com>
2019-11-11 14:12:47 +0200

    Fix check for fuel efficient pref key

--- OsmAnd/src/net/osmand/plus/settings/RouteParametersFragment.java
+++ OsmAnd/src/net/osmand/plus/settings/RouteParametersFragment.java
@@ -286,7 +286,7 @@ public class RouteParametersFragment extends BaseSettingsFragment implements OnP
 			}
 			recalculateRoute();
 			return true;
-		} else if (GeneralRouter.USE_SHORTEST_WAY.equals(key) && newValue instanceof Boolean) {
+		} else if ("prouting_short_way".equals(key) && newValue instanceof Boolean) {
 			return app.getSettings().FAST_ROUTE_MODE.setModeValue(getSelectedAppMode(), !(Boolean) newValue);
 		}
 
