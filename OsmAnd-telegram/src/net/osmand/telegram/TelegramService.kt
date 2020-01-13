88b89085e608d2571e7e6bc147cb7da5eb70524d
veliymolfar <veliymolfar@gmail.com>
2020-01-13 16:17:58 +0200

    fix widget status (with enabled monitoring)

--- OsmAnd-telegram/src/net/osmand/telegram/TelegramService.kt
+++ OsmAnd-telegram/src/net/osmand/telegram/TelegramService.kt
@@ -245,7 +245,9 @@ class TelegramService : Service(), LocationListener, TelegramIncomingMessagesLis
 					oldestTime = -1
 				}
 				app().showLocationHelper.addOrUpdateStatusWidget(oldestTime, isSending)
-			}
+            } else {
+                app().showLocationHelper.addOrUpdateStatusWidget(-1, false)
+            }
 			startWidgetUpdates()
 		}, UPDATE_WIDGET_INTERVAL_MS)
 	}
