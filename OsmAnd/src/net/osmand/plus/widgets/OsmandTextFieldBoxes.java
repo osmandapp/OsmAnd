c87177782af212d1741add54cba831a5ab033365
Nazar-Kutz <vitamikuna@gmail.com>
2020-02-28 18:36:39 +0200

    Remove unnecessary code

--- OsmAnd/src/net/osmand/plus/widgets/OsmandTextFieldBoxes.java
+++ OsmAnd/src/net/osmand/plus/widgets/OsmandTextFieldBoxes.java
@@ -2,14 +2,10 @@ package net.osmand.plus.widgets;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 
 import studio.carbonylgroup.textfieldboxes.TextFieldBoxes;
 
 public class OsmandTextFieldBoxes extends TextFieldBoxes {
-
-	private boolean useOsmandKeyboard;
 
 	public OsmandTextFieldBoxes(Context context) {
 		super(context);
@@ -21,42 +17,6 @@ public class OsmandTextFieldBoxes extends TextFieldBoxes {
 
 	public OsmandTextFieldBoxes(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
-	}
-
-	public void setUseOsmandKeyboard(boolean useOsmandKeyboard) {
-		this.useOsmandKeyboard = useOsmandKeyboard;
-	}
-
-	@Override
-	protected void onFinishInflate() {
-		super.onFinishInflate();
-		if (editText != null) {
-			this.panel.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View view) {
-					select();
-				}
-			});
-
-			this.iconImageButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					select();
-				}
-			});
-		}
-	}
-
-	public void select() {
-		if(!OsmandTextFieldBoxes.this.isActivated()) {
-			OsmandTextFieldBoxes.this.activate(true);
-		}
-
-		OsmandTextFieldBoxes.this.setHasFocus(true);
-		if (!useOsmandKeyboard) {
-			OsmandTextFieldBoxes.this.inputMethodManager.showSoftInput(OsmandTextFieldBoxes.this.editText, InputMethodManager.SHOW_IMPLICIT);
-		}
-		performClick();
 	}
 
 }
