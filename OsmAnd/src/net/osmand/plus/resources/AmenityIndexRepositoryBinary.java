ea9863991d4d3c85870dc71f5b6f48e398f329be
veliymolfar <veliymolfar@gmail.com>
2020-05-18 17:04:25 +0300

    remove unused imports

--- OsmAnd/src/net/osmand/plus/resources/AmenityIndexRepositoryBinary.java
+++ OsmAnd/src/net/osmand/plus/resources/AmenityIndexRepositoryBinary.java
@@ -26,10 +26,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class AmenityIndexRepositoryBinary implements AmenityIndexRepository {
 
