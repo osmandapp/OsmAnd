0682d3b3bcab4c4cbd57672160aa1b03441f0b0b
veliymolfar <veliymolfar@gmail.com>
2020-05-18 17:00:05 +0300

    fix subCategories

--- OsmAnd/src/net/osmand/plus/resources/AmenityIndexRepositoryBinary.java
+++ OsmAnd/src/net/osmand/plus/resources/AmenityIndexRepositoryBinary.java
@@ -26,8 +26,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class AmenityIndexRepositoryBinary implements AmenityIndexRepository {
 
@@ -82,14 +84,12 @@ public class AmenityIndexRepositoryBinary implements AmenityIndexRepository {
 
 			PoiCategory poiCategory = poiTypes.getPoiCategoryByName(categoryName);
 			List<String> deltaSubCategories = null;
-			for (List<String> subList : subCategories) {
-				for (String subCategory : subList) {
-					if (poiCategory.getPoiTypeByKeyName(subCategory) != null) {
-						if (deltaSubCategories == null) {
-							deltaSubCategories = new ArrayList<>();
-						}
-						deltaSubCategories.add(subCategory);
+			for (String subCategory : subCategories.get(i)) {
+				if (poiCategory.getPoiTypeByKeyName(subCategory) == null) {
+					if (deltaSubCategories == null) {
+						deltaSubCategories = new ArrayList<>();
 					}
+					deltaSubCategories.add(subCategory);
 				}
 			}
 			if (deltaSubCategories != null) {
