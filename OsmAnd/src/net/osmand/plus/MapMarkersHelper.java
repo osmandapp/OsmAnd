a03bad42f7a25abc516a2e4406643ec65a94fe81
Vitaliy <chumva.forever@gmail.com>
2020-05-22 16:03:36 +0300

    Remove unnecessary changes

--- OsmAnd/src/net/osmand/plus/MapMarkersHelper.java
+++ OsmAnd/src/net/osmand/plus/MapMarkersHelper.java
@@ -1161,10 +1161,11 @@ public class MapMarkersHelper {
 					removeGroupActiveMarkers(group, true);
 					return;
 				}
-				List<FavouritePoint> favPoints = new ArrayList<>(favGroup.getPoints());
-				for (FavouritePoint fp : favPoints) {
+				for (FavouritePoint fp : favGroup.getPoints()) {
 					addNewMarkerIfNeeded(group, groupMarkers, new LatLon(fp.getLatitude(), fp.getLongitude()), fp.getName(), fp, null);
 				}
+
+
 			} else if (group.getType() == MapMarkersGroup.GPX_TYPE) {
 				GpxSelectionHelper gpxHelper = ctx.getSelectedGpxHelper();
 				File file = new File(group.getId());
