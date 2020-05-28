ed5f75193419152b7efc7d00da80cf3c4b2edfdb
Dima-1 <softmail32@gmail.com>
2020-05-28 11:46:38 +0300

    Fix review

--- OsmAnd/src/net/osmand/plus/settings/fragments/DialogsAndNotificationsSettingsFragment.java
+++ OsmAnd/src/net/osmand/plus/settings/fragments/DialogsAndNotificationsSettingsFragment.java
@@ -1,8 +1,5 @@
 package net.osmand.plus.settings.fragments;
 
-import android.view.LayoutInflater;
-import android.view.View;
-
 import androidx.preference.Preference;
 import androidx.preference.SwitchPreferenceCompat;
 
@@ -11,11 +8,6 @@ import net.osmand.plus.R;
 public class DialogsAndNotificationsSettingsFragment extends BaseSettingsFragment {
 
 	public static final String TAG = DialogsAndNotificationsSettingsFragment.class.getSimpleName();
-
-	@Override
-	protected void createToolbar(LayoutInflater inflater, View view) {
-		super.createToolbar(inflater, view);
-	}
 
 	@Override
 	protected void setupPreferences() {
