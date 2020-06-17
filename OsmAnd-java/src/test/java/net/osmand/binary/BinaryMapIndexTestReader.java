b1bf712bcb3796629cc82de3bc17b8376e53564f
Victor Shcherb <victor.shcherb@gmail.com>
2020-06-17 14:44:47 +0200

    fix compilation

---
+++ OsmAnd-java/src/test/java/net/osmand/binary/BinaryMapIndexTestReader.java
@@ -0,0 +1,12 @@
+package net.osmand.binary;
+
+import java.io.File;
+
+public class BinaryMapIndexTestReader {
+
+	public static BinaryMapIndexReader buildTestReader(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
