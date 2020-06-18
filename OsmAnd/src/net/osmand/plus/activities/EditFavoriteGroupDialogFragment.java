a28ac55bbf720598a6f87d791ee044ca110b9440
Dima-1 <softmail32@gmail.com>
2020-06-18 14:37:00 +0300

    Fix #9196 Color selection for favorites point group outside of screen

--- OsmAnd/src/net/osmand/plus/activities/EditFavoriteGroupDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/activities/EditFavoriteGroupDialogFragment.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,6 +31,7 @@ import net.osmand.plus.MapMarkersHelper;
 import net.osmand.plus.MapMarkersHelper.MapMarkersGroup;
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.R;
+import net.osmand.plus.UiUtilities;
 import net.osmand.plus.base.MenuBottomSheetDialogFragment;
 import net.osmand.plus.base.bottomsheetmenu.BaseBottomSheetItem;
 import net.osmand.plus.base.bottomsheetmenu.BottomSheetItemWithCompoundButton;
@@ -73,12 +74,13 @@ public class EditFavoriteGroupDialogFragment extends MenuBottomSheetDialogFragme
 					public void onClick(View v) {
 						Activity activity = getActivity();
 						if (activity != null) {
-							AlertDialog.Builder b = new AlertDialog.Builder(activity);
+							Context themedContext = UiUtilities.getThemedContext(activity, nightMode);
+							AlertDialog.Builder b = new AlertDialog.Builder(themedContext);
 							b.setTitle(R.string.favorite_category_name);
-							final EditText nameEditText = new EditText(activity);
+							final EditText nameEditText = new EditText(themedContext);
 							nameEditText.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 							nameEditText.setText(group.getName());
-							LinearLayout container = new LinearLayout(activity);
+							LinearLayout container = new LinearLayout(themedContext);
 							int sidePadding = AndroidUtils.dpToPx(activity, 24f);
 							int topPadding = AndroidUtils.dpToPx(activity, 4f);
 							container.setPadding(sidePadding, topPadding, sidePadding, topPadding);
@@ -105,9 +107,8 @@ public class EditFavoriteGroupDialogFragment extends MenuBottomSheetDialogFragme
 				.create();
 		items.add(editNameItem);
 
-		final int themeRes = nightMode ? R.style.OsmandDarkTheme : R.style.OsmandLightTheme;
-		final View changeColorView = View.inflate(new ContextThemeWrapper(getContext(), themeRes),
-				R.layout.change_fav_color, null);
+		final View changeColorView = UiUtilities.getInflater(getContext(), nightMode)
+				.inflate(R.layout.change_fav_color, null, false);
 		((ImageView) changeColorView.findViewById(R.id.change_color_icon))
 				.setImageDrawable(getContentIcon(R.drawable.ic_action_appearance));
 		updateColorView((ImageView) changeColorView.findViewById(R.id.colorImage));
@@ -118,13 +119,15 @@ public class EditFavoriteGroupDialogFragment extends MenuBottomSheetDialogFragme
 					public void onClick(View v) {
 						Activity activity = getActivity();
 						if (activity != null) {
-							final ListPopupWindow popup = new ListPopupWindow(activity);
+							final ListPopupWindow popup = new ListPopupWindow(v.getContext());
 							popup.setAnchorView(v);
 							popup.setContentWidth(AndroidUtils.dpToPx(app, 200f));
 							popup.setModal(true);
 							popup.setDropDownGravity(Gravity.END | Gravity.TOP);
 							if (AndroidUiHelper.isOrientationPortrait(activity)) {
-								popup.setVerticalOffset(AndroidUtils.dpToPx(app, 48f));
+								if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+									popup.setVerticalOffset(AndroidUtils.dpToPx(app, 48f));
+								}
 							} else {
 								popup.setVerticalOffset(AndroidUtils.dpToPx(app, -48f));
 							}
