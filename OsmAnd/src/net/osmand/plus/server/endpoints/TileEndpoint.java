485ee88a13d2f75cad88ab9899e9d7a3022d6a07
simon <myhosting2012@gmail.com>
2020-09-07 15:35:03 +0300

    remove useless html

--- OsmAnd/src/net/osmand/plus/server/endpoints/TileEndpoint.java
+++ OsmAnd/src/net/osmand/plus/server/endpoints/TileEndpoint.java
@@ -1,18 +1,15 @@
 package net.osmand.plus.server.endpoints;
 
 import android.graphics.Bitmap;
-import androidx.fragment.app.FragmentActivity;
 import fi.iki.elonen.NanoHTTPD;
 import net.osmand.PlatformUtil;
 import net.osmand.data.RotatedTileBox;
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.activities.MapActivity;
 import net.osmand.plus.render.MapRenderRepositories;
-import net.osmand.plus.resources.AsyncLoadingThread;
 import net.osmand.plus.server.ApiEndpoint;
 import net.osmand.plus.server.IMapOnImageDrawn;
 import net.osmand.plus.server.OsmAndHttpServer;
-import net.osmand.plus.server.ServerFragment;
 import org.apache.commons.logging.Log;
 
 import java.io.ByteArrayInputStream;
@@ -25,7 +22,6 @@ import java.util.Scanner;
 import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
 
 public class TileEndpoint implements ApiEndpoint, IMapOnImageDrawn {
-	private static final int RENDER_WAIT_THRESHOLD = 5000;
 	private static final Object lock = new Object();
 	private static final int TILE_SIZE_PX = 512;
 	private static final Log LOG = PlatformUtil.getLog(TileEndpoint.class);
@@ -49,6 +45,8 @@ public class TileEndpoint implements ApiEndpoint, IMapOnImageDrawn {
 	@Override
 	public void onDraw(RotatedTileBox viewport, Bitmap bmp) {
 		map.put(viewport, bmp);
+		this.viewPort = viewport;
+		this.resultBitmap = bmp;
 	}
 
 	@Override
@@ -89,36 +87,16 @@ public class TileEndpoint implements ApiEndpoint, IMapOnImageDrawn {
 				.setPixelDimensions(TILE_SIZE_PX, TILE_SIZE_PX, 0.5f, 0.5f).build();
 		final MapRenderRepositories renderer = application.getResourceManager().getRenderer();
 		mapActivity.getMapView().setCurrentViewport(rotatedTileBox);
-//		application.getResourceManager().updateRendererMap(rotatedTileBox, new AsyncLoadingThread.OnMapLoadedListener() {
-//			@Override
-//			public void onMapLoaded(boolean interrupted) {
-//				map.put(rotatedTileBox, renderer.getBitmap());
-//			}
-//		});
 		Bitmap bmp = null;
-		int sleepTime = 500;
 		int timeout = 0;
-
-
-//		try {
-//			while ((viewPort == null && !rotatedTileBox.equals(viewPort)) || timeout < 5000) {
-//				Thread.sleep(100);
-//				timeout += 100;
-//			}
-//			return resultBitmap;
-//		} catch (InterruptedException e) {
-//			LOG.error(e);
-//		}
-		while ((bmp = map.get(rotatedTileBox)) == null) {
-			try {
-				Thread.sleep(sleepTime);
-			} catch (InterruptedException e) {
-				LOG.error(e);
+		try {
+			while ((viewPort == null && !rotatedTileBox.equals(viewPort)) || timeout < 5000) {
+				Thread.sleep(100);
+				timeout += 100;
 			}
-			sleepTime += 500;
-			if (sleepTime > RENDER_WAIT_THRESHOLD) {
-				break;
-			}
+			return resultBitmap;
+		} catch (InterruptedException e) {
+			LOG.error(e);
 		}
 		return bmp;
 	}
