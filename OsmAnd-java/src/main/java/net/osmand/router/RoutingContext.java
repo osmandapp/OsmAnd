69a93078e16435a94d227d6ecb95a69a846f462a
simon <myhosting2012@gmail.com>
2020-09-22 13:04:03 +0300

    exclude duplicated dialog fixed

--- OsmAnd-java/src/main/java/net/osmand/router/RoutingContext.java
+++ OsmAnd-java/src/main/java/net/osmand/router/RoutingContext.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import gnu.trove.procedure.TObjectProcedure;
 import org.apache.commons.logging.Log;
 
 import gnu.trove.iterator.TIntObjectIterator;
