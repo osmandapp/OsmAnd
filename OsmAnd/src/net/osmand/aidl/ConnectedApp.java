ff06ccb000709cfe46c122654ba759dd406dfd6d
Dima-1 <softmail32@gmail.com>
2020-10-13 09:45:24 +0300

    Fix merge

--- OsmAnd/src/net/osmand/aidl/ConnectedApp.java
+++ OsmAnd/src/net/osmand/aidl/ConnectedApp.java
@@ -18,7 +18,6 @@ import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.settings.backend.CommonPreference;
 import net.osmand.plus.R;
 import net.osmand.plus.activities.MapActivity;
-import net.osmand.plus.settings.backend.ApplicationMode;
 import net.osmand.plus.views.OsmandMapLayer;
 import net.osmand.plus.views.layers.AidlMapLayer;
 import net.osmand.plus.views.layers.MapInfoLayer;
