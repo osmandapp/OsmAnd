29a57dc562c6004bd632537c0c9742a3e7b45e87
simon <myhosting2012@gmail.com>
2020-12-15 15:22:10 +0200

    check db file exists added

--- OsmAnd/src/net/osmand/plus/AppInitializer.java
+++ OsmAnd/src/net/osmand/plus/AppInitializer.java
@@ -460,7 +460,7 @@ public class AppInitializer implements IProgress {
 		app.mapViewTrackingUtilities = startupInit(new MapViewTrackingUtilities(app), MapViewTrackingUtilities.class);
 
 		// TODOTRAVEL_OBF_HELPER check ResourceManager and use TravelObfHelper
-		app.travelHelper = TravelObfHelper.checkIfObfFileExists(app) ? new TravelObfHelper(app) : new TravelDbHelper(app);
+		app.travelHelper = !TravelDbHelper.checkIfDbFileExists(app) ? new TravelObfHelper(app) : new TravelDbHelper(app);
 		app.travelHelper.initializeDataOnAppStartup();
 		app.travelHelper = startupInit(app.travelHelper, TravelHelper.class);
 
