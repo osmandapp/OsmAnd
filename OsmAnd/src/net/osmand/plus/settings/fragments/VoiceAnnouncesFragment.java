7a279eae76785bc83bd94a613f57c02b3b9a173f
cepprice <super.cepprice@gmail.com>
2021-01-14 19:06:03 +0500

    Remove unneeded code

--- OsmAnd/src/net/osmand/plus/settings/fragments/VoiceAnnouncesFragment.java
+++ OsmAnd/src/net/osmand/plus/settings/fragments/VoiceAnnouncesFragment.java
@@ -18,7 +18,6 @@ import androidx.preference.PreferenceViewHolder;
 import androidx.preference.SwitchPreferenceCompat;
 
 import net.osmand.AndroidUtils;
-import net.osmand.plus.OsmandPlugin;
 import net.osmand.plus.R;
 import net.osmand.plus.UiUtilities;
 import net.osmand.plus.Version;
@@ -28,7 +27,6 @@ import net.osmand.plus.download.DownloadActivity;
 import net.osmand.plus.download.DownloadActivityType;
 import net.osmand.plus.helpers.FileNameTranslationHelper;
 import net.osmand.plus.helpers.enums.MetricsConstants;
-import net.osmand.plus.routing.data.AnnounceTimeDistances;
 import net.osmand.plus.settings.backend.ApplicationMode;
 import net.osmand.plus.settings.backend.OsmandSettings;
 import net.osmand.plus.settings.bottomsheets.AnnouncementTimeBottomSheet;
@@ -45,7 +43,6 @@ public class VoiceAnnouncesFragment extends BaseSettingsFragment implements OnPr
 
 	private static final String MORE_VALUE = "MORE_VALUE";
 	private static final String ARRIVAL_DISTANCE_FACTOR = "arrival_distance_factor";
-	private static final String VOICE_PROMPTS_TIMETABLE = "voice_prompts_timetable";
 
 	@Override
 	public void onDisplayPreferenceDialog(Preference preference) {
@@ -115,7 +112,6 @@ public class VoiceAnnouncesFragment extends BaseSettingsFragment implements OnPr
 
 		setupKeepInformingPref();
 		setupArrivalAnnouncementPref();
-		updateVoicePromptsTimes();
 		setupVoiceProviderPref();
 
 		if (!Version.isBlackberry(app)) {
@@ -125,18 +121,6 @@ public class VoiceAnnouncesFragment extends BaseSettingsFragment implements OnPr
 		enableDisablePreferences(!settings.VOICE_MUTE.getModeValue(getSelectedAppMode()));
 		setupSpeakCamerasPref();
 		setupSpeedCamerasAlert();
-	}
-
-	private void updateVoicePromptsTimes() {
-		Preference pref = findPreference(VOICE_PROMPTS_TIMETABLE);
-//		if (OsmandPlugin.isDevelopment()) {
-//			AnnounceTimeDistances atd = new AnnounceTimeDistances(getSelectedAppMode(), settings);
-//			pref.setSummary(atd.getTurnsDescription().trim());
-//			pref.setVisible(true);
-//		} else {
-//			pref.setVisible(false);
-//		}
-		pref.setVisible(false);
 	}
 
 	private void setupSpeedLimitExceedPref() {
@@ -294,9 +278,6 @@ public class VoiceAnnouncesFragment extends BaseSettingsFragment implements OnPr
 						settings.SPEAK_SPEED_CAMERA.getId(), false, ApplyQueryType.SNACK_BAR);
 			}
 		}
-		if (prefId.equals(settings.ARRIVAL_DISTANCE_FACTOR.getId())) {
-			updateVoicePromptsTimes();
-		}
 		if (prefId.equals(settings.AUDIO_MANAGER_STREAM.getId())) {
 			return onConfirmPreferenceChange(
 					settings.AUDIO_MANAGER_STREAM.getId(), newValue, ApplyQueryType.SNACK_BAR);
@@ -334,8 +315,6 @@ public class VoiceAnnouncesFragment extends BaseSettingsFragment implements OnPr
 		String prefId = preference.getKey();
 		if (settings.SPEED_CAMERAS_UNINSTALLED.getId().equals(prefId)) {
 			SpeedCamerasBottomSheet.showInstance(requireActivity().getSupportFragmentManager(), this);
-		} else if (VOICE_PROMPTS_TIMETABLE.equals(prefId)) {
-			app.showToastMessage(String.valueOf(preference.getSummary()));
 		}
 		return super.onPreferenceClick(preference);
 	}
