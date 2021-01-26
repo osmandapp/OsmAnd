069f526a571156d5be281c003800371b1ab306c1
Skalii <skaliy.d@gmail.com>
2021-01-27 01:31:44 +0200

    fix re-adding top padding

--- OsmAnd/src/net/osmand/plus/widgets/OsmandTextFieldBoxes.java
+++ OsmAnd/src/net/osmand/plus/widgets/OsmandTextFieldBoxes.java
@@ -26,8 +26,8 @@ public class OsmandTextFieldBoxes extends TextFieldBoxes {
 		floatingLabel.setVisibility(View.GONE);
 		labelSpace.setVisibility(View.GONE);
 		labelSpaceBelow.setVisibility(View.GONE);
-		int paddingH = getResources().getDimensionPixelSize(R.dimen.route_info_card_details_margin);
-		inputLayout.setPadding(0, paddingH, 0, paddingH);
+		int paddingV = getResources().getDimensionPixelSize(R.dimen.route_info_card_details_margin);
+		inputLayout.setPadding(0, paddingV, 0, paddingV);
 	}
 
 	public void setGravityFloatingLabel(int gravity) {
@@ -43,7 +43,8 @@ public class OsmandTextFieldBoxes extends TextFieldBoxes {
 				if (floatingLabel.getLineCount() > 1) {
 					inputLayout.setPadding(
 							inputLayout.getPaddingLeft(),
-							inputLayout.getPaddingTop() + getResources().getDimensionPixelSize(R.dimen.pages_item_padding),
+							getResources().getDimensionPixelOffset(useDenseSpacing ? R.dimen.dense_editTextLayout_padding_top : R.dimen.editTextLayout_padding_top) +
+									getResources().getDimensionPixelSize(useDenseSpacing ? R.dimen.context_menu_first_line_top_margin : R.dimen.content_padding_small),
 							inputLayout.getPaddingRight(),
 							inputLayout.getPaddingBottom()
 					);
