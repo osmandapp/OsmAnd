959e7f411c0e2b6fd498eebcfbd19638d4d3ba94
androiddevkotlin <64539346+androiddevkotlin@users.noreply.github.com>
2021-01-26 14:54:33 +0200

    Upload Photo: bottom sheet with the progress bar
    Uplaod progress dialog
    Upload complete dialog

---
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/UploadPhotosAsyncTask.java
@@ -0,0 +1,203 @@
+package net.osmand.plus.mapcontextmenu;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import androidx.fragment.app.FragmentActivity;
+
+import net.osmand.AndroidUtils;
+import net.osmand.PlatformUtil;
+import net.osmand.data.LatLon;
+import net.osmand.osm.io.NetworkUtils;
+import net.osmand.plus.OsmandApplication;
+import net.osmand.plus.R;
+import net.osmand.plus.activities.MapActivity;
+import net.osmand.plus.dialogs.UploadPhotoWithProgressBarBottomSheet;
+import net.osmand.plus.mapcontextmenu.builders.cards.ImageCard.GetImageCardsTask;
+import net.osmand.plus.mapcontextmenu.builders.cards.ImageCard.GetImageCardsTask.GetImageCardsListener;
+import net.osmand.plus.openplacereviews.OPRConstants;
+import net.osmand.plus.openplacereviews.OprStartFragment;
+import net.osmand.plus.osmedit.opr.OpenDBAPI;
+import net.osmand.util.Algorithms;
+
+import org.apache.commons.logging.Log;
+import org.openplacereviews.opendb.util.exception.FailedVerificationException;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.Map;
+
+public class UploadPhotosAsyncTask extends AsyncTask<Void, Integer, Void> {
+
+	private static final Log LOG = PlatformUtil.getLog(UploadPhotosAsyncTask.class);
+
+	private static final int MAX_IMAGE_LENGTH = 2048;
+
+	private final OsmandApplication app;
+	private final WeakReference<MapActivity> activityRef;
+	private UploadPhotoWithProgressBarBottomSheet progress;
+
+	private final OpenDBAPI openDBAPI = new OpenDBAPI();
+	private final LatLon latLon;
+	private final List<Uri> data;
+	private final String[] placeId;
+	private final Map<String, String> params;
+	private final GetImageCardsListener imageCardListener;
+
+	public UploadPhotosAsyncTask(MapActivity activity, List<Uri> data, LatLon latLon, String[] placeId,
+								 Map<String, String> params, GetImageCardsListener imageCardListener) {
+		app = (OsmandApplication) activity.getApplicationContext();
+		activityRef = new WeakReference<>(activity);
+		this.data = data;
+		this.latLon = latLon;
+		this.params = params;
+		this.placeId = placeId;
+		this.imageCardListener = imageCardListener;
+	}
+
+	@Override
+	protected void onPreExecute() {
+		FragmentActivity activity = activityRef.get();
+		if (AndroidUtils.isActivityNotDestroyed(activity)) {
+			progress = UploadPhotoWithProgressBarBottomSheet.showInstance(activity.getSupportFragmentManager(), data.size());
+		}
+	}
+
+	protected Void doInBackground(Void... uris) {
+		for (int i = 0; i < data.size(); i++) {
+			if (isCancelled()) {
+				break;
+			}
+			Uri uri = data.get(i);
+			handleSelectedImage(uri);
+			publishProgress(i);
+		}
+		return null;
+	}
+
+	@Override
+	protected void onProgressUpdate(Integer... values) {
+		if (progress != null) {
+			progress.onProgressUpdate(values[0]);
+		}
+	}
+
+	@Override
+	protected void onPostExecute(Void aVoid) {
+		if (progress != null) {
+			progress.onUploadingFinished();
+		}
+	}
+
+	private void handleSelectedImage(final Uri uri) {
+		InputStream inputStream = null;
+		try {
+			inputStream = app.getContentResolver().openInputStream(uri);
+			if (inputStream != null) {
+				uploadImageToPlace(inputStream);
+			}
+		} catch (Exception e) {
+			LOG.error(e);
+			app.showToastMessage(R.string.cannot_upload_image);
+		} finally {
+			Algorithms.closeStream(inputStream);
+		}
+	}
+
+	private void uploadImageToPlace(InputStream image) {
+		InputStream serverData = new ByteArrayInputStream(compressImageToJpeg(image));
+		final String baseUrl = OPRConstants.getBaseUrl(app);
+		// all these should be constant
+		String url = baseUrl + "api/ipfs/image";
+		String response = NetworkUtils.sendPostDataRequest(url, "file", "compressed.jpeg", serverData);
+		if (response != null) {
+			int res = 0;
+			try {
+				StringBuilder error = new StringBuilder();
+				String privateKey = app.getSettings().OPR_ACCESS_TOKEN.get();
+				String username = app.getSettings().OPR_USERNAME.get();
+				res = openDBAPI.uploadImage(
+						placeId,
+						baseUrl,
+						privateKey,
+						username,
+						response, error);
+				if (res != 200) {
+					app.showToastMessage(error.toString());
+				} else {
+					//ok, continue
+				}
+			} catch (FailedVerificationException e) {
+				LOG.error(e);
+				checkTokenAndShowScreen();
+			}
+			if (res != 200) {
+				//image was uploaded but not added to blockchain
+				checkTokenAndShowScreen();
+			} else {
+				String str = app.getString(R.string.successfully_uploaded_pattern, 1, 1);
+				app.showToastMessage(str);
+				//refresh the image
+
+				MapActivity activity = activityRef.get();
+				if (activity != null) {
+					MenuBuilder.execute(new GetImageCardsTask(activity, latLon, params, imageCardListener));
+				}
+			}
+		} else {
+			checkTokenAndShowScreen();
+		}
+	}
+
+	//This method runs on non main thread
+	private void checkTokenAndShowScreen() {
+		final String baseUrl = OPRConstants.getBaseUrl(app);
+		final String name = app.getSettings().OPR_USERNAME.get();
+		final String privateKey = app.getSettings().OPR_ACCESS_TOKEN.get();
+		if (openDBAPI.checkPrivateKeyValid(baseUrl, name, privateKey)) {
+			String str = app.getString(R.string.cannot_upload_image);
+			app.showToastMessage(str);
+		} else {
+			app.runInUIThread(new Runnable() {
+				@Override
+				public void run() {
+					MapActivity activity = activityRef.get();
+					if (activity != null) {
+						OprStartFragment.showInstance(activity.getSupportFragmentManager());
+					}
+				}
+			});
+		}
+	}
+
+	private byte[] compressImageToJpeg(InputStream image) {
+		BufferedInputStream bufferedInputStream = new BufferedInputStream(image);
+		Bitmap bmp = BitmapFactory.decodeStream(bufferedInputStream);
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		int h = bmp.getHeight();
+		int w = bmp.getWidth();
+		boolean scale = false;
+		while (w > MAX_IMAGE_LENGTH || h > MAX_IMAGE_LENGTH) {
+			w = w / 2;
+			h = h / 2;
+			scale = true;
+		}
+		if (scale) {
+			Matrix matrix = new Matrix();
+			matrix.postScale(w, h);
+			Bitmap resizedBitmap = Bitmap.createBitmap(
+					bmp, 0, 0, w, h, matrix, false);
+			bmp.recycle();
+			bmp = resizedBitmap;
+		}
+		bmp.compress(Bitmap.CompressFormat.JPEG, 90, os);
+		return os.toByteArray();
+	}
+}
