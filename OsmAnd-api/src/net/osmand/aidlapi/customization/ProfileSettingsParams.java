65facb4af39b7e41a9e37ed720cc2896ed6e1878
cepprice <super.cepprice@gmail.com>
2021-01-28 18:11:24 +0500

    Add silent setting

--- OsmAnd-api/src/net/osmand/aidlapi/customization/ProfileSettingsParams.java
+++ OsmAnd-api/src/net/osmand/aidlapi/customization/ProfileSettingsParams.java
@@ -15,16 +15,18 @@ public class ProfileSettingsParams extends AidlParams {
 
 	public static final String VERSION_KEY = "version";
 	public static final String REPLACE_KEY = "replace";
+	public static final String SILENT_IMPORT_KEY = "silent_import";
 	public static final String LATEST_CHANGES_KEY = "latestChanges";
 	public static final String PROFILE_SETTINGS_URI_KEY = "profileSettingsUri";
 	private Uri profileSettingsUri;
 	private String latestChanges;
 	private int version;
 	private ArrayList<String> settingsTypeKeyList = new ArrayList<>();
-	boolean replace;
+	private boolean silent;
+	private boolean replace;
 
 	public ProfileSettingsParams(Uri profileSettingsUri, ArrayList<AExportSettingsType> settingsTypeList, boolean replace,
-	                             String latestChanges, int version) {
+	                             boolean silent, String latestChanges, int version) {
 		this.profileSettingsUri = profileSettingsUri;
 		for (AExportSettingsType settingsType : settingsTypeList) {
 			settingsTypeKeyList.add(settingsType.name());
@@ -32,6 +34,7 @@ public class ProfileSettingsParams extends AidlParams {
 		this.replace = replace;
 		this.latestChanges = latestChanges;
 		this.version = version;
+		this.silent = silent;
 	}
 
 	public ProfileSettingsParams(Parcel in) {
@@ -70,6 +73,10 @@ public class ProfileSettingsParams extends AidlParams {
 		return replace;
 	}
 
+	public boolean isSilent() {
+		return silent;
+	}
+
 	@Override
 	public void writeToBundle(Bundle bundle) {
 		bundle.putInt(VERSION_KEY, version);
@@ -77,6 +84,7 @@ public class ProfileSettingsParams extends AidlParams {
 		bundle.putParcelable(PROFILE_SETTINGS_URI_KEY, profileSettingsUri);
 		bundle.putStringArrayList(SETTINGS_TYPE_KEY, settingsTypeKeyList);
 		bundle.putBoolean(REPLACE_KEY, replace);
+		bundle.putBoolean(SILENT_IMPORT_KEY, silent);
 	}
 
 	@Override
@@ -86,5 +94,6 @@ public class ProfileSettingsParams extends AidlParams {
 		profileSettingsUri = bundle.getParcelable(PROFILE_SETTINGS_URI_KEY);
 		settingsTypeKeyList = bundle.getStringArrayList(SETTINGS_TYPE_KEY);
 		replace = bundle.getBoolean(REPLACE_KEY);
+		silent = bundle.getBoolean(SILENT_IMPORT_KEY);
 	}
 }
