2561ee99e111fd3b55db8b35cb72b112bf59d541
Skalii <skaliy.d@gmail.com>
2021-02-23 01:50:50 +0200

    fix replace checks;

--- OsmAnd/src/net/osmand/plus/importfiles/ImportHelper.java
+++ OsmAnd/src/net/osmand/plus/importfiles/ImportHelper.java
@@ -673,10 +673,10 @@ public class ImportHelper {
 	public static List<FavouritePoint> asFavourites(OsmandApplication app, List<WptPt> wptPts, String fileName, boolean forceImportFavourites) {
 		List<FavouritePoint> favourites = new ArrayList<>();
 		for (WptPt p : wptPts) {
-			if (p.name == null) {
+			if (Algorithms.isEmpty(p.name)) {
 				p.name = Algorithms.getFileNameWithoutExtension(fileName);
 			}
-			if (p.name != null) {
+			if (!Algorithms.isEmpty(p.name)) {
 				final String fpCat;
 				if (p.category == null) {
 					if (forceImportFavourites) {
