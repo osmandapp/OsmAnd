e6caccf45fe1d82a9cb0efc6a438e930ab127485
Vitaliy <chumva.forever@gmail.com>
2021-04-16 21:28:13 +0300

    Fix old markers synchronization

--- OsmAnd/src/net/osmand/plus/AppInitializer.java
+++ OsmAnd/src/net/osmand/plus/AppInitializer.java
@@ -685,7 +685,7 @@ public class AppInitializer implements IProgress {
 			// restore backuped favorites to normal file
 			restoreBackupForFavoritesFiles();
 			notifyEvent(InitEvents.RESTORE_BACKUPS);
-			app.itineraryHelper.syncItineraryGroups();
+			app.itineraryHelper.syncMarkersGroups();
 			app.searchUICore.initSearchUICore();
 
 			checkLiveUpdatesAlerts();
