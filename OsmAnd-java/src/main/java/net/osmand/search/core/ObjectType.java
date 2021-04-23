8ea831683581a0c02deb3c7ab620317e4deb1367
Victor Shcherb <victor.shcherb@gmail.com>
2021-04-23 17:37:12 +0200

    Update search result

--- OsmAnd-java/src/main/java/net/osmand/search/core/ObjectType.java
+++ OsmAnd-java/src/main/java/net/osmand/search/core/ObjectType.java
@@ -52,15 +52,15 @@ public enum ObjectType {
 		switch (t) {
 			case HOUSE:
 			case STREET_INTERSECTION:
-				return 4;
+				return 5;
 			case STREET:
-				return 3;
+				return 4;
 			case CITY:
 			case VILLAGE:
 			case POSTCODE:
-				return 2;
+				return 3;
 			case POI:
-				return 1;
+				return 2;
 			default:
 				return 1;
 		}
