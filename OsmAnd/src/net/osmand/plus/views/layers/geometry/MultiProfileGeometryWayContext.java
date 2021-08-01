507bc9ae4a48bf19bc44d0e80ad18b676c6841c9
cepprice <super.cepprice@gmail.com>
2021-08-02 00:30:01 +0500

    Update UI of multiprofile plan route

--- OsmAnd/src/net/osmand/plus/views/layers/geometry/MultiProfileGeometryWayContext.java
+++ OsmAnd/src/net/osmand/plus/views/layers/geometry/MultiProfileGeometryWayContext.java
@@ -26,12 +26,13 @@ public class MultiProfileGeometryWayContext extends GeometryWayContext {
 	@DrawableRes
 	private static final int STRAIGHT_LINE_ICON_RES = R.drawable.ic_action_split_interval;
 
-	private static final float PATH_BORDER_WIDTH_DP = 14;
-	private static final float PATH_WIDTH_DP = 10;
-	private static final float PROFILE_ICON_BORDER_WIDTH_DP = 2;
+	private static final float PATH_WIDTH_DP = 4;
+	private static final float PATH_BORDER_WIDTH_DP = PATH_WIDTH_DP + 2;
+	private static final float PROFILE_ICON_BORDER_WIDTH_DP = 1;
 
 	private static final float MIN_PROFILE_ICON_MARGIN_DP = 30;
-	private static final float PROFILE_ICON_FRAME_SIZE_DP = 70;
+	private static final float PROFILE_ICON_FRAME_SIZE_DP = 18;
+	private static final float PROFILE_ICON_SIZE_DP = 12;
 
 	private final UiUtilities iconsCache;
 
@@ -93,17 +94,18 @@ public class MultiProfileGeometryWayContext extends GeometryWayContext {
 			framedProfileIconBitmap = Bitmap.createBitmap((int) profileIconFrameSizePx,
 					(int) profileIconFrameSizePx, Bitmap.Config.ARGB_8888);
 			Canvas canvas = new Canvas(framedProfileIconBitmap);
-			float center = framedProfileIconBitmap.getWidth() / 2f;
+			float iconCenter = framedProfileIconBitmap.getWidth() / 2f;
 
-			canvas.drawCircle(center, center, center / 2, profileIconBackgroundPaint);
+			float frameRadius = iconCenter - PROFILE_ICON_BORDER_WIDTH_DP;
+			canvas.drawCircle(iconCenter, iconCenter, frameRadius, profileIconBackgroundPaint);
 			profileIconBorderPaint.setColor(color);
-			canvas.drawCircle(center, center, center / 2, profileIconBorderPaint);
+			canvas.drawCircle(iconCenter, iconCenter, frameRadius, profileIconBorderPaint);
 
-			float iconSize = center - getDensity() * 10;
+			float profileIconSize = PROFILE_ICON_SIZE_DP * getDensity();
 			Bitmap profileIconBitmap = AndroidUtils.createScaledBitmap(
-					iconsCache.getPaintedIcon(iconRes, color), (int) iconSize, (int) iconSize);
-			canvas.drawBitmap(profileIconBitmap, center - iconSize / 2, center - iconSize / 2,
-					profileIconBorderPaint);
+					iconsCache.getPaintedIcon(iconRes, color), (int) profileIconSize, (int) profileIconSize);
+			canvas.drawBitmap(profileIconBitmap, iconCenter - profileIconSize / 2,
+					iconCenter - profileIconSize / 2, profileIconBorderPaint);
 
 			profileIconsBitmapCache.put(key, framedProfileIconBitmap);
 		}
@@ -122,6 +124,6 @@ public class MultiProfileGeometryWayContext extends GeometryWayContext {
 
 	@ColorInt
 	public int getStraightLineColor() {
-		return ContextCompat.getColor(getCtx(), ProfileIconColors.DARK_YELLOW.getColor(isNightMode()));
+		return ContextCompat.getColor(getCtx(), isNightMode() ? R.color.osmand_orange : R.color.color_myloc_distance);
 	}
 }
