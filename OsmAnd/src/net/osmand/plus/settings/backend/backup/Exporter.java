525b9dbc685de8f4c488ec633e02822775ccd5b9
max-klaus <alex.morozov.osm@gmail.com>
2021-08-05 11:37:58 +0300

    Fix #12379

--- OsmAnd/src/net/osmand/plus/settings/backend/backup/Exporter.java
+++ OsmAnd/src/net/osmand/plus/settings/backend/backup/Exporter.java
@@ -11,14 +11,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import static net.osmand.plus.settings.backend.backup.SettingsHelper.VERSION;
 
 public abstract class Exporter {
 
-	private final Map<String, SettingsItem> items;
+	private final List<SettingsItem> items;
 	private final Map<String, String> additionalParams;
 	private ExportProgressListener progressListener;
 
@@ -26,18 +28,16 @@ public abstract class Exporter {
 
 	public Exporter(@Nullable ExportProgressListener progressListener) {
 		this.progressListener = progressListener;
-		this.items = new LinkedHashMap<>();
+		this.items = new ArrayList<>();
 		this.additionalParams = new LinkedHashMap<>();
 	}
 
-	public void addSettingsItem(SettingsItem item) throws IllegalArgumentException {
-		if (items.containsKey(item.getName())) {
-			throw new IllegalArgumentException("Already has such item: " + item.getName());
-		}
-		items.put(item.getName(), item);
+	public void addSettingsItem(@NonNull SettingsItem item) throws IllegalArgumentException {
+		items.add(item);
 	}
 
-	public Map<String, SettingsItem> getItems() {
+	@NonNull
+	public List<SettingsItem> getItems() {
 		return items;
 	}
 
@@ -65,7 +65,7 @@ public abstract class Exporter {
 	public abstract void export() throws JSONException, IOException;
 
 	protected void writeItems(@NonNull AbstractWriter writer) throws IOException {
-		for (SettingsItem item : getItems().values()) {
+		for (SettingsItem item : getItems()) {
 			writer.write(item);
 			if (isCancelled()) {
 				break;
@@ -80,7 +80,7 @@ public abstract class Exporter {
 			json.put(param.getKey(), param.getValue());
 		}
 		JSONArray itemsJson = new JSONArray();
-		for (SettingsItem item : items.values()) {
+		for (SettingsItem item : items) {
 			itemsJson.put(new JSONObject(item.toJson()));
 		}
 		json.put("items", itemsJson);
