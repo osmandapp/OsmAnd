295e6f91b7a6eb56acde1b0242da4808ce86e358
nnngrach <nnngrach@gmail.com>
2021-08-05 11:08:45 +0300

    review fix

--- OsmAnd/src/net/osmand/plus/parkingpoint/ParkingPositionPlugin.java
+++ OsmAnd/src/net/osmand/plus/parkingpoint/ParkingPositionPlugin.java
@@ -370,8 +370,7 @@ public class ParkingPositionPlugin extends OsmandPlugin {
 				cal.add(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
 				cal.add(Calendar.MINUTE, timePicker.getCurrentMinute());
 				setParkingTime(cal.getTimeInMillis());
-				mapActivity.getMyApplication().getFavorites().getSpecialPoint(SpecialPointType.PARKING).setTimestamp(cal.getTimeInMillis());
-				mapActivity.getMyApplication().getFavorites().saveCurrentPointsIntoFile();
+				app.getFavorites().setParkingPoint(getParkingPosition(), null, getParkingTime(), isParkingEventAdded());
 				CheckBox addCalendarEvent = (CheckBox) setTimeParking.findViewById(R.id.check_event_in_calendar);
 				if (addCalendarEvent.isChecked()) {
 					addCalendarEvent(setTimeParking.getContext());
