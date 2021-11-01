cd243bf59dba8014362ef8a1d36d1c38e7c84f17
alisa911 <alisa.91150@gmail.com>
2021-11-01 20:18:13 +0200

    add test hanging routing

--- OsmAnd-java/src/test/java/net/osmand/router/RouteTestingTest.java
+++ OsmAnd-java/src/test/java/net/osmand/router/RouteTestingTest.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.concurrent.TimeUnit;
 
 import net.osmand.binary.BinaryMapIndexReader;
 
