87e7856466b56fbe22dcfc2c8bed2f2b60a528b8
Vitaliy <chumva.forever@gmail.com>
2021-11-11 11:07:29 +0200

    Disable android auto paywall

--- OsmAnd/src/net/osmand/plus/inapp/InAppPurchaseHelper.java
+++ OsmAnd/src/net/osmand/plus/inapp/InAppPurchaseHelper.java
@@ -176,7 +176,7 @@ public abstract class InAppPurchaseHelper {
 	}
 
 	public static boolean isAndroidAutoAvailable(@NonNull OsmandApplication ctx) {
-		return Version.isDeveloperBuild(ctx) || Version.isPaidVersion(ctx);
+		return true;
 	}
 
 	public static boolean isFullVersionPurchased(@NonNull OsmandApplication ctx) {
