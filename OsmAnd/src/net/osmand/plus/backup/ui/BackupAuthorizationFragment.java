710e0cc8db25f538fd8e19f3b51b855b18751653
mmh-dev <murod.hodjaev@gmail.com>
2021-11-20 09:39:06 +0600

    added Backup & Restore background image

--- OsmAnd/src/net/osmand/plus/backup/ui/BackupAuthorizationFragment.java
+++ OsmAnd/src/net/osmand/plus/backup/ui/BackupAuthorizationFragment.java
@@ -3,16 +3,22 @@ package net.osmand.plus.backup.ui;
 import static net.osmand.plus.UiUtilities.setupDialogButton;
 import static net.osmand.plus.importfiles.ImportHelper.ImportType.SETTINGS;
 
+import android.content.res.Resources;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.preference.Preference;
@@ -33,6 +39,9 @@ import net.osmand.plus.inapp.InAppPurchaseHelper.InAppPurchaseTaskType;
 import net.osmand.plus.settings.backend.ApplicationMode;
 import net.osmand.plus.settings.fragments.BaseSettingsFragment;
 import net.osmand.plus.settings.fragments.ExportSettingsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BackupAuthorizationFragment extends BaseSettingsFragment implements InAppPurchaseListener {
 
@@ -111,6 +120,13 @@ public class BackupAuthorizationFragment extends BaseSettingsFragment implements
 		View signUpButton = holder.itemView.findViewById(R.id.sign_up_button);
 		View signInButton = holder.itemView.findViewById(R.id.sign_in_button);
 
+		LinearLayout iconsContainer = (LinearLayout) holder.itemView.findViewById(R.id.icons_container);
+		int width = (int) (Resources.getSystem().getDisplayMetrics().widthPixels * 1.2);
+		FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width, LinearLayout.LayoutParams.WRAP_CONTENT);
+		params.leftMargin =  - (int) (Resources.getSystem().getDisplayMetrics().widthPixels * 0.02);
+		iconsContainer.setLayoutParams(params);
+		fillIconsContainer(iconsContainer);
+
 		boolean subscribed = InAppPurchaseHelper.isOsmAndProAvailable(app);
 		if (subscribed) {
 			setupAuthorizeButton(signUpButton, DialogButtonType.PRIMARY, R.string.register_opr_create_new_account, true);
@@ -124,6 +140,78 @@ public class BackupAuthorizationFragment extends BaseSettingsFragment implements
 			setupDialogButton(isNightMode(), signUpButton, DialogButtonType.PRIMARY, R.string.get_plugin);
 		}
 		setupAuthorizeButton(signInButton, DialogButtonType.SECONDARY, R.string.register_opr_have_account, false);
+	}
+	private void fillIconsContainer(LinearLayout view) {
+		List<Integer> yellowIcons = new ArrayList<>();
+		yellowIcons.add(R.drawable.ic_action_photo);
+		yellowIcons.add(R.drawable.ic_action_favorite);
+		yellowIcons.add(R.drawable.ic_action_micro_dark);
+		yellowIcons.add(R.drawable.ic_notification_track);
+		yellowIcons.add(R.drawable.ic_type_video);
+		yellowIcons.add(R.drawable.ic_action_info_dark);
+		yellowIcons.add(R.drawable.ic_action_openstreetmap_logo);
+		yellowIcons.add(R.drawable.ic_action_flag);
+
+		List<Integer> blueIcons = new ArrayList<>();
+		blueIcons.add(R.drawable.ic_map);
+		blueIcons.add(R.drawable.ic_action_settings);
+		blueIcons.add(R.drawable.ic_layer_top);
+		blueIcons.add(R.drawable.ic_plugin_srtm);
+		blueIcons.add(R.drawable.ic_action_plan_route);
+		blueIcons.add(R.drawable.ic_action_map_style);
+		blueIcons.add(R.drawable.ic_action_file_routing);
+		blueIcons.add(R.drawable.ic_action_hillshade_dark);
+
+		List<Integer> greenIcons = new ArrayList<>();
+		greenIcons.add(R.drawable.ic_action_gdirections_dark);
+		greenIcons.add(R.drawable.ic_action_settings);
+		greenIcons.add(R.drawable.ic_action_map_language);
+		greenIcons.add(R.drawable.ic_action_car_dark);
+		greenIcons.add(R.drawable.ic_action_pedestrian_dark);
+		greenIcons.add(R.drawable.ic_action_volume_up);
+		greenIcons.add(R.drawable.ic_action_sun);
+		greenIcons.add(R.drawable.ic_action_ruler_unit);
+
+		createIcons(view, yellowIcons, ContextCompat.getColor(view.getContext(),R.color.purchase_sc_toolbar_active_dark));
+		createIcons(view, blueIcons, ContextCompat.getColor(view.getContext(),R.color.backup_restore_icons_blue));
+		createIcons(view, greenIcons, ContextCompat.getColor(view.getContext(),R.color.purchase_save_discount));
+	}
+
+	private void createIcons(View view, List<Integer> icons, int color) {
+		LinearLayout row = new LinearLayout(view.getContext());
+		LinearLayout.LayoutParams rowParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		if (color != ContextCompat.getColor(view.getContext(),R.color.backup_restore_icons_blue)){
+			rowParams.leftMargin = AndroidUtils.dpToPx(view.getContext(), 25);
+		}
+		rowParams.bottomMargin = AndroidUtils.dpToPx(view.getContext(), 9);
+
+		int ovalSize = (int) (Resources.getSystem().getDisplayMetrics().widthPixels * 0.1);
+		int iconSize = (int) (Resources.getSystem().getDisplayMetrics().widthPixels * 0.067);
+
+		for (Integer i:icons) {
+			GradientDrawable oval = new GradientDrawable();
+			oval.setShape(GradientDrawable.OVAL);
+			oval.setStroke(AndroidUtils.dpToPx(view.getContext(), 1), color);
+			oval.setSize(ovalSize, ovalSize);
+			oval.setAlpha(51);
+
+			LinearLayout container = new LinearLayout(view.getContext());
+			LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+			containerParams.gravity = Gravity.CENTER;
+			containerParams.rightMargin = (int) (Resources.getSystem().getDisplayMetrics().widthPixels * 0.045);
+			container.setBackground(oval);
+
+			ImageView icon = new ImageView(view.getContext());
+			icon.setImageResource(i);
+			icon.setColorFilter(color);
+			LinearLayout.LayoutParams iconParams = new LinearLayout.LayoutParams(iconSize, iconSize);
+			iconParams.leftMargin = iconSize / 4;
+			iconParams.topMargin = iconSize / 4;
+
+			container.addView(icon, iconParams);
+			row.addView(container, containerParams);
+		}
+		((ViewGroup)view).addView(row, rowParams);
 	}
 
 	private void setupAuthorizeButton(View view, DialogButtonType buttonType, @StringRes int textId, final boolean signUp) {
