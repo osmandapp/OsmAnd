f5686d232b8ebe22cca03bc5ca8112674e42025a
nazar-kutz <vitaminkuna@gmail.com>
2022-01-24 16:21:03 +0200

    Extract default attributes initialization to DefaultAttributes class

--- OsmAnd/src/net/osmand/plus/widgets/chips/DefaultAttributes.java
+++ OsmAnd/src/net/osmand/plus/widgets/chips/DefaultAttributes.java
@@ -1,6 +1,16 @@
 package net.osmand.plus.widgets.chips;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+
 import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import net.osmand.plus.R;
+import net.osmand.plus.utils.AndroidUtils;
 
 class DefaultAttributes {
 
@@ -34,5 +44,77 @@ class DefaultAttributes {
 	@ColorInt
 	public int strokeColor;
 	public int strokeWidth;
+
+	private DefaultAttributes() {
+	}
+
+	public static DefaultAttributes createInstance(@NonNull Context context,
+	                                               @Nullable AttributeSet attrs,
+	                                               int defStyleAttr) {
+		TypedArray a = context.getTheme().obtainStyledAttributes(
+				attrs,
+				R.styleable.HorizontalChipsView,
+				defStyleAttr,
+				0
+		);
+
+		DefaultAttributes defAttrs = new DefaultAttributes();
+
+		defAttrs.titleColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipTitleColor,
+				getColorFromAttr(context, R.attr.chip_content_color));
+		defAttrs.titleSelectedColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipTitleSelectedColor,
+				getColor(context, R.color.color_white));
+		defAttrs.titleDisabledColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipTitleDisabledColor,
+				getColorFromAttr(context, R.attr.inactive_text_color));
+
+		defAttrs.iconColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipIconColor,
+				getColorFromAttr(context, R.attr.chip_content_color));
+		defAttrs.iconSelectedColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipIconSelectedColor,
+				getColor(context, R.color.color_white));
+		defAttrs.iconDisabledColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipIconDisabledColor,
+				getColorFromAttr(context, R.attr.inactive_text_color));
+
+		defAttrs.bgColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipBgColor,
+				getColor(context, R.color.color_transparent));
+		defAttrs.bgSelectedColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipBgSelectedColor,
+				getColorFromAttr(context, R.attr.active_color_basic));
+		defAttrs.bgDisabledColor = a.getInteger(
+				R.styleable.HorizontalChipsView_chipBgDisabledColor,
+				getColor(context, R.color.color_transparent));
+		defAttrs.bgRippleId = resolveAttribute(context, R.attr.chip_ripple);
+
+		defAttrs.drawablePaddingPx = getDimension(context, R.dimen.content_padding_half);
+
+		defAttrs.strokeColor = getColorFromAttr(context, R.attr.stroked_buttons_and_links_outline);
+		defAttrs.strokeWidth = AndroidUtils.dpToPx(context, 1);
+
+		return defAttrs;
+	}
+
+	@ColorInt
+	private static int getColorFromAttr(@NonNull Context ctx, int attr) {
+		return getColor(ctx, resolveAttribute(ctx, attr));
+	}
+
+	private static int resolveAttribute(@NonNull Context ctx, int attr) {
+		return AndroidUtils.resolveAttribute(ctx, attr);
+	}
+
+	@ColorInt
+	private static int getColor(@NonNull Context ctx, int colorId) {
+		return ContextCompat.getColor(ctx, colorId);
+	}
+
+	private static int getDimension(@NonNull Context ctx, int dimen) {
+		return ctx.getResources().getDimensionPixelSize(dimen);
+	}
 
 }
--- OsmAnd/src/net/osmand/plus/widgets/chips/HorizontalChipsView.java
+++ OsmAnd/src/net/osmand/plus/widgets/chips/HorizontalChipsView.java
@@ -1,18 +1,13 @@
 package net.osmand.plus.widgets.chips;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.util.AttributeSet;
 
-import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import net.osmand.plus.R;
-import net.osmand.plus.utils.AndroidUtils;
 import net.osmand.plus.widgets.chips.ChipsAdapter.OnSelectChipListener;
 
 import java.util.List;
@@ -21,7 +16,7 @@ public class HorizontalChipsView extends RecyclerView {
 
 	final private ChipsAdapter adapter;
 	final private ChipsDataHolder holder = new ChipsDataHolder();
-	final private DefaultAttributes defAttrs = new DefaultAttributes();
+	final private DefaultAttributes defAttrs;
 
 	public HorizontalChipsView(@NonNull Context context) {
 		this(context, null);
@@ -40,7 +35,7 @@ public class HorizontalChipsView extends RecyclerView {
 		setLayoutManager(new LinearLayoutManager(context, RecyclerView.HORIZONTAL, false));
 		setClipToPadding(false);
 
-		initDefaultAttrs(context, attrs, defStyleAttr);
+		defAttrs = DefaultAttributes.createInstance(context, attrs, defStyleAttr);
 		adapter = new ChipsAdapter(context, holder, defAttrs);
 		setAdapter(adapter);
 	}
@@ -71,71 +66,6 @@ public class HorizontalChipsView extends RecyclerView {
 
 	public void smoothScrollTo(@NonNull ChipItem chip) {
 		smoothScrollToPosition(holder.indexOf(chip));
-	}
-
-	private void initDefaultAttrs(@NonNull Context context,
-	                              @Nullable AttributeSet attrs,
-	                              int defStyleAttr) {
-		TypedArray a = context.getTheme().obtainStyledAttributes(
-				attrs,
-				R.styleable.HorizontalChipsView,
-				defStyleAttr,
-				0
-		);
-
-		defAttrs.titleColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipTitleColor,
-				getColorFromAttr(R.attr.chip_content_color));
-		defAttrs.titleSelectedColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipTitleSelectedColor,
-				getColor(R.color.color_white));
-		defAttrs.titleDisabledColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipTitleDisabledColor,
-				getColorFromAttr(R.attr.inactive_text_color));
-
-		defAttrs.iconColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipIconColor,
-				getColorFromAttr(R.attr.chip_content_color));
-		defAttrs.iconSelectedColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipIconSelectedColor,
-				getColor(R.color.color_white));
-		defAttrs.iconDisabledColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipIconDisabledColor,
-				getColorFromAttr(R.attr.inactive_text_color));
-
-		defAttrs.bgColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipBgColor,
-				getColor(R.color.color_transparent));
-		defAttrs.bgSelectedColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipBgSelectedColor,
-				getColorFromAttr(R.attr.active_color_basic));
-		defAttrs.bgDisabledColor = a.getInteger(
-				R.styleable.HorizontalChipsView_chipBgDisabledColor,
-				getColor(R.color.color_transparent));
-		defAttrs.bgRippleId = resolveAttribute(R.attr.chip_ripple);
-
-		defAttrs.drawablePaddingPx = getDimension(R.dimen.content_padding_half);
-
-		defAttrs.strokeColor = getColorFromAttr(R.attr.stroked_buttons_and_links_outline);
-		defAttrs.strokeWidth = AndroidUtils.dpToPx(context, 1);
-	}
-
-	@ColorInt
-	private int getColorFromAttr(int attr) {
-		return getColor(resolveAttribute(attr));
-	}
-
-	private int resolveAttribute(int attr) {
-		return AndroidUtils.resolveAttribute(getContext(), attr);
-	}
-
-	@ColorInt
-	private int getColor(int colorId) {
-		return ContextCompat.getColor(getContext(), colorId);
-	}
-
-	private int getDimension(int dimen) {
-		return getResources().getDimensionPixelSize(dimen);
 	}
 
 }
