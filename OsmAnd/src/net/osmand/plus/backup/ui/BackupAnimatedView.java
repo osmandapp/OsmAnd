4fd8e3b275630130bf0b7d6f83403c968b066b2e
mmh-dev <murod.hodjaev@gmail.com>
2022-02-07 16:40:41 +0600

    icons are obtained from cache, fixed crash on screen rotation

--- OsmAnd/src/net/osmand/plus/backup/ui/BackupAnimatedView.java
+++ OsmAnd/src/net/osmand/plus/backup/ui/BackupAnimatedView.java
@@ -10,67 +10,63 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.content.res.AppCompatResources;
 
+import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.R;
 import net.osmand.plus.utils.AndroidUtils;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class BackupAnimatedView extends View {
 	private Paint paint = new Paint();
 	private final int iconSize = AndroidUtils.dpToPx(getContext(), 36);
 	private final int rowMargin = AndroidUtils.dpToPx(getContext(), 16);
+	OsmandApplication app;
 
 	public BackupAnimatedView(Context context, @Nullable AttributeSet attrs) {
 		super(context, attrs);
 		paint.setStyle(Style.STROKE);
 		paint.setStrokeWidth(AndroidUtils.dpToPx(context, 1));
-		paint.setAlpha(128);
+		app = (OsmandApplication) context.getApplicationContext();
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		buildRow(canvas, IconColorType.YELLOW);
-		buildRow(canvas, IconColorType.BLUE);
-		buildRow(canvas, IconColorType.GREEN);
+		buildRow(canvas, IconColorType.YELLOW, 0);
+		buildRow(canvas, IconColorType.BLUE, 1);
+		buildRow(canvas, IconColorType.GREEN, 2);
 	}
 
-	private void buildRow(Canvas canvas, IconColorType colorType) {
+	private void buildRow(Canvas canvas, IconColorType colorType, int rowNumber) {
 		int screenSize = Resources.getSystem().getDisplayMetrics().widthPixels;
-		int index = 0;
 		int xOffset = 0;
 		int xOffsetStep = iconSize + rowMargin;
 		if (colorType == IconColorType.YELLOW || colorType == IconColorType.GREEN) {
 			xOffset = rowMargin;
 		}
-		List<Integer> iconsList = getIconsList(colorType);
 
 		while (screenSize > 0) {
-			drawIcon(canvas, iconsList.get(index), colorType.getColor(), xOffset);
+			drawIcon(canvas, getIconId(), colorType.getColor(), xOffset, rowNumber);
 			xOffset += xOffsetStep;
 			screenSize -= xOffsetStep;
-			index++;
 		}
 	}
 
-	private void drawIcon(Canvas canvas, int drawable, int color, int xOffset) {
+	private void drawIcon(Canvas canvas, int drawableId, int color, int xOffset, int rowNumber) {
 		int rowBottomMargin = iconSize / 4;
 		int center = iconSize / 2;
-		Drawable icon = AppCompatResources.getDrawable(getContext(), drawable);
+		Drawable icon = app.getUIUtilities().getIcon(drawableId);
 		paint.setColor(getResources().getColor(color));
+		paint.setAlpha(51);
 		int x = center + xOffset;
 		int y = center;
 
 		if (icon != null) {
 			icon.setTint(getResources().getColor(color));
-			if (color == IconColorType.BLUE.getColor()) {
-				y += iconSize + rowBottomMargin;
-			} else if (color == IconColorType.GREEN.getColor()) {
-				y += (iconSize + rowBottomMargin) * 2;
-			}
+			y += rowNumber * (iconSize + rowBottomMargin);
 			icon.setBounds((x - icon.getIntrinsicWidth() / 2),
 					y - icon.getIntrinsicHeight() / 2,
 					x + icon.getIntrinsicWidth() / 2,
@@ -80,40 +76,33 @@ public class BackupAnimatedView extends View {
 		}
 	}
 
-	private List<Integer> getIconsList(IconColorType colorType) {
-		List<Integer> icons = new ArrayList<>();
-
-		if (colorType == IconColorType.YELLOW) {
-			icons.add(R.drawable.ic_action_photo);
-			icons.add(R.drawable.ic_action_favorite);
-			icons.add(R.drawable.ic_action_micro_dark);
-			icons.add(R.drawable.ic_notification_track);
-			icons.add(R.drawable.ic_type_video);
-			icons.add(R.drawable.ic_action_info_dark);
-			icons.add(R.drawable.ic_action_openstreetmap_logo);
-			icons.add(R.drawable.ic_action_flag);
-
-		} else if (colorType == IconColorType.BLUE) {
-			icons.add(R.drawable.ic_map);
-			icons.add(R.drawable.ic_action_settings);
-			icons.add(R.drawable.ic_layer_top);
-			icons.add(R.drawable.ic_plugin_srtm);
-			icons.add(R.drawable.ic_action_plan_route);
-			icons.add(R.drawable.ic_action_map_style);
-			icons.add(R.drawable.ic_action_file_routing);
-			icons.add(R.drawable.ic_action_hillshade_dark);
-
-		} else {
-			icons.add(R.drawable.ic_action_gdirections_dark);
-			icons.add(R.drawable.ic_action_settings);
-			icons.add(R.drawable.ic_action_map_language);
-			icons.add(R.drawable.ic_action_car_dark);
-			icons.add(R.drawable.ic_action_pedestrian_dark);
-			icons.add(R.drawable.ic_action_volume_up);
-			icons.add(R.drawable.ic_action_sun);
-			icons.add(R.drawable.ic_action_ruler_unit);
-		}
-		return icons;
+	private int getIconId() {
+		List<Integer> icons = Arrays.asList(
+				R.drawable.ic_action_photo,
+				R.drawable.ic_action_favorite,
+				R.drawable.ic_action_micro_dark,
+				R.drawable.ic_notification_track,
+				R.drawable.ic_type_video,
+				R.drawable.ic_action_info_dark,
+				R.drawable.ic_action_openstreetmap_logo,
+				R.drawable.ic_action_flag,
+				R.drawable.ic_map,
+				R.drawable.ic_layer_top,
+				R.drawable.ic_plugin_srtm,
+				R.drawable.ic_action_plan_route,
+				R.drawable.ic_action_map_style,
+				R.drawable.ic_action_file_routing,
+				R.drawable.ic_action_hillshade_dark,
+				R.drawable.ic_action_gdirections_dark,
+				R.drawable.ic_action_settings,
+				R.drawable.ic_action_map_language,
+				R.drawable.ic_action_car_dark,
+				R.drawable.ic_action_pedestrian_dark,
+				R.drawable.ic_action_volume_up,
+				R.drawable.ic_action_sun,
+				R.drawable.ic_action_ruler_unit);
+		int randomIndex = new Random().nextInt(icons.size());
+		return icons.get(randomIndex);
 	}
 
 	enum IconColorType {
