9879e91451a12ce76718ad423dfbf59611191140
cepprice <super.cepprice@gmail.com>
2022-06-06 22:24:12 +0500

    Add option to import multi track as one file #14281

--- OsmAnd/src/net/osmand/plus/importfiles/ui/HeaderViewHolder.java
+++ OsmAnd/src/net/osmand/plus/importfiles/ui/HeaderViewHolder.java
@@ -6,22 +6,30 @@ import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView.ViewHolder;
-
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.R;
 import net.osmand.plus.helpers.FontCache;
+import net.osmand.plus.importfiles.ui.ImportTracksAdapter.ImportTracksListener;
 import net.osmand.plus.utils.ColorUtilities;
 import net.osmand.plus.widgets.style.CustomTypefaceSpan;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
 class HeaderViewHolder extends ViewHolder {
 
-	final TextView title;
+	final ImportTracksListener listener;
 
-	HeaderViewHolder(@NonNull View itemView) {
+	final TextView title;
+	final View importAsOneTrackButton;
+
+	HeaderViewHolder(@NonNull View itemView, @Nullable ImportTracksListener listener) {
 		super(itemView);
+		this.listener = listener;
+
 		title = itemView.findViewById(R.id.title);
+		importAsOneTrackButton = itemView.findViewById(R.id.import_as_one_track);
 	}
 
 	public void bindView(@NonNull OsmandApplication app, @NonNull String fileName, int tracksCount, boolean nightMode) {
@@ -39,5 +47,11 @@ class HeaderViewHolder extends ViewHolder {
 		index = description.lastIndexOf(size);
 		spannable.setSpan(new CustomTypefaceSpan(typeface), index, index + size.length(), 0);
 		title.setText(spannable);
+
+		importAsOneTrackButton.setOnClickListener(v -> {
+			if (listener != null) {
+				listener.onImportAsOneTrackClicked();
+			}
+		});
 	}
 }
