5c6c08896063b41d85832a09c98f2f23f09347fc
Chumva <chumva.forever@gmail.com>
2022-06-30 22:12:10 +0300

    Simple refactoring

--- OsmAnd/src/net/osmand/plus/DialogListItemAdapter.java
+++ OsmAnd/src/net/osmand/plus/DialogListItemAdapter.java
@@ -7,36 +7,37 @@ import android.widget.BaseAdapter;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
-import net.osmand.plus.utils.UiUtilities;
-
 import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.view.ContextThemeWrapper;
+
+import net.osmand.plus.utils.UiUtilities;
 
 public class DialogListItemAdapter extends BaseAdapter {
 
 	public static final int INVALID_ID = -1;
 
-	private String[] mData;
-	private boolean multiChoice;
-	private boolean nightMode;
+	private final String[] mData;
+	private final boolean multiChoice;
+	private final boolean nightMode;
 	private int selected = INVALID_ID;
-	private boolean checkedItems[];
-	private OsmandApplication app;
-	@ColorInt private int compoundButtonColor = INVALID_ID;
+	private final boolean[] checkedItems;
+	@ColorInt
+	private int compoundButtonColor = INVALID_ID;
+
 	private AlertDialog dialog;
-	private View.OnClickListener listener;
-	private LayoutInflater inflater;
-	
+	private final View.OnClickListener listener;
+	private final LayoutInflater inflater;
+
 	public static DialogListItemAdapter createSingleChoiceAdapter(String[] mData, boolean nightMode, int selected, OsmandApplication app,
 	                                                              @ColorInt int compoundButtonColor, int themeRes, final View.OnClickListener listener) {
-		
+
 		return new DialogListItemAdapter(mData, selected, null, nightMode, app, compoundButtonColor, themeRes, listener, false);
 	}
 
-	public static DialogListItemAdapter createMultiChoiceAdapter(String[] mData, boolean nightMode, boolean checkedItems[], OsmandApplication app,
+	public static DialogListItemAdapter createMultiChoiceAdapter(String[] mData, boolean nightMode, boolean[] checkedItems, OsmandApplication app,
 	                                                             @ColorInt int compoundButtonColor, int themeRes, final View.OnClickListener listener) {
-		
+
 		return new DialogListItemAdapter(mData, INVALID_ID, checkedItems, nightMode, app, compoundButtonColor, themeRes, listener, true);
 	}
 
@@ -47,7 +48,6 @@ public class DialogListItemAdapter extends BaseAdapter {
 		this.checkedItems = checkedItems;
 		this.nightMode = nightMode;
 		this.multiChoice = multiChoice;
-		this.app = app;
 		this.compoundButtonColor = compoundButtonColor;
 		this.listener = listener;
 		inflater = LayoutInflater.from(new ContextThemeWrapper(app, themeRes));
@@ -87,23 +87,17 @@ public class DialogListItemAdapter extends BaseAdapter {
 			cb = view.findViewById(R.id.checkbox);
 			view.findViewById(R.id.radio).setVisibility(View.INVISIBLE);
 			cb.setChecked(checkedItems[position]);
-			button.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					listener.onClick(v);
-					cb.setChecked(!cb.isChecked());
-				}
+			button.setOnClickListener(v -> {
+				listener.onClick(v);
+				cb.setChecked(!cb.isChecked());
 			});
 		} else {
 			cb = view.findViewById(R.id.radio);
 			view.findViewById(R.id.checkbox).setVisibility(View.INVISIBLE);
 			cb.setChecked(position == selected);
-			button.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					listener.onClick(v);
-					dialog.dismiss();
-				}
+			button.setOnClickListener(v -> {
+				listener.onClick(v);
+				dialog.dismiss();
 			});
 		}
 		cb.setVisibility(View.VISIBLE);
