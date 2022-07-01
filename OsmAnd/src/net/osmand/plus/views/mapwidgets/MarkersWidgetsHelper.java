b2cbdcfbec2c6fbd8a0bb0d20ce5133e996416ba
Chumva <chumva.forever@gmail.com>
2022-07-01 12:53:22 +0300

    Fix cached markers widgets

--- OsmAnd/src/net/osmand/plus/views/mapwidgets/MarkersWidgetsHelper.java
+++ OsmAnd/src/net/osmand/plus/views/mapwidgets/MarkersWidgetsHelper.java
@@ -16,25 +16,28 @@ import net.osmand.plus.R;
 import net.osmand.plus.activities.MapActivity;
 import net.osmand.plus.mapmarkers.MapMarker;
 import net.osmand.plus.mapmarkers.MapMarkersHelper;
+import net.osmand.plus.settings.backend.OsmandSettings;
 import net.osmand.plus.views.AnimateDraggingMapThread;
 import net.osmand.plus.views.OsmandMapTileView;
 import net.osmand.plus.views.mapwidgets.MapWidgetRegistry.WidgetsRegistryListener;
 import net.osmand.plus.views.mapwidgets.widgets.MapWidget;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MarkersWidgetsHelper implements WidgetsRegistryListener {
 
 	private final MapActivity mapActivity;
+	private final OsmandSettings settings;
 	private final MapWidgetRegistry widgetRegistry;
 
-	private final List<MapWidget> barWidgets = new ArrayList<>();
-	private final List<MapWidget> sideFirstWidgets = new ArrayList<>();
-	private final List<MapWidget> sideSecondWidgets = new ArrayList<>();
+	private final Set<MapWidget> barWidgets = new HashSet<>();
+	private final Set<MapWidget> sideFirstWidgets = new HashSet<>();
+	private final Set<MapWidget> sideSecondWidgets = new HashSet<>();
 
 	public MarkersWidgetsHelper(@NonNull MapActivity mapActivity) {
 		this.mapActivity = mapActivity;
+		settings = mapActivity.getMyApplication().getSettings();
 		widgetRegistry = mapActivity.getMapLayers().getMapWidgetRegistry();
 		widgetRegistry.addWidgetsRegistryListener(this);
 	}
@@ -45,7 +48,7 @@ public class MarkersWidgetsHelper implements WidgetsRegistryListener {
 		setCustomLatLon(sideSecondWidgets, latLon);
 	}
 
-	public void setCustomLatLon(@NonNull List<MapWidget> widgets, @Nullable LatLon latLon) {
+	public void setCustomLatLon(@NonNull Set<MapWidget> widgets, @Nullable LatLon latLon) {
 		for (MapWidget widget : widgets) {
 			if (widget instanceof CustomLatLonListener) {
 				((CustomLatLonListener) widget).setCustomLatLon(latLon);
@@ -81,18 +84,39 @@ public class MarkersWidgetsHelper implements WidgetsRegistryListener {
 
 	@Override
 	public void onWidgetRegistered(@NonNull MapWidgetInfo widgetInfo, @Nullable WidgetType widgetType) {
-		if (widgetType == MARKERS_TOP_BAR) {
-			barWidgets.add(widgetInfo.widget);
-		} else if (widgetType == SIDE_MARKER_1) {
-			sideFirstWidgets.add(widgetInfo.widget);
-		} else if (widgetType == SIDE_MARKER_2) {
-			sideSecondWidgets.add(widgetInfo.widget);
+		if (isMarkerWidget(widgetType)) {
+			if (widgetType == MARKERS_TOP_BAR) {
+				barWidgets.add(widgetInfo.widget);
+			} else if (widgetType == SIDE_MARKER_1) {
+				sideFirstWidgets.add(widgetInfo.widget);
+			} else if (widgetType == SIDE_MARKER_2) {
+				sideSecondWidgets.add(widgetInfo.widget);
+			}
 		}
 	}
 
 	@Override
 	public void onWidgetVisibilityChanged(@NonNull MapWidgetInfo widgetInfo) {
+		WidgetType widgetType = widgetInfo.widget.getWidgetType();
+		if (isMarkerWidget(widgetType) && !widgetInfo.isEnabledForAppMode(settings.getApplicationMode())) {
+			if (widgetType == MARKERS_TOP_BAR) {
+				barWidgets.remove(widgetInfo.widget);
+			} else if (widgetType == SIDE_MARKER_1) {
+				sideFirstWidgets.remove(widgetInfo.widget);
+			} else if (widgetType == SIDE_MARKER_2) {
+				sideSecondWidgets.remove(widgetInfo.widget);
+			}
+		}
+	}
 
+	private boolean isMarkerWidget(@Nullable WidgetType widgetType) {
+		return widgetType == MARKERS_TOP_BAR || widgetType == SIDE_MARKER_1 || widgetType == SIDE_MARKER_2;
+	}
+
+	public void clearWidgets() {
+		barWidgets.clear();
+		sideFirstWidgets.clear();
+		sideSecondWidgets.clear();
 	}
 
 	public static void showMarkerOnMap(@NonNull MapActivity mapActivity, int index) {
