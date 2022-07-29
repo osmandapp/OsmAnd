24f966c2bf005aafbe50d005d123ffc1bfd75ad5
Chumva <chumva.forever@gmail.com>
2022-07-29 21:01:03 +0300

    Fix #14873

--- OsmAnd/src/net/osmand/plus/plugins/mapillary/MapillaryPlugin.java
+++ OsmAnd/src/net/osmand/plus/plugins/mapillary/MapillaryPlugin.java
@@ -231,7 +231,7 @@ public class MapillaryPlugin extends OsmandPlugin {
 	public void createWidgets(@NonNull MapActivity mapActivity, @NonNull List<MapWidgetInfo> widgetsInfos, @NonNull ApplicationMode appMode) {
 		MapWidgetRegistry widgetRegistry = app.getOsmandMap().getMapLayers().getMapWidgetRegistry();
 		MapWidget widget = createMapWidgetForParams(mapActivity, WidgetType.MAPILLARY);
-		widgetsInfos.add(widgetRegistry.createWidgetInfo(widget));
+		widgetsInfos.add(widgetRegistry.createWidgetInfo(widget, appMode));
 	}
 
 	public void setWidgetVisible(MapActivity mapActivity, boolean visible) {
