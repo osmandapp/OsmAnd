9969107cce8e69d95a51bf50d8579960a1792c40
alex-osm <alex.morozov.osm@gmail.com>
2022-08-25 16:00:29 +0300

    Remove unused const

--- OsmAnd/src/net/osmand/plus/NavigationService.java
+++ OsmAnd/src/net/osmand/plus/NavigationService.java
@@ -44,7 +44,6 @@ public class NavigationService extends Service {
 	public static int USED_BY_NAVIGATION = 1;
 	public static int USED_BY_GPX = 2;
 	public static int USED_BY_CAR_APP = 4;
-	public static int USED_BY_ANT = 8;
 	public static final String USAGE_INTENT = "SERVICE_USED_BY";
 
 	private final NavigationServiceBinder binder = new NavigationServiceBinder();
