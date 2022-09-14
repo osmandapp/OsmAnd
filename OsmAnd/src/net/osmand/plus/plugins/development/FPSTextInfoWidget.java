58290f003826c656c28c8a1ad750c22a959e8e5b
Victor Shcherb <victor.shcherb@gmail.com>
2022-09-14 15:39:32 +0300

    Add fps info

--- OsmAnd/src/net/osmand/plus/plugins/development/FPSTextInfoWidget.java
+++ OsmAnd/src/net/osmand/plus/plugins/development/FPSTextInfoWidget.java
@@ -1,5 +1,7 @@
 package net.osmand.plus.plugins.development;
 
+import android.os.SystemClock;
+
 import static net.osmand.plus.views.mapwidgets.WidgetType.FPS;
 
 import androidx.annotation.NonNull;
@@ -9,10 +11,16 @@ import net.osmand.plus.activities.MapActivity;
 import net.osmand.plus.views.OsmandMapTileView;
 import net.osmand.plus.views.layers.base.OsmandMapLayer.DrawSettings;
 import net.osmand.plus.views.mapwidgets.widgets.TextInfoWidget;
+import net.osmand.core.android.MapRendererView;
 
 public class FPSTextInfoWidget extends TextInfoWidget {
 
 	private final OsmandMapTileView mapView;
+	private static final int FRAME_BUFFER_LENGTH = 20;
+	private long startMs = 0;
+	private int startFrameId;
+	private long swapMs = 0;
+	private int swapFrameId;
 
 	public FPSTextInfoWidget(@NonNull MapActivity mapActivity) {
 		super(mapActivity, FPS);
@@ -23,9 +31,34 @@ public class FPSTextInfoWidget extends TextInfoWidget {
 
 	@Override
 	public void updateInfo(@Nullable DrawSettings drawSettings) {
-		if (!mapView.isMeasureFPS()) {
-			mapView.setMeasureFPS(true);
+		MapRendererView mv = mapView.getMapRenderer();
+		if (mv != null) {
+			int frameId = mv.getFrameId();
+			long now = SystemClock.elapsedRealtime();
+			String fps = "- FPS";
+			if (frameId > startFrameId && now > startMs && startMs != 0) {
+				fps = String.format("%.1f FPS",
+						1000.0 / (now - startMs) * (frameId - startFrameId));
+			}
+			if (startMs == 0) {
+				startMs = now;
+				startFrameId = frameId;
+			} else if (swapMs == 0 && (frameId - startFrameId) > FRAME_BUFFER_LENGTH) {
+				swapMs = now;
+				swapFrameId = frameId;
+			} else if (swapMs == 0 && (frameId - swapFrameId) > FRAME_BUFFER_LENGTH) {
+				startMs = swapMs;
+				startFrameId = swapFrameId;
+				swapMs = now;
+				swapFrameId = frameId;
+			}
+
+			setText("", fps);
+		} else {
+			if (!mapView.isMeasureFPS()) {
+				mapView.setMeasureFPS(true);
+			}
+			setText("", (int) mapView.getFPS() + "/" + (int) mapView.getSecondaryFPS() + " FPS");
 		}
-		setText("", (int) mapView.getFPS() + "/" + (int) mapView.getSecondaryFPS() + " FPS");
 	}
 }
