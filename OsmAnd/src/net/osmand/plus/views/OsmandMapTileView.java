1ebd6a6b76a7e5dc68f01c9e5f0a6981bb6ea25f
Hardy <hardy.mueller@web.de>
2022-12-12 12:20:53 +0100

    Update OsmandMapTileView.java

--- OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
+++ OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
@@ -1579,6 +1579,9 @@ public class OsmandMapTileView implements IMapDownloaderCallback {
 				angleRelative = 0;
 			}
 			rotateToAnimate(initialViewport.getRotate() + angleRelative);
+			if (angleRelative != 0) {
+				application.getMapViewTrackingUtilities().setRotationNoneToManual();
+			}
 			int newZoom = getZoom();
 			if (application.accessibilityEnabled()) {
 				if (newZoom != initialViewport.getZoom()) {
