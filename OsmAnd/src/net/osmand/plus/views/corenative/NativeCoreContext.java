eecfb869fe2ba9204ebe71787825d2069700c6cf
Alex Andrewschenko <andrewschenko@gmail.com>
2023-01-05 16:22:52 +0100

    Set index cache file path

--- OsmAnd/src/net/osmand/plus/views/corenative/NativeCoreContext.java
+++ OsmAnd/src/net/osmand/plus/views/corenative/NativeCoreContext.java
@@ -51,15 +51,20 @@ public class NativeCoreContext {
 				mgr.getDefaultDisplay().getMetrics(dm);
 
 				ObfsCollection obfsCollection = new ObfsCollection();
+				obfsCollection.setIndexCacheFile(app.getCacheDir().getAbsolutePath() + "/ind_core.cache");
 				obfsCollection.addDirectory(directory.getAbsolutePath(), false);
 				obfsCollection.addDirectory(app.getAppPath(IndexConstants.ROADS_INDEX_DIR).getAbsolutePath(), false);
 				obfsCollection.addDirectory(app.getAppPath(IndexConstants.LIVE_INDEX_DIR).getAbsolutePath(), false);
 
-				if (PluginsHelper.isActive(NauticalMapsPlugin.class) || InAppPurchaseHelper.isDepthContoursPurchased(app)) {
-					obfsCollection.addDirectory(app.getAppPath(IndexConstants.NAUTICAL_INDEX_DIR).getAbsolutePath(), false);
+				if (app.getAppPath(IndexConstants.NAUTICAL_INDEX_DIR).exists() &&
+					(PluginsHelper.isActive(NauticalMapsPlugin.class) ||
+					InAppPurchaseHelper.isDepthContoursPurchased(app))) {
+						obfsCollection.addDirectory(app.getAppPath(IndexConstants.NAUTICAL_INDEX_DIR).getAbsolutePath(), false);
 				}
-				if (PluginsHelper.isActive(SRTMPlugin.class) || InAppPurchaseHelper.isContourLinesPurchased(app)) {
-					obfsCollection.addDirectory(app.getAppPath(IndexConstants.SRTM_INDEX_DIR).getAbsolutePath(), false);
+				if (app.getAppPath(IndexConstants.SRTM_INDEX_DIR).exists() &&
+					(PluginsHelper.isActive(SRTMPlugin.class) ||
+					InAppPurchaseHelper.isContourLinesPurchased(app))) {
+						obfsCollection.addDirectory(app.getAppPath(IndexConstants.SRTM_INDEX_DIR).getAbsolutePath(), false);
 				}
 
 				mapRendererContext = new MapRendererContext(app, dm.density);
