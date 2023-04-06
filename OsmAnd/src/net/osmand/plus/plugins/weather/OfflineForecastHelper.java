3046134f026acb7654eb33c767d5033559e8de85
Chumva <chumva.forever@gmail.com>
2023-04-06 17:27:13 +0300

    Remove debug logs from OfflineForecastHelper

--- OsmAnd/src/net/osmand/plus/plugins/weather/OfflineForecastHelper.java
+++ OsmAnd/src/net/osmand/plus/plugins/weather/OfflineForecastHelper.java
@@ -154,12 +154,10 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 	}
 
 	public void downloadForecastByRegion(@NonNull WorldRegion region) {
-		LOG.debug("[Download] [" + region.getRegionId() + "] Call 'downloadForecastByRegion' progress is null");
 		downloadForecastByRegion(region, null);
 	}
 
 	public boolean downloadForecastByRegion(@NonNull WorldRegion region, @Nullable IProgress progress) {
-		LOG.debug("[Download] [" + region.getRegionId() + "] Call 'downloadForecastByRegion' with progress");
 		if (!WeatherHelper.isWeatherSupported(app)) {
 			LOG.error("[Download] [" + region.getRegionId() + "] Failed. Weather isn't allowed with current configuration.");
 			return false;
@@ -181,9 +179,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 		QuadRect regionBounds = getRegionBounds(region);
 		LatLon topLeft = new LatLon(regionBounds.top, regionBounds.left);
 		LatLon bottomRight = new LatLon(regionBounds.bottom, regionBounds.right);
-		LOG.debug("[Download] [" + regionId + "]. Region bounds: " + regionBounds);
-		LOG.debug("[Download] [" + regionId + "]. TopLeft: " + topLeft);
-		LOG.debug("[Download] [" + regionId + "]. BottomRight: " + bottomRight);
 
 		setOfflineForecastProgressInfo(regionId, 0);
 		setPreferenceDownloadState(regionId, WeatherForecastDownloadState.IN_PROGRESS);
@@ -223,9 +218,7 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 			queryController.swigReleaseOwnership();
 			callback.swigReleaseOwnership();
 
-			LOG.debug("[Download] [" + regionId + "] Call resource manager: time = " + dateTime + ", " + (i + 1) + "/" + FORECAST_DATES_COUNT);
 			weatherResourcesManager.downloadGeoTiles(request, callback.getBinding());
-			LOG.debug("[Download] [" + regionId + "] Tile " + (i + 1) + "/" + FORECAST_DATES_COUNT + " downloaded");
 			dateTime += HOUR_IN_MILLIS * (i < 24 ? 1 : 3);
 		}
 
@@ -234,7 +227,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 
 	public void checkAndStopWeatherDownload(@NonNull WeatherIndexItem weatherIndexItem) {
 		String regionId = weatherIndexItem.getRegionId();
-		LOG.debug("[Download] [" + regionId + "] Call 'checkAndStopWeatherDownload'");
 		if (!isWeatherSupported(app)) {
 			LOG.error("[Download] [" + regionId + "] Can't stop weather download. Weather isn't allowed with current configuration.");
 			return;
@@ -252,7 +244,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 	}
 
 	public void prepareToStopDownloading(@NonNull String regionId) {
-		LOG.debug("[Download] [" + regionId + "] Call 'prepareToStopDownloading'");
 		totalCacheSize.reset(true);
 		if (isDownloadStateInProgress(regionId)) {
 			setPreferenceDownloadState(regionId, UNDEFINED);
@@ -268,7 +259,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 
 	public void calculateCacheSizeIfNeeded(@NonNull WeatherIndexItem indexItem, @Nullable OnCompleteCallback callback) {
 		String regionId = indexItem.getRegionId();
-		LOG.debug("[Calculate size] [" + regionId + "] Call 'calculateCacheSizeIfNeeded'");
 		if (!isWeatherSupported(app)) {
 			LOG.error("[Calculate size] [" + regionId + "] Can't calculate cache size. Weather isn't allowed with this configuration.");
 			notifyOnComplete(callback);
@@ -285,20 +275,15 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 				long containerSize = getOfflineForecastSizeInfo(regionId, false);
 				String size = decimalFormat.format(containerSize / (1024f * 1024f));
 				indexItem.updateSize(size, contentSize, containerSize);
-				LOG.debug("[Calculate size] [" + regionId + "] Calculation success: " +
-						"contentSize = " + contentSize + ", " +
-						"containerSize = " + containerSize + ", " + "size = " + size);
 				notifyOnComplete(callback);
 			});
 		} else {
-			LOG.debug("[Calculate size] [" + regionId + "] Already calculated");
 			notifyOnComplete(callback);
 		}
 	}
 
 	public void calculateCacheSize(@NonNull WorldRegion region, @Nullable OnCompleteCallback callback) {
 		String regionId = region.getRegionId();
-		LOG.debug("[Calculate size] [" + regionId + "] Call 'calculateCacheSize'");
 		if (!isWeatherSupported(app)) {
 			LOG.error("[Calculate size] [" + regionId + "] Can't calculate cache size. Weather isn't allowed with current configuration.");
 			notifyOnComplete(callback);
@@ -312,25 +297,18 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 		setOfflineForecastSizeInfo(regionId, 0, false);
 		setOfflineForecastSizesInfoCalculated(regionId, false);
 		runAsync(() -> {
-			LOG.debug("[Calculate size] [" + regionId + "] Run async calculation");
 			List<Long> tileIds = getTileIds(region);
 			TileIdList qTileIds = NativeUtilities.convertToQListTileIds(tileIds);
 			ZoomLevel zoom = getGeoTileZoom();
-			LOG.debug("[Calculate size] [" + regionId + "] Zoom = " + zoom + ", Tile ids = " + tileIds);
 			if (!qTileIds.isEmpty()) {
-				LOG.debug("[Calculate size] [" + regionId + "] Call resource manager");
 				BigInteger calculatedSize = weatherResourcesManager.calculateDbCacheSize(qTileIds, new TileIdList(), zoom);
 				long localSize = calculatedSize != null ? calculatedSize.longValue() : 0;
 				long updatesSize = calculateApproxUpdatesSize(tileIds);
-				LOG.debug("[Calculate size] [" + regionId + "] Calculated: local=" + localSize + ", updates=" + updatesSize);
 				setOfflineForecastSizeInfo(regionId, localSize, true);
 				setOfflineForecastSizeInfo(regionId, updatesSize, false);
 				setOfflineForecastSizesInfoCalculated(regionId, true);
 			}
-			runInUiThread(() -> {
-				LOG.debug("[Calculate size] [" + regionId + "] Notify on calculated");
-				notifyOnComplete(callback);
-			});
+			runInUiThread(() -> notifyOnComplete(callback));
 		});
 	}
 
@@ -339,9 +317,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 	}
 
 	public void calculateTotalCacheSizeAsync(boolean forceCalculation) {
-		LOG.debug("[Calculate size] [Total] Call calculateTotalCacheSizeAsync " +
-				"[isCalculated = " + totalCacheSize.isCalculated() + "], " +
-				"[forceCalculation = " + forceCalculation + "]");
 		if (!isWeatherSupported(app)) {
 			LOG.error("[Calculate size] [Total] Can't calculate. Weather isn't allowed with current configuration.");
 			return;
@@ -352,53 +327,42 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 		}
 		if ((totalCacheSize.isCalculated() && !forceCalculation)) {
 			// calculation is not required
-			LOG.debug("[Calculate size] [Total] Calculation is not required. Already calculated and don't need force calculation");
+			LOG.info("[Calculate size] [Total] Calculation is not required. Already calculated and don't need force calculation");
 			return;
 		}
-		LOG.debug("[Calculate size] [Total] isTotalCacheSizeCalculationInProgress = " + isTotalCacheSizeCalculationInProgress());
-		LOG.debug("[Calculate size] [Total] isClearOnlineCacheInProgress = " + isClearOnlineCacheInProgress());
 		if (isTotalCacheSizeCalculationInProgress() || isClearOnlineCacheInProgress()) {
-			LOG.debug("[Calculate size] [Total] Calculation is not required. Calculation in progress or clear online cache in progress");
+			LOG.info("[Calculate size] [Total] Calculation is not required. Calculation in progress or clear online cache in progress");
 			return;
 		}
 		runAsync(() -> {
 			totalCacheSizeCalculationInProgress = true;
 			notifyOnWeatherCacheSizeChanged();
-			LOG.debug("[Calculate size] [Total] Notify on start.");
 
 			calculateTotalCacheSize(false, forceCalculation);
 			calculateTotalCacheSize(true, forceCalculation);
 
 			totalCacheSizeCalculationInProgress = false;
-			LOG.debug("[Calculate size] [Total] Notify on finish.");
 			notifyOnWeatherCacheSizeChanged();
 		});
 	}
 
 	private void calculateTotalCacheSize(boolean forLocal, boolean forceCalculation) {
-		LOG.debug("[Calculate size] [Total] Calculate total cache size. isCalculated for "
-				+ ((forLocal ? "local" : "online") + ": " + totalCacheSize.isCalculated(forLocal)));
 		List<Long> offlineTileIds = getOfflineTileIds();
 		TileIdList qOfflineTileIds = NativeUtilities.convertToQListTileIds(offlineTileIds);
 		ZoomLevel zoom = getGeoTileZoom();
 		if (!totalCacheSize.isCalculated(forLocal) || forceCalculation) {
-			LOG.debug("[Calculate size] [Total] Offline tile ids: " + offlineTileIds);
 			if (forLocal && qOfflineTileIds.isEmpty()) {
-				LOG.debug("[Calculate size] [Total] Skip total offline cache size calculation. Offline tiles aren't downloaded.");
 				// skip calculation if no offline tiles are downloaded
 				totalCacheSize.set(0, true);
 			} else {
 				TileIdList tileIds = forLocal ? qOfflineTileIds : new TileIdList();
 				TileIdList excludeIds = forLocal ? new TileIdList() : qOfflineTileIds;
-				LOG.debug("[Calculate size] [Total] Call resources manager [" + (forLocal ? "local cache" : "online cache") + "]");
 				BigInteger calculatedSize = weatherResourcesManager.calculateDbCacheSize(tileIds, excludeIds, zoom);
 				long size = calculatedSize != null ? calculatedSize.longValue() : 0;
-				LOG.debug("[Calculate size] [Total] Calculated [" + (forLocal ? "local cache" : "online cache") + "]. Result = " + size + " bytes.");
 				totalCacheSize.set(size, forLocal);
 			}
 		}
 		// notify after cache was calculated
-		LOG.debug("[Calculate size] [Total] Notify on cache was calculated [" + (forLocal ? "local cache" : "online cache") + "]");
 		notifyOnWeatherCacheSizeChanged();
 	}
 
@@ -415,7 +379,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 	}
 
 	private void clearOnlineCache() {
-		LOG.debug("[Clear] [All online] Call 'clearOnlineCache'");
 		clearOnlineCacheInProgress = true;
 		totalCacheSize.reset(false);
 
@@ -423,12 +386,10 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 		TileIdList qOfflineTileIds = NativeUtilities.convertToQListTileIds(offlineTileIds);
 		ZoomLevel zoom = getGeoTileZoom();
 		// remove all tiles except related to offline weather forecast
-		LOG.debug("[Clear] [All online] Call resources manager. Zoom = " + zoom + ", offlineTileIds = " + offlineTileIds);
 		weatherResourcesManager.clearDbCache(new TileIdList(), qOfflineTileIds, zoom);
 		runInUiThread(this::updateWeatherLayers);
 
 		clearOnlineCacheInProgress = false;
-		LOG.debug("[Clear] [All online] Notify on finish");
 		notifyOnWeatherCacheSizeChanged();
 	}
 
@@ -475,7 +436,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 
 	@Override
 	public void onResetTotalWeatherCacheSize() {
-		LOG.debug("[Calculate size] [Total] Notify onResetTotalWeatherCacheSize");
 		notifyOnWeatherCacheSizeChanged();
 	}
 
@@ -504,13 +464,10 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 	}
 
 	private void removeLocalForecast(@NonNull String[] regionIds, boolean refreshMap, boolean notifyUserOnFinish) {
-		LOG.debug("[Clear] [" + Arrays.toString(regionIds) + "] Call 'removeLocalForecast' " +
-				"[refreshMap = " + refreshMap + "], " + "[notifyUserOnFinish = " + notifyUserOnFinish + "]");
 		List<String> regionIdsList = Arrays.asList(regionIds);
 		regionsRemoveInProgress = Algorithms.addAllToList(regionsRemoveInProgress, regionIdsList);
 
 		// notify before remove and after region ids were registered
-		LOG.debug("[Clear] [" + Arrays.toString(regionIds) + "] Notify on start. Regions remove in progress: " + regionsRemoveInProgress);
 		runInUiThread(this::notifyOnRemoveLocalForecastEvent);
 
 		totalCacheSize.reset();
@@ -533,7 +490,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 		TileIdList qTileIds = NativeUtilities.convertToQListTileIds(tileIds);
 		ZoomLevel zoom = getGeoTileZoom();
 		if (!qTileIds.isEmpty()) {
-			LOG.debug("[Clear] [" + Arrays.toString(regionIds) + "] Call resources manager. Zoom = " + zoom + "tileIds = " + tileIds);
 			weatherResourcesManager.clearDbCache(qTileIds, new TileIdList(), zoom);
 		}
 		if (notifyUserOnFinish) {
@@ -548,7 +504,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 		app.getDownloadThread().updateLoadedFiles();
 
 		// notify after remove was completed and region ids were unregistered
-		LOG.debug("[Clear] [" + Arrays.toString(regionIds) + "] Notify on finish. Regions remove in progress: " + regionsRemoveInProgress);
 		runInUiThread(this::notifyOnRemoveLocalForecastEvent);
 
 		if (refreshMap) {
@@ -609,7 +564,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 			}
 			outdated = daysGone >= 7;
 		}
-		LOG.debug("[Check outdated] Call isForecastOutdated [" + regionId + "]. Result = " + outdated);
 		return outdated;
 	}
 
@@ -722,7 +676,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 					.append(" ").append(DownloadActivityType.WEATHER_FORECAST.getString(app));
 			String message = app.getString(R.string.shared_string_downloading_formatted, taskName);
 			int totalWork = getProgressDestination(regionId);
-			LOG.debug("[Download] Started [" + regionId + "]. Total work = " + totalWork);
 			progress.startTask(message, totalWork);
 		}
 	}
@@ -742,17 +695,11 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 			progress.remaining(remainingWork);
 		}
 
-		LOG.debug("[Download] [" + regionId + "] Call 'onUpdateDownloadProgress' " +
-				"success = " + success + ", " + "destination = " + destinationTilesCount + ", " +
-				"downloaded = " + downloadedTilesCount + ", " + "progress = " + currentProgress);
-
 		if (currentProgress >= 1.f) {
 			setPreferenceDownloadState(regionId, FINISHED);
 			long lastUpdateTime = getTimeForTimeZone(System.currentTimeMillis(), "GMT").getTime();
 			setPreferenceLastUpdate(regionId, lastUpdateTime);
 			totalCacheSize.reset();
-
-			LOG.debug("[Download] [" + regionId + "] 100% download finished. Progress = " + progress + ", lastUpdateTime = " + lastUpdateTime);
 
 			runInUiThread(() -> {
 				updateWeatherLayers();
@@ -762,14 +709,12 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 	}
 
 	public void addWeatherIndexItems(@NonNull IndexFileList indexes) {
-		LOG.debug("[Add Index Items] Call addWeatherIndexItems");
 		if (!isWeatherSupported(app)) {
 			LOG.error("[Add Index Items] Can't add weather indexes. Weather isn't allowed with current configuration.");
 			return;
 		}
 		for (WorldRegion region : app.getRegions().getFlattenedWorldRegions()) {
 			boolean shouldHaveWeatherForecast = shouldHaveWeatherForecast(region);
-			LOG.debug("[Add Index Items] [" + region.getRegionId() + "] should have forecast = " + shouldHaveWeatherForecast);
 			if (shouldHaveWeatherForecast) {
 				WeatherIndexItem index = createIndexItem(region);
 				cachedWeatherIndexes.put(index.getRegionId(), index);
@@ -793,9 +738,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 		} else {
 			timestamp = getPreferenceLastUpdate(regionId);
 		}
-		LOG.debug("[Add Index Items] Create an Index Item [" + regionId + "] " +
-				"timestamp = " + timestamp + ", " + "size = " + size + ", " +
-				"contentSize = " + contentSize + ", " + "containerSize = " + containerSize);
 		return new WeatherIndexItem(region, timestamp, size, contentSize, containerSize);
 	}
 
@@ -814,17 +756,14 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 
 	public boolean checkIfItemOutdated(@NonNull WeatherIndexItem weatherIndexItem) {
 		String regionId = weatherIndexItem.getRegionId();
-		LOG.debug("[Check outdated] [" + regionId + "] Call 'checkIfItemOutdated'");
 		if (!isDownloadStateFinished(regionId)) {
 			return false;
 		}
-		LOG.debug("[Check outdated] [" + regionId + "] set downloaded 'true'");
 		weatherIndexItem.setDownloaded(true);
 		boolean outdated = isForecastOutdated(regionId);
 		weatherIndexItem.setOutdated(outdated);
 		long lastUpdateTime = getPreferenceLastUpdate(regionId);
 		if (lastUpdateTime != -1) {
-			LOG.debug("[Check outdated] [" + regionId + "] set local timestamp = " + lastUpdateTime);
 			weatherIndexItem.setLocalTimestamp(lastUpdateTime);
 		}
 		return outdated;
@@ -973,11 +912,6 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 		if (worldIndexItem != null && !items.contains(worldIndexItem)) {
 			items.add(0, worldIndexItem);
 		}
-		LOG.debug("[Find Indexes] Found: " + items.size() + " indexes.");
-		for (int i = 0; i < items.size(); i++) {
-			WeatherIndexItem indexItem = (WeatherIndexItem) items.get(i);
-			LOG.debug("[Find Indexes] Found '" + indexItem.getRegionId() + "' " + (i + 1) + "/" + items.size());
-		}
 		return items;
 	}
 
@@ -999,10 +933,8 @@ public class OfflineForecastHelper implements ResetTotalWeatherCacheSizeListener
 
 	@NonNull
 	private OfflineForecastInfo getOrCreateCachedInfo(@NonNull String regionId) {
-		LOG.debug("[Info] Call getOrCreateCachedInfo [" + regionId + "]");
 		OfflineForecastInfo info = getCachedInfo(regionId);
 		if (info == null) {
-			LOG.debug("[Info] Create cached info [" + regionId + "]");
 			info = new OfflineForecastInfo();
 			offlineForecastInfo.put(regionId, info);
 		}
