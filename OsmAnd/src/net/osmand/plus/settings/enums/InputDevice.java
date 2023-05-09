5a7d9ae3359b1707c5a212a568a0fd646401a1e6
nazar-kutz <vitaminkuna@gmail.com>
2023-05-09 17:49:19 +0300

    Fix "External Input Device" preference. Implement "None" property

--- OsmAnd/src/net/osmand/plus/settings/enums/InputDevice.java
+++ OsmAnd/src/net/osmand/plus/settings/enums/InputDevice.java
@@ -6,12 +6,13 @@ import net.osmand.plus.R;
 
 public enum InputDevice {
 
+	NONE(R.string.shared_string_none, 0),
 	KEYBOARD(R.string.sett_generic_ext_input, 1),
 	WUNDER_LINQ(R.string.sett_wunderlinq_ext_input, 2),
 	PARROT(R.string.sett_parrot_ext_input, 3);
 
-	private int titleId;
-	private int value;
+	private final int titleId;
+	private final int value;
 
 	InputDevice(@StringRes int titleId, int value) {
 		this.titleId = titleId;
@@ -25,5 +26,14 @@ public enum InputDevice {
 
 	public int getValue() {
 		return value;
+	}
+
+	public static InputDevice getByValue(int value) {
+		for (InputDevice device : values()) {
+			if (device.getValue() == value) {
+				return device;
+			}
+		}
+		return NONE;
 	}
 }
