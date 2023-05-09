a570cd34fea1ab59aa26d73d2c66424af202580a
Alex Andrewschenko <andrewschenko@gmail.com>
2023-05-09 10:13:56 +0200

    Add needed import

--- OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
+++ OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
@@ -38,6 +38,7 @@ import net.osmand.PlatformUtil;
 import net.osmand.core.android.MapRendererView;
 import net.osmand.core.jni.MapAnimator;
 import net.osmand.core.jni.PointI;
+import net.osmand.core.jni.PointD;
 import net.osmand.core.jni.ZoomLevel;
 import net.osmand.data.LatLon;
 import net.osmand.data.QuadPoint;
