b6997cf1177c3533018fec89993b3d702f49e6fa
chumv <chumva.forever@gmail.com>
2024-02-16 17:59:13 +0200

    Code cleanup

--- OsmAnd/src/net/osmand/plus/auto/screens/SearchResultsScreen.java
+++ OsmAnd/src/net/osmand/plus/auto/screens/SearchResultsScreen.java
@@ -66,9 +66,11 @@ public final class SearchResultsScreen extends BaseSearchScreen implements Defau
 			builder.setLoading(true);
 		} else {
 			builder.setLoading(false);
-			builder.setItemList(itemList == null
-					? new ItemList.Builder().setNoItemsMessage(getCarContext().getString(R.string.location_not_found)).build()
-					: itemList);
+			if (itemList != null) {
+				builder.setItemList(itemList);
+			} else {
+				builder.setItemList(new ItemList.Builder().setNoItemsMessage(getApp().getString(R.string.location_not_found)).build());
+			}
 		}
 		return builder.build();
 	}
@@ -122,7 +124,7 @@ public final class SearchResultsScreen extends BaseSearchScreen implements Defau
 
 	@Override
 	public void onSearchDone(@NonNull SearchPhrase phrase, @Nullable List<SearchResult> searchResults,
-							 @Nullable ItemList itemList, int resultsCount) {
+	                         @Nullable ItemList itemList, int resultsCount) {
 		SearchWord lastSelectedWord = phrase.getLastSelectedWord();
 		if (showResult && resultsCount == 0 && lastSelectedWord != null) {
 			showResult(lastSelectedWord.getResult());
