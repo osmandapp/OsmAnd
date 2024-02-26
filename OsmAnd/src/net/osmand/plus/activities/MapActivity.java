6dd254a9e388ce13fa169d38cf07c5d07ac3765d
chumv <chumva.forever@gmail.com>
2024-02-26 13:19:47 +0200

    Small cleanup

--- OsmAnd/src/net/osmand/plus/activities/MapActivity.java
+++ OsmAnd/src/net/osmand/plus/activities/MapActivity.java
@@ -843,11 +843,9 @@ public class MapActivity extends OsmandActionBarActivity implements DownloadEven
 				while (!tb.containsLatLon(prevCenter.getLatitude(), prevCenter.getLongitude()) && tb.getZoom() > zoom - MAX_ZOOM_OUT_STEPS) {
 					tb.setZoom(tb.getZoom() - 1);
 				}
-				if (tb.containsLatLon(prevCenter.getLatitude(), prevCenter.getLongitude())) {
-					mapContextMenu.setMapZoom(tb.getZoom());
-				} else {
-					mapContextMenu.setMapZoom(zoom);
-				}
+				boolean containsPrevious = tb.containsLatLon(prevCenter.getLatitude(), prevCenter.getLongitude());
+				mapContextMenu.setMapZoom(containsPrevious ? tb.getZoom() : zoom);
+
 				if (toShow instanceof GpxDisplayItem) {
 					trackDetailsMenu.setGpxItem((GpxDisplayItem) toShow);
 					trackDetailsMenu.show();
