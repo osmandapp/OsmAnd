fe6ba247242395988315ac0743899b8da2b5c6c7
chumv <chumva.forever@gmail.com>
2024-04-18 12:44:01 +0300

    Fix compilation

--- OsmAnd/src/net/osmand/plus/myplaces/tracks/dialogs/AvailableTracksFragment.java
+++ OsmAnd/src/net/osmand/plus/myplaces/tracks/dialogs/AvailableTracksFragment.java
@@ -22,7 +22,6 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import net.osmand.PlatformUtil;
 import net.osmand.plus.R;
 import net.osmand.plus.configmap.tracks.TrackFolderLoaderTask.LoadTracksListener;
 import net.osmand.plus.configmap.tracks.TrackItem;
@@ -515,11 +514,7 @@ public class AvailableTracksFragment extends BaseTrackFolderFragment implements 
 						}
 					}
 				}
-				if (fragment instanceof SmartFolderFragment) {
-					((SmartFolderFragment) fragment).updateContent(loadTracksFinished);
-				} else {
-					fragment.updateContent();
-				}
+				fragment.updateContent();
 			}
 		};
 	}
