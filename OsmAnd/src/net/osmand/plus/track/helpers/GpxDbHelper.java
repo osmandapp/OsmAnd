9eec8e17f66a8aab45cd0f6a5df8a64ec1ecdab0
Hardy <hardy.mueller@web.de>
2024-05-05 18:16:37 +0200

    Remove surplus import

--- OsmAnd/src/net/osmand/plus/track/helpers/GpxDbHelper.java
+++ OsmAnd/src/net/osmand/plus/track/helpers/GpxDbHelper.java
@@ -19,7 +19,6 @@ import org.apache.commons.logging.Log;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
