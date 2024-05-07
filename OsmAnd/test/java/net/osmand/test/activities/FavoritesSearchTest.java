3106714e5176663e387c945df29286d091f8c13f
Frank Knoll <Knoll_Frank@web.de>
2024-05-07 18:12:18 +0200

    refactoring

--- OsmAnd/test/java/net/osmand/test/activities/FavoritesSearchTest.java
+++ OsmAnd/test/java/net/osmand/test/activities/FavoritesSearchTest.java
@@ -55,69 +55,12 @@ public class FavoritesSearchTest extends AndroidTest {
     @Test
     public void assertIssue19242IsFixed_b() {
         ActivityScenario.launch(MapActivity.class);
-        final ViewInteraction appCompatImageButton =
-                onView(
-                        allOf(
-                                withId(R.id.map_menu_button),
-                                withContentDescription(R.string.backToMenu),
-                                childAtPosition(
-                                        childAtPosition(
-                                                withId(R.id.bottom_controls_container),
-                                                3),
-                                        0),
-                                isDisplayed()));
-        appCompatImageButton.perform(click());
-
-        final DataInteraction linearLayout =
-                onData(anything())
-                        .inAdapterView(
-                                allOf(
-                                        withId(R.id.menuItems),
-                                        childAtPosition(
-                                                withId(R.id.drawer_relative_layout),
-                                                0)))
-                        .atPosition(3);
-        linearLayout.perform(click());
-
-        final ViewInteraction actionMenuItemView =
-                onView(
-                        allOf(
-                                withContentDescription("Filter"),
-                                childAtPosition(
-                                        childAtPosition(
-                                                withId(me.zhanghai.android.materialprogressbar.R.id.action_bar),
-                                                3),
-                                        0),
-                                isDisplayed()));
-        actionMenuItemView.perform(click());
-
-        final ViewInteraction appCompatEditText =
-                onView(
-                        allOf(
-                                withId(R.id.searchEditText),
-                                childAtPosition(
-                                        allOf(
-                                                withId(R.id.search_container),
-                                                childAtPosition(
-                                                        withId(R.id.toolbar),
-                                                        0)),
-                                        0),
-                                isDisplayed()));
-        appCompatEditText.perform(replaceText("Morgenstelle"), closeSoftKeyboard());
-
-        final DataInteraction linearLayout2 =
-                onData(anything())
-                        .inAdapterView(
-                                allOf(
-                                        withId(android.R.id.list),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                1)))
-                        .atPosition(2);
-        linearLayout2.perform(click());
-
+        openMapMenu();
+        openMyPlaces();
+        openSearch();
+        enterSearchQuery("Morgenstelle");
+        selectFirstSearchResult();
         pressBack();
-
         pressBack();
 
         final ViewInteraction editText =
@@ -130,6 +73,77 @@ public class FavoritesSearchTest extends AndroidTest {
                                                 withParent(withId(R.id.toolbar)))),
                                 isDisplayed()));
         editText.check(matches(withText("Morgenstelle")));
+    }
+
+    private static void openMapMenu() {
+        final ViewInteraction appCompatImageButton =
+                onView(
+                        allOf(
+                                withId(R.id.map_menu_button),
+                                withContentDescription(R.string.backToMenu),
+                                childAtPosition(
+                                        childAtPosition(
+                                                withId(R.id.bottom_controls_container),
+                                                3),
+                                        0),
+                                isDisplayed()));
+        appCompatImageButton.perform(click());
+    }
+
+    private static void openMyPlaces() {
+        final DataInteraction linearLayout =
+                onData(anything())
+                        .inAdapterView(
+                                allOf(
+                                        withId(R.id.menuItems),
+                                        childAtPosition(
+                                                withId(R.id.drawer_relative_layout),
+                                                0)))
+                        .atPosition(3);
+        linearLayout.perform(click());
+    }
+
+    private static void openSearch() {
+        final ViewInteraction actionMenuItemView =
+                onView(
+                        allOf(
+                                withContentDescription("Filter"),
+                                childAtPosition(
+                                        childAtPosition(
+                                                withId(me.zhanghai.android.materialprogressbar.R.id.action_bar),
+                                                3),
+                                        0),
+                                isDisplayed()));
+        actionMenuItemView.perform(click());
+    }
+
+    private static void enterSearchQuery(final String searchQuery) {
+        final ViewInteraction appCompatEditText =
+                onView(
+                        allOf(
+                                withId(R.id.searchEditText),
+                                childAtPosition(
+                                        allOf(
+                                                withId(R.id.search_container),
+                                                childAtPosition(
+                                                        withId(R.id.toolbar),
+                                                        0)),
+                                        0),
+                                isDisplayed()));
+        appCompatEditText.perform(replaceText(searchQuery), closeSoftKeyboard());
+    }
+
+    private static void selectFirstSearchResult() {
+        final DataInteraction linearLayout2 =
+                onData(anything())
+                        .inAdapterView(
+                                allOf(
+                                        withId(android.R.id.list),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                1)))
+                        .atPosition(2);
+        linearLayout2.perform(click());
     }
 
     private void importFavorite(final File favoriteAssetFile, final FragmentActivity activity) {
