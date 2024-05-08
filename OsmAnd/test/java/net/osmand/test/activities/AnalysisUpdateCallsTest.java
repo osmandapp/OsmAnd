fad79bf3095e545a651c4a020901c56bc295518a
Corwin-Kh <serge.kharchenko@gmail.com>
2024-05-08 13:28:35 +0300

    Added fps check

--- OsmAnd/test/java/net/osmand/test/activities/AnalysisUpdateCallsTest.java
+++ OsmAnd/test/java/net/osmand/test/activities/AnalysisUpdateCallsTest.java
@@ -1,47 +1,28 @@
 package net.osmand.test.activities;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static net.osmand.test.common.EspressoUtils.waitForView;
-import static net.osmand.test.common.Interactions.openNavigationMenu;
-import static net.osmand.test.common.Interactions.setRouteStart;
-import static net.osmand.test.common.Interactions.startNavigation;
-import static net.osmand.test.common.Matchers.childAtPosition;
 import static net.osmand.test.common.OsmAndDialogInteractions.skipAppStartDialogs;
-import static org.hamcrest.Matchers.allOf;
 
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.util.Range;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingPolicies;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import net.osmand.data.LatLon;
+import net.osmand.core.android.MapRendererView;
 import net.osmand.gpx.GPXFile;
 import net.osmand.gpx.GpxParameter;
 import net.osmand.plus.OsmandApplication;
-import net.osmand.plus.R;
 import net.osmand.plus.Version;
 import net.osmand.plus.activities.MapActivity;
-import net.osmand.plus.configmap.tracks.TrackTabType;
 import net.osmand.plus.track.GpxSelectionParams;
-import net.osmand.plus.track.helpers.GPXDatabase;
 import net.osmand.plus.track.helpers.GpxDataItem;
 import net.osmand.plus.track.helpers.GpxDbHelper;
-import net.osmand.plus.track.helpers.GpxUiHelper;
+import net.osmand.plus.views.OsmandMapTileView;
 import net.osmand.test.common.AndroidTest;
 import net.osmand.test.common.BaseIdlingResource;
 import net.osmand.test.common.ResourcesImporter;
@@ -55,9 +36,7 @@ import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @LargeTest
@@ -71,6 +50,9 @@ public class AnalysisUpdateCallsTest extends AndroidTest {
 			new ActivityScenarioRule<>(MapActivity.class);
 
 	private ObserveDistToFinishIdlingResource observeDistToFinishIdlingResource;
+
+	private OsmandMapTileView mapView;
+	private int startFrameId;
 
 	@Before
 	@Override
@@ -101,6 +83,14 @@ public class AnalysisUpdateCallsTest extends AndroidTest {
 		GPXFile gpxFile = new GPXFile(Version.getFullVersion(app));
 		gpxFile.path = testItem.getFile().getPath();
 		app.getSelectedGpxHelper().selectGpxFile(gpxFile, params);
+
+		mActivityScenarioRule.getScenario().onActivity(activity -> {
+			mapView = activity.getMapView();
+			MapRendererView rendererView = mapView.getMapRenderer();
+			if (rendererView != null) {
+				startFrameId = rendererView.getFrameId();
+			}
+		});
 
 		observeDistToFinishIdlingResource = new ObserveDistToFinishIdlingResource(app);
 		registerIdlingResources(observeDistToFinishIdlingResource);
@@ -135,10 +125,16 @@ public class AnalysisUpdateCallsTest extends AndroidTest {
 			super(app);
 			Handler handler = new Handler(Looper.getMainLooper());
 			Runnable checkLeftDistanceTask = () -> {
-				GpxDbHelper dbHelper = app.getGpxDbHelper();
-				GpxDataItem testItem = getTestGpxItem(dbHelper);
+				MapRendererView rendererView = mapView.getMapRenderer();
+				if (rendererView != null) {
+					if(rendererView.getFrameId() - startFrameId < 50) {
+						throw new AssertionError("Map rendering to slow");
+					}
+				} else {
+					throw new AssertionError("Failed to get map renderer");
+				}
 				if (GpxDbHelper.readTrackItemCount > 2) {
-					throw new AssertionError("To many updates of analysis");
+					throw new AssertionError("To many updates of analysis " + GpxDbHelper.readTrackItemCount);
 				}
 				idle = true;
 				notifyIdleTransition();
