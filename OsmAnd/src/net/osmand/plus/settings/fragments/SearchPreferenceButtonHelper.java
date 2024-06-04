776ded915929e033234d1d9fb8f83dc10eca9559
Frank Knoll <Knoll_Frank@web.de>
2024-06-04 21:51:58 +0200

    refactoring

--- OsmAnd/src/net/osmand/plus/settings/fragments/SearchPreferenceButtonHelper.java
+++ OsmAnd/src/net/osmand/plus/settings/fragments/SearchPreferenceButtonHelper.java
@@ -3,30 +3,42 @@ package net.osmand.plus.settings.fragments;
 import android.view.View;
 import android.widget.ImageView;
 
-import androidx.preference.PreferenceFragmentCompat;
-
 import com.bytehamster.lib.preferencesearch.SearchConfiguration;
 import com.bytehamster.lib.preferencesearch.SearchPreferenceFragments;
 
 import net.osmand.plus.R;
 
+import java.util.Optional;
+
 class SearchPreferenceButtonHelper {
 
     private final BaseSettingsFragment baseSettingsFragment;
+    private static Optional<SearchConfiguration> searchConfigurationCache = Optional.empty();
 
     public SearchPreferenceButtonHelper(final BaseSettingsFragment baseSettingsFragment) {
         this.baseSettingsFragment = baseSettingsFragment;
     }
 
     public void configureSearchPreferenceButton(final ImageView searchPreferenceButton) {
-        searchPreferenceButton.setOnClickListener(v -> showSearchPreferenceFragment(new MainSettingsFragment()));
+        searchPreferenceButton.setOnClickListener(v -> showSearchPreferenceFragment());
         searchPreferenceButton.setImageDrawable(baseSettingsFragment.getIcon(R.drawable.searchpreference_ic_search));
         searchPreferenceButton.setVisibility(View.VISIBLE);
     }
 
-    private void showSearchPreferenceFragment(final PreferenceFragmentCompat root) {
-        final SearchConfiguration searchConfiguration = baseSettingsFragment.getMapActivity().createSearchConfiguration(root);
-        final SearchPreferenceFragments searchPreferenceFragments = new SearchPreferenceFragments(searchConfiguration);
+    private void showSearchPreferenceFragment() {
+        final SearchPreferenceFragments searchPreferenceFragments =
+                new SearchPreferenceFragments(getSearchConfiguration());
         searchPreferenceFragments.showSearchPreferenceFragment();
+    }
+
+    private SearchConfiguration getSearchConfiguration() {
+        if (!searchConfigurationCache.isPresent()) {
+            searchConfigurationCache =
+                    Optional.of(
+                            baseSettingsFragment
+                                    .getMapActivity()
+                                    .createSearchConfiguration(new MainSettingsFragment()));
+        }
+        return searchConfigurationCache.get();
     }
 }
