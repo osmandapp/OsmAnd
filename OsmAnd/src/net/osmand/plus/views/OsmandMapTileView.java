106b46b1fbe4aac7f7544727c0f451ee12625ed5
0xRe1nk0 <0xre1nk0@gmail.com>
2024-06-20 12:10:41 +0300

    Add adjusted tilt angle to map 3D button

--- OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
+++ OsmAnd/src/net/osmand/plus/views/OsmandMapTileView.java
@@ -589,13 +589,18 @@ public class OsmandMapTileView implements IMapDownloaderCallback {
 	}
 
 	private void adjustTiltAngle(@NonNull Zoom zoom) {
-		int baseZoom = zoom.getBaseZoom();
+		int angle = getAdjustedTiltAngle(zoom.getBaseZoom());
+		animatedDraggingThread.startTilting(angle, AnimateDraggingMapThread.ZOOM_ANIMATION_TIME);
+	}
+
+	public int getAdjustedTiltAngle(int baseZoom) {
 		if (baseZoom >= MIN_ZOOM_LEVEL_TO_ADJUST_CAMERA_TILT && baseZoom <= MAX_ZOOM_LIMIT) {
 			int angle = 90 - (baseZoom - 2) * 5;
 			if (angle >= MIN_ALLOWED_ELEVATION_ANGLE && angle < DEFAULT_ELEVATION_ANGLE) {
-				animatedDraggingThread.startTilting(angle, AnimateDraggingMapThread.ZOOM_ANIMATION_TIME);
+				return angle;
 			}
 		}
+		return (int) DEFAULT_ELEVATION_ANGLE;
 	}
 
 	public void setIntZoom(int zoom) {
