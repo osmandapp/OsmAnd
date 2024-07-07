2b1f3317ea47717f0c924c76898c989c5f68077d
Frank Knoll <Knoll_Frank@web.de>
2024-07-07 02:19:05 +0200

    refactoring

--- OsmAnd/src/net/osmand/plus/settings/fragments/search/CustomPreferenceDescriptionsFactory.java
+++ OsmAnd/src/net/osmand/plus/settings/fragments/search/CustomPreferenceDescriptionsFactory.java
@@ -1,12 +1,15 @@
 package net.osmand.plus.settings.fragments.search;
 
-import static net.osmand.plus.settings.fragments.search.EditTextPreferenceExDescriptionFactory.getEditTextPreferenceExDescription;
-import static net.osmand.plus.settings.fragments.search.ListPreferenceExDescriptionFactory.getListPreferenceExDescription;
-import static net.osmand.plus.settings.fragments.search.MultiSelectBooleanPreferenceDescriptionFactory.getMultiSelectBooleanPreferenceDescription;
-import static net.osmand.plus.settings.fragments.search.SwitchPreferenceExDescriptionFactory.getSwitchPreferenceExDescription;
+import static net.osmand.plus.settings.fragments.search.Strings.joinNonNullElements;
+
+import net.osmand.plus.settings.preferences.EditTextPreferenceEx;
+import net.osmand.plus.settings.preferences.ListPreferenceEx;
+import net.osmand.plus.settings.preferences.MultiSelectBooleanPreference;
+import net.osmand.plus.settings.preferences.SwitchPreferenceEx;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import de.KnollFrank.lib.preferencesearch.search.provider.PreferenceDescription;
 
@@ -26,5 +29,49 @@ public class CustomPreferenceDescriptionsFactory {
 				getSwitchPreferenceExDescription(),
 				getMultiSelectBooleanPreferenceDescription(),
 				getEditTextPreferenceExDescription());
+	}
+
+	private static PreferenceDescription<ListPreferenceEx> getListPreferenceExDescription() {
+		return new PreferenceDescription<>(
+				ListPreferenceEx.class,
+				preference -> String.join(
+						", ",
+						Strings.concat(
+								Optional.ofNullable(preference.getEntries()),
+								Optional.ofNullable(preference.getDescription()))));
+	}
+
+	private static PreferenceDescription<SwitchPreferenceEx> getSwitchPreferenceExDescription() {
+		return new PreferenceDescription<>(
+				SwitchPreferenceEx.class,
+				preference ->
+						joinNonNullElements(
+								", ",
+								Arrays.asList(
+										preference.getSummaryOff(),
+										preference.getSummaryOn(),
+										preference.getDescription())));
+	}
+
+	private static PreferenceDescription<MultiSelectBooleanPreference> getMultiSelectBooleanPreferenceDescription() {
+		return new PreferenceDescription<>(
+				MultiSelectBooleanPreference.class,
+				preference ->
+						String.join(
+								", ",
+								Strings.concat(
+										Optional.ofNullable(preference.getEntries()),
+										Optional.ofNullable(preference.getDescription()))));
+	}
+
+	private static PreferenceDescription<EditTextPreferenceEx> getEditTextPreferenceExDescription() {
+		return new PreferenceDescription<>(
+				EditTextPreferenceEx.class,
+				preference ->
+						joinNonNullElements(
+								", ",
+								Arrays.asList(
+										preference.getText(),
+										preference.getDescription())));
 	}
 }
--- OsmAnd/src/net/osmand/plus/settings/fragments/search/Strings.java
+++ OsmAnd/src/net/osmand/plus/settings/fragments/search/Strings.java
@@ -1,10 +1,23 @@
 package net.osmand.plus.settings.fragments.search;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 class Strings {
+
+	public static List<CharSequence> concat(final Optional<CharSequence[]> entries,
+											final Optional<String> description) {
+		final Builder<CharSequence> builder = ImmutableList.builder();
+		entries.map(Arrays::asList).ifPresent(builder::addAll);
+		description.ifPresent(builder::add);
+		return builder.build();
+	}
 
 	public static String joinNonNullElements(final String delimiter, final List<? extends CharSequence> elements) {
 		return String.join(delimiter, filterNonNull(elements));
