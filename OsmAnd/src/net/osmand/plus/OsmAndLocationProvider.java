e979bdfa06c1988a3ac6dfadb7dd0b8ccb4d3293
alex-osm <alex.morozov.osm@gmail.com>
2024-08-06 15:32:33 +0300

    Fix early usage of network location

--- OsmAnd/src/net/osmand/plus/OsmAndLocationProvider.java
+++ OsmAnd/src/net/osmand/plus/OsmAndLocationProvider.java
@@ -489,7 +489,7 @@ public class OsmAndLocationProvider implements SensorEventListener {
 		if (app.getRoutingHelper().isFollowingMode()) {
 			return true;
 		}
-		if ((System.currentTimeMillis() - lastTimeGPSLocationFixed) < NOT_SWITCH_TO_NETWORK_WHEN_GPS_LOST_MS) {
+		if ((System.currentTimeMillis() - lastTimeGPSLocationFixed) < NOT_SWITCH_TO_NETWORK_WHEN_GPS_LOST_MS || lastTimeGPSLocationFixed == 0) {
 			return true;
 		}
 		return isRunningOnEmulator();
