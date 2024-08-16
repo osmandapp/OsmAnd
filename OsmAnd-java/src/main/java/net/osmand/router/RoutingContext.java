b7e9e4367170a72860fa3d6ef7d7086f062ff530
ivanPyrohivskyi <ivan.pirogovsky@gmail.com>
2024-08-16 14:46:20 +0300

    Refactoring

--- OsmAnd-java/src/main/java/net/osmand/router/RoutingContext.java
+++ OsmAnd-java/src/main/java/net/osmand/router/RoutingContext.java
@@ -2,7 +2,14 @@ package net.osmand.router;
 
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.HashSet;
 import java.util.Map.Entry;
 
 import org.apache.commons.logging.Log;
