3d79ae0f702eb63d7f3b9df16b981398b4c7b3f9
Hardy <hardy.mueller@web.de>
2024-09-02 13:59:57 +0200

    Prevent writing Grpup appearance to every single point (GPXUtilities.java)

--- OsmAnd-java/src/main/java/net/osmand/gpx/GPXUtilities.java
+++ OsmAnd-java/src/main/java/net/osmand/gpx/GPXUtilities.java
@@ -1122,7 +1122,7 @@ public class GPXUtilities {
 	private static void writePoints(XmlSerializer serializer, GPXFile file, IProgress progress) throws IOException {
 		for (WptPt l : file.points) {
 			serializer.startTag(null, "wpt"); //$NON-NLS-1$
-			writeWpt(serializer, l, progress);
+			writeWpt(serializer, l, progress, file);
 			serializer.endTag(null, "wpt"); //$NON-NLS-1$
 		}
 	}
@@ -1135,7 +1135,7 @@ public class GPXUtilities {
 
 			for (WptPt p : route.points) {
 				serializer.startTag(null, "rtept"); //$NON-NLS-1$
-				writeWpt(serializer, p, progress);
+				writeWpt(serializer, p, progress, file);
 				serializer.endTag(null, "rtept"); //$NON-NLS-1$
 			}
 			writeExtensions(serializer, route, null);
@@ -1154,7 +1154,7 @@ public class GPXUtilities {
 					writeNotNullText(serializer, "name", segment.name);
 					for (WptPt p : segment.points) {
 						serializer.startTag(null, "trkpt"); //$NON-NLS-1$
-						writeWpt(serializer, p, progress);
+						writeWpt(serializer, p, progress, file);
 						serializer.endTag(null, "trkpt"); //$NON-NLS-1$
 					}
 					assignRouteExtensionWriter(segment);
@@ -1248,7 +1248,7 @@ public class GPXUtilities {
 		}
 	}
 
-	private static void writeWpt(XmlSerializer serializer, WptPt p, IProgress progress) throws IOException {
+	private static void writeWpt(XmlSerializer serializer, WptPt p, IProgress progress, GPXFile file) throws IOException {
 		serializer.attribute(null, "lat", LAT_LON_FORMAT.format(p.lat));
 		serializer.attribute(null, "lon", LAT_LON_FORMAT.format(p.lon));
 
@@ -1283,6 +1283,18 @@ public class GPXUtilities {
 			String profile = extensions.get(PROFILE_TYPE_EXTENSION);
 			if (GAP_PROFILE_TYPE.equals(profile)) {
 				extensions.remove(PROFILE_TYPE_EXTENSION);
+			}
+		}
+		if (p.category != null && file.pointsGroups.get(p.category) != null) {
+			PointsGroup pointsGroup = file.pointsGroups.get(p.category);
+			if (p.getColor() == pointsGroup.color) {
+				extensions.remove(COLOR_NAME_EXTENSION);
+			}
+			if (p.getIconName().equals(pointsGroup.iconName)) {
+				extensions.remove(ICON_NAME_EXTENSION);
+			}
+			if (p.getBackgroundType().equals(pointsGroup.backgroundType)) {
+				extensions.remove(BACKGROUND_TYPE_EXTENSION);
 			}
 		}
 		assignExtensionWriter(p, extensions);
