875ac474ecd8da300864faffa19e084a71c7aeb2
chumv <chumva.forever@gmail.com>
2024-09-05 13:44:06 +0300

    Extend MapButton from AppCompatImageButton & use custom appearance

--- OsmAnd/src/net/osmand/plus/views/controls/maphudbuttons/MapButton.java
+++ OsmAnd/src/net/osmand/plus/views/controls/maphudbuttons/MapButton.java
@@ -1,226 +1,315 @@
 package net.osmand.plus.views.controls.maphudbuttons;
 
-import static android.widget.ImageView.ScaleType.FIT_CENTER;
-import static net.osmand.plus.views.layers.base.OsmandMapLayer.setMapButtonIcon;
+import static android.graphics.drawable.GradientDrawable.RECTANGLE;
+import static android.widget.ImageView.ScaleType.CENTER;
+import static net.osmand.plus.quickaction.ButtonAppearanceParams.BIG_SIZE_DP;
+import static net.osmand.plus.quickaction.ButtonAppearanceParams.ROUND_RADIUS_DP;
+import static net.osmand.plus.quickaction.ButtonAppearanceParams.TRANSPARENT_ALPHA;
+import static net.osmand.plus.settings.backend.preferences.FabMarginPreference.setFabButtonMargin;
 
+import android.content.Context;
+import android.graphics.Outline;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.View.OnAttachStateChangeListener;
+import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.widget.FrameLayout;
 
 import androidx.annotation.ColorInt;
-import androidx.annotation.ColorRes;
-import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.content.res.AppCompatResources;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.core.util.Pair;
+import androidx.core.view.ViewCompat;
 
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.R;
 import net.osmand.plus.activities.MapActivity;
-import net.osmand.plus.base.containers.ThemedIconId;
 import net.osmand.plus.helpers.AndroidUiHelper;
+import net.osmand.plus.quickaction.ButtonAppearanceParams;
+import net.osmand.plus.render.RenderingIcons;
 import net.osmand.plus.settings.backend.OsmandSettings;
+import net.osmand.plus.settings.backend.preferences.FabMarginPreference;
+import net.osmand.plus.utils.AndroidUtils;
+import net.osmand.plus.utils.ColorUtilities;
 import net.osmand.plus.utils.UiUtilities;
+import net.osmand.plus.views.OsmandMapTileView;
+import net.osmand.plus.views.layers.base.OsmandMapLayer;
 import net.osmand.plus.views.mapwidgets.WidgetsVisibilityHelper;
+import net.osmand.plus.views.mapwidgets.configure.buttons.MapButtonState;
 import net.osmand.util.Algorithms;
 
-public abstract class MapButton {
-
-	@ColorRes
-	public static final int DEFAULT_DAY_ICON_COLOR_ID = R.color.map_button_icon_color_light;
-	@ColorRes
-	public static final int DEFAULT_NIGHT_ICON_COLOR_ID = R.color.map_button_icon_color_dark;
+public abstract class MapButton extends AppCompatImageButton implements OnAttachStateChangeListener {
 
 	protected final OsmandApplication app;
 	protected final OsmandSettings settings;
-	protected final MapActivity mapActivity;
-	protected final UiUtilities iconsCache;
-	protected final WidgetsVisibilityHelper visibilityHelper;
+	protected final UiUtilities uiUtilities;
 
-	protected final String id;
-	protected final ImageView view;
-	protected final boolean alwaysVisible;
+	protected MapActivity mapActivity;
+	protected WidgetsVisibilityHelper visibilityHelper;
 
-	private ThemedIconId themedIconId;
-	@DrawableRes
-	private int dayBackgroundId;
-	@DrawableRes
-	private int nightBackgroundId;
+	protected ButtonAppearanceParams appearanceParams;
+	protected ButtonAppearanceParams customAppearanceParams;
 
-	@ColorRes
-	private int dayIconColorId = DEFAULT_DAY_ICON_COLOR_ID;
-	@ColorRes
-	private int nightIconColorId = DEFAULT_NIGHT_ICON_COLOR_ID;
+	protected int strokeWidth;
+	protected boolean nightMode;
+	protected boolean invalidated = true;
+	protected boolean alwaysVisible;
+	protected boolean useCustomPosition;
+	protected boolean useDefaultAppearance;
+
+	protected boolean routeDialogOpened;
+	protected boolean showBottomButtons;
 
 	@ColorInt
-	private Integer iconColor;
+	protected int iconColor;
+	@ColorInt
+	protected int backgroundColor;
+	@ColorInt
+	protected int backgroundPressedColor;
 
-	private boolean nightMode;
-	private boolean forceUpdate = true;
+	public MapButton(@NonNull Context context) {
+		this(context, null);
+	}
 
-	private boolean routeDialogOpened;
-	private boolean showBottomButtons;
+	public MapButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
 
-	public MapButton(@NonNull MapActivity mapActivity, @NonNull ImageView view, @NonNull String id, boolean alwaysVisible) {
-		this.id = id;
-		this.view = view;
-		this.mapActivity = mapActivity;
-		this.alwaysVisible = alwaysVisible;
-		this.app = mapActivity.getMyApplication();
+	public MapButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+
+		this.app = (OsmandApplication) context.getApplicationContext();
 		this.settings = app.getSettings();
-		this.iconsCache = app.getUIUtilities();
+		this.uiUtilities = app.getUIUtilities();
+		this.strokeWidth = app.getResources().getDimensionPixelSize(R.dimen.map_button_stroke);
+
+		setupShadow();
+		addOnAttachStateChangeListener(this);
+		setNightMode(app.getDaynightHelper().isNightModeForMapControls());
+	}
+
+	@NonNull
+	public String getButtonId() {
+		MapButtonState buttonState = getButtonState();
+		return buttonState != null ? buttonState.getId() : "";
+	}
+
+	@NonNull
+	public OsmandMapTileView getMapView() {
+		return app.getOsmandMap().getMapView();
+	}
+
+	public void setMapActivity(@NonNull MapActivity mapActivity) {
+		this.mapActivity = mapActivity;
 		this.visibilityHelper = mapActivity.getWidgetsVisibilityHelper();
 	}
 
-	@NonNull
-	public String getId() {
-		return id;
-	}
+	@Nullable
+	public abstract MapButtonState getButtonState();
 
 	@NonNull
-	public ImageView getView() {
-		return view;
+	public ButtonAppearanceParams getAppearanceParams() {
+		MapButtonState buttonState = getButtonState();
+		if (buttonState != null) {
+			return useDefaultAppearance ? buttonState.createDefaultAppearanceParams() : buttonState.createAppearanceParams();
+		}
+		return createDefaultAppearanceParams();
 	}
 
-	protected void setRoundTransparentBackground() {
-		setBackground(R.drawable.btn_circle_trans, R.drawable.btn_circle_night);
+	public void setCustomAppearanceParams(@Nullable ButtonAppearanceParams customAppearanceParams) {
+		this.customAppearanceParams = customAppearanceParams;
 	}
 
-	protected void setBackground(@DrawableRes int backgroundId) {
-		if (dayBackgroundId != backgroundId || nightBackgroundId != backgroundId) {
-			dayBackgroundId = backgroundId;
-			nightBackgroundId = backgroundId;
-			forceUpdate = true;
+	public void setInvalidated(boolean invalidated) {
+		this.invalidated = invalidated;
+	}
+
+	public void setAlwaysVisible(boolean alwaysVisible) {
+		this.alwaysVisible = alwaysVisible;
+	}
+
+	public void setUseCustomPosition(boolean useCustomPosition) {
+		this.useCustomPosition = useCustomPosition;
+	}
+
+	public void setUseDefaultAppearance(boolean useDefaultAppearance) {
+		this.useDefaultAppearance = useDefaultAppearance;
+	}
+
+	public void setNightMode(boolean nightMode) {
+		if (this.nightMode != nightMode) {
+			this.nightMode = nightMode;
+			this.invalidated = true;
 		}
 	}
 
-	protected void setBackground(@DrawableRes int dayBackgroundId, @DrawableRes int nightBackgroundId) {
-		if (this.dayBackgroundId != dayBackgroundId || this.nightBackgroundId != nightBackgroundId) {
-			this.dayBackgroundId = dayBackgroundId;
-			this.nightBackgroundId = nightBackgroundId;
-			this.forceUpdate = true;
+	public void setRouteDialogOpened(boolean routeDialogOpened) {
+		if (this.routeDialogOpened != routeDialogOpened) {
+			this.routeDialogOpened = routeDialogOpened;
+			this.invalidated = true;
 		}
 	}
 
-	protected void setIconId(@DrawableRes int iconId) {
-		setIconId(new ThemedIconId(iconId, iconId));
-	}
-
-	protected void setIconId(@NonNull ThemedIconId themedIconId) {
-		if (!Algorithms.objectEquals(this.themedIconId, themedIconId)) {
-			this.themedIconId = themedIconId;
-			this.forceUpdate = true;
+	public void setShowBottomButtons(boolean showBottomButtons) {
+		if (this.showBottomButtons != showBottomButtons) {
+			this.showBottomButtons = showBottomButtons;
+			this.invalidated = true;
 		}
 	}
 
-	protected void resetIconColors() {
-		if (dayIconColorId != DEFAULT_DAY_ICON_COLOR_ID || nightIconColorId != DEFAULT_NIGHT_ICON_COLOR_ID || iconColor != null) {
-			dayIconColorId = DEFAULT_DAY_ICON_COLOR_ID;
-			nightIconColorId = DEFAULT_NIGHT_ICON_COLOR_ID;
-			iconColor = null;
-			forceUpdate = true;
+	public void update() {
+		updateVisibility();
+		if (getVisibility() != View.VISIBLE) {
+			return;
+		}
+		updateColors(nightMode);
+
+		ButtonAppearanceParams params = getAppearanceParams();
+		if (invalidated || !Algorithms.objectEquals(appearanceParams, params) || customAppearanceParams != null) {
+			this.appearanceParams = customAppearanceParams != null ? customAppearanceParams : params;
+			this.invalidated = false;
+
+			updateIcon();
+			updateSize();
+			updateBackground();
 		}
 	}
 
-	protected void setIconColorId(@ColorRes int iconColorId) {
-		setIconColorId(iconColorId, iconColorId);
-	}
-
-	protected void setIconColorId(@ColorRes int dayIconColorId, @ColorRes int nightIconColorId) {
-		if (this.dayIconColorId != dayIconColorId || this.nightIconColorId != nightIconColorId) {
-			this.dayIconColorId = dayIconColorId;
-			this.nightIconColorId = nightIconColorId;
-			this.forceUpdate = true;
-		}
-	}
-
-	public void updateIcon(boolean nightMode) {
-		if (nightBackgroundId != 0 && dayBackgroundId != 0) {
-			view.setBackground(AppCompatResources.getDrawable(app, nightMode ? nightBackgroundId : dayBackgroundId));
-		}
-		Drawable drawable = null;
-		int iconId = themedIconId != null ? themedIconId.getIconId(nightMode) : 0;
-		if (iconId != 0) {
-			if (iconColor != null) {
-				drawable = iconsCache.getPaintedIcon(iconId, iconColor);
-			} else {
-				drawable = iconsCache.getIcon(iconId, getIconColorId());
-			}
-		}
-		if (drawable != null) {
-			setDrawable(drawable);
-		}
-	}
-
-	@ColorRes
-	protected int getIconColorId() {
-		return nightMode ? nightIconColorId : dayIconColorId;
+	protected void updateColors(boolean nightMode) {
+		setIconColor(ColorUtilities.getMapButtonIconColor(getContext(), nightMode));
+		setBackgroundColors(ColorUtilities.getMapButtonBackgroundColor(getContext(), nightMode),
+				ColorUtilities.getMapButtonBackgroundPressedColor(getContext(), nightMode));
 	}
 
 	protected void setIconColor(@ColorInt int iconColor) {
-		if (!Algorithms.objectEquals(this.iconColor, iconColor)) {
+		if (this.iconColor != iconColor) {
 			this.iconColor = iconColor;
-			this.forceUpdate = true;
+			this.invalidated = true;
 		}
 	}
 
-	public void update(boolean nightMode, boolean routeDialogOpened, boolean bottomButtonsAllowed) {
-		this.routeDialogOpened = routeDialogOpened;
-		this.showBottomButtons = bottomButtonsAllowed;
-
-		updateVisibility();
-
-		if (view.getVisibility() != View.VISIBLE) {
-			return;
+	protected void setBackgroundColors(@ColorInt int backgroundColor, @ColorInt int backgroundPressedColor) {
+		if (this.backgroundColor != backgroundColor || this.backgroundPressedColor != backgroundPressedColor) {
+			this.backgroundColor = backgroundColor;
+			this.backgroundPressedColor = backgroundPressedColor;
+			this.invalidated = true;
 		}
+	}
 
-		updateState(nightMode);
-
-		if (this.nightMode == nightMode && !forceUpdate) {
-			return;
+	protected void updateIcon() {
+		String iconName = appearanceParams.getIconName();
+		int iconId = AndroidUtils.getDrawableId(app, iconName);
+		if (iconId == 0) {
+			iconId = RenderingIcons.getBigIconResourceId(iconName);
 		}
-		this.forceUpdate = false;
-		this.nightMode = nightMode;
+		if (iconId != 0) {
+			Drawable drawable;
+			MapButtonState buttonState = getButtonState();
+			if (buttonState != null) {
+				drawable = getButtonState().getIcon(iconId, iconColor, nightMode, true);
+			} else {
+				drawable = iconColor != 0 ? uiUtilities.getPaintedIcon(iconId, iconColor) : uiUtilities.getIcon(iconId);
+			}
+			OsmandMapLayer.setMapButtonIcon(this, drawable, CENTER);
+		}
+	}
 
-		updateIcon(nightMode);
+	protected void updateSize() {
+		int size = AndroidUtils.dpToPx(getContext(), appearanceParams.getSize());
+		ViewGroup.LayoutParams params = getLayoutParams();
+		params.height = size;
+		params.width = size;
+		setLayoutParams(params);
+	}
+
+	protected void updateBackground() {
+		Context context = getContext();
+		int size = AndroidUtils.dpToPx(context, appearanceParams.getSize());
+		int cornerRadius = AndroidUtils.dpToPx(context, appearanceParams.getCornerRadius());
+
+		GradientDrawable normal = new GradientDrawable();
+		normal.setSize(size, size);
+		normal.setShape(RECTANGLE);
+		normal.setColor(ColorUtilities.getColorWithAlpha(backgroundColor, appearanceParams.getOpacity()));
+		normal.setCornerRadius(cornerRadius);
+		normal.setStroke(strokeWidth, ColorUtilities.getColor(context, nightMode ? R.color.map_widget_dark_stroke : R.color.map_widget_light_trans));
+
+		GradientDrawable pressed = new GradientDrawable();
+		pressed.setSize(size, size);
+		pressed.setShape(RECTANGLE);
+		pressed.setColor(backgroundPressedColor);
+		pressed.setCornerRadius(cornerRadius);
+		pressed.setStroke(strokeWidth, ColorUtilities.getColor(context, nightMode ? R.color.map_widget_dark_stroke : R.color.map_widget_light_pressed));
+
+		setBackground(AndroidUtils.createPressedStateListDrawable(normal, pressed));
+	}
+
+	private void setupShadow() {
+		setOutlineProvider(new ViewOutlineProvider() {
+			@Override
+			public void getOutline(View view, Outline outline) {
+				Drawable background = view.getBackground();
+				if (background != null) {
+					background.getOutline(outline);
+				} else {
+					outline.setRect(0, 0, view.getWidth(), view.getHeight());
+				}
+				outline.setAlpha(1);
+			}
+		});
+		ViewCompat.setElevation(this, 5.0f);
+	}
+
+	@Override
+	public void onViewAttachedToWindow(@NonNull View v) {
+		updateMargins();
+		setInvalidated(true);
+		update();
+	}
+
+	@Override
+	public void onViewDetachedFromWindow(@NonNull View view) {
+
 	}
 
 	protected abstract boolean shouldShow();
 
-	protected void updateState(boolean nightMode) {
-		// Not implemented
-	}
-
-	protected void setDrawable(@Nullable Drawable drawable) {
-		setMapButtonIcon(view, drawable, FIT_CENTER);
-	}
-
 	public boolean updateVisibility() {
-		return updateVisibility(shouldShow());
+		MapButtonState buttonState = getButtonState();
+		boolean enabled = buttonState != null && buttonState.isEnabled();
+		return updateVisibility(alwaysVisible || enabled && shouldShow());
 	}
 
 	protected boolean updateVisibility(boolean visible) {
 		if (visible) {
-			visible = app.getAppCustomization().isFeatureEnabled(id);
+			visible = app.getAppCustomization().isFeatureEnabled(getButtonId());
 		}
-		return AndroidUiHelper.updateVisibility(view, visible);
+		return AndroidUiHelper.updateVisibility(this, visible);
 	}
 
-	protected boolean isNightMode() {
-		return nightMode;
+	public void updateMargins() {
+		MapButtonState buttonState = getButtonState();
+		FabMarginPreference preference = buttonState != null ? buttonState.getFabMarginPref() : null;
+		if (mapActivity != null && useCustomPosition && preference != null) {
+			FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
+			if (AndroidUiHelper.isOrientationPortrait(mapActivity)) {
+				Pair<Integer, Integer> margins = preference.getPortraitFabMargins();
+				Pair<Integer, Integer> defMargins = preference.getDefaultPortraitMargins();
+				setFabButtonMargin(mapActivity, this, params, margins, defMargins.first, defMargins.second);
+			} else {
+				Pair<Integer, Integer> margins = preference.getLandscapeFabMargin();
+				Pair<Integer, Integer> defMargins = preference.getDefaultLandscapeMargins();
+				setFabButtonMargin(mapActivity, this, params, margins, defMargins.first, defMargins.second);
+			}
+		}
 	}
 
-	protected boolean isRouteDialogOpened() {
-		return routeDialogOpened;
-	}
-
-	protected boolean isShowBottomButtons() {
-		return showBottomButtons;
-	}
-
-	public void onDestroyButton() {
-	}
-
-	public void refresh() {
+	@NonNull
+	public ButtonAppearanceParams createDefaultAppearanceParams() {
+		return new ButtonAppearanceParams("ic_quick_action", BIG_SIZE_DP, TRANSPARENT_ALPHA, ROUND_RADIUS_DP);
 	}
 }
