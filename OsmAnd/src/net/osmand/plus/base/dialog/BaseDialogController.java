213c645490f576f99fce4a96eae404997e693f95
frankknoll <Knoll_Frank@web.de>
2024-09-25 19:29:54 +0200

    adding some TODOs

--- OsmAnd/src/net/osmand/plus/base/dialog/BaseDialogController.java
+++ OsmAnd/src/net/osmand/plus/base/dialog/BaseDialogController.java
@@ -13,7 +13,6 @@ import net.osmand.plus.base.dialog.interfaces.controller.IDialogController;
 import net.osmand.plus.base.dialog.interfaces.dialog.IDialog;
 import net.osmand.plus.base.dialog.interfaces.dialog.IDialogNightModeInfoProvider;
 import net.osmand.plus.settings.bottomsheets.CustomizableBottomSheet;
-import net.osmand.plus.settings.fragments.profileappearance.ProfileAppearanceFragment;
 import net.osmand.plus.utils.UiUtilities;
 
 public abstract class BaseDialogController implements IDialogController {
