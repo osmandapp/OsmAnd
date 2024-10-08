a7ce036e6c6d00e614c408f16fdcef1762000b20
chumv <chumva.forever@gmail.com>
2024-10-08 17:24:58 +0300

    Fix #19164

--- OsmAnd/src/net/osmand/plus/views/controls/maphudbuttons/MapButtonTouchListener.java
+++ OsmAnd/src/net/osmand/plus/views/controls/maphudbuttons/MapButtonTouchListener.java
@@ -1,6 +1,7 @@
 package net.osmand.plus.views.controls.maphudbuttons;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -12,14 +13,19 @@ import androidx.annotation.NonNull;
 
 import net.osmand.plus.R;
 import net.osmand.plus.utils.AndroidUtils;
-import net.osmand.plus.views.controls.MapButtonsLayout;
+import net.osmand.plus.views.controls.MapHudLayout;
 
 public class MapButtonTouchListener implements OnTouchListener {
 
+	private final int padding;
 	private int initialMarginX = 0;
 	private int initialMarginY = 0;
 	private float initialTouchX = 0;
 	private float initialTouchY = 0;
+
+	public MapButtonTouchListener(@NonNull Context context) {
+		padding = AndroidUtils.calculateTotalSizePx(context, R.dimen.map_button_margin);
+	}
 
 	@SuppressLint("ClickableViewAccessibility")
 	@Override
@@ -37,8 +43,8 @@ public class MapButtonTouchListener implements OnTouchListener {
 				view.setAlpha(1f);
 
 				ViewParent parent = view.getParent();
-				if (parent instanceof MapButtonsLayout layout) {
-					layout.updateButton((MapButton) view);
+				if (parent instanceof MapHudLayout layout) {
+					layout.updateButton((MapButton) view, true);
 				}
 				return true;
 			}
@@ -46,26 +52,30 @@ public class MapButtonTouchListener implements OnTouchListener {
 				if (initialMarginX == 0 && initialMarginY == 0 && initialTouchX == 0 && initialTouchY == 0) {
 					setUpInitialValues(view, event);
 				}
-				int padding = AndroidUtils.calculateTotalSizePx(view.getContext(), R.dimen.map_button_margin);
-				FrameLayout parent = (FrameLayout) view.getParent();
-				FrameLayout.LayoutParams param = (FrameLayout.LayoutParams) view.getLayoutParams();
-				int deltaX = (int) (initialTouchX - event.getRawX());
-				int deltaY = (int) (initialTouchY - event.getRawY());
-				int newMarginX = interpolate(initialMarginX + deltaX, view.getWidth(), parent.getWidth() - padding * 2);
-				int newMarginY = interpolate(initialMarginY + deltaY, view.getHeight(), parent.getHeight() - padding * 2);
-
-				if (view.getHeight() + newMarginY <= parent.getHeight() - padding * 2 && newMarginY > 0) {
-					param.bottomMargin = newMarginY;
-				}
-				if (view.getWidth() + newMarginX <= parent.getWidth() - padding * 2 && newMarginX > 0) {
-					param.rightMargin = newMarginX;
-				}
-				view.setLayoutParams(param);
+				moveButton(view, event);
 				return true;
 			}
 		}
 		return false;
 	}
+
+	private void moveButton(@NonNull View view, @NonNull MotionEvent event) {
+		FrameLayout parent = (FrameLayout) view.getParent();
+		FrameLayout.LayoutParams param = (FrameLayout.LayoutParams) view.getLayoutParams();
+		int deltaX = (int) (initialTouchX - event.getRawX());
+		int deltaY = (int) (initialTouchY - event.getRawY());
+		int newMarginX = interpolate(initialMarginX + deltaX, view.getWidth(), parent.getWidth() - padding * 2);
+		int newMarginY = interpolate(initialMarginY + deltaY, view.getHeight(), parent.getHeight() - padding * 2);
+
+		if (view.getHeight() + newMarginY <= parent.getHeight() - padding * 2 && newMarginY > 0) {
+			param.bottomMargin = newMarginY;
+		}
+		if (view.getWidth() + newMarginX <= parent.getWidth() - padding * 2 && newMarginX > 0) {
+			param.rightMargin = newMarginX;
+		}
+		view.setLayoutParams(param);
+	}
+
 
 	private int interpolate(int value, int divider, int boundsSize) {
 		if (value <= divider && value > 0) {
