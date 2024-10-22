80c3cd76fd118fa27ceb35a0b5ce7ba314fbb276
0xRe1nk0 <0xre1nk0@gmail.com>
2024-10-22 07:23:03 +0300

    Show split type if possible

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/MenuBuilder.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/MenuBuilder.java
@@ -423,8 +423,20 @@ public class MenuBuilder {
 		for (int i = 0; i < menuObjects.size(); i++) {
 			MenuObject menuObject = menuObjects.get(i);
 			View container = createRowContainer(app, null);
+			String rowTextPrefix, rowText;
 			String title = menuObject.getTitleStr();
-			String textPrefix = MenuObjectUtils.getSecondLineText(menuObject);
+			if (title.contains(":")) {
+				String[] splitTitle = title.split(":", 2);
+				rowTextPrefix = splitTitle[0];
+				rowText = splitTitle[1];
+				if (rowText.startsWith(" ")) {
+					rowText = rowText.substring(1);
+				}
+				rowText = Algorithms.capitalizeFirstLetter(rowText);
+			} else {
+				rowTextPrefix = MenuObjectUtils.getSecondLineText(menuObject);
+				rowText = title;
+			}
 			OnClickListener onClickListener = v -> {
 				MapActivity mapActivity = getMapActivity();
 				if (mapActivity != null) {
@@ -433,7 +445,7 @@ public class MenuBuilder {
 					contextMenuLayer.showContextMenu(menuObject.getLatLon(), menuObject.getPointDescription(), menuObject.getObject(), contextObject);
 				}
 			};
-			buildDetailsRow(container, null, title, textPrefix, null, null, false, onClickListener);
+			buildDetailsRow(container, null, rowText, rowTextPrefix, null, null, false, onClickListener);
 			llv.addView(container);
 		}
 		return new CollapsableView(llv, this, true);
