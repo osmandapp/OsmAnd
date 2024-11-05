3b1ee068006837d99446c22918a06b9ba09fdc2a
Vitaliy <chumva.forever@gmail.com>
2024-11-05 16:55:19 +0200

    Revert "Try to fix nextDirInfo for AA"

--- OsmAnd/src/net/osmand/plus/auto/TripHelper.java
+++ OsmAnd/src/net/osmand/plus/auto/TripHelper.java
@@ -154,7 +154,7 @@ public class TripHelper {
 		stepBuilder.setManeuver(maneuver);
 		stepBuilder.setCue(getNextCue(nextDirInfo, turnType, nextTurnType));
 
-		nextDirInfo = routingHelper.getNextRouteDirectionInfo(calc, true);
+		nextDirInfo = routingHelper.getNextRouteDirectionInfo(calc, false);
 		if (nextDirInfo != null && nextDirInfo.directionInfo != null && nextDirInfo.directionInfo.getTurnType() != null) {
 			int[] lanes = nextDirInfo.directionInfo.getTurnType().getLanes();
 			int locimminent = nextDirInfo.imminent;
