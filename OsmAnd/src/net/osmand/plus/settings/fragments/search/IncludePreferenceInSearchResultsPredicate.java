fd7004b9f9f41650d7bc9bd2aa10a24b0da8eab9
frankknoll <Knoll_Frank@web.de>
2024-11-06 15:13:28 +0100

    handling all inactive plugins in IncludePreferenceInSearchResultsPredicate

--- OsmAnd/src/net/osmand/plus/settings/fragments/search/IncludePreferenceInSearchResultsPredicate.java
+++ OsmAnd/src/net/osmand/plus/settings/fragments/search/IncludePreferenceInSearchResultsPredicate.java
@@ -1,45 +1,43 @@
 package net.osmand.plus.settings.fragments.search;
 
-import static net.osmand.plus.settings.fragments.search.PreferenceMarker.isPreferenceConnectedToPlugin;
-
 import androidx.preference.PreferenceFragmentCompat;
 
 import net.osmand.plus.plugins.OsmandPlugin;
 import net.osmand.plus.plugins.PluginsHelper;
-import net.osmand.plus.plugins.development.DevelopmentSettingsFragment;
-import net.osmand.plus.plugins.development.OsmandDevelopmentPlugin;
-import net.osmand.plus.plugins.monitoring.MonitoringSettingsFragment;
-import net.osmand.plus.plugins.monitoring.OsmandMonitoringPlugin;
+import net.osmand.plus.settings.fragments.SettingsScreenType;
+
+import javax.annotation.Nullable;
 
 import de.KnollFrank.lib.settingssearch.db.preference.pojo.SearchablePreferencePOJO;
 
 class IncludePreferenceInSearchResultsPredicate implements de.KnollFrank.lib.settingssearch.provider.IncludePreferenceInSearchResultsPredicate {
 
 	@Override
-	public boolean includePreferenceInSearchResults(final SearchablePreferencePOJO preference, final Class<? extends PreferenceFragmentCompat> hostOfPreference) {
+	public boolean includePreferenceInSearchResults(final SearchablePreferencePOJO preference,
+													final Class<? extends PreferenceFragmentCompat> hostOfPreference) {
 		return !isPreferenceConnectedToAnInactivePlugin(preference, hostOfPreference);
 	}
 
-	private static boolean isPreferenceConnectedToAnInactivePlugin(final SearchablePreferencePOJO preference, final Class<? extends PreferenceFragmentCompat> hostOfPreference) {
-		// FK-TODO: handle also all the other plugins besides OsmandDevelopmentPlugin
-		return isPreferenceConnectedToInactivePlugin(
-				preference,
-				hostOfPreference,
-				DevelopmentSettingsFragment.class,
-				OsmandDevelopmentPlugin.class) ||
-				isPreferenceConnectedToInactivePlugin(
-						preference,
-						hostOfPreference,
-						MonitoringSettingsFragment.class,
-						OsmandMonitoringPlugin.class);
+	private static boolean isPreferenceConnectedToAnInactivePlugin(final SearchablePreferencePOJO preference,
+																   final Class<? extends PreferenceFragmentCompat> hostOfPreference) {
+		return PluginsHelper
+				.getAvailablePlugins()
+				.stream()
+				.filter(plugin -> !plugin.isActive())
+				.anyMatch(inactivePlugin -> isPreferenceConnectedToPlugin(preference, hostOfPreference, inactivePlugin));
 	}
 
-	private static boolean isPreferenceConnectedToInactivePlugin(
+	private static boolean isPreferenceConnectedToPlugin(
 			final SearchablePreferencePOJO preference,
 			final Class<? extends PreferenceFragmentCompat> hostOfPreference,
+			final OsmandPlugin plugin) {
+		return isPreferenceOnSettingsScreen(hostOfPreference, plugin.getSettingsScreenType()) ||
+				PreferenceMarker.isPreferenceConnectedToPlugin(preference, plugin.getClass());
+	}
+
+	private static boolean isPreferenceOnSettingsScreen(
 			final Class<? extends PreferenceFragmentCompat> preferenceFragment,
-			final Class<? extends OsmandPlugin> plugin) {
-		return (preferenceFragment.equals(hostOfPreference) || isPreferenceConnectedToPlugin(preference, plugin)) &&
-				!PluginsHelper.isActive(plugin);
+			final @Nullable SettingsScreenType settingsScreenType) {
+		return settingsScreenType != null && settingsScreenType.fragmentName.equals(preferenceFragment.getName());
 	}
 }
