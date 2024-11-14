4bc33f1cde02f3720354b51df27429c8fb9358b0
Corwin-Kh <serge.kharchenko@gmail.com>
2024-11-14 14:45:55 +0200

    Fixed restore map state when opening RoutePreviewScreen from phone

--- OsmAnd/src/net/osmand/plus/auto/screens/RoutePreviewScreen.java
+++ OsmAnd/src/net/osmand/plus/auto/screens/RoutePreviewScreen.java
@@ -2,6 +2,8 @@ package net.osmand.plus.auto.screens;
 
 import static net.osmand.search.core.ObjectType.GPX_TRACK;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.text.SpannableString;
 
 import androidx.annotation.NonNull;
@@ -65,6 +67,7 @@ public final class RoutePreviewScreen extends BaseAndroidAutoScreen implements I
 
 	private CompassMode savedCompassMode = CompassMode.NORTH_IS_UP;
 	private float prevElevationAngle = 90;
+	private float prevRotationAngle = 0;
 
 
 	private final StateChangedListener<Void> stateChangedListener = new StateChangedListener<Void>() {
@@ -146,9 +149,10 @@ public final class RoutePreviewScreen extends BaseAndroidAutoScreen implements I
 	public void onCreate(@NonNull LifecycleOwner owner) {
 		getApp().getRoutingHelper().addListener(this);
 		getApp().getTargetPointsHelper().addListener(stateChangedListener);
-		savedCompassMode = getApp().getSettings().getCompassMode();
-		getApp().getSettings().setCompassMode(CompassMode.NORTH_IS_UP);
 		OsmandMapTileView mapView = getApp().getOsmandMap().getMapView();
+		savedCompassMode = getApp().getSettings().getCompassMode();
+		prevRotationAngle = mapView.getRotate();
+		getApp().getSettings().setCompassMode(CompassMode.NORTH_IS_UP);
 		prevElevationAngle = mapView.normalizeElevationAngle(mapView.getElevationAngle());
 		if (getApp().getRoutingHelper().isRouteCalculated()) {
 			updateRoute(true);
@@ -162,9 +166,6 @@ public final class RoutePreviewScreen extends BaseAndroidAutoScreen implements I
 		OsmandApplication app = getApp();
 		RoutingHelper routingHelper = app.getRoutingHelper();
 		routingHelper.removeListener(this);
-		app.getSettings().setCompassMode(savedCompassMode);
-		OsmandMapTileView mapView = getApp().getOsmandMap().getMapView();
-		mapView.setElevationAngle(prevElevationAngle);
 		if (routingHelper.isRoutePlanningMode()) {
 			app.stopNavigation();
 		}
@@ -175,6 +176,24 @@ public final class RoutePreviewScreen extends BaseAndroidAutoScreen implements I
 	@Override
 	public void onStart(@NonNull LifecycleOwner owner) {
 		recenterMap();
+	}
+
+	@Override
+	public void onResume(@NonNull LifecycleOwner owner) {
+		if (getApp().getRoutingHelper().isRouteCalculated()) {
+			new Handler(Looper.getMainLooper()).postDelayed(() -> {
+				zoomMapToRoute();
+			}, 500);
+			zoomMapToRoute();
+		}
+	}
+
+	@Override
+	public void onStop(@NonNull LifecycleOwner owner) {
+		getApp().getSettings().setCompassMode(savedCompassMode);
+		OsmandMapTileView mapView = getApp().getOsmandMap().getMapView();
+		mapView.setElevationAngle(prevElevationAngle);
+		mapView.setRotate(prevRotationAngle, true);
 	}
 
 	@NonNull
@@ -221,13 +240,17 @@ public final class RoutePreviewScreen extends BaseAndroidAutoScreen implements I
 
 	@Override
 	public void newRouteIsCalculated(boolean newRoute, ValueHolder<Boolean> showToast) {
+		zoomMapToRoute();
+		updateRoute(newRoute);
+	}
+
+	private void zoomMapToRoute() {
 		RoutingHelper rh = getApp().getRoutingHelper();
 		QuadRect mapRect = RoutingHelperUtils.getRouteRect(getApp(), rh.getRoute());
 		if (mapRect != null) {
 			LOG.info("ZOOM Route " + mapRect);
 			adjustMapToRect(getApp().getMapViewTrackingUtilities().getDefaultLocation(), mapRect);
 		}
-		updateRoute(newRoute);
 	}
 
 	@Override
