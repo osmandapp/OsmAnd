678139d316d481a7a175386a16e348865348816b
0xRe1nk0 <0xre1nk0@gmail.com>
2024-11-22 12:55:51 +0200

    Remove necessary pref set

--- OsmAnd/src/net/osmand/plus/views/mapwidgets/configure/settings/BaseResizableWidgetSettingFragment.java
+++ OsmAnd/src/net/osmand/plus/views/mapwidgets/configure/settings/BaseResizableWidgetSettingFragment.java
@@ -108,7 +108,6 @@ public class BaseResizableWidgetSettingFragment extends WidgetSettingsBaseFragme
 			updateRowWidgets(widgetInfo);
 		}
 
-		widgetSizePref.set(selectedWidgetSize);
 		if (widgetInfo != null) {
 			if (widgetInfo.widget instanceof ISupportWidgetResizing widgetResizing) {
 				widgetResizing.recreateView();
