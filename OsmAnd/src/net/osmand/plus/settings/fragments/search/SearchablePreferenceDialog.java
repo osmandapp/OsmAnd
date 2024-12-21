ee4437b96a6718b25e6a9c79a271a28929dc1cc2
frankknoll <Knoll_Frank@web.de>
2024-12-21 23:26:56 +0100

    removing a done TODO

--- OsmAnd/src/net/osmand/plus/settings/fragments/search/SearchablePreferenceDialog.java
+++ OsmAnd/src/net/osmand/plus/settings/fragments/search/SearchablePreferenceDialog.java
@@ -4,7 +4,6 @@ import androidx.fragment.app.FragmentManager;
 
 import net.osmand.plus.OsmandApplication;
 
-// FK-TODO: make at least all BasePreferenceBottomSheets implement this interface?
 public interface SearchablePreferenceDialog {
 
 	void show(FragmentManager fragmentManager, OsmandApplication app);
