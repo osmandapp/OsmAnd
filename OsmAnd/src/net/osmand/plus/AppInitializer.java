bb60fd614c0f14b28358863a1b666c7f51399423
frankknoll <Knoll_Frank@web.de>
2024-12-26 23:44:07 +0100

    whenCustomAppModesKeysChangedThenRebuildSearchDatabase()

--- OsmAnd/src/net/osmand/plus/AppInitializer.java
+++ OsmAnd/src/net/osmand/plus/AppInitializer.java
@@ -1,9 +1,34 @@
 package net.osmand.plus;
 
 import static net.osmand.IndexConstants.SETTINGS_DIR;
-import static net.osmand.plus.AppInitEvents.*;
+import static net.osmand.plus.AppInitEvents.BROUTER_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.FAVORITES_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.GPX_DB_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.INDEXES_RELOADED;
+import static net.osmand.plus.AppInitEvents.INDEX_REGION_BOUNDARIES;
+import static net.osmand.plus.AppInitEvents.INIT_RENDERERS;
+import static net.osmand.plus.AppInitEvents.LIVE_UPDATES_ALERTS_CHECKED;
+import static net.osmand.plus.AppInitEvents.LOAD_GPX_TRACKS;
+import static net.osmand.plus.AppInitEvents.MARKERS_GROUPS_SYNCED;
+import static net.osmand.plus.AppInitEvents.NATIVE_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.NATIVE_OPEN_GL_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.POI_FILTERS_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.POI_TYPES_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.ROUTING_CONFIG_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.SAVE_GPX_TRACKS;
+import static net.osmand.plus.AppInitEvents.SEARCH_UI_CORE_INITIALIZED;
+import static net.osmand.plus.AppInitEvents.TASK_CHANGED;
+import static net.osmand.plus.AppInitEvents.TRAVEL_INITIALIZED;
 import static net.osmand.plus.AppVersionUpgradeOnInit.LAST_APP_VERSION;
-import static net.osmand.plus.liveupdates.LiveUpdatesHelper.*;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.TimeOfDay;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.UpdateFrequency;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.getPendingIntent;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.preferenceForLocalIndex;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.preferenceLastSuccessfulUpdateCheck;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.preferenceTimeOfDayToUpdate;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.preferenceUpdateFrequency;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.runLiveUpdate;
+import static net.osmand.plus.liveupdates.LiveUpdatesHelper.setAlarmForPendingIntent;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -22,6 +47,7 @@ import net.osmand.IProgress;
 import net.osmand.IndexConstants;
 import net.osmand.OnResultCallback;
 import net.osmand.PlatformUtil;
+import net.osmand.StateChangedListener;
 import net.osmand.aidl.OsmandAidlApi;
 import net.osmand.core.android.NativeCore;
 import net.osmand.map.OsmandRegions;
@@ -39,7 +65,13 @@ import net.osmand.plus.download.local.LocalIndexHelper;
 import net.osmand.plus.download.local.LocalItem;
 import net.osmand.plus.feedback.AnalyticsHelper;
 import net.osmand.plus.feedback.FeedbackHelper;
-import net.osmand.plus.helpers.*;
+import net.osmand.plus.helpers.ColorPaletteHelper;
+import net.osmand.plus.helpers.DayNightHelper;
+import net.osmand.plus.helpers.LauncherShortcutsHelper;
+import net.osmand.plus.helpers.LockHelper;
+import net.osmand.plus.helpers.Model3dHelper;
+import net.osmand.plus.helpers.TargetPointsHelper;
+import net.osmand.plus.helpers.WaypointHelper;
 import net.osmand.plus.importfiles.ImportHelper;
 import net.osmand.plus.inapp.InAppPurchaseHelperImpl;
 import net.osmand.plus.inapp.InAppPurchaseUtils;
@@ -104,6 +136,8 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import de.KnollFrank.lib.settingssearch.search.SearchDatabaseDirectoryIO;
+
 public class AppInitializer implements IProgress {
 
 	private static final String EXCEPTION_FILE_SIZE = "EXCEPTION_FS";
@@ -125,6 +159,7 @@ public class AppInitializer implements IProgress {
 	private boolean routingConfigInitialized;
 	private String taskName;
 	private SharedPreferences startPrefs;
+	private StateChangedListener<String> onCustomAppModesKeysChanged;
 
 	public interface LoadRoutingFilesCallback {
 		void onRoutingFilesLoaded();
@@ -276,6 +311,7 @@ public class AppInitializer implements IProgress {
 		} else {
 			settings.setApplicationMode(settings.DEFAULT_APPLICATION_MODE.get());
 		}
+		whenCustomAppModesKeysChangedThenRebuildSearchDatabase();
 		startTime = System.currentTimeMillis();
 		getLazyRoutingConfig();
 		app.applyTheme(app);
@@ -338,6 +374,16 @@ public class AppInitializer implements IProgress {
 		app.trackSortModesHelper = startupInit(new TrackSortModesHelper(app), TrackSortModesHelper.class);
 
 		initOpeningHoursParser();
+	}
+
+	private void whenCustomAppModesKeysChangedThenRebuildSearchDatabase() {
+		// FK-TODO: when profile name within "Profile Appearance" preference changes then also rebuildSearchDatabase()
+		onCustomAppModesKeysChanged = s -> rebuildSearchDatabase();
+		app.getSettings().CUSTOM_APP_MODES_KEYS.addListener(onCustomAppModesKeysChanged);
+	}
+
+	private void rebuildSearchDatabase() {
+		new SearchDatabaseDirectoryIO(app).removeSearchDatabaseDirectories4AllLocales();
 	}
 
 	private void initOpeningHoursParser() {
@@ -459,10 +505,10 @@ public class AppInitializer implements IProgress {
 
 
 	public synchronized void initVoiceDataInDifferentThread(@NonNull Context context,
-	                                                        @NonNull ApplicationMode applicationMode,
-	                                                        @NonNull String voiceProvider,
-	                                                        @Nullable Runnable onFinishInitialization,
-	                                                        boolean showProgress) {
+															@NonNull ApplicationMode applicationMode,
+															@NonNull String voiceProvider,
+															@Nullable Runnable onFinishInitialization,
+															boolean showProgress) {
 		String progressTitle = app.getString(R.string.loading_data);
 		String progressMessage = app.getString(R.string.voice_data_initializing);
 		ProgressDialog progressDialog = showProgress && context instanceof Activity
@@ -540,6 +586,7 @@ public class AppInitializer implements IProgress {
 			}
 		}
 	}
+
 	private void checkLiveUpdatesAlerts() {
 		OsmandSettings settings = app.getSettings();
 		if (InAppPurchaseUtils.isLiveUpdatesAvailable(app) && settings.IS_LIVE_UPDATES_ON.get()) {
@@ -769,7 +816,7 @@ public class AppInitializer implements IProgress {
 	}
 
 	public void addOnProgressListener(@NonNull AppInitEvents trackedEvent,
-	                                  @NonNull OnResultCallback<AppInitializer> callback) {
+									  @NonNull OnResultCallback<AppInitializer> callback) {
 		addListener(new AppInitializeListener() {
 			@Override
 			public void onProgress(@NonNull AppInitializer init, @NonNull AppInitEvents event) {
