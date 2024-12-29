496bb026614d76eaec82ceb11a3d5b633968fcf3
frankknoll <Knoll_Frank@web.de>
2024-12-29 22:27:10 +0100

    refactoring

--- OsmAnd/test/java/net/osmand/plus/settings/fragments/search/ISettingsSearchTest.java
+++ OsmAnd/test/java/net/osmand/plus/settings/fragments/search/ISettingsSearchTest.java
@@ -2,7 +2,7 @@ package net.osmand.plus.settings.fragments.search;
 
 import net.osmand.plus.OsmandApplication;
 
-public interface ISettingsSearchTest {
+interface ISettingsSearchTest {
 
 	void testSearchAndFind(OsmandApplication app);
 }
