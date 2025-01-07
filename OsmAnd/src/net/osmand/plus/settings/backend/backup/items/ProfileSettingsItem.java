35dcac8ef925dc5caf9cca392ae554fe8d726ac3
chumv <chumva.forever@gmail.com>
2025-01-08 00:49:30 +0200

    Try to fix #21566

--- OsmAnd/src/net/osmand/plus/settings/backend/backup/items/ProfileSettingsItem.java
+++ OsmAnd/src/net/osmand/plus/settings/backend/backup/items/ProfileSettingsItem.java
@@ -1,5 +1,10 @@
 package net.osmand.plus.settings.backend.backup.items;
 
+import static net.osmand.plus.settings.enums.SunPositionMode.SUN_POSITION_MODE;
+import static net.osmand.plus.views.mapwidgets.widgetstates.ResizableWidgetState.SIMPLE_WIDGET_SIZE_ID;
+import static net.osmand.plus.views.mapwidgets.widgetstates.SunriseSunsetWidgetState.SUN_POSITION_WIDGET_MODE_ID;
+import static net.osmand.router.GeneralRouter.USE_SHORTEST_WAY;
+
 import android.content.Context;
 
 import androidx.annotation.NonNull;
@@ -21,19 +26,16 @@ import net.osmand.plus.settings.backend.backup.SettingsItemReader;
 import net.osmand.plus.settings.backend.backup.SettingsItemType;
 import net.osmand.plus.settings.backend.backup.SettingsItemWriter;
 import net.osmand.plus.settings.backend.preferences.OsmandPreference;
-import net.osmand.router.GeneralRouter;
+import net.osmand.plus.settings.enums.SunPositionMode;
+import net.osmand.plus.settings.enums.WidgetSize;
+import net.osmand.plus.views.mapwidgets.widgetstates.MapMarkerSideWidgetState.MarkerClickBehaviour;
+import net.osmand.plus.views.mapwidgets.widgetstates.MapMarkerSideWidgetState.SideMarkerMode;
 import net.osmand.util.Algorithms;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ProfileSettingsItem extends OsmandSettingsItem {
 
@@ -266,7 +268,7 @@ public class ProfileSettingsItem extends OsmandSettingsItem {
 	@Nullable
 	@Override
 	public SettingsItemReader<? extends SettingsItem> getReader() {
-		return new OsmandSettingsItemReader<ProfileSettingsItem>(this) {
+		return new OsmandSettingsItemReader<>(this) {
 			@Override
 			protected void readPreferenceFromJson(@NonNull OsmandPreference<?> preference, @NonNull JSONObject json) throws JSONException {
 				if (!appModeBeanPrefsIds.contains(preference.getId())) {
@@ -276,26 +278,24 @@ public class ProfileSettingsItem extends OsmandSettingsItem {
 
 			@Override
 			public void readPreferencesFromJson(JSONObject json) {
-				getSettings().getContext().runInUIThread(() -> {
+				app.runInUIThread(() -> {
 					OsmandSettings settings = getSettings();
 					Map<String, OsmandPreference<?>> prefs = settings.getRegisteredPreferences();
 					Iterator<String> iterator = json.keys();
 					while (iterator.hasNext()) {
 						String key = iterator.next();
-						OsmandPreference<?> p = prefs.get(key);
-						if (p == null) {
-							if (OsmandSettings.isRoutingPreference(key)) {
-								p = settings.registerStringPreference(key, "");
-							}
+						OsmandPreference<?> preference = prefs.get(key);
+						if (preference == null) {
+							String value = json.optString(key);
+							preference = createPreference(settings, key, value);
 						}
-						if (p != null) {
+						if (preference != null) {
 							try {
-								readPreferenceFromJson(p, json);
-								if (OsmandSettings.isRoutingPreference(p.getId())) {
-									if (p.getId().endsWith(GeneralRouter.USE_SHORTEST_WAY)) {
-										settings.FAST_ROUTE_MODE.setModeValue(appMode,
-												!settings.getCustomRoutingBooleanProperty(GeneralRouter.USE_SHORTEST_WAY, false).getModeValue(appMode));
-									}
+								readPreferenceFromJson(preference, json);
+								if (OsmandSettings.isRoutingPreference(preference.getId())
+										&& preference.getId().endsWith(USE_SHORTEST_WAY)) {
+									settings.FAST_ROUTE_MODE.setModeValue(appMode,
+											!settings.getCustomRoutingBooleanProperty(USE_SHORTEST_WAY, false).getModeValue(appMode));
 								}
 							} catch (Exception e) {
 								SettingsHelper.LOG.error("Failed to read preference: " + key, e);
@@ -306,6 +306,31 @@ public class ProfileSettingsItem extends OsmandSettingsItem {
 					}
 					settings.setLastModePreferencesEditTime(appMode, lastModifiedTime);
 				});
+			}
+
+			@Nullable
+			private OsmandPreference<?> createPreference(@NonNull OsmandSettings settings,
+					@NonNull String key, @Nullable String value) {
+				if (!Algorithms.isEmpty(value)) {
+					boolean boolPref = "true".equalsIgnoreCase(value) || "false".equalsIgnoreCase(value);
+					if (OsmandSettings.isRoutingPreference(key)) {
+						return boolPref ? settings.getCustomRoutingBooleanProperty(key,
+								Boolean.parseBoolean(value)) : settings.getCustomRoutingProperty(key, value);
+					} else if (OsmandSettings.isRendererPreference(key)) {
+						return boolPref ? settings.getCustomRenderBooleanProperty(key) : settings.getCustomRenderProperty(key);
+					} else if (boolPref) {
+						return settings.registerBooleanPreference(key, Boolean.parseBoolean(value));
+					} else if (key.startsWith(SIMPLE_WIDGET_SIZE_ID)) {
+						return settings.registerEnumStringPreference(key, WidgetSize.MEDIUM, WidgetSize.values(), WidgetSize.class);
+					} else if (key.startsWith(SUN_POSITION_WIDGET_MODE_ID)) {
+						return settings.registerEnumStringPreference(key, SUN_POSITION_MODE, SunPositionMode.values(), SunPositionMode.class);
+					} else if (key.contains("map_marker_mode")) {
+						return settings.registerEnumStringPreference(key, SideMarkerMode.DISTANCE, SideMarkerMode.values(), SideMarkerMode.class);
+					} else if (key.contains("map_marker_click_behaviour")) {
+						return settings.registerEnumStringPreference(key, MarkerClickBehaviour.SWITCH_MODE, MarkerClickBehaviour.values(), MarkerClickBehaviour.class);
+					}
+				}
+				return null;
 			}
 		};
 	}
