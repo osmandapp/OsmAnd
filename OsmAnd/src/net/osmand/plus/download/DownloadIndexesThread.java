bd877e76c8c51a4ea638c68ba0f018eef02ec9f2
frankknoll <Knoll_Frank@web.de>
2025-01-07 18:04:03 +0100

    reorganizing imports

--- OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
+++ OsmAnd/src/net/osmand/plus/download/DownloadIndexesThread.java
@@ -44,13 +44,7 @@ import org.apache.commons.logging.Log;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @SuppressLint({"NewApi", "DefaultLocale"})
