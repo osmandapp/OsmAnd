cbcc05468e7589dbce5fce1ae8fd9a33b87c76e5
frankknoll <Knoll_Frank@web.de>
2025-01-07 17:49:17 +0100

    reorganizing imports

--- OsmAnd/src/net/osmand/plus/backup/BackupImporter.java
+++ OsmAnd/src/net/osmand/plus/backup/BackupImporter.java
@@ -37,17 +37,8 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class BackupImporter {
