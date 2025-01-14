dd320229c41bf947466f3d6e61fad64218f8ddbd
nazar-kutz <vitaminkuna@gmail.com>
2025-01-14 11:34:33 +0200

    Fix already translated but incorrect strings

--- OsmAnd/src/net/osmand/plus/auto/screens/SettingsScreen.java
+++ OsmAnd/src/net/osmand/plus/auto/screens/SettingsScreen.java
@@ -44,7 +44,7 @@ public final class SettingsScreen extends BaseAndroidAutoScreen {
 				.build()
         );
 		sectionABuilder.addItem(new Row.Builder()
-				.setTitle(getCarContext().getString(R.string.display_distance_to_first_intermediate))
+				.setTitle(getCarContext().getString(R.string.display_distance_to_first_intermediate_title))
 				.addText(getCarContext().getString(R.string.display_distance_to_first_intermediate_summary))
 				.setToggle(
 						new Toggle.Builder(osmandSettings.USE_LEFT_DISTANCE_TO_INTERMEDIATE::set)
