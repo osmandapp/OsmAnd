13ec1a0763eb67df620f1b6c0be6e4de27952c15
RZR-UA <youonly@ukr.net>
2025-01-30 16:16:41 +0100

    Add piste:name to REQUIRED_TAGS_ANY

--- OsmAnd/src/net/osmand/plus/track/clickable/ClickableWayHelper.java
+++ OsmAnd/src/net/osmand/plus/track/clickable/ClickableWayHelper.java
@@ -49,7 +49,7 @@ public class ClickableWayHelper {
     public static final Map<String, String> FORBIDDEN_TAGS =
             Map.of("area", "yes", "access", "no", "aerialway", "*");
     public static final Set<String> REQUIRED_TAGS_ANY =
-            Set.of("name", "ref");
+            Set.of("name", "ref", "piste:name");
     public static final Map<String, String> GPX_COLORS = Map.ofEntries(
             Map.entry("0", "brown"),
             Map.entry("1", "green"),
