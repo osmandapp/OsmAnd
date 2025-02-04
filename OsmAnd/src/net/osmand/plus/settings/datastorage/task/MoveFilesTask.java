a83e7463d231214dabf46d7d78ba556da0c48a18
yohann1c <yohann.hazan2@gmail.com>
2025-02-04 16:19:37 +0100

    Changement mineur

--- OsmAnd/src/net/osmand/plus/settings/datastorage/task/MoveFilesTask.java
+++ OsmAnd/src/net/osmand/plus/settings/datastorage/task/MoveFilesTask.java
@@ -106,11 +106,7 @@ public class MoveFilesTask extends AsyncTask<Void, Object, Map<String, Pair<Stri
 
 	@Override
 	protected Map<String, Pair<String, Long>> doInBackground(Void... params) {
-		File destinationDir = new File(to.getDirectory());
 		Map<String, Pair<String, Long>> errors = new HashMap<>();
-		if (!FileUtils.isWritable(destinationDir, true)) {
-			return errors;
-		}
 		FileCopyListener fileCopyListener = getCopyFilesListener();
 
 		long remainingSize = filesSize.first;
@@ -134,8 +130,6 @@ public class MoveFilesTask extends AsyncTask<Void, Object, Map<String, Pair<Stri
 				String error = copyFile(from.getDirectory(), to.getDirectory(), filePathWithoutStorage, fileName, outputDirectory, fileCopyListener);
 				if (error != null) {
 					errors.put(fileName, new Pair<>(error, fileLength));
-				} else {
-					file.delete();
 				}
 				fileCopyListener.onFileCopyFinished(fileName, FileUtils.APPROXIMATE_FILE_SIZE_BYTES / 1024);
 			} else {
@@ -143,6 +137,7 @@ public class MoveFilesTask extends AsyncTask<Void, Object, Map<String, Pair<Stri
 				int deltaProgress = (int) ((fileLength + FileUtils.APPROXIMATE_FILE_SIZE_BYTES) / 1024);
 				fileCopyListener.onFileCopyFinished(fileName, deltaProgress);
 			}
+			file.delete();
 			publishProgress(new Pair<>(files.size() - i, remainingSize));
 		}
 		progressHelper.onFinishTask();
