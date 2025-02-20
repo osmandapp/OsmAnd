b38526f7ed4d753f6301d941de90ad475c79fb3d
RZR-UA <youonly@ukr.net>
2025-02-20 11:37:15 +0100

    TODO

--- OsmAnd/src/net/osmand/plus/wikivoyage/data/TravelObfHelper.java
+++ OsmAnd/src/net/osmand/plus/wikivoyage/data/TravelObfHelper.java
@@ -1333,8 +1333,11 @@ public class TravelObfHelper implements TravelHelper {
 				track.getSegments().add(trkSegment);
 			}
 			gpxFile.setTracks(new ArrayList<>());
+
+			gpxFile.getTracks().add(track); // TODO analyze both DS and GPT methods
 //			gpxFile.getTracks().add(OverlappedSegmentsMergerDS.mergeSegmentsWithOverlapHandling(track));
-			gpxFile.getTracks().add(OverlappedSegmentsMergerGPT.mergeSegmentsWithOverlapHandling(track));
+//			gpxFile.getTracks().add(OverlappedSegmentsMergerGPT.mergeSegmentsWithOverlapHandling(track));
+
 			if (!(article instanceof TravelGpx)) {
 				gpxFile.setRef(article.ref);
 			}
