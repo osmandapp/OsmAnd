6411ce5baac6bfdc461750b980a3aabcd8c563b9
nazar-kutz <vitaminkuna@gmail.com>
2025-02-28 20:28:10 +0200

    Make getCenterPointIcon return type to "Object"

--- OsmAnd/src/net/osmand/plus/dialogs/CenterIconProvider.java
+++ OsmAnd/src/net/osmand/plus/dialogs/CenterIconProvider.java
@@ -2,9 +2,7 @@ package net.osmand.plus.dialogs;
 
 import androidx.annotation.Nullable;
 
-import net.osmand.plus.views.PointImageDrawable;
-
 public interface CenterIconProvider {
 	@Nullable
-	PointImageDrawable getCenterPointIcon();
+	Object getCenterPointIcon();
 }
