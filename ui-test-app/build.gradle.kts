399d57c16c1dbc3eae23a020d55db13e2bf954bd
Corwin-Kh <serge.kharchenko@gmail.com>
2025-03-04 16:10:13 +0200

    synced agp versions

--- ui-test-app/build.gradle.kts
+++ ui-test-app/build.gradle.kts
@@ -1,4 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-	id("com.android.application") version "8.9" apply false
+	id("com.android.application") version "8.7.3" apply false
 }
