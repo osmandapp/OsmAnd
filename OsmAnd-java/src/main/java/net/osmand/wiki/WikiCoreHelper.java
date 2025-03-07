d0cf30132fc1c11b3ca0fec4bd2c28626d1381bd
chumv <chumva.forever@gmail.com>
2025-03-07 23:28:11 +0200

    Try to fix #22033

--- OsmAnd-java/src/main/java/net/osmand/wiki/WikiCoreHelper.java
+++ OsmAnd-java/src/main/java/net/osmand/wiki/WikiCoreHelper.java
@@ -7,7 +7,6 @@ import com.google.gson.annotations.SerializedName;
 
 import net.osmand.PlatformUtil;
 import net.osmand.data.Amenity;
-import net.osmand.data.QuadRect;
 import net.osmand.osm.io.NetworkUtils;
 import net.osmand.shared.data.KQuadRect;
 import net.osmand.util.Algorithms;
@@ -17,7 +16,6 @@ import org.apache.commons.logging.Log;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -105,18 +103,22 @@ public class WikiCoreHelper {
 			// article // category
 			String url = "";
 			String baseApiActionUrl = OSMAND_API_ENDPOINT + WIKI_PLACE_ACTION;
-			if (!Algorithms.isEmpty(wikidataId)) {
-				url += baseApiActionUrl + "article=" + URLEncoder.encode(wikidataId, StandardCharsets.UTF_8);
-			}
-			if (!Algorithms.isEmpty(wikiCategory)) {
-				url += (url.isEmpty() ? baseApiActionUrl : "&") + "category=" + URLEncoder.encode(wikiCategory, StandardCharsets.UTF_8);
-			}
-			if (!Algorithms.isEmpty(wikiTitle)) {
-				url += (url.isEmpty() ? baseApiActionUrl : "&") + "wiki=" + URLEncoder.encode(wikiTitle, StandardCharsets.UTF_8);
-			}
-			if (!url.isEmpty()) {
-				url += "&" + "addMetaData=" + URLEncoder.encode("true", StandardCharsets.UTF_8);
-				getImagesOsmAndAPIRequestV2(url, wikiImages);
+			try {
+				if (!Algorithms.isEmpty(wikidataId)) {
+					url += baseApiActionUrl + "article=" + URLEncoder.encode(wikidataId, "UTF-8");
+				}
+				if (!Algorithms.isEmpty(wikiCategory)) {
+					url += (url.isEmpty() ? baseApiActionUrl : "&") + "category=" + URLEncoder.encode(wikiCategory, "UTF-8");
+				}
+				if (!Algorithms.isEmpty(wikiTitle)) {
+					url += (url.isEmpty() ? baseApiActionUrl : "&") + "wiki=" + URLEncoder.encode(wikiTitle, "UTF-8");
+				}
+				if (!url.isEmpty()) {
+					url += "&" + "addMetaData=" + URLEncoder.encode("true", "UTF-8");
+					getImagesOsmAndAPIRequestV2(url, wikiImages);
+				}
+			} catch (UnsupportedEncodingException e) {
+				LOG.error(e);
 			}
 		} else {
 			if (!Algorithms.isEmpty(wikidataId)) {
