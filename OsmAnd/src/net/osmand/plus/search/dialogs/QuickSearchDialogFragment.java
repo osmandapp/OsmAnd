8ae0a0cc8d50477e149effc7d4e49c6e87eaec09
RZR-UA <90444451+RZR-UA@users.noreply.github.com>
2025-03-18 10:17:59 +0100

    Fix Routes as top category, add to Search history (#22088)
    Do not ignore Routes in QuickSearchDialogFragment
    Add addNewItemToHistory for TravelGpx in Search

--- OsmAnd/src/net/osmand/plus/search/dialogs/QuickSearchDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/search/dialogs/QuickSearchDialogFragment.java
@@ -1134,11 +1134,8 @@ public class QuickSearchDialogFragment extends DialogFragment implements OsmAndC
 			SearchResultCollection res = searchUICore.shallowSearch(SearchAmenityTypesAPI.class, "", null);
 			if (res != null) {
 				List<QuickSearchListItem> rows = new ArrayList<>();
-				PoiCategory routesCategory = app.getPoiTypes().getRoutes();
 				for (SearchResult sr : res.getCurrentSearchResults()) {
-					if (sr.object != routesCategory) {
-						rows.add(new QuickSearchListItem(app, sr));
-					}
+					rows.add(new QuickSearchListItem(app, sr));
 				}
 				rows.add(new QuickSearchButtonListItem(app, R.drawable.ic_world_globe_dark, app.getString(R.string.search_online_address), view -> {
 					OsmandSettings settings = app.getSettings();
--- OsmAnd/src/net/osmand/plus/search/dialogs/QuickSearchListFragment.java
+++ OsmAnd/src/net/osmand/plus/search/dialogs/QuickSearchListFragment.java
@@ -220,6 +220,11 @@ public abstract class QuickSearchListFragment extends OsmAndListFragment {
 			if (amenity.isRouteTrack()) {
 				TravelHelper travelHelper = app.getTravelHelper();
 				TravelGpx travelGpx = new TravelGpx(amenity);
+
+				SearchHistoryHelper historyHelper = SearchHistoryHelper.getInstance(app);
+				historyHelper.addNewItemToHistory(searchResult.location.getLatitude(),
+						searchResult.location.getLongitude(), pair.first, HistorySource.SEARCH);
+
 				travelHelper.openTrackMenu(travelGpx, getMapActivity(), amenity.getGpxFileName(null), amenity.getLocation(), true);
 				return; // TravelGpx
 			}
