d03a48bb9a0d6ddbe254746ab3080fc37f8b6212
alex-osm <alex.morozov.osm@gmail.com>
2025-03-21 14:36:30 +0300

    Fix wiki icons transformed to poi type

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/MenuBuilder.java
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/MenuBuilder.java
@@ -503,7 +503,7 @@ public class MenuBuilder {
 
 					View amenitiesRow = createRowContainer(viewGroup.getContext(), NEAREST_POI_KEY);
 					firstRow = insertIndex == 0 || isDividerAtPosition(viewGroup, insertIndex - 1);
-					buildNearestRow(amenitiesRow, amenities, AmenityMenuController.getRightIconId(amenity), text, NEAREST_POI_KEY);
+					buildNearestRow(amenitiesRow, amenities, AmenityMenuController.getRightIconId(app, amenity), text, NEAREST_POI_KEY);
 					viewGroup.addView(amenitiesRow, insertIndex);
 
 					buildNearestRowDividerIfMissing(viewGroup, insertIndex);
