a440395ec67fbe04749cfa96e866c30136f86784
frankknoll <Knoll_Frank@web.de>
2025-03-31 12:44:20 +0200

    refactoring

--- OsmAnd/test/java/net/osmand/plus/settings/fragments/search/PluginsHelper.java
+++ OsmAnd/test/java/net/osmand/plus/settings/fragments/search/PluginsHelper.java
@@ -3,31 +3,24 @@ package net.osmand.plus.settings.fragments.search;
 import net.osmand.plus.OsmandApplication;
 import net.osmand.plus.plugins.OsmandPlugin;
 
-// FK-TODO: refactor
 class PluginsHelper {
 
-	public static OsmandPlugin enablePlugin(final Class<? extends OsmandPlugin> plugin, final OsmandApplication app) {
+	public static OsmandPlugin enablePlugin(final Class<? extends OsmandPlugin> plugin,
+											final OsmandApplication app) {
+		return setPlugin(plugin, app, true);
+	}
+
+	public static OsmandPlugin disablePlugin(final Class<? extends OsmandPlugin> plugin,
+											 final OsmandApplication app) {
+		return setPlugin(plugin, app, false);
+	}
+
+	private static OsmandPlugin setPlugin(final Class<? extends OsmandPlugin> plugin,
+										  final OsmandApplication app,
+										  final boolean enable) {
 		final OsmandPlugin osmandPlugin = getPlugin(plugin);
-		enablePlugin(osmandPlugin, app);
+		setPlugin(app, osmandPlugin, enable);
 		return osmandPlugin;
-	}
-
-	public static OsmandPlugin disablePlugin(final Class<? extends OsmandPlugin> plugin, final OsmandApplication app) {
-		final OsmandPlugin osmandPlugin = getPlugin(plugin);
-		disablePlugin(osmandPlugin, app);
-		return osmandPlugin;
-	}
-
-	private static void enablePlugin(final OsmandPlugin plugin, final OsmandApplication app) {
-		setPlugin(app, plugin, true);
-	}
-
-	private static void disablePlugin(final OsmandPlugin plugin, final OsmandApplication app) {
-		setPlugin(app, plugin, false);
-	}
-
-	private static void setPlugin(final OsmandApplication app, final OsmandPlugin plugin, final boolean enable) {
-		net.osmand.plus.plugins.PluginsHelper.enablePlugin(null, app, plugin, enable);
 	}
 
 	private static <T extends OsmandPlugin> T getPlugin(final Class<T> plugin) {
@@ -38,5 +31,9 @@ class PluginsHelper {
 				.map(plugin::cast)
 				.findFirst()
 				.orElseThrow();
+	}
+
+	private static void setPlugin(final OsmandApplication app, final OsmandPlugin plugin, final boolean enable) {
+		net.osmand.plus.plugins.PluginsHelper.enablePlugin(null, app, plugin, enable);
 	}
 }
