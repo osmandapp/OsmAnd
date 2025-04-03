3fb83d673c4262538180f75edcfa29e8253d930f
Corwin-Kh <serge.kharchenko@gmail.com>
2025-04-03 13:42:55 +0300

    Fixed show visible items on map in list

--- OsmAnd/src/net/osmand/plus/views/layers/POIMapLayer.java
+++ OsmAnd/src/net/osmand/plus/views/layers/POIMapLayer.java
@@ -757,6 +757,7 @@ public class POIMapLayer extends OsmandMapLayer implements IContextMenuProvider,
 				this.showTopPlacesPreviews = showTopPlacesPreviews;
 				if (updated || showTopPlacesPreviewsChanged || topPlacesBox == null || !topPlacesBox.containsTileBox(tileBox)) {
 					List<Amenity> places = data.getResults();
+					updateVisiblePlaces(data.getDisplayedResults(), tileBox.getLatLonBounds());
                     if (showTopPlacesPreviews && places != null) {
                         RotatedTileBox extendedBox = tileBox.copy();
                         int bigIconSize = getBigIconSize();
@@ -764,7 +765,6 @@ public class POIMapLayer extends OsmandMapLayer implements IContextMenuProvider,
                         topPlacesBox = extendedBox;
 						updateTopPlaces(places, tileBox.getLatLonBounds(), zoom);
 						updateTopPlacesCollection();
-						updateVisiblePlaces(data.getDisplayedResults(), tileBox.getLatLonBounds());
                     } else {
                         clearMapMarkersCollections();
 						cancelLoadingImages();
