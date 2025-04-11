3e51f754c31904e6b8c4da5abaa070bd3666b437
0xRe1nk0 <0xre1nk0@gmail.com>
2025-04-11 11:25:54 +0300

    small cleanup

--- OsmAnd/src/net/osmand/plus/views/layers/MapInfoLayer.java
+++ OsmAnd/src/net/osmand/plus/views/layers/MapInfoLayer.java
@@ -7,7 +7,6 @@ import android.graphics.Rect;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
