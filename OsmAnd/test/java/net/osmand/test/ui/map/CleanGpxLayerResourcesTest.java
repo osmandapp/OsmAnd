582be4fab40f6e16cecd5bcf1657797ea968380a
0xRe1nk0 <0xre1nk0@gmail.com>
2025-07-08 12:32:28 +0300

    update test

--- OsmAnd/test/java/net/osmand/test/ui/map/CleanGpxLayerResourcesTest.java
+++ OsmAnd/test/java/net/osmand/test/ui/map/CleanGpxLayerResourcesTest.java
@@ -3,6 +3,8 @@ package net.osmand.test.ui.map;
 import static net.osmand.test.common.OsmAndDialogInteractions.refreshMap;
 import static net.osmand.test.common.OsmAndDialogInteractions.skipAppStartDialogs;
 
+import static org.junit.Assert.assertNotEquals;
+
 import android.os.Handler;
 import android.os.Looper;
 
@@ -24,6 +26,7 @@ import net.osmand.plus.views.layers.GPXLayer;
 import net.osmand.shared.data.KQuadRect;
 import net.osmand.shared.gpx.GpxFile;
 import net.osmand.test.common.AndroidTest;
+import net.osmand.test.common.BaseIdlingResource;
 import net.osmand.test.common.ResourcesImporter;
 import net.osmand.util.Algorithms;
 
@@ -51,6 +54,9 @@ public class CleanGpxLayerResourcesTest extends AndroidTest {
 	private File file;
 	private GpxFile importedGpxFile;
 
+	int originalHash;
+	int newHash;
+
 	@Before
 	@Override
 	public void setup() {
@@ -70,7 +76,6 @@ public class CleanGpxLayerResourcesTest extends AndroidTest {
 						gpxFile.setPath(file.getAbsolutePath());
 						selectionHelper.selectGpxFile(gpxFile, GpxSelectionParams.getDefaultSelectionParams());
 						importedGpxFile = gpxFile;
-						moveMapToGpx(gpxFile);
 					}
 				}
 			});
@@ -82,74 +87,112 @@ public class CleanGpxLayerResourcesTest extends AndroidTest {
 	@Test
 	public void test() throws Throwable {
 		skipAppStartDialogs(app);
-		moveMapToGpx(importedGpxFile);
+		moveMapToGpx();
 
 		IdlingResource idlingResource = new GpxRendererChangeIdlingResource(app);
 		registerIdlingResources(idlingResource);
 		Espresso.onIdle();
 		unregisterIdlingResources(idlingResource);
+
+		assertNotEquals("Cached segments didn't updated", originalHash, newHash);
 	}
 
-	private void moveMapToGpx(@Nullable GpxFile gpx) {
-		if (gpx == null) {
-			return;
+	private boolean moveMapToGpx() {
+		if (importedGpxFile == null) {
+			return false;
 		}
 
-		KQuadRect rect = gpx.getRect();
+		KQuadRect rect = importedGpxFile.getRect();
 		if (rect.getLeft() != 0 && rect.getRight() != 0) {
 			app.getOsmandMap().getMapView().fitRectToMap(rect.getLeft(), rect.getRight(),
 					rect.getTop(), rect.getBottom(), (int) rect.width(), (int) rect.height(), 0);
 		}
+		return true;
 	}
 
-	public class GpxRendererChangeIdlingResource implements IdlingResource {
+	public class GpxRendererChangeIdlingResource extends BaseIdlingResource {
+
+		private static final int STEP_MOVE_MAP_TO_GPX = 1;
+		private static final int STEP_GET_ORIGINAL_CACHE_HASH = 2;
+		private static final int STEP_REFRESH_MAP = 3;
+		private static final int STEP_GET_NEW_HASH = 4;
+		private int currentStep = STEP_MOVE_MAP_TO_GPX;
+
+		private static final int CHECKS_COUNT = 30;
+		private static final int CHECK_INTERVAL_MS = 1000;
+		private int checksCounter;
 
 		private final OsmandApplication app;
-		private ResourceCallback callback;
+		private final GPXLayer gpxLayer;
 		private boolean isIdle = false;
 
 		public GpxRendererChangeIdlingResource(@NonNull OsmandApplication app) {
+			super(app);
+
 			this.app = app;
-			startSequence();
+			gpxLayer = app.getOsmandMap().getMapLayers().getGpxLayer();
+			startHandler();
 		}
 
-		private void startSequence() {
+		private void startHandler(){
 			Handler handler = new Handler(Looper.getMainLooper());
-
 			handler.postDelayed(() -> {
-				GPXLayer gpxLayer = app.getOsmandMap().getMapLayers().getGpxLayer();
-				int originalHash = gpxLayer.getSegmentsCacheHash();
-				scenarioRule.getScenario().onActivity(activity -> gpxLayer.setMapRendererChanged(true));
-				refreshMap(app);
+				checksCounter++;
 
-				handler.postDelayed(() -> {
-					int newHash = gpxLayer.getSegmentsCacheHash();
-
-					assert originalHash != newHash : "Cached segments didn't updated";
-
+				if (currentStep == STEP_MOVE_MAP_TO_GPX && moveMapToGpx()) {
+					currentStep = STEP_GET_ORIGINAL_CACHE_HASH;
+				} else if (currentStep == STEP_GET_ORIGINAL_CACHE_HASH && getOriginalCacheHash()) {
+					currentStep = STEP_REFRESH_MAP;
+				} else if (currentStep == STEP_REFRESH_MAP) {
+					refreshMap(app);
+					currentStep = STEP_GET_NEW_HASH;
+				} else if (currentStep == STEP_GET_NEW_HASH && getNewCacheHash()) {
 					isIdle = true;
-					if (callback != null) {
-						callback.onTransitionToIdle();
-					}
+				}
 
-				}, 1000);
-
-			}, 1000);
+				if (isIdleNow()) {
+					notifyIdleTransition();
+				} else {
+					startHandler();
+				}
+			}, CHECK_INTERVAL_MS);
 		}
 
-		@Override
-		public String getName() {
-			return getClass().getSimpleName();
+		private boolean getNewCacheHash() {
+			newHash = gpxLayer.getSegmentsCacheHash();
+			return newHash != 0;
+		}
+
+		private boolean getOriginalCacheHash() {
+			GPXLayer gpxLayer = app.getOsmandMap().getMapLayers().getGpxLayer();
+			originalHash = gpxLayer.getSegmentsCacheHash();
+			if (originalHash == 0) {
+				return false;
+			}
+
+			scenarioRule.getScenario().onActivity(activity -> gpxLayer.setMapRendererChanged(true));
+			return true;
 		}
 
 		@Override
 		public boolean isIdleNow() {
-			return isIdle;
-		}
+			if (isIdle) {
+				return true;
+			}
 
-		@Override
-		public void registerIdleTransitionCallback(ResourceCallback callback) {
-			this.callback = callback;
+			if (checksCounter >= CHECKS_COUNT) {
+				String stepName = switch (currentStep) {
+					case STEP_MOVE_MAP_TO_GPX -> "STEP_MOVE_MAP_TO_GPX";
+					case STEP_GET_ORIGINAL_CACHE_HASH -> "STEP_GET_ORIGINAL_CACHE_HASH";
+					case STEP_REFRESH_MAP -> "STEP_REFRESH_MAP";
+					case STEP_GET_NEW_HASH -> "STEP_GET_NEW_HASH";
+					default -> "UNKNOWN_STEP";
+				};
+
+				throw new AssertionError("GpxRendererChangeIdlingResource timeout at step: " + stepName);
+			}
+
+			return false;
 		}
 	}
 }
