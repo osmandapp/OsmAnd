ce116e3fed1a19adcc9f02de4f7d9eb3d2e7219f
Corwin-Kh <serge.kharchenko@gmail.com>
2025-07-09 17:19:04 +0300

    Added try-catch to grant permisssions

--- OsmAnd/src/net/osmand/plus/auto/screens/RequestPermissionScreen.java
+++ OsmAnd/src/net/osmand/plus/auto/screens/RequestPermissionScreen.java
@@ -15,6 +15,9 @@ import androidx.car.app.model.Template;
 
 import net.osmand.plus.R;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +25,8 @@ import java.util.List;
  * Screen for asking the user to grant location permission.
  */
 public class RequestPermissionScreen extends BaseAndroidAutoScreen {
+
+	private static final Log log = LogFactory.getLog(RequestPermissionScreen.class);
 
 	/**
 	 * Callback called when the location permission is granted.
@@ -50,7 +55,8 @@ public class RequestPermissionScreen extends BaseAndroidAutoScreen {
 
 		String message = getCarContext().getString(R.string.location_access_request_title);
 
-		OnClickListener listener = ParkedOnlyOnClickListener.create(() ->
+		OnClickListener listener = ParkedOnlyOnClickListener.create(() -> {
+			try {
 				getCarContext().requestPermissions(
 						permissions,
 						(approved, rejected) -> {
@@ -61,7 +67,11 @@ public class RequestPermissionScreen extends BaseAndroidAutoScreen {
 								}
 							}
 							finish();
-						}));
+						});
+			} catch (RuntimeException error) {
+				log.error("Can't request permissions", error);
+			}
+		});
 
 		Action action = new Action.Builder()
 				.setTitle(getCarContext().getString(R.string.location_access_request_action))
