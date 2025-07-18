37d4dd5a68c6432984513d3c59bc1fc2489aa4fc
chumv <chumva.forever@gmail.com>
2025-07-18 23:07:40 +0300

    Allow downloading deleted files from cloud

--- OsmAnd/src/net/osmand/plus/backup/ui/ChangesTabFragment.java
+++ OsmAnd/src/net/osmand/plus/backup/ui/ChangesTabFragment.java
@@ -149,7 +149,7 @@ public abstract class ChangesTabFragment extends BaseOsmAndFragment implements O
 
 	public void downloadCloudVersions() {
 		for (CloudChangeItem item : items) {
-			if (item.operation != SYNC_OPERATION_DELETE && item.remoteFile != null && !settingsHelper.isSyncing(item.fileName)) {
+			if (item.remoteFile != null && !settingsHelper.isSyncing(item.fileName)) {
 				settingsHelper.syncSettingsItems(item.fileName, item.localFile, item.remoteFile, UNIQUE, SYNC_OPERATION_DOWNLOAD);
 			}
 		}
