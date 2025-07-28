7a0f516c447cbf91e80fc84fb136ddf3481fab16
nazar-kutz <vitaminkuna@gmail.com>
2025-07-28 14:46:52 +0300

    call super method

--- OsmAnd/src/net/osmand/plus/plugins/audionotes/TakeMediaNoteAction.java
+++ OsmAnd/src/net/osmand/plus/plugins/audionotes/TakeMediaNoteAction.java
@@ -29,7 +29,7 @@ public abstract class TakeMediaNoteAction extends SelectMapLocationAction {
 			if (plugin.isRecording()) {
 				plugin.stopRecording(mapActivity, false);
 			} else {
-				execute(mapActivity, params);
+				super.execute(mapActivity, params);
 			}
 		}
 	}
