5e4099f8a5620ecd9cd47f54c6460e845bfb84de
Dima-1 <softmail32@gmail.com>
2025-08-07 15:47:41 +0300

    Fix parsing error

--- OsmAnd-shared/src/jvmMain/kotlin/net/osmand/shared/gpx/helper/ImportHelper.kt
+++ OsmAnd-shared/src/jvmMain/kotlin/net/osmand/shared/gpx/helper/ImportHelper.kt
@@ -6,39 +6,14 @@ import net.osmand.shared.gpx.GpxUtilities.loadGpxFile
 import net.osmand.shared.gpx.helper.ImportGpx.errorImport
 import net.osmand.shared.gpx.helper.ImportGpx.loadGPXFileFromKml
 import net.osmand.shared.io.SourceInputStream
-import net.osmand.shared.util.LoggerFactory
+
 import okio.Source
 import okio.source
 import java.io.IOException
-import java.io.StringWriter
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
-import javax.xml.transform.TransformerException
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.TransformerFactoryConfigurationError
-import javax.xml.transform.stream.StreamResult
-import javax.xml.transform.stream.StreamSource
 
 actual object ImportHelper {
-	private val LOG = LoggerFactory.getLogger(this::class.simpleName!!)
-
-	actual fun kml2Gpx(kml: Source): String? {
-		try {
-			val xmlSource: javax.xml.transform.Source = StreamSource(SourceInputStream(kml))
-			val xsltSource: javax.xml.transform.Source =
-				StreamSource(this::class.java.getResourceAsStream("/kml2gpx.xslt"))
-			val sw = StringWriter()
-
-			TransformerFactory.newInstance().newTransformer(xsltSource).transform(xmlSource, StreamResult(sw))
-
-			return sw.toString()
-		} catch (e: TransformerFactoryConfigurationError) {
-			LOG.error(e.toString(), e)
-		} catch (e: TransformerException) {
-			LOG.error(e.toString(), e)
-		}
-		return null
-	}
 
 	@Throws(IOException::class)
 	actual fun loadGPXFileFromArchive(source: Source): Pair<GpxFile, Long> {
