25adaf717e70d6c1bb50f810b6a8f4c3aa1d44e3
0xRe1nk0 <0xre1nk0@gmail.com>
2025-08-18 15:53:40 +0300

    hide widget name if less than 4 symbols visible

--- OsmAnd/src/net/osmand/plus/views/mapwidgets/widgets/SimpleWidget.java
+++ OsmAnd/src/net/osmand/plus/views/mapwidgets/widgets/SimpleWidget.java
@@ -1,11 +1,14 @@
 package net.osmand.plus.views.mapwidgets.widgets;
 
+import static android.view.View.INVISIBLE;
 import static net.osmand.plus.utils.AndroidUtils.dpToPx;
 import static net.osmand.plus.views.mapwidgets.WidgetsPanel.BOTTOM;
 
 import android.graphics.Typeface;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
+import android.text.Layout;
+import android.text.TextPaint;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +24,7 @@ import androidx.annotation.Nullable;
 import net.osmand.plus.R;
 import net.osmand.plus.activities.MapActivity;
 import net.osmand.plus.helpers.AndroidUiHelper;
+import net.osmand.plus.utils.AndroidUtils;
 import net.osmand.plus.views.mapwidgets.OutlinedTextContainer;
 import net.osmand.plus.settings.backend.ApplicationMode;
 import net.osmand.plus.settings.backend.preferences.CommonPreference;
@@ -50,7 +54,7 @@ public abstract class SimpleWidget extends TextInfoWidget implements ISupportWid
 	private boolean isFullRow;
 
 	public SimpleWidget(@NonNull MapActivity mapActivity, @NonNull WidgetType widgetType,
-			@Nullable String customId, @Nullable WidgetsPanel panel) {
+	                    @Nullable String customId, @Nullable WidgetsPanel panel) {
 		super(mapActivity, widgetType, customId, panel);
 		widgetState = new SimpleWidgetState(app, customId, widgetType, getDefaultWidgetSize());
 
@@ -81,6 +85,7 @@ public abstract class SimpleWidget extends TextInfoWidget implements ISupportWid
 		return isVerticalWidget() ? WidgetSize.MEDIUM : WidgetSize.SMALL;
 	}
 
+	@Override
 	public void updateValueAlign(boolean fullRow) {
 		if (WidgetSize.SMALL != getWidgetSizePref().get()) {
 			ViewGroup.LayoutParams textViewLayoutParams = textView.getLayoutParams();
@@ -90,6 +95,14 @@ public abstract class SimpleWidget extends TextInfoWidget implements ISupportWid
 				params.setMarginStart(dpToPx(app, (shouldShowIcon() || fullRow) ? 36 : 0));
 				params.setMarginEnd(dpToPx(app, fullRow ? 36 : 0));
 			}
+		}
+	}
+
+	@Override
+	public void updateHiddenNameText() {
+		if (widgetName != null && widgetName.getVisibility() == View.GONE) {
+			widgetName.setVisibility(INVISIBLE);
+			checkForMaxWidgetName();
 		}
 	}
 
@@ -229,14 +242,49 @@ public abstract class SimpleWidget extends TextInfoWidget implements ISupportWid
 	}
 
 	protected void updateWidgetName() {
-		String widgetName = getWidgetName();
-		if (widgetName != null && this.widgetName != null) {
+		String newWidgetName = getWidgetName();
+		if (newWidgetName != null && this.widgetName != null) {
+
 			String additionalName = getAdditionalWidgetName();
 			if (additionalName != null) {
-				widgetName = widgetName + ", " + additionalName;
+				newWidgetName = newWidgetName + ", " + additionalName;
 			}
-			this.widgetName.setText(widgetName);
+
+			String oldWidgetName = String.valueOf(this.widgetName.getText());
+			this.widgetName.setText(newWidgetName);
+
+			if (!oldWidgetName.equals(newWidgetName)) {
+				if (widgetName.getVisibility() == View.GONE) {
+					widgetName.setVisibility(INVISIBLE);
+				}
+				checkForMaxWidgetName();
+			}
 		}
+	}
+
+	private void checkForMaxWidgetName() {
+		if (widgetName == null) {
+			return;
+		}
+		widgetName.post(() -> {
+			String text = widgetName.getText().toString();
+
+			String firstFourSymbols = (text.length() > 4 ? text.substring(0, 4) : text).toUpperCase();
+
+			if (text.length() > 4) {
+				firstFourSymbols += "…";
+			}
+
+			TextPaint paint = widgetName.getPaint();
+			float requiredWidth = paint.measureText(firstFourSymbols);
+			float availableWidth = widgetName.getWidth() - widgetName.getPaddingLeft() - widgetName.getPaddingRight();
+
+			if (availableWidth < requiredWidth) {
+				widgetName.setVisibility(View.GONE);
+			} else {
+				widgetName.setVisibility(View.VISIBLE);
+			}
+		});
 	}
 
 	@Nullable
@@ -246,7 +294,7 @@ public abstract class SimpleWidget extends TextInfoWidget implements ISupportWid
 
 	@Override
 	public void copySettingsFromMode(@NonNull ApplicationMode sourceAppMode,
-			@NonNull ApplicationMode appMode, @Nullable String customId) {
+	                                 @NonNull ApplicationMode appMode, @Nullable String customId) {
 		if (widgetState != null) {
 			widgetState.copyPrefsFromMode(sourceAppMode, appMode, customId);
 		}
