4b2ed30119a0a4bb6e51b97304de4d0427b137cb
chumv <chumva.forever@gmail.com>
2025-08-22 16:57:10 +0300

    Fix compilation

--- OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/rows/behaviour/EleRowBehaviour.kt
+++ OsmAnd/src/net/osmand/plus/mapcontextmenu/builders/rows/behaviour/EleRowBehaviour.kt
@@ -2,37 +2,33 @@ package net.osmand.plus.mapcontextmenu.builders.rows.behaviour
 
 import net.osmand.PlatformUtil
 import net.osmand.plus.utils.OsmAndFormatter
-import net.osmand.shared.settings.enums.MetricsConstants.*
+import net.osmand.shared.settings.enums.AltitudeMetrics
 
 object EleRowBehaviour : DefaultPoiAdditionalRowBehaviour() {
 
-    val LOG = PlatformUtil.getLog(EleRowBehaviour::class.java)!!
+	val LOG = PlatformUtil.getLog(EleRowBehaviour::class.java)!!
 
-    override fun applyCustomRules(
-		params: PoiRowParams
-	) {
+	override fun applyCustomRules(params: PoiRowParams) {
 		super.applyCustomRules(params)
 		with(params) {
-		    var vl = value
-		    val metricSystem = app.settings.METRIC_SYSTEM.get()
-		    try {
-			    val distance: Float = vl.toFloat()
-			    vl = OsmAndFormatter.getFormattedAlt(distance.toDouble(), app, metricSystem)
-			    val collapsibleVal = if (metricSystem == MILES_AND_FEET
-				    || metricSystem == MILES_AND_YARDS
-				    || metricSystem == NAUTICAL_MILES_AND_FEET
-			    ) {
-				    OsmAndFormatter.getFormattedAlt(distance.toDouble(), app, KILOMETERS_AND_METERS)
-			    } else {
-				    OsmAndFormatter.getFormattedAlt(distance.toDouble(), app, MILES_AND_FEET)
-			    }
-			    val elevationData: MutableSet<String> = HashSet()
-			    elevationData.add(collapsibleVal)
-			    builder.setCollapsableView(menuBuilder.getDistanceCollapsableView(elevationData))
-		    } catch (ex: NumberFormatException) {
-			    LOG.error(ex)
-		    }
-		    builder.setText(vl)
-	    }
-    }
+			var vl = value
+			val altitudeMetrics = app.settings.ALTITUDE_METRIC.get()
+			try {
+				val distance: Float = vl.toFloat()
+				vl = OsmAndFormatter.getFormattedAlt(distance.toDouble(), app, altitudeMetrics)
+
+				val collapsibleVal = if (altitudeMetrics == AltitudeMetrics.FEET) {
+					OsmAndFormatter.getFormattedAlt(distance.toDouble(), app, AltitudeMetrics.METERS)
+				} else {
+					OsmAndFormatter.getFormattedAlt(distance.toDouble(), app, AltitudeMetrics.FEET)
+				}
+				val elevationData: MutableSet<String> = HashSet()
+				elevationData.add(collapsibleVal)
+				builder.setCollapsableView(menuBuilder.getDistanceCollapsableView(elevationData))
+			} catch (ex: NumberFormatException) {
+				LOG.error(ex)
+			}
+			builder.setText(vl)
+		}
+	}
 }
