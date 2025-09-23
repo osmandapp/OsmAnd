dc2316d21005c11809e2665df86137c41edc6eb9
alex-dev <alex.dev@osmand.net>
2025-09-23 16:39:21 +0300

    Update build.gradle.kts

--- OsmAnd-shared/build.gradle.kts
+++ OsmAnd-shared/build.gradle.kts
@@ -99,7 +99,7 @@ android {
 	}
 }
 
-version = System.getenv("OSMAND_SHARED_ANDROID_BINARIES_IVY_REVISION") ?: "master-snapshot"
+version = System.getenv("OSMAND_BINARIES_IVY_REVISION") ?: "5.1"
 publishing {
 	repositories {
 		ivy {
