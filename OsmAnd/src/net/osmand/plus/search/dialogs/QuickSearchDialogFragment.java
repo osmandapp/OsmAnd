62f310c1caaafa261db8d0353b7feeb1f91d5095
ivanPyrohivskyi <ivan.pirogovsky@gmail.com>
2025-09-29 15:52:07 +0300

    Fix show on map for brands

--- OsmAnd/src/net/osmand/plus/search/dialogs/QuickSearchDialogFragment.java
+++ OsmAnd/src/net/osmand/plus/search/dialogs/QuickSearchDialogFragment.java
@@ -2210,6 +2210,8 @@ public class QuickSearchDialogFragment extends BaseFullScreenDialogFragment impl
 			poiUIFilter = app.getPoiFilters().getFilter(topIndexFilter, acceptedTypes);
 			processTopIndexAfterLoad = ProcessTopIndex.NO;
 			return poiUIFilter;
+		} else if (processAfter == ProcessTopIndex.MAP) {
+			return app.getPoiFilters().getFilter(topIndexFilter, null);
 		}
 		processTopIndexAfterLoad = processAfter;
 		return null;
