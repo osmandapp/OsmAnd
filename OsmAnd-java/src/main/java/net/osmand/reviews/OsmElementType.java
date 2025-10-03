4709e741b0f30a79b2fe97bb1032b57a11911ec9
Maciek Makowski <maciek@mmakowski.com>
2025-10-03 11:49:12 +0200

    allow reviews for ways

---
+++ OsmAnd-java/src/main/java/net/osmand/reviews/OsmElementType.java
@@ -0,0 +1,5 @@
+package net.osmand.reviews;
+
+public enum OsmElementType {
+    NODE, WAY
+}
--- OsmAnd-java/src/main/java/net/osmand/reviews/ReviewedPlace.java
+++ OsmAnd-java/src/main/java/net/osmand/reviews/ReviewedPlace.java
@@ -5,11 +5,13 @@ import java.util.List;
 /**
  * @param lat     latitude of the POI
  * @param lon     longitude of the POI
- * @param osmId   the OSM node id, where known
+ * @param elementType the {@code OsmElementType} of the POI
+ * @param osmId   the OSM node or way id, where known
  * @param reviews the reviews of this place
  */
 public record ReviewedPlace(double lat,
                             double lon,
+                            OsmElementType elementType,
                             Long osmId,
                             List<Review> reviews) {
 }
