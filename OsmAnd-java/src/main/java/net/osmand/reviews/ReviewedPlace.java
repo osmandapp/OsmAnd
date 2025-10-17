ae336b2a7155c136dd98a707e3252dd02d785056
Maciek Makowski <maciek@mmakowski.com>
2025-10-17 20:29:47 +0200

    use location object instead of double lat/lon in ReviewedPlace

--- OsmAnd-java/src/main/java/net/osmand/reviews/ReviewedPlace.java
+++ OsmAnd-java/src/main/java/net/osmand/reviews/ReviewedPlace.java
@@ -1,16 +1,16 @@
 package net.osmand.reviews;
 
+import net.osmand.data.LatLon;
+
 import java.util.List;
 
 /**
- * @param lat     latitude of the POI
- * @param lon     longitude of the POI
+ * @param location    location of the POI
  * @param elementType the {@code OsmElementType} of the POI
- * @param osmId   the OSM node or way id, where known
- * @param reviews the reviews of this place
+ * @param osmId       the OSM node or way id, where known
+ * @param reviews     the reviews of this place
  */
-public record ReviewedPlace(double lat,
-                            double lon,
+public record ReviewedPlace(LatLon location,
                             OsmElementType elementType,
                             Long osmId,
                             List<Review> reviews) {
