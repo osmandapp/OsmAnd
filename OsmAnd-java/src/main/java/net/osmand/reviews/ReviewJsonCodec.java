a760ac77b031c9711eaf371f43d5cf0d844d15a3
Maciek Makowski <maciek@mmakowski.com>
2025-10-19 19:39:53 +0200

    use org.json instead of gson for review serialisation/deserialisation
    gson relies on reflection, so should not be used on Android
    where the symbols can be modified during release build optimisations.

--- OsmAnd-java/src/main/java/net/osmand/reviews/ReviewJsonCodec.java
+++ OsmAnd-java/src/main/java/net/osmand/reviews/ReviewJsonCodec.java
@@ -1,44 +1,65 @@
 package net.osmand.reviews;
 
-import com.google.gson.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.lang.reflect.Type;
+import java.net.URI;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Encodes/Decodes a collection of {@link Review}s as JSON.
  */
-// TODO: use something other than GSON
 public final class ReviewJsonCodec {
-    private final Gson gson = new GsonBuilder()
-            .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
-            .create();
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public String toJson(Iterable<Review> reviews) {
-        return gson.toJson(reviews);
+        JSONArray array = new JSONArray();
+        for (Review review : reviews) {
+            array.put(toJson(review));
+        }
+        return array.toString();
     }
 
     public Iterable<Review> fromJson(String json) {
-        Review[] reviews = gson.fromJson(json, Review[].class);
-        return Arrays.asList(reviews);
+        JSONArray array = new JSONArray(json);
+        List<Review> reviews = new ArrayList<>();
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            reviews.add(fromJson(object));
+        }
+        return reviews;
     }
 
-    private static class LocalDateTypeAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+    private JSONObject toJson(Review review) {
+        JSONObject json = new JSONObject();
+        json.put("id", review.id());
+        if (review.opinion() != null) json.put("opinion", review.opinion());
+        json.put("rating", review.rating());
+        json.put("author", review.author());
+        json.put("date", review.date().format(formatter));
+        if (review.link() != null) json.put("link", review.link().toString());
+        return json;
+    }
 
-        private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private Review fromJson(JSONObject object) {
+        checkFieldsPresent(object, "id", "rating", "author", "date");
+        String id = object.getString("id");
+        String opinion = object.has("opinion") ? object.getString("opinion") : null;
+        int rating = object.getInt("rating");
+        String author = object.getString("author");
+        LocalDate date = LocalDate.parse(object.getString("date"), formatter);
+        URI link = object.has("link") ? URI.create(object.getString("link")) : null;
+        return new Review(id, opinion, rating, author, date, link);
+    }
 
-        @Override
-        public JsonElement serialize(final LocalDate date, final Type typeOfSrc,
-                                     final JsonSerializationContext context) {
-            return new JsonPrimitive(date.format(formatter));
-        }
-
-        @Override
-        public LocalDate deserialize(final JsonElement json, final Type typeOfT,
-                                     final JsonDeserializationContext context) throws JsonParseException {
-            return LocalDate.parse(json.getAsString(), formatter);
+    private void checkFieldsPresent(JSONObject object, String... fieldNames) {
+        for (String fieldName : fieldNames) {
+            if (!object.has(fieldName)) {
+                throw new IllegalArgumentException("missing required Review field: " + fieldName + "; object=" + object);
+            }
         }
     }
 }
