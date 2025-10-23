1f998695081399b04f4c8265059a44fad16b1e81
vshcherb <victor.shcherb@gmail.com>
2025-10-23 17:24:04 +0300

    Fix building is selected as main word for Street 128-B

--- OsmAnd-java/src/main/java/net/osmand/binary/CommonWords.java
+++ OsmAnd-java/src/main/java/net/osmand/binary/CommonWords.java
@@ -55,7 +55,7 @@ public class CommonWords {
 	private static int letters(String s) {
 		int count = 0;
 		for (int i = 0; i < s.length(); i++) {
-			if (!Character.isDigit(s.charAt(i))) {
+			if (!Character.isDigit(s.charAt(i)) && Character.isLetter(s.charAt(i))) {
 				count++;
 			}
 		}
