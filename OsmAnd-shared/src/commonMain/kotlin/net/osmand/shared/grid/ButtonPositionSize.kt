d925cb6745607badc8bd2e2e8ebce042da3349b5
vshcherb <victor.shcherb@gmail.com>
2025-11-05 00:45:39 +0200

    Simplify border check

--- OsmAnd-shared/src/commonMain/kotlin/net/osmand/shared/grid/ButtonPositionSize.kt
+++ OsmAnd-shared/src/commonMain/kotlin/net/osmand/shared/grid/ButtonPositionSize.kt
@@ -379,10 +379,6 @@ class ButtonPositionSize {
 			var ok = false;
 			while (!ok && xMove && toMove.marginX <= totalWidth - toMove.width) {
 				if (!moveAndCheck(toMove, overlap, totalWidth, totalHeight, 1, 0)) {
-					if (moveAndCheck(toMove, overlap, totalWidth, totalHeight, space, 0)) {
-						// move back not enough space for space
-						moveAndCheck(toMove, overlap, totalWidth, totalHeight, -space, 0);
-					}
 					ok = true;
 				}
 			}
@@ -392,10 +388,6 @@ class ButtonPositionSize {
 			}
 			while (!ok && yMove && toMove.marginY <= totalHeight - toMove.height) {
 				if (!moveAndCheck(toMove, overlap, totalWidth, totalHeight, 0, 1)) {
-					if (moveAndCheck(toMove, overlap, totalWidth, totalHeight, 0, space)) {
-						// move back not enough space for space
-						moveAndCheck(toMove, overlap, totalWidth, totalHeight, 0, -space);
-					}
 					ok = true;
 				}
 			}
