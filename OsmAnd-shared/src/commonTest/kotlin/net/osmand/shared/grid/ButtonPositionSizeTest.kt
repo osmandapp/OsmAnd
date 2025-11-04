8bb33790153ea5b3e71cd76691732c43b69f9bf5
vshcherb <victor.shcherb@gmail.com>
2025-11-05 00:34:43 +0200

    Test new layout buttons

--- OsmAnd-shared/src/commonTest/kotlin/net/osmand/shared/grid/ButtonPositionSizeTest.kt
+++ OsmAnd-shared/src/commonTest/kotlin/net/osmand/shared/grid/ButtonPositionSizeTest.kt
@@ -5,6 +5,39 @@ import kotlin.test.assertTrue
 
 class ButtonPositionSizeTest {
 
+
+	@Test
+	fun testLayout0() {
+		ButtonPositionSize.DEBUG_PRINT = true;
+		ButtonPositionSize.ALTERNATIVE_ALGORITHM = true;
+		val buttons = listOf(
+			ButtonPositionSize("map_right_widgets_panel", 15, false, true).
+				setSize(15, 21).setMoveDescendantsHorizontal(),
+			ButtonPositionSize("map_top_widgets_panel", 114, true, true).
+				setSize(114, 12).setMargin(0, 0).setMoveDescendantsVertical(),
+			ButtonPositionSize("map_bottom_widgets_panel", 68, true, false).
+				setSize(68, 9).setMargin(22, 0).setMoveDescendantsVertical(),
+
+			ButtonPositionSize("map.view.layers", 6, true, true),
+			ButtonPositionSize("map.view.quick_search", 6, true, true).setMoveHorizontal(),
+			ButtonPositionSize("map.view.compass", 6, true, true).setMoveVertical(),
+
+			ButtonPositionSize("map.view.zoom_out", 7, false, false),
+			ButtonPositionSize("map.view.zoom_id", 7, false, false).setMoveVertical(),
+			ButtonPositionSize("map.view.back_to_loc", 7, false, false).setMoveHorizontal(),
+
+			ButtonPositionSize("map.view.menu", 7, true, false),
+			ButtonPositionSize("map.view.route_planning", 7, true, false).setMoveHorizontal(),
+
+			ButtonPositionSize("map.quick_actions1", 7, false, false).setMargin(8, 7),
+			ButtonPositionSize("map.quick_actions2", 7, false, true).setMargin(0, 15),
+		)
+		val compute = ButtonPositionSize.computeNonOverlap(1, buttons, 114, 45);
+		assertTrue(compute);
+//		assertTrue { check(buttons, "map.view.zoom_out", 107.0, 38.0) }
+
+	}
+
 	@Test
 	fun testLayout1() {
 		ButtonPositionSize.DEBUG_PRINT = true;
