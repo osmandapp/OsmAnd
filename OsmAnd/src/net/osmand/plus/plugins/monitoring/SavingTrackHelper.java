90186843a4b79169ba90314c690cab2a46a16706
nazar-kutz <vitaminkuna@gmail.com>
2025-11-07 21:45:17 +0200

    Mark first and last point of individual segments in general segment (to fix incorrect distance calculation in analysis for current recording track)

--- OsmAnd/src/net/osmand/plus/plugins/monitoring/SavingTrackHelper.java
+++ OsmAnd/src/net/osmand/plus/plugins/monitoring/SavingTrackHelper.java
@@ -616,7 +616,7 @@ public class SavingTrackHelper extends SQLiteOpenHelper implements IRouteInforma
 		trkPoints++;
 	}
 
-	private void addTrackPoint(WptPt pt, boolean newSegment, long time) {
+	private void addTrackPoint(WptPt point, boolean newSegment, long time) {
 		Track track = currentTrack.getModifiableGpxFile().getTracks().get(0);
 		if (newSegment) {
 			currentTrack.addEmptySegmentToDisplay();
@@ -634,10 +634,25 @@ public class SavingTrackHelper extends SQLiteOpenHelper implements IRouteInforma
 			}
 		}
 
-		if (pt != null) {
-			currentTrack.appendTrackPointToDisplay(pt, app);
-			TrkSegment lt = track.getSegments().get(track.getSegments().size() - 1);
-			lt.getPoints().add(pt);
+		// Add point to the current segment
+		if (point != null) {
+			currentTrack.appendTrackPointToDisplay(point, app);
+			TrkSegment currentSegment = segments.get(segments.size() - 1);
+			List<WptPt> currentSegmentPoints = currentSegment.getPoints();
+			currentSegmentPoints.add(point);
+			if (currentSegmentPoints.size() == 1) {
+				// Mark current point as start for segment
+				point.setFirstPoint(true);
+				// Mark previous point as last for segment
+				if (segments.size() > 1) {
+					TrkSegment previousSegment = segments.get(segments.size() - 2);
+					List<WptPt> points = previousSegment.getPoints();
+					if (!points.isEmpty()) {
+						WptPt previousPoint = points.get(points.size() - 1);
+						previousPoint.setLastPoint(true);
+					}
+				}
+			}
 		}
 		if (segmentAdded) {
 			currentTrack.processPoints(app);
