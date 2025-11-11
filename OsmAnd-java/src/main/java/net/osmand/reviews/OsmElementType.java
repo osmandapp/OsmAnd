baef4ea7ffe3a9fbc6d84e915458f92f3913b5fd
Maciek Makowski <maciek@mmakowski.com>
2025-11-11 16:59:36 +0100

    use Entity.EntityType instead of a new enum

--- OsmAnd-java/src/main/java/net/osmand/reviews/OsmElementType.java
+++
@@ -1,6 +0,0 @@
-package net.osmand.reviews;
-
-// TODO: consider using net.osmand.osm.edit.Entity.EntityType
-public enum OsmElementType {
-    NODE, RELATION, WAY
-}
--- OsmAnd-java/src/main/java/net/osmand/reviews/ReviewedPlace.java
+++ OsmAnd-java/src/main/java/net/osmand/reviews/ReviewedPlace.java
@@ -1,19 +1,20 @@
 package net.osmand.reviews;
 
 import net.osmand.data.LatLon;
+import net.osmand.osm.edit.Entity;
 
 import java.util.List;
 
 /**
  * @param location        location of the POI
- * @param elementType     the {@code OsmElementType} of the POI
+ * @param elementType     the {@code EntityType} of the POI
  * @param osmId           the OSM node or way id, where known
  * @param name            the name of the place
  * @param aggregateRating the aggregate (usually average) rating of this place
  * @param reviews         the reviews of this place
  */
 public record ReviewedPlace(LatLon location,
-                            OsmElementType elementType,
+                            Entity.EntityType elementType,
                             Long osmId,
                             String name,
                             int aggregateRating,
