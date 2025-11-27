b7c8758f50f0d3f9335c0229117d20d1c0454155
Corwin-Kh <serge.kharchenko@gmail.com>
2025-11-27 12:23:50 +0200

    Added logs for zoom and tilt actions

--- OsmAnd/src/net/osmand/plus/plugins/development/OsmandDevelopmentPlugin.java
+++ OsmAnd/src/net/osmand/plus/plugins/development/OsmandDevelopmentPlugin.java
@@ -97,6 +97,7 @@ public class OsmandDevelopmentPlugin extends OsmandPlugin {
 	private final StateChangedListener<Boolean> msaaListener;
 	private final StateChangedListener<Boolean> sphericalListener;
 	private final StateChangedListener<Boolean> map3DObjectsListener;
+	public static final String ZOOM_TILT_ANIMATION_LOG_TAG = "zoom_tilt_animation_log_tag";
 
 	private static final Log LOG_termal = PlatformUtil.getLog("ThermalState");
 
