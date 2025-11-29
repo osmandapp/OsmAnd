a9938fb83b4f9396f57f21fe440a3cd458e48f13
frankknoll <Knoll_Frank@web.de>
2025-11-29 20:01:29 +0100

    refining SearchDatabaseRootedAtConfigureMapFragmentAdapter

--- OsmAnd/src/net/osmand/plus/settings/fragments/search/SearchDatabaseRootedAtConfigureMapFragmentAdapter.java
+++ OsmAnd/src/net/osmand/plus/settings/fragments/search/SearchDatabaseRootedAtConfigureMapFragmentAdapter.java
@@ -5,12 +5,17 @@ import android.view.View;
 import androidx.annotation.IdRes;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.common.collect.Sets;
+
 import net.osmand.plus.plugins.rastermaps.TileSourceTemplatesProvider;
+import net.osmand.plus.settings.backend.ApplicationMode;
 import net.osmand.plus.settings.fragments.MainSettingsFragment;
 
 import org.jgrapht.Graph;
 
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 import de.KnollFrank.lib.settingssearch.PreferenceScreenWithHost;
 import de.KnollFrank.lib.settingssearch.PreferenceScreenWithHostProvider;
@@ -40,17 +45,45 @@ public class SearchDatabaseRootedAtConfigureMapFragmentAdapter {
 			final TileSourceTemplatesProvider tileSourceTemplatesProvider) {
 		preferencesDatabase
 				.searchablePreferenceScreenGraphDAO()
-				.persist(getAdaptedGraph(graph, newConfiguration, activityContext, tileSourceTemplatesProvider));
+				.persist(
+						getAdaptedGraph(
+								graph,
+								getApplicationModesWithoutDefault(),
+								newConfiguration,
+								activityContext,
+								tileSourceTemplatesProvider));
 	}
 
 	private SearchablePreferenceScreenGraph getAdaptedGraph(final SearchablePreferenceScreenGraph graph,
+															final Set<ApplicationMode> applicationModes,
+															final Configuration newConfiguration,
+															final FragmentActivity activityContext,
+															final TileSourceTemplatesProvider tileSourceTemplatesProvider) {
+		return applicationModes
+				.stream()
+				.reduce(
+						graph,
+						(currentGraph, applicationMode) ->
+								getAdaptedGraph(
+										currentGraph,
+										applicationMode,
+										newConfiguration,
+										activityContext,
+										tileSourceTemplatesProvider),
+						(graph1, graph2) -> {
+							throw new UnsupportedOperationException("Parallel stream not supported");
+						});
+	}
+
+	private SearchablePreferenceScreenGraph getAdaptedGraph(final SearchablePreferenceScreenGraph graph,
+															final ApplicationMode applicationMode,
 															final Configuration newConfiguration,
 															final FragmentActivity activityContext,
 															final TileSourceTemplatesProvider tileSourceTemplatesProvider) {
 		FragmentContainerViewAdder.addInvisibleFragmentContainerViewWithIdToParent(
 				activityContext.findViewById(android.R.id.content),
 				FRAGMENT_CONTAINER_VIEW_ID);
-		final SearchablePreferenceScreen searchablePreferenceScreen = findSearchablePreferenceScreen(graph);
+		final SearchablePreferenceScreen searchablePreferenceScreen = findSearchablePreferenceScreen(graph, applicationMode);
 		final SearchDatabaseConfig searchDatabaseConfig =
 				SearchDatabaseConfigFactory.createSearchDatabaseConfig(
 						MainSettingsFragment.class,
@@ -89,11 +122,11 @@ public class SearchDatabaseRootedAtConfigureMapFragmentAdapter {
 				.getSearchablePreferenceScreenGraph(root);
 	}
 
-	private SearchablePreferenceScreen findSearchablePreferenceScreen(final SearchablePreferenceScreenGraph graphToSearchIn) {
+	private SearchablePreferenceScreen findSearchablePreferenceScreen(final SearchablePreferenceScreenGraph graphToSearchIn, final ApplicationMode applicationMode) {
 		return SearchablePreferenceScreens
 				.findSearchablePreferenceScreenById(
 						graphToSearchIn.graph().vertexSet(),
-						"en-net.osmand.plus.configmap.ConfigureMapFragment$ConfigureMapFragmentProxy Bundle[{app_mode_key=car, configureSettingsSearch=true}]")
+						String.format("en-net.osmand.plus.configmap.ConfigureMapFragment$ConfigureMapFragmentProxy Bundle[{app_mode_key=%s, configureSettingsSearch=true}]", applicationMode.getStringKey()))
 				.orElseThrow();
 	}
 
@@ -118,5 +151,11 @@ public class SearchDatabaseRootedAtConfigureMapFragmentAdapter {
 										searchDatabaseConfig.preferenceSearchablePredicate),
 								activityContext),
 						searchDatabaseConfig.principalAndProxyProvider));
+	}
+
+	private static Set<ApplicationMode> getApplicationModesWithoutDefault() {
+		return Sets.difference(
+				new HashSet<>(ApplicationMode.allPossibleValues()),
+				Set.of(ApplicationMode.DEFAULT));
 	}
 }
