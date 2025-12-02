f5d07a17994c5a45c0828dfc71c8154658d88ff7
nazar-kutz <vitaminkuna@gmail.com>
2025-12-02 11:32:15 +0200

    Implement UI for setting bicycle width

---
+++ OsmAnd/src/net/osmand/plus/settings/vehiclesize/BicycleSizes.java
@@ -0,0 +1,22 @@
+package net.osmand.plus.settings.vehiclesize;
+
+import net.osmand.plus.R;
+import net.osmand.plus.base.containers.Limits;
+import net.osmand.plus.base.containers.ThemedIconId;
+import net.osmand.plus.settings.vehiclesize.containers.Assets;
+
+public class BicycleSizes extends VehicleSizes {
+
+	@Override
+	protected void collectSizesData() {
+		ThemedIconId icon = new ThemedIconId(R.drawable.img_help_cycleway_width_day, R.drawable.img_help_cycleway_width_night);
+		Assets assets = new Assets(icon, R.string.bicycle_width_limit_description);
+		Limits<Float> limits = new Limits<>(0.3f, 1.0f);
+		add(SizeType.WIDTH, assets, limits);
+	}
+
+	@Override
+	protected boolean useCentimetersInsteadOfMeters() {
+		return true;
+	}
+}
