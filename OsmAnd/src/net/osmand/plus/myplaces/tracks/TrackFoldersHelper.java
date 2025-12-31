5caa0501f8a9fec741da6ef16c662921156f3bae
nazar-kutz <vitaminkuna@gmail.com>
2025-12-31 13:25:57 +0200

    Extract specific dialog code from DialogManager

--- OsmAnd/src/net/osmand/plus/myplaces/tracks/TrackFoldersHelper.java
+++ OsmAnd/src/net/osmand/plus/myplaces/tracks/TrackFoldersHelper.java
@@ -4,17 +4,20 @@ import static net.osmand.plus.importfiles.ImportHelper.IMPORT_FILE_REQUEST;
 import static net.osmand.plus.importfiles.OnSuccessfulGpxImport.OPEN_GPX_CONTEXT_MENU;
 import static net.osmand.plus.settings.fragments.ExportSettingsFragment.SELECTED_TYPES;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
 import net.osmand.CallbackWithObject;
@@ -56,16 +59,21 @@ import net.osmand.plus.track.helpers.save.SaveGpxHelper;
 import net.osmand.plus.utils.AndroidUtils;
 import net.osmand.plus.utils.FileUtils;
 import net.osmand.plus.utils.UiUtilities;
+import net.osmand.plus.widgets.alert.AlertDialogData;
+import net.osmand.plus.widgets.alert.AlertDialogExtra;
+import net.osmand.plus.widgets.alert.CustomAlert;
 import net.osmand.plus.widgets.popup.PopUpMenu;
 import net.osmand.plus.widgets.popup.PopUpMenuDisplayData;
 import net.osmand.plus.widgets.popup.PopUpMenuItem;
 import net.osmand.shared.gpx.GpxFile;
 import net.osmand.shared.gpx.RouteActivityHelper;
+import net.osmand.shared.gpx.SmartFolderHelper;
 import net.osmand.shared.gpx.TrackFolderLoaderTask;
 import net.osmand.shared.gpx.TrackFolderLoaderTask.LoadTracksListener;
 import net.osmand.shared.gpx.TrackItem;
 import net.osmand.shared.gpx.data.TrackFolder;
 import net.osmand.shared.gpx.data.TracksGroup;
+import net.osmand.shared.gpx.filters.BaseTrackFilter;
 import net.osmand.shared.io.KFile;
 import net.osmand.util.Algorithms;
 
@@ -175,9 +183,7 @@ public class TrackFoldersHelper implements OnTrackFileMoveListener {
 			items.add(new PopUpMenuItem.Builder(app)
 					.setTitleId(R.string.add_smart_folder)
 					.setIcon(uiUtilities.getThemedIcon(R.drawable.ic_action_folder_smart_outlined))
-					.setOnClickListener(v -> {
-						app.getDialogManager().showSaveSmartFolderDialog(activity, fragment.isNightMode(), null);
-					})
+					.setOnClickListener(v -> showSaveSmartFolderDialog(activity, fragment.isNightMode(), null))
 					.create());
 		}
 
@@ -417,6 +423,34 @@ public class TrackFoldersHelper implements OnTrackFileMoveListener {
 		});
 		builder.setNegativeButton(R.string.shared_string_cancel, null);
 		builder.show();
+	}
+
+	public static void showSaveSmartFolderDialog(@NonNull FragmentActivity activity, boolean nightMode,
+	                                             @Nullable List<BaseTrackFilter> filters) {
+		OsmandApplication app = (OsmandApplication) activity.getApplication();
+		int titleResId = filters == null ? R.string.add_smart_folder : R.string.save_as_smart_folder;
+		AlertDialogData dialogData = new AlertDialogData(activity, nightMode)
+				.setTitle(titleResId)
+				.setNegativeButton(R.string.shared_string_cancel, null);
+		dialogData.setPositiveButton(R.string.shared_string_save, (dialog, which) -> {
+			Object extra = dialogData.getExtra(AlertDialogExtra.EDIT_TEXT);
+			if (extra instanceof EditText) {
+				String newSmartFolderName = ((EditText) extra).getText().toString();
+				if (Algorithms.isBlank(newSmartFolderName)) {
+					app.showToastMessage(R.string.empty_name);
+				} else {
+					SmartFolderHelper smartFolderHelper = app.getSmartFolderHelper();
+					if (smartFolderHelper.isSmartFolderPresent(newSmartFolderName)) {
+						app.showShortToastMessage(R.string.smart_folder_name_present);
+					} else {
+						smartFolderHelper.saveNewSmartFolder(newSmartFolderName, filters);
+						dialog.dismiss();
+					}
+				}
+			}
+		});
+		String caption = activity.getString(R.string.enter_new_name);
+		CustomAlert.showInput(dialogData, activity, null, caption);
 	}
 
 	private void dismissFragment(@NonNull Fragment fragment, boolean dismissImmediately) {
