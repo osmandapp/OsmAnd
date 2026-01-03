ab93218ebbe5004044a0bdc2ab8941bbc722a79a
nazar-kutz <vitaminkuna@gmail.com>
2026-01-03 14:49:35 +0200

    Use Limits instead of Pair for range representation

--- OsmAnd-shared/src/commonMain/kotlin/net/osmand/shared/gpx/organization/enums/OrganizeByType.kt
+++ OsmAnd-shared/src/commonMain/kotlin/net/osmand/shared/gpx/organization/enums/OrganizeByType.kt
@@ -1,5 +1,6 @@
 package net.osmand.shared.gpx.organization.enums
 
+import net.osmand.shared.data.Limits
 import net.osmand.shared.gpx.filters.TrackFilterType
 import net.osmand.shared.util.Localization
 
@@ -8,7 +9,7 @@ enum class OrganizeByType(
 	val nameResId: String,
 	val filterType: TrackFilterType,
 	val category: OrganizeByCategory,
-	val stepRange: Pair<Int, Int>?
+	val stepRange: Limits? = null
 ) {
 	// General
 	ACTIVITY(
@@ -22,19 +23,20 @@ enum class OrganizeByType(
 		nameResId = "duration",
 		filterType = TrackFilterType.DURATION,
 		category = OrganizeByCategory.GENERAL,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)
+	),
 	TIME_IN_MOTION(
 		iconResId = "ic_action_time_in_motion",
 		nameResId = "moving_time",
 		filterType = TrackFilterType.TIME_IN_MOTION,
 		category = OrganizeByCategory.GENERAL,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	LENGTH(
 		iconResId = "ic_action_length",
 		nameResId = "shared_string_length",
 		filterType = TrackFilterType.LENGTH,
 		category = OrganizeByCategory.GENERAL,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 
 	// Date & Time
 	YEAR_OF_CREATION(
@@ -70,13 +72,13 @@ enum class OrganizeByType(
 		nameResId = "organize_by_max_speed",
 		filterType = TrackFilterType.MAX_SPEED,
 		category = OrganizeByCategory.SPEED,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	AVG_SPEED(
 		iconResId = "ic_action_speed_average",
 		nameResId = "avg_speed",
 		filterType = TrackFilterType.AVERAGE_SPEED,
 		category = OrganizeByCategory.SPEED,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 
 	// Altitude and elevation
 	MAX_ALTITUDE(
@@ -84,25 +86,25 @@ enum class OrganizeByType(
 		nameResId = "organize_by_max_altitude",
 		filterType = TrackFilterType.MAX_ALTITUDE,
 		category = OrganizeByCategory.ALTITUDE_ELEVATION,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	AVG_ALTITUDE(
 		iconResId = "ic_action_altitude_average",
 		nameResId = "shared_string_avg_altitude",
 		filterType = TrackFilterType.AVERAGE_ALTITUDE,
 		category = OrganizeByCategory.ALTITUDE_ELEVATION,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	UPHILL(
 		iconResId = "ic_action_altitude_ascent",
 		nameResId = "shared_string_uphill",
 		filterType = TrackFilterType.UPHILL,
 		category = OrganizeByCategory.ALTITUDE_ELEVATION,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	DOWNHILL(
 		iconResId = "ic_action_altitude_descent",
 		nameResId = "shared_string_downhill",
 		filterType = TrackFilterType.DOWNHILL,
 		category = OrganizeByCategory.ALTITUDE_ELEVATION,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 
 	// Sensors
 	SENSOR_SPEED_MAX(
@@ -110,61 +112,61 @@ enum class OrganizeByType(
 		nameResId = "max_sensor_speed",
 		filterType = TrackFilterType.MAX_SENSOR_SPEED,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	SENSOR_SPEED_AVG(
 		iconResId = "ic_action_sensor_speed_outlined",
 		nameResId = "avg_sensor_speed",
 		filterType = TrackFilterType.AVERAGE_SENSOR_SPEED,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	HEART_RATE_MAX(
 		iconResId = "ic_action_sensor_heart_rate_outlined",
 		nameResId = "max_sensor_heartrate",
 		filterType = TrackFilterType.MAX_SENSOR_HEART_RATE,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	HEART_RATE_AVG(
 		iconResId = "ic_action_sensor_heart_rate_outlined",
 		nameResId = "avg_sensor_heartrate",
 		filterType = TrackFilterType.AVERAGE_SENSOR_HEART_RATE,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	CADENCE_MAX(
 		iconResId = "ic_action_sensor_cadence_outlined",
 		nameResId = "max_sensor_cadence",
 		filterType = TrackFilterType.MAX_SENSOR_CADENCE,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	CADENCE_AVG(
 		iconResId = "ic_action_sensor_cadence_outlined",
 		nameResId = "avg_sensor_cadence",
 		filterType = TrackFilterType.AVERAGE_SENSOR_CADENCE,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	POWER_MAX(
 		iconResId = "ic_action_sensor_bicycle_power_outlined",
 		nameResId = "max_sensor_bycicle_power",
 		filterType = TrackFilterType.MAX_SENSOR_BICYCLE_POWER,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	POWER_AVG(
 		iconResId = "ic_action_sensor_bicycle_power_outlined",
 		nameResId = "avg_sensor_bycicle_power",
 		filterType = TrackFilterType.AVERAGE_SENSOR_BICYCLE_POWER,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	TEMP_MAX(
 		iconResId = "ic_action_sensor_temperature_outlined",
 		nameResId = "max_sensor_temperature",
 		filterType = TrackFilterType.MAX_SENSOR_TEMPERATURE,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180)),
+		stepRange = Limits(0, 180)),
 	TEMP_AVG(
 		iconResId = "ic_action_sensor_temperature_outlined",
 		nameResId = "avg_sensor_temperature",
 		filterType = TrackFilterType.AVERAGE_SENSOR_TEMPERATURE,
 		category = OrganizeByCategory.SENSORS,
-		stepRange = Pair(0, 180));
+		stepRange = Limits(0, 180));
 
 	fun getName(): String {
 		return Localization.getString(nameResId)
