2ca5bed5aacb905873c9b509ac3536ef2897667f
alex-dev <alex.dev@osmand.net>
2026-01-08 19:42:59 +0300

    Refactor AstroDataProvider

--- OsmAnd/src/net/osmand/plus/plugins/astro/AstroDataProvider.kt
+++ OsmAnd/src/net/osmand/plus/plugins/astro/AstroDataProvider.kt
@@ -1,15 +1,23 @@
 package net.osmand.plus.plugins.astro
 
 import android.content.Context
+import android.graphics.Color
+import androidx.core.graphics.toColorInt
 import io.github.cosinekitty.astronomy.Body
+import net.osmand.PlatformUtil
 import net.osmand.plus.plugins.astro.SkyObject.Type
 import net.osmand.plus.plugins.astro.utils.AstroUtils.bodyColor
 import net.osmand.plus.plugins.astro.utils.AstroUtils.bodyName
+import org.json.JSONArray
 
 abstract class AstroDataProvider {
 
 	private var cachedSkyObjects:List<SkyObject>?=null
 	private var cachedConstellations:List<Constellation>?=null
+
+	companion object {
+		private val LOG = PlatformUtil.getLog(AstroDataProvider::class.java)
+	}
 
 	abstract fun getInitialSkyObjectsImpl(ctx: Context): List<SkyObject>
 
@@ -75,6 +83,48 @@ abstract class AstroDataProvider {
 					color = color
 				)
 			)
+		}
+	}
+
+	protected fun getTypeColor(type: Type): Int {
+		return when (type) {
+			Type.STAR -> Color.WHITE
+			Type.GALAXY, Type.GALAXY_CLUSTER -> Color.LTGRAY
+			Type.BLACK_HOLE -> Color.MAGENTA
+			Type.NEBULA -> "#E0CEF5".toColorInt() // Light
+			Type.OPEN_CLUSTER -> "#FFFFE0".toColorInt() // Light yellow
+			Type.GLOBULAR_CLUSTER -> "#FFFACD".toColorInt() // Lemon chiffon
+			else -> Color.WHITE
+		}
+	}
+
+	protected fun parseLines(json: String?): List<Pair<Int, Int>> {
+		if (json.isNullOrEmpty()) return emptyList()
+		val list = mutableListOf<Pair<Int, Int>>()
+		try {
+			val jsonArray = JSONArray(json)
+			for (i in 0 until jsonArray.length()) {
+				val segment = jsonArray.getJSONArray(i)
+				if (segment.length() >= 2) {
+					list.add(segment.getInt(0) to segment.getInt(1))
+				}
+			}
+		} catch (e: Exception) {
+			LOG.error("Error parsing constellation lines: $json", e)
+		}
+		return list
+	}
+
+	protected fun generateId(type: Type, name: String): String {
+		return when (type) {
+			Type.STAR -> name.lowercase().replace(' ', '_')
+			Type.GALAXY -> name.lowercase().replace(' ', '_')
+				.filter { it.isLetterOrDigit() || it == '_' }
+
+			Type.BLACK_HOLE -> "bh_" + name.lowercase().replace('*', '_').replace(' ', '_')
+				.replace('-', '_').replace('(', '_').replace(')', '_')
+
+			else -> name.lowercase().replace(' ', '_')
 		}
 	}
 }
