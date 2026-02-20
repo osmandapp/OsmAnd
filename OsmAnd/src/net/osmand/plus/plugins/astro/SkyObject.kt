c81a8c61fa31afaeb8d05da32745b00989ac9804
0xRe1nk0 <0xre1nk0@gmail.com>
2026-02-20 10:14:04 +0200

    add type names

--- OsmAnd/src/net/osmand/plus/plugins/astro/SkyObject.kt
+++ OsmAnd/src/net/osmand/plus/plugins/astro/SkyObject.kt
@@ -46,13 +46,21 @@ open class SkyObject(
 	// Cache helper
 	open var lastUpdateTime: Double = -1.0
 ) {
-	enum class Type {
-		STAR, GALAXY, BLACK_HOLE, PLANET, SUN, MOON,
-		NEBULA, OPEN_CLUSTER, GLOBULAR_CLUSTER, GALAXY_CLUSTER, CONSTELLATION;
+	enum class Type(val titleKey: String) {
+		STAR("astro_type_star"),
+		GALAXY("astro_type_galaxy"),
+		BLACK_HOLE("astro_type_black_hole"),
+		PLANET("astro_type_planet"),
+		SUN("astro_type_star"),
+		MOON("astro_type_satellite"),
 
-		fun isSunSystem(): Boolean {
-			return this == SUN || this == MOON || this == PLANET
-		}
+		NEBULA("astro_type_nebula"),
+		OPEN_CLUSTER("astro_type_open_cluster"),
+		GLOBULAR_CLUSTER("astro_type_globular_cluster"),
+		GALAXY_CLUSTER("astro_type_galaxy_cluster"),
+		CONSTELLATION("astro_type_constellation");
+
+		fun isSunSystem(): Boolean = this == SUN || this == MOON || this == PLANET
 	}
 
 	override fun equals(other: Any?): Boolean {
