17097095344965ce104ca05d3b7334182dc0039c
nazar-kutz <vitaminkuna@gmail.com>
2026-03-16 10:45:00 +0200

    Reload terrain modes when edit/create related gradient file

--- OsmAnd/src/net/osmand/plus/card/color/palette/gradient/GradientPaletteController.kt
+++ OsmAnd/src/net/osmand/plus/card/color/palette/gradient/GradientPaletteController.kt
@@ -335,6 +335,7 @@ open class GradientPaletteController(
 			selectPaletteItem(resultItem)
 			notifyUpdatePaletteSelection(oldSelected, resultItem)
 		}
+		updateExternalDependencies()
 		editedItem = null
 	}
 
