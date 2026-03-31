a2be7ba6cfc220114521ab8aa5ca3475965cfd6e
chumv <chumva.forever@gmail.com>
2026-03-31 16:45:42 +0300

    Add UiUtilities.decodeResource

--- OsmAnd/src/net/osmand/plus/utils/UiUtilities.java
+++ OsmAnd/src/net/osmand/plus/utils/UiUtilities.java
@@ -5,8 +5,10 @@ import static net.osmand.plus.views.mapwidgets.TopToolbarController.NO_COLOR;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.Typeface;
@@ -861,8 +863,16 @@ public class UiUtilities {
 		return true;
 	}
 
+	public static Bitmap decodeResource(Resources res, int id) {
+		return decodeResource(res, id, null);
+	}
+
+	public static Bitmap decodeResource(Resources res, int id, Options opts) {
+		return BitmapFactory.decodeResource(res, id, opts);
+	}
+
 	public Bitmap getScaledBitmap(@UiContext Context context, @DrawableRes int drawableId, float scale) {
-		Bitmap bitmap = BitmapFactory.decodeResource(context == null ? app.getResources() : context.getResources(), drawableId);
+		Bitmap bitmap = decodeResource(context == null ? app.getResources() : context.getResources(), drawableId);
 		if (bitmap != null && scale != 1f && scale > 0) {
 			bitmap = AndroidUtils.scaleBitmap(bitmap,
 					(int) (bitmap.getWidth() * scale), (int) (bitmap.getHeight() * scale), false);
