a5b138a239180f0637ca1aa85deade4ae9b52204
Vitaliy <chumva.forever@gmail.com>
2026-04-02 13:34:45 +0300

    Extend Memory widget to support multiple memory types (#24736)

--- OsmAnd/src/net/osmand/plus/plugins/development/widget/MemoryInfoWidget.java
+++ OsmAnd/src/net/osmand/plus/plugins/development/widget/MemoryInfoWidget.java
@@ -2,29 +2,46 @@ package net.osmand.plus.plugins.development.widget;
 
 import static net.osmand.plus.views.mapwidgets.WidgetType.DEV_MEMORY;
 
+import android.os.Debug;
+import android.os.Debug.MemoryInfo;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import net.osmand.plus.activities.MapActivity;
+import net.osmand.plus.settings.backend.ApplicationMode;
+import net.osmand.plus.settings.backend.preferences.OsmandPreference;
 import net.osmand.plus.utils.AndroidUtils;
 import net.osmand.plus.views.layers.base.OsmandMapLayer.DrawSettings;
 import net.osmand.plus.views.mapwidgets.WidgetsPanel;
 import net.osmand.plus.views.mapwidgets.widgets.SimpleWidget;
+import net.osmand.plus.views.mapwidgets.widgetstates.MemoryWidgetState;
+import net.osmand.plus.views.mapwidgets.widgetstates.MemoryWidgetState.MemoryInfoType;
+import net.osmand.util.Algorithms;
 
 public class MemoryInfoWidget extends SimpleWidget {
 
-
 	private static final long UPDATE_INTERVAL_MS = 1000;
 
-	private long cachedUsedMemory;
+	private final MemoryWidgetState widgetState;
 
+	private MemoryInfoType memoryInfoType;
+
+	private MemoryInfo memoryInfo;
 	private long lastUpdateTime;
-	private boolean memoryChanged;
+	private long cachedMemoryValue;
 
-	public MemoryInfoWidget(@NonNull MapActivity mapActivity, @Nullable String customId, @Nullable WidgetsPanel widgetsPanel) {
+	public MemoryInfoWidget(@NonNull MapActivity mapActivity, @NonNull MemoryWidgetState widgetState,
+			@Nullable String customId, @Nullable WidgetsPanel widgetsPanel) {
 		super(mapActivity, DEV_MEMORY, customId, widgetsPanel);
+		this.widgetState = widgetState;
+	}
+
+	@NonNull
+	public OsmandPreference<MemoryInfoType> getMemoryInfoTypePref() {
+		return widgetState.getMemoryInfoTypePref();
 	}
 
 	@Override
@@ -35,31 +52,82 @@ public class MemoryInfoWidget extends SimpleWidget {
 	}
 
 	@Override
-	protected void updateSimpleWidgetInfo(@Nullable DrawSettings drawSettings) {
-		if (checkMemoryChanged() && isTimeToUpdate()) {
-			memoryChanged = false;
-			lastUpdateTime = System.currentTimeMillis();
-			setText(getFormattedValue(), null);
-		}
+	protected OnClickListener getOnClickListener() {
+		return v -> {
+			widgetState.changeToNextState();
+			updateSimpleWidgetInfo(null);
+			updateWidgetName();
+		};
 	}
 
-	private boolean checkMemoryChanged() {
-		Runtime runtime = Runtime.getRuntime();
-		long usedMemory = runtime.totalMemory() - runtime.freeMemory();
+	@Nullable
+	@Override
+	protected String getWidgetName() {
+		return widgetState != null ? app.getString(widgetState.getMemoryInfoType().titleId) : null;
+	}
 
-		if (cachedUsedMemory != usedMemory) {
-			cachedUsedMemory = usedMemory;
-			memoryChanged = true;
+	@Nullable
+	@Override
+	public MemoryWidgetState getWidgetState() {
+		return widgetState;
+	}
+
+	@Override
+	public void copySettingsFromMode(@NonNull ApplicationMode sourceAppMode, @NonNull ApplicationMode appMode, @Nullable String customId) {
+		super.copySettingsFromMode(sourceAppMode, appMode, customId);
+		widgetState.copyPrefsFromMode(sourceAppMode, appMode, customId);
+	}
+
+	@Override
+	protected void updateSimpleWidgetInfo(@Nullable DrawSettings drawSettings) {
+		MemoryInfoType newType = widgetState.getMemoryInfoType();
+		boolean typeChanged = newType != memoryInfoType;
+
+		if (isTimeToUpdate() || typeChanged) {
+			lastUpdateTime = System.currentTimeMillis();
+
+			long newMemoryValue = calculateMemoryValue(newType);
+			if (typeChanged || cachedMemoryValue != newMemoryValue) {
+				memoryInfoType = newType;
+				cachedMemoryValue = newMemoryValue;
+
+				String text = AndroidUtils.formatSize(app, cachedMemoryValue, true);
+				setText(text, memoryInfoType.shortName);
+			}
 		}
-		return memoryChanged;
 	}
 
 	private boolean isTimeToUpdate() {
 		return System.currentTimeMillis() - lastUpdateTime > UPDATE_INTERVAL_MS;
 	}
 
-	@NonNull
-	private String getFormattedValue() {
-		return AndroidUtils.formatSize(app, cachedUsedMemory, true);
+	private long calculateMemoryValue(@NonNull MemoryInfoType infoType) {
+		Runtime runtime = Runtime.getRuntime();
+		long maxMemory = runtime.maxMemory();
+		long freeMemory = runtime.freeMemory();
+		long totalMemory = runtime.totalMemory();
+		long usedMemory = totalMemory - freeMemory;
+		long effectiveMax = (maxMemory == Long.MAX_VALUE) ? totalMemory : maxMemory;
+
+		switch (infoType) {
+			case LIMIT:
+				return effectiveMax;
+			case ALLOCATED:
+				return totalMemory;
+			case AVAILABLE:
+				return Math.max(0L, effectiveMax - usedMemory);
+			case NATIVE:
+				return Debug.getNativeHeapAllocatedSize();
+			case GRAPHICS:
+				if (memoryInfo == null) {
+					memoryInfo = new Debug.MemoryInfo();
+				}
+				Debug.getMemoryInfo(memoryInfo);
+				String graphics = memoryInfo.getMemoryStat("summary.graphics");
+				return Algorithms.parseLongSilently(graphics, 0) * 1024L;
+			case USED:
+			default:
+				return usedMemory;
+		}
 	}
 }
