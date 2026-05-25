16031fe0cde85331600596c3ef321401e6634fa5
nazar-kutz <vitaminkuna@gmail.com>
2026-05-25 23:18:51 +0300

    refactor(gallery): extract span settings helper and holder extensions

---
+++ OsmAnd/src/net/osmand/plus/gallery/data/GalleryKey.kt
@@ -0,0 +1,15 @@
+package net.osmand.plus.gallery.data
+
+import net.osmand.data.LatLon
+
+sealed class GalleryKey {
+
+	data class Location(
+		val latLon: LatLon,
+		val params: Map<String, String>
+	) : GalleryKey()
+
+	data class Favorite(val id: String) : GalleryKey()
+
+	data class Waypoint(val id: String) : GalleryKey()
+}
