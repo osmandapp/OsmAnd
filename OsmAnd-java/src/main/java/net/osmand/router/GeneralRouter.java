4dfb93467240e07d6913004a1339ef50d87cae77
Dmytro Makariev <dmytro.makariev@globallogic.com>
2026-06-02 23:58:53 +0300

    Make GeneralRouter evalCache thread-safe (R1)
    Use ConcurrentHashMap for routing attribute caches to avoid corruption when routing runs concurrently.

--- OsmAnd-java/src/main/java/net/osmand/router/GeneralRouter.java
+++ OsmAnd-java/src/main/java/net/osmand/router/GeneralRouter.java
@@ -21,6 +21,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.set.hash.TLongHashSet;
@@ -215,7 +216,7 @@ public class GeneralRouter implements VehicleRouter {
 		int l = RouteDataObjectAttribute.values().length;
 		evalCache = new Map[l];
 		for (int i = 0; i < l; i++) {
-			evalCache[i] = new HashMap<>();
+			evalCache[i] = new ConcurrentHashMap<>();
 		}
 	}
 
@@ -607,7 +608,7 @@ public class GeneralRouter implements VehicleRouter {
 		if (USE_CACHE) {
 			Map<IntHolder, Float> rM = ch.get(reg);
 			if (rM == null) {
-				rM = new HashMap<IntHolder, Float>();
+				rM = new ConcurrentHashMap<>();
 				ch.put(reg, rM);
 			}
 			rM.put(new IntHolder(types, extra), val);
