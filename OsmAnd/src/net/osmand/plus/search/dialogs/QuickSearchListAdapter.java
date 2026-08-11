beff447b309097991e5de174f3e7f162868787dc
0xRe1nk0 <0xre1nk0@gmail.com>
2026-08-11 13:13:16 +0300

    Use new coordinate item

--- OsmAnd/src/net/osmand/plus/search/dialogs/QuickSearchListAdapter.java
+++ OsmAnd/src/net/osmand/plus/search/dialogs/QuickSearchListAdapter.java
@@ -527,6 +527,12 @@ public class QuickSearchListAdapter extends ArrayAdapter<QuickSearchListItem> {
 			SearchResultViewHolder.bindFullSearchResult(view, listItem);
 			updateCompass(view, listItem, updateLocationViewCache, useMapCenter);
 			setupCheckBox(position, view, listItem);
+		} else if (SearchResultViewHolder.isCoordinatesItem(searchResult)) {
+			view = getConvertView(convertView, R.layout.search_legacy_history_list_item);
+			SearchResultViewHolder.bindSearchResult(view, listItem, calendar);
+			SearchResultViewHolder.bindCoordinatesSearchResult(view, listItem);
+			updateCompass(view, listItem, updateLocationViewCache, useMapCenter);
+			setupCheckBox(position, view, listItem);
 		} else if (listItem.isLegacyHistoryItem()) {
 			view = getConvertView(convertView, R.layout.search_legacy_history_list_item);
 			SearchResultViewHolder.bindSearchResult(view, listItem, calendar);
