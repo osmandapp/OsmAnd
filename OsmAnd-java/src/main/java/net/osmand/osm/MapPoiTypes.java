9d2bc48fa3d9ca99423270c8670c47d20974d590
RZR-UA <youonly@ukr.net>
2026-08-12 18:02:36 +0200

    Speed getAnyPoiAdditionalTypeByKey 33x up (cache)

--- OsmAnd-java/src/main/java/net/osmand/osm/MapPoiTypes.java
+++ OsmAnd-java/src/main/java/net/osmand/osm/MapPoiTypes.java
@@ -628,6 +628,8 @@ public class MapPoiTypes {
 		if (otherCategory == null) {
 			throw new IllegalArgumentException("No poi category other");
 		}
+		this.poiAdditionalByKeyHitCache = new HashMap<>();
+		this.poiAdditionalByKeyMissCache = new HashSet<>();
 		init = true;
 		log.info("Time to init poi types " + (System.currentTimeMillis() - time)); //$NON-NLS-1$
 	}
@@ -824,7 +826,7 @@ public class MapPoiTypes {
 		return null;
 	}
 
-	public AbstractPoiType getPoiAdditionalType(PoiCategory category, String name) {
+	private AbstractPoiType getPoiAdditionalType(PoiCategory category, String name) {
 		PoiType add = getPoiAdditionalByKey(category, name);
 		if (add != null) {
 			return add;
@@ -844,14 +846,31 @@ public class MapPoiTypes {
 		return null;
 	}
 
+	private volatile Set<String> poiAdditionalByKeyMissCache = new HashSet<>();
+	private volatile Map<String, AbstractPoiType> poiAdditionalByKeyHitCache = new HashMap<>();
+
 	public AbstractPoiType getAnyPoiAdditionalTypeByKey(String name) {
-		for (int i = 0; i < categories.size(); i++) {
-			PoiCategory pc = categories.get(i);
+		Set<String> missCache = poiAdditionalByKeyMissCache;
+		if (missCache.contains(name)) {
+			return null;
+		}
+		Map<String, AbstractPoiType> hitCache = poiAdditionalByKeyHitCache;
+		AbstractPoiType cachedType = hitCache.get(name);
+		if (cachedType != null) {
+			return cachedType;
+		}
+		for (PoiCategory pc : categories) {
 			AbstractPoiType add = getPoiAdditionalType(pc, name);
 			if (add != null) {
+				Map<String, AbstractPoiType> freshHitCache = new HashMap<>(hitCache);
+				freshHitCache.put(name, add);
+				poiAdditionalByKeyHitCache = freshHitCache;
 				return add;
 			}
 		}
+		Set<String> freshMissCache = new HashSet<>(missCache);
+		freshMissCache.add(name);
+		poiAdditionalByKeyMissCache = freshMissCache;
 		return null;
 	}
 
