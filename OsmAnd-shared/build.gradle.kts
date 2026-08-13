76bac5d34c15c67230678018fa6beba9468c09f3
vshcherb <victor.shcherb@gmail.com>
2026-08-13 16:55:16 +0300

    Add 5.4 versions

--- OsmAnd-shared/build.gradle.kts
+++ OsmAnd-shared/build.gradle.kts
@@ -107,7 +107,7 @@ android {
 	}
 }
 
-version = System.getenv("OSMAND_SHARED_ANDROID_BINARIES_IVY_REVISION") ?: "master-snapshot"
+version = System.getenv("OSMAND_SHARED_ANDROID_BINARIES_IVY_REVISION") ?: "5.4"
 publishing {
 	repositories {
 		ivy {
