1120a4e1072256d742ea5a792ebc015387c430fa
ivanPyrohivskyi <ivan.pirogovsky@gmail.com>
2026-08-20 14:51:29 +0300

    Refactoring

--- OsmAnd-java/src/main/java/net/osmand/osm/edit/Relation.java
+++ OsmAnd-java/src/main/java/net/osmand/osm/edit/Relation.java
@@ -6,10 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import net.osmand.data.Amenity;
 import net.osmand.data.LatLon;
-import net.osmand.data.MapObject;
-import net.osmand.data.QuadRect;
 
 import static net.osmand.osm.edit.OSMSettings.*;
 
