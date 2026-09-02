40a8200fab1d3f79932bb340064e1c7d9d104f8d
RZR-UA <youonly@ukr.net>
2026-09-02 15:34:24 +0200

    Add test testSearchRegionByRef() for FR-55 (LLM)

--- OsmAnd-java/src/test/java/net/osmand/map/OsmandRegionsTest.java
+++ OsmAnd-java/src/test/java/net/osmand/map/OsmandRegionsTest.java
@@ -1,5 +1,6 @@
 package net.osmand.map;
 
+import net.osmand.CollatorStringMatcher;
 import net.osmand.PlatformUtil;
 import net.osmand.binary.BinaryMapDataObject;
 
@@ -21,6 +22,19 @@ public class OsmandRegionsTest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    public void testSearchRegionByRef() {
+        WorldRegion meuse = osmandRegions.getRegionDataByDownloadName("france_great-east_meuse_europe");
+        Assert.assertNotNull(meuse);
+        Assert.assertFalse(matches("55", meuse));
+        Assert.assertTrue(matches("FR-55", meuse));
+    }
+
+    private boolean matches(String query, WorldRegion region) {
+        return new CollatorStringMatcher(query, CollatorStringMatcher.StringMatcherMode.CHECK_EQUALS_FROM_SPACE)
+                .matches(region.getRegionSearchText());
     }
 
     @Test
