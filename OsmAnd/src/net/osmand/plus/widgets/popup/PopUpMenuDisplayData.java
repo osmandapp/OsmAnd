17a4ca8d8ddfb534ee090d2af568f0836b46030c
Dmytro Uzhva <kraativ@gmail.com>
2026-09-04 15:58:20 +0300

    Deprecate obsolete fields in PopUpMenuDisplayData (#3294)
    - Mark layoutId, nightMode, widthMode, showCompound, and limitHeight as deprecated
    - Document reasons for deprecation as Compose handles theming, intrinsic sizing, and compound controls directly

--- OsmAnd/src/net/osmand/plus/widgets/popup/PopUpMenuDisplayData.java
+++ OsmAnd/src/net/osmand/plus/widgets/popup/PopUpMenuDisplayData.java
@@ -14,15 +14,43 @@ public class PopUpMenuDisplayData {
 	private static final int DEFAULT_LAYOUT_ID = R.layout.popup_menu_item;
 
 	public View anchorView;
+
+	/**
+	 * @deprecated Unused with Compose dropdown menus. Custom XML layout is not inflated.
+	 */
+	@Deprecated
 	public @LayoutRes int layoutId = DEFAULT_LAYOUT_ID;
+
 	public @ColorInt int bgColor;
+
+	/**
+	 * @deprecated Unused with Compose dropdown menus. Theme is resolved from the context theme.
+	 */
+	@Deprecated
 	public boolean nightMode;
+
+	/**
+	 * @deprecated Unused with Compose dropdown menus. Width is measured intrinsically.
+	 */
+	@Deprecated
 	public PopUpMenuWidthMode widthMode = PopUpMenuWidthMode.AS_ANCHOR_VIEW;
+
 	public List<PopUpMenuItem> menuItems;
 	public OnPopUpMenuItemClickListener onItemClickListener;
 	public CustomDropDown customDropDown = CustomDropDown.AUTO_DROP_DOWN;
+
+	/**
+	 * @deprecated Unused with Compose dropdown menus. Compound controls are configured on each {@link PopUpMenuItem}.
+	 */
+	@Deprecated
 	public boolean showCompound = true;
+
+	/**
+	 * @deprecated Unused with Compose dropdown menus. Height and scrolling are managed automatically by Compose.
+	 */
+	@Deprecated
 	public boolean limitHeight = false;
+
 	public Integer dropDownGravity;
 	public Integer horizontalOffset;
 	public Integer verticalOffset;
