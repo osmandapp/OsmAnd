ee4a119a4a6648df18f773e41e939f1c5e410c5a
Grotty <gateway07@outlook.com>
2026-09-08 09:51:26 +0300

    add new abbreviations

--- OsmAnd-java/src/main/java/net/osmand/binary/Abbreviations.java
+++ OsmAnd-java/src/main/java/net/osmand/binary/Abbreviations.java
@@ -35,6 +35,12 @@ public class Abbreviations {
 		commonSkipOtherCnt.add(full.toLowerCase());
 	}
 
+	private static void addSearchAbbreviation(String full, String... keys) {
+		for (String key : keys) {
+			searchAbbreviations.put(key, full);
+		}
+	}
+
 	private static void addConjunction(String key) {
 		conjunctions.add(key);
 		commonSkipOtherCnt.add(key);
@@ -102,6 +108,149 @@ public class Abbreviations {
 		searchAbbreviations.put("fifth", "5th");
 		searchAbbreviations.put("sixth", "6th");
 		searchAbbreviations.put("seventh", "7th");
+
+		// Unambiguous English address variants used by Nominatim.
+		addSearchAbbreviation("access", "accs");
+		addSearchAbbreviation("alley", "aly");
+		addSearchAbbreviation("alleyway", "alwy");
+		addSearchAbbreviation("anex annex", "anx");
+		addSearchAbbreviation("beach", "bch");
+		addSearchAbbreviation("bend", "bnd");
+		addSearchAbbreviation("bluff", "blf");
+		addSearchAbbreviation("bluffs", "blfs");
+		addSearchAbbreviation("bridge", "bdge", "brdg", "brg");
+		addSearchAbbreviation("bypass", "bps", "byp", "bypa");
+		addSearchAbbreviation("byway", "bywy");
+		addSearchAbbreviation("canyon", "cyn");
+		addSearchAbbreviation("causeway", "caus", "cswy", "cway");
+		addSearchAbbreviation("center centre", "cen", "ctr");
+		addSearchAbbreviation("circle", "cir");
+		addSearchAbbreviation("cliff", "clf");
+		addSearchAbbreviation("cliffs", "clfs");
+		addSearchAbbreviation("corner", "cnr", "crn");
+		addSearchAbbreviation("course", "crse");
+		addSearchAbbreviation("court", "crt");
+		addSearchAbbreviation("creek", "crk");
+		addSearchAbbreviation("crescent", "cres");
+		addSearchAbbreviation("crossing", "crsg", "csg", "xing");
+		addSearchAbbreviation("crossroad", "crd", "xrd");
+		addSearchAbbreviation("crossroads", "xrds");
+		addSearchAbbreviation("cul-de-sac", "cds", "csac");
+		addSearchAbbreviation("dale", "dle");
+		addSearchAbbreviation("drive", "drv");
+		addSearchAbbreviation("driveway", "drwy", "dvwy", "dwy");
+		addSearchAbbreviation("entrance", "ent");
+		addSearchAbbreviation("esplanade", "espl");
+		addSearchAbbreviation("expressway", "expy", "expwy", "xway");
+		addSearchAbbreviation("extension", "ext", "exten", "exts");
+		addSearchAbbreviation("falls", "fls");
+		addSearchAbbreviation("field", "fld");
+		addSearchAbbreviation("fields", "flds");
+		addSearchAbbreviation("ford", "frd");
+		addSearchAbbreviation("fords", "frds");
+		addSearchAbbreviation("forest", "frst");
+		addSearchAbbreviation("forge", "frg");
+		addSearchAbbreviation("forges", "frgs");
+		addSearchAbbreviation("fork", "frk");
+		addSearchAbbreviation("forks", "frks");
+		addSearchAbbreviation("freeway", "frwy", "fwy");
+		addSearchAbbreviation("gardens", "gdns");
+		addSearchAbbreviation("gate gates", "gte");
+		addSearchAbbreviation("gateway", "gtwy");
+		addSearchAbbreviation("glade", "gld", "glde");
+		addSearchAbbreviation("glen", "gln");
+		addSearchAbbreviation("green", "grn");
+		addSearchAbbreviation("greens", "grns");
+		addSearchAbbreviation("grove", "gro", "grv");
+		addSearchAbbreviation("groves", "grvs");
+		addSearchAbbreviation("gully", "gly");
+		addSearchAbbreviation("harbor harbour", "hbr", "harbr");
+		addSearchAbbreviation("harbors", "hbrs");
+		addSearchAbbreviation("haven", "hvn");
+		addSearchAbbreviation("heights", "hgts", "hts");
+		addSearchAbbreviation("hill", "hl");
+		addSearchAbbreviation("hills", "hls");
+		addSearchAbbreviation("hollow", "holw");
+		addSearchAbbreviation("inlet", "inlt");
+		addSearchAbbreviation("junction", "jct", "jctn", "jnc");
+		addSearchAbbreviation("junctions", "jcts");
+		addSearchAbbreviation("knoll", "knl");
+		addSearchAbbreviation("knolls", "knls");
+		addSearchAbbreviation("lagoon", "lgn");
+		addSearchAbbreviation("lakes", "lks");
+		addSearchAbbreviation("laneway", "lnwy");
+		addSearchAbbreviation("light", "lgt");
+		addSearchAbbreviation("manor", "mnr");
+		addSearchAbbreviation("manors", "mnrs");
+		addSearchAbbreviation("meadow", "mdw");
+		addSearchAbbreviation("meadows", "mdws");
+		addSearchAbbreviation("motorway", "mtwy", "mwy");
+		addSearchAbbreviation("mountain", "mtn");
+		addSearchAbbreviation("mountains", "mtns");
+		addSearchAbbreviation("orchard", "orch");
+		addSearchAbbreviation("parade", "pde");
+		addSearchAbbreviation("parkway", "pkwy", "pky");
+		addSearchAbbreviation("passage", "psge");
+		addSearchAbbreviation("pathway", "phwy", "ptway");
+		addSearchAbbreviation("pines", "pnes");
+		addSearchAbbreviation("plain", "pln");
+		addSearchAbbreviation("plains", "plns");
+		addSearchAbbreviation("plaza", "plz", "plza");
+		addSearchAbbreviation("point", "pnt");
+		addSearchAbbreviation("port", "prt");
+		addSearchAbbreviation("promenade", "prm");
+		addSearchAbbreviation("ranch", "rnch");
+		addSearchAbbreviation("range", "rge", "rnge");
+		addSearchAbbreviation("rapids", "rpds");
+		addSearchAbbreviation("ridge", "rdg", "rdge");
+		addSearchAbbreviation("ridges", "rdgs");
+		addSearchAbbreviation("river", "riv", "rvr");
+		addSearchAbbreviation("roads", "rds");
+		addSearchAbbreviation("roadway", "rdwy");
+		addSearchAbbreviation("route", "rte");
+		addSearchAbbreviation("shoal", "shl");
+		addSearchAbbreviation("shore", "shr");
+		addSearchAbbreviation("shores", "shrs");
+		addSearchAbbreviation("skyway", "skwy");
+		addSearchAbbreviation("slope", "slpe");
+		addSearchAbbreviation("sound", "snd");
+		addSearchAbbreviation("spring", "spg");
+		addSearchAbbreviation("springs", "spgs");
+		addSearchAbbreviation("square", "sq");
+		addSearchAbbreviation("station", "sta", "stn");
+		addSearchAbbreviation("stream", "strm");
+		addSearchAbbreviation("summit", "smt");
+		addSearchAbbreviation("terrace", "tce", "terr");
+		addSearchAbbreviation("track", "trak", "trk");
+		addSearchAbbreviation("trail", "trl");
+		addSearchAbbreviation("tunnel", "tun", "tunl");
+		addSearchAbbreviation("turnpike", "tpk", "tpke");
+		addSearchAbbreviation("underpass", "upas", "ups");
+		addSearchAbbreviation("valley", "vly");
+		addSearchAbbreviation("view", "vw");
+		addSearchAbbreviation("views", "vws");
+		addSearchAbbreviation("village", "vlg", "villge");
+		addSearchAbbreviation("vista", "vis", "vst", "vsta");
+		addSearchAbbreviation("walk", "wlk");
+		addSearchAbbreviation("walkway", "wkwy", "wky");
+		addSearchAbbreviation("waters", "wtr");
+		addSearchAbbreviation("way", "wy");
+		addSearchAbbreviation("wharf", "whrf");
+		addSearchAbbreviation("wynd", "wyn");
+
+		// Country-specific single-word address components.
+		addSearchAbbreviation("barangay", "bgy", "brgy");
+		addSearchAbbreviation("compound", "cmpd", "cpd");
+		addSearchAbbreviation("department", "dept");
+		addSearchAbbreviation("province", "prov");
+		addSearchAbbreviation("subdivision", "subd", "subdiv");
+		addSearchAbbreviation("bazaar", "bazar", "bzr");
+		addSearchAbbreviation("cantonment", "cant", "cantt");
+		addSearchAbbreviation("district", "dist", "distt");
+		addSearchAbbreviation("government", "govt");
+		addSearchAbbreviation("markaz", "mkz");
+		addSearchAbbreviation("sector", "sec", "sect", "sectr");
+		addSearchAbbreviation("service", "ser", "serv");
 	}
 
 	// common housenumber additions
@@ -112,9 +261,15 @@ public class Abbreviations {
 		buildingAbbreviations.put("quater", "Quater");
 		// american
 		buildingAbbreviations.put("bldg", "Building");
+		buildingAbbreviations.put("bldgs", "buildings");
+		buildingAbbreviations.put("bldngs", "buildings");
 		buildingAbbreviations.put("ste", "Suite");
 		buildingAbbreviations.put("unt", "Unit");
 		buildingAbbreviations.put("apt", "Apartment");
+		buildingAbbreviations.put("apts", "apartments");
+		buildingAbbreviations.put("blk", "block");
+		buildingAbbreviations.put("flts", "flats");
+		buildingAbbreviations.put("hse", "house");
 		buildingAbbreviations.put("fl", "Floor");
 		buildingAbbreviations.put("flr", "Floor");
 		buildingAbbreviations.put("bsmt", "Basement");
