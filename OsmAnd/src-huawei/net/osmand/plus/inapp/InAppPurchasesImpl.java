4adb8017c4a8f08b2d2cb1355c259597ea74e9d3
Vitaliy <chumva.forever@gmail.com>
2026-09-11 16:52:20 +0300

    Update full_version_price default price for #25870

--- OsmAnd/src-huawei/net/osmand/plus/inapp/InAppPurchasesImpl.java
+++ OsmAnd/src-huawei/net/osmand/plus/inapp/InAppPurchasesImpl.java
@@ -93,6 +93,11 @@ public class InAppPurchasesImpl extends InAppPurchases {
 		public boolean isLegacy() {
 			return false;
 		}
+
+		@Override
+		public String getDefaultPrice(Context ctx) {
+			return ctx.getString(R.string.full_version_price);
+		}
 	}
 
 	private static class InAppPurchaseDepthContoursFree extends InAppPurchaseDepthContours {
